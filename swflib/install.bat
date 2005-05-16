3c4a02808ceae751ebe793dbf06194e32cccc197
Nicolas Cannasse <ncannasse@gmail.com>
2005-05-16 06:57:18 +0000

    added swfPic and Png

--- swflib/install.bat
+++ swflib/install.bat
@@ -1,6 +1,6 @@
-@ocamake -a *.ml -o swfLib.cma
-@ocamake -a *.ml -o swfLib.cmxa
-@cp -f swf.cmi swfParser.cmi swfZip.cmi actionScript.cmi swfLib.lib swfLib.cma swfLib.cmxa c:\ocaml\lib
-@ocamake -a *.ml -o swfLib.cma -clean
-@ocamake -a *.ml -o swfLib.cmxa -clean
+@ocamake -a *.ml *.mli -o swfLib.cma
+@ocamake -a *.ml *.mli -o swfLib.cmxa
+@cp -f swf.cmi png.cmi swfPic.cmi swfParser.cmi swfZip.cmi actionScript.cmi swfLib.lib swfLib.cma swfLib.cmxa c:\ocaml\lib
+@ocamake -a *.ml *.mli -o swfLib.cma -clean
+@ocamake -a *.ml *.mli -o swfLib.cmxa -clean
 @pause
