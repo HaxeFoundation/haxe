d5f6ef41593d2355e338a3d7f3e3ea22004bcc0f
Nicolas Cannasse <ncannasse@gmail.com>
2006-04-22 10:29:40 +0000

    added events.

--- std/flash/MovieClipLoader.hx
+++ std/flash/MovieClipLoader.hx
@@ -8,4 +8,10 @@ extern class MovieClipLoader
 	function loadClip(url:String, target:Dynamic):Bool;
 	function removeListener(listener:Dynamic):Bool;
 	function unloadClip(target:Dynamic):Bool;
+
+	function onLoadInit(target:MovieClip) : Void;
+	function onLoadStart(target:MovieClip) : Void;
+	function onLoadProgress(target:MovieClip, loaded : Int, total : Int) : Void;
+	function onLoadComplete(target:MovieClip) : Void;
+	function onLoadError(target:MovieClip, error:String) : Void;
 }
