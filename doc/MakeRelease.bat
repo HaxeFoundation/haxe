fa661f68bc1e87ee99defd5a3df10e949cb2763c
Nicolas Cannasse <ncannasse@gmail.com>
2006-06-05 14:31:20 +0000

    added doc generation.

--- doc/MakeRelease.bat
+++ doc/MakeRelease.bat
@@ -1,10 +1,19 @@
 @echo off
 rm -rf haxe-release
 mkdir haxe-release
+mkdir haxe-release\doc
 cp ../haxe.exe ../haxesetup.exe CHANGES.txt LICENSE.txt haxeserver.bat haxe-release
 cp -R ../std haxe-release
 
 cd haxe-release\std
+
+haxe all.hxml
+cd tools
+haxe docview.hxml
+neko docview ../flash.xml ../neko.xml ../js.xml
+mv index.html content ../../doc
+cd ..
+
 rm -rf CVS .cvsignore */CVS */.cvsignore */*/CVS */*/.cvsignore */*/*/CVS */*/*/.cvsignore
 rm -rf all.n all.js all.swf *.xml tools/docview.n tools/index.html tools/content
 
