a8a22319d4364486e82665aed1525d3994e63146
Pascal Peridont <pperidont@motion-twin.com>
2006-07-14 09:29:57 +0000

    *** empty log message ***

--- std/mtwin/mail/Part.hx
+++ std/mtwin/mail/Part.hx
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2006, Motion-Twin
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MOTION-TWIN "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE HAXE PROJECT CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
 package mtwin.mail;
 
 import neko.Utf8;
@@ -198,9 +222,9 @@ class MetaPart<T> {
 		if( content.length > 0 ){
 			switch( getHeader("Content-Transfer-Encoding") ){
 				case "base64":
-					ret.add( Tools.encodeBase64(content,"\r\n") + "\r\n" );
+					ret.add( Tools.encodeBase64(content) + "\r\n" );
 				case "quoted-printable":
-					ret.add( Tools.encodeQuotedPrintable(content,"\r\n") + "\r\n" );
+					ret.add( Tools.encodeQuotedPrintable(content) + "\r\n" );
 				default:
 					ret.add( content + "\r\n" );
 			}
@@ -337,7 +361,7 @@ class MetaPart<T> {
 			if( cte == "quoted-printable" ){
 				content = Tools.decodeQuotedPrintable( content );
 			}else if( cte == "base64" ){
-				content = Tools.decodeBase64( content,"\r\n" );
+				content = Tools.decodeBase64( content );
 			}
 		}
 
