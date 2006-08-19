7ccd9455fbfed6038dcc1ba7f68c7f71ec36932c
Nicolas Cannasse <ncannasse@gmail.com>
2006-08-19 14:04:11 +0000

    fixed seek

--- std/neko/io/FileOutput.hx
+++ std/neko/io/FileOutput.hx
@@ -61,8 +61,8 @@ class FileOutput extends Output {
 		file_close(__f);
 	}
 
-	public function seek( p : Int, pos : FileSeek ) : Int {
-		return file_seek(__f,p,switch( pos ) { case SeekBegin: 0; case SeekCur: 1; case SeekEnd: 2; });
+	public function seek( p : Int, pos : FileSeek ) {
+		file_seek(__f,p,switch( pos ) { case SeekBegin: 0; case SeekCur: 1; case SeekEnd: 2; });
 	}
 
 	public function tell() : Int {
