5b44d51afc8bf65e4d0f24d8f6e6665ee5a04068
Nicolas Cannasse <ncannasse@gmail.com>
2006-08-23 15:04:31 +0000

    clean first

--- swflib/install.bat
+++ swflib/install.bat
@@ -1,6 +1,6 @@
+@ocamake -a *.ml *.mli -o swfLib.cma -clean
+@ocamake -a *.ml *.mli -o swfLib.cmxa -clean
 @ocamake -a *.ml *.mli -o swfLib.cma
 @ocamake -a *.ml *.mli -o swfLib.cmxa
 @cp -f as3parse.cmi as3code.cmi as3.cmi swf.cmi png.cmi swfPic.cmi swfParser.cmi swfZip.cmi actionScript.cmi swfLib.lib swfLib.cma swfLib.cmxa c:\ocaml\lib
-@ocamake -a *.ml *.mli -o swfLib.cma -clean
-@ocamake -a *.ml *.mli -o swfLib.cmxa -clean
 @pause
