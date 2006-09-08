8e6b0ab4428da588f315f5417728695f238453c4
Nicolas Cannasse <ncannasse@gmail.com>
2006-09-08 08:57:44 +0000

    added smc.

--- std/flash/MovieClip.hx
+++ std/flash/MovieClip.hx
@@ -151,6 +151,10 @@ implements Dynamic
   	function lineGradientStyle( fillType:String, colors:Array<Int>, alphas:Array<+Float>, ratios:Array<+Float>, matrix:Dynamic, ?spreadMethod : String, ?interpolationMethod:String, ?focalPointRatio:Float ) : Void;
 #end
 
+// MT extension
+#if flash_strict
+	var smc : flash.MovieClip;
+#end
 	private static function __init__() : Void untyped {
 		flash.MovieClip = _global["MovieClip"];
 	}
