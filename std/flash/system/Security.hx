fb1eb83949b603045e3d6c788966d19af9125664
Nicolas Cannasse <ncannasse@gmail.com>
2006-11-20 14:29:29 +0000

    added params.

--- std/flash/system/Security.hx
+++ std/flash/system/Security.hx
@@ -2,8 +2,8 @@ package flash.system;
 
 extern class Security
 {
-	static function allowDomain():Void;
-	static function allowInsecureDomain():Void;
+	static function allowDomain( domain : String ):Void;
+	static function allowInsecureDomain( domain : String ):Void;
 	static function loadPolicyFile(url:String):Void;
 
 	private static function __init__() : Void untyped {
