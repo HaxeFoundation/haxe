9ea1fc6bc6aa59e02c18b5911543cab6f4685739
Nicolas Cannasse <ncannasse@gmail.com>
2007-01-01 17:27:00 +0000

    1.10

--- main.ml
+++ main.ml
@@ -120,9 +120,9 @@ let rec process_params acc = function
 
 and init params =
 try	
-	let version = 109 in
+	let version = 110 in
 	let version_str = Printf.sprintf "%d.%.2d" (version / 100) (version mod 100) in
-	let usage = "Haxe Compiler " ^ version_str ^ " - (c)2005-2006 Motion-Twin\n Usage : haxe.exe [options] <class names...>\n Options :" in
+	let usage = "Haxe Compiler " ^ version_str ^ " - (c)2005-2007 Motion-Twin\n Usage : haxe.exe [options] <class names...>\n Options :" in
 	let classes = ref [([],"Std")] in
 	let target = ref No in
 	let swf_in = ref None in
