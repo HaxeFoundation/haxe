b7e085971b8be5f7d3986c9c1f81d8831e324223
Nicolas Cannasse <ncannasse@gmail.com>
2007-02-27 08:52:37 +0000

    minor completion fix.

--- parser.ml
+++ parser.ml
@@ -506,7 +506,7 @@ and expr = parser
 		| [< >] -> serror())
 	| [< '(POpen,p1); e = expr; '(PClose,p2); s >] -> expr_next (EParenthesis e, punion p1 p2) s
 	| [< '(BkOpen,p1); l = parse_array_decl; '(BkClose,p2); s >] -> expr_next (EArrayDecl l, punion p1 p2) s
-	| [< '(Kwd Function,p1); '(POpen,_); al = psep Comma parse_fun_param; '(PClose,_); t = parse_type_opt; e = expr; s >] ->
+	| [< '(Kwd Function,p1); '(POpen,_); al = psep Comma parse_fun_param; '(PClose,_); t = parse_type_opt; e = toplevel_expr; s >] ->
 		let f = {
 			f_type = t;
 			f_args = al;
