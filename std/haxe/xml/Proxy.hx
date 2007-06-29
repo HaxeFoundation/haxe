700b34e6375d3dc3360105385de90e6eb1a3bc02
Nicolas Cannasse <ncannasse@gmail.com>
2007-06-29 10:58:34 +0000

    added

---
+++ std/haxe/xml/Proxy.hx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2005-2007, The haXe Project Contributors
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE HAXE PROJECT CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE HAXE PROJECT CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package haxe.xml;
+
+/**
+	This proxy can be inherited with an XML file name parameter.
+	It will	only allow access to fields which corresponds to an "id" attribute
+	value in the XML file :
+
+	[
+	class MyXml extends haxe.xml.Proxy<"my.xml",MyStructure> {
+	}
+	...
+	var h = new Hash<MyStructure>();
+	// ... fill h with "my.xml" content
+	var m = new MyXml(h.get);
+	trace(m.myNode.structField);
+	// access to "myNode" is only possible
+	// if you have an id="myNode" attribute
+	// in your XML, and completion works as well
+	]
+**/
+class Proxy<Const,T> {
+
+	var f : String -> T;
+
+	public function new(f) {
+		this.f = f;
+	}
+
+	function __resolve(k) {
+		return f(k);
+	}
+
+}
