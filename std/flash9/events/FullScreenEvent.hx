a4484479ca52078545e898614884cbf563be7d3f
Nicolas Cannasse <ncannasse@gmail.com>
2007-09-04 18:24:37 +0000

    fix

--- std/flash9/events/FullScreenEvent.hx
+++ std/flash9/events/FullScreenEvent.hx
@@ -1,7 +1,7 @@
 package flash.events;
 
 extern class FullScreenEvent extends flash.events.ActivityEvent {
-		var fullScreen(default,null);
-		function new(type:String, ?bubbles:Bool, ?cancelable:Bool, ?fullScreen:Bool );
+		var fullScreen(default,null) : Bool;
+		function new(type:String, ?bubbles:Bool, ?cancelable:Bool, ?fullScreen:Bool ) : Void;
 		static var FULL_SCREEN : String;
 }
