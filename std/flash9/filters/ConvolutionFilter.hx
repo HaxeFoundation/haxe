e91dd98c7af50c5b167fc2cafc8a30a38bfb18bd
Nicolas Cannasse <ncannasse@gmail.com>
2008-07-21 15:35:10 +0000

    fix

--- std/flash9/filters/ConvolutionFilter.hx
+++ std/flash9/filters/ConvolutionFilter.hx
@@ -6,9 +6,9 @@ extern class ConvolutionFilter extends BitmapFilter {
 	var clamp : Bool;
 	var color : UInt;
 	var divisor : Float;
-	var matrix : Array<Float>;
+	var matrix : Array<Dynamic>;
 	var matrixX : Float;
 	var matrixY : Float;
 	var preserveAlpha : Bool;
-	function new(?matrixX : Float, ?matrixY : Float, ?matrix : Array<Float>, ?divisor : Float, ?bias : Float, ?preserveAlpha : Bool, ?clamp : Bool, ?color : UInt, ?alpha : Float) : Void;
+	function new(?matrixX : Float, ?matrixY : Float, ?matrix : Array<Dynamic>, ?divisor : Float, ?bias : Float, ?preserveAlpha : Bool, ?clamp : Bool, ?color : UInt, ?alpha : Float) : Void;
 }
