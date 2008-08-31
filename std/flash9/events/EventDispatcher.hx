cdfc39e030de9b8ed96e6407dbbd605c089bdfab
Nicolas Cannasse <ncannasse@gmail.com>
2008-08-31 13:37:15 +0000

    fixed IEventDispatcher (Dynamic->Void was causing some trouble with inheritance)

--- std/flash9/events/EventDispatcher.hx
+++ std/flash9/events/EventDispatcher.hx
@@ -2,10 +2,10 @@ package flash.events;
 
 extern class EventDispatcher implements IEventDispatcher {
 	function new(?target : flash.events.IEventDispatcher) : Void;
-	function addEventListener(type : String, listener : Dynamic -> Void, ?useCapture : Bool, ?priority : Int, ?useWeakReference : Bool) : Void;
+	function addEventListener(type : String, listener : Dynamic, ?useCapture : Bool, ?priority : Int, ?useWeakReference : Bool) : Void;
 	function dispatchEvent(event : Event) : Bool;
 	function hasEventListener(type : String) : Bool;
-	function removeEventListener(type : String, listener : Dynamic -> Void, ?useCapture : Bool) : Void;
+	function removeEventListener(type : String, listener : Dynamic, ?useCapture : Bool) : Void;
 	function toString() : String;
 	function willTrigger(type : String) : Bool;
 }
--- std/flash9/events/IEventDispatcher.hx
+++ std/flash9/events/IEventDispatcher.hx
@@ -1,9 +1,9 @@
 package flash.events;
 
 extern interface IEventDispatcher {
-	function addEventListener(type : String, listener : Dynamic -> Void, ?useCapture : Bool, ?priority : Int, ?useWeakReference : Bool) : Void;
+	function addEventListener(type : String, listener : Dynamic, ?useCapture : Bool, ?priority : Int, ?useWeakReference : Bool) : Void;
 	function dispatchEvent(event : Event) : Bool;
 	function hasEventListener(type : String) : Bool;
-	function removeEventListener(type : String, listener : Dynamic -> Void, ?useCapture : Bool) : Void;
+	function removeEventListener(type : String, listener : Dynamic, ?useCapture : Bool) : Void;
 	function willTrigger(type : String) : Bool;
 }
