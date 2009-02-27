f30fb98caf586cd408ca527b69b1c4c0219e3596
Nicolas Cannasse <ncannasse@gmail.com>
2009-02-27 08:31:54 +0000

    minor fix

--- std/flash9/net/SharedObject.hx
+++ std/flash9/net/SharedObject.hx
@@ -10,7 +10,7 @@ extern class SharedObject extends flash.events.EventDispatcher {
 	function clear() : Void;
 	function close() : Void;
 	function connect(myConnection : NetConnection, ?params : String) : Void;
-	function flush(?minDiskSpace : Int) : String;
+	function flush(?minDiskSpace : Int) : SharedObjectFlushStatus;
 	function send( ?p1 : Dynamic, ?p2 : Dynamic, ?p3 : Dynamic, ?p4 : Dynamic, ?p5 : Dynamic ) : Void;
 	function setDirty(propertyName : String) : Void;
 	function setProperty(propertyName : String, ?value : Dynamic) : Void;
