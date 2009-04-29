58462ae8e57a32e60b21d773b1edbf4822eaa7b0
Hugh Sanderson <gamehaxe@gmail.com>
2009-04-29 14:19:55 +0000

    Added a GC interface to cpp

---
+++ std/cpp/vm/Gc.hx
@@ -0,0 +1,15 @@
+package cpp.vm;
+
+class Gc
+{
+   static public function enable(inEnable:Bool) : Void
+   {
+      untyped __global__.__hxcpp_enable(inEnable);
+   }
+
+   static public function run(major:Bool) : Void
+   {
+      untyped __global__.__hxcpp_collect();
+   }
+
+}
