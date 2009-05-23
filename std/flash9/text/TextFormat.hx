f442adaf81815d3bf1b0b8b8769771c83ae5c744
Nicolas Cannasse <ncannasse@gmail.com>
2009-05-23 10:41:51 +0000

    fix

--- std/flash9/text/TextFormat.hx
+++ std/flash9/text/TextFormat.hx
@@ -2,23 +2,23 @@ package flash.text;
 
 extern class TextFormat {
 	var align : TextFormatAlign;
-	var blockIndent : Dynamic;
-	var bold : Dynamic;
-	var bullet : Dynamic;
-	var color : Dynamic;
+	var blockIndent : Null<Float>;
+	var bold : Null<Bool>;
+	var bullet : Null<Bool>;
+	var color : Null<UInt>;
 	var display : TextFormatDisplay;
 	var font : String;
-	var indent : Dynamic;
-	var italic : Dynamic;
-	var kerning : Dynamic;
-	var leading : Dynamic;
-	var leftMargin : Dynamic;
-	var letterSpacing : Dynamic;
-	var rightMargin : Dynamic;
-	var size : Dynamic;
+	var indent : Null<Float>;
+	var italic : Null<Bool>;
+	var kerning : Null<Bool>;
+	var leading : Null<Float>;
+	var leftMargin : Null<Float>;
+	var letterSpacing : Null<Float>;
+	var rightMargin : Null<Float>;
+	var size : Null<Float>;
 	var tabStops : Array<UInt>;
 	var target : String;
-	var underline : Dynamic;
+	var underline : Null<Bool>;
 	var url : String;
-	function new(?font : String, ?size : Dynamic, ?color : Dynamic, ?bold : Dynamic, ?italic : Dynamic, ?underline : Dynamic, ?url : String, ?target : String, ?align : String, ?leftMargin : Dynamic, ?rightMargin : Dynamic, ?indent : Dynamic, ?leading : Dynamic) : Void;
+	function new(?font : String, ?size : Null<Float>, ?color : Null<UInt>, ?bold : Null<Bool>, ?italic : Null<Bool>, ?underline : Null<Bool>, ?url : String, ?target : String, ?align : TextFormatAlign, ?leftMargin : Null<Float>, ?rightMargin : Null<Float>, ?indent : Null<Float>, ?leading : Null<Float>) : Void;
 }
