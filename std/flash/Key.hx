0a5699ccb04f489e03178edba3a50b5271038173
Franco Ponticelli <franco.ponticelli@gmail.com>
2009-05-28 17:05:25 +0000

    - minor: added Key.isAccessible()

--- std/flash/Key.hx
+++ std/flash/Key.hx
@@ -33,6 +33,7 @@ extern class Key
 
 	static function getAscii():Int;
 	static function getCode():Int;
+	static function isAccessible():Bool;
 	static function isDown(code:Int):Bool;
 	static function isToggled(code:Int):Bool;
 
