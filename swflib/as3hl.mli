d2d8d2373975bb682cedc77949ac6d9d4506dd5e
Nicolas Cannasse <ncannasse@gmail.com>
2010-04-02 13:01:20 +0000

    minor

--- swflib/as3hl.mli
+++ swflib/as3hl.mli
@@ -187,7 +187,7 @@ and hl_function = {
 	hlf_init_scope : int;
 	hlf_max_scope : int;
 	mutable hlf_code : hl_opcode array;
-	hlf_trys : hl_try_catch array;
+	mutable hlf_trys : hl_try_catch array;
 	hlf_locals : (hl_name * hl_name option * hl_slot * bool) array; (* bool = const - mostly false *)
 }
 
