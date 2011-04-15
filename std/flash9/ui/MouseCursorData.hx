f93dcba66a807b88d0ecc81483c0c8c66d875e6b
Nicolas Cannasse <ncannasse@gmail.com>
2011-04-15 13:49:44 +0000

    fix

--- std/flash9/ui/MouseCursorData.hx
+++ std/flash9/ui/MouseCursorData.hx
@@ -1,7 +1,7 @@
 package flash.ui;
 
 @:final @:require(flash10_2) extern class MouseCursorData {
-	var data : flash.Vector<BitmapData>;
+	var data : flash.Vector<flash.display.BitmapData>;
 	var frameRate : Float;
 	var hotSpot : flash.geom.Point;
 	var name : String;
