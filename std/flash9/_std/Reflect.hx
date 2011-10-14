a92d218165914652e3284c7354fe08be333810f6
Nicolas Cannasse <ncannasse@gmail.com>
2011-10-14 19:32:55 +0000

    fixed issue #535

--- std/flash9/_std/Reflect.hx
+++ std/flash9/_std/Reflect.hx
@@ -29,8 +29,9 @@
 		return o.hasOwnProperty( field );
 	}
 
-	public inline static function field( o : Dynamic, field : String ) : Dynamic untyped {
-		return (o == null) ? null : o[field];
+	public static function field( o : Dynamic, field : String ) : Dynamic untyped {
+		// sealed classes will throw an exception
+		return try o[field] catch( e : Dynamic ) null;
 	}
 
 	public inline static function setField( o : Dynamic, field : String, value : Dynamic ) : Void untyped {
