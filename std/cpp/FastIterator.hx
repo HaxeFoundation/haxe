c7286f20bc9df83436eee1bbb9dadd74ea938df1
Simon Krajewski <simon@haxe.org>
2012-07-27 14:01:15 +0000

    added @:generic to cpp/FastIterator.hx

--- std/cpp/FastIterator.hx
+++ std/cpp/FastIterator.hx
@@ -1,6 +1,9 @@
 package cpp;
 
-extern class FastIterator<T> implements haxe.rtti.Generic
+#if haxe3
+@:generic
+#end
+extern class FastIterator<T> #if !haxe3 implements haxe.rtti.Generic #end
 {
 	public function hasNext():Bool;
 	public function next():T;
