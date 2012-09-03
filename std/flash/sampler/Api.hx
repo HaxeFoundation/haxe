523f678331ba031b89b152434c31434798dfcacb
Nicolas Cannasse <ncannasse@gmail.com>
2012-09-03 10:34:56 +0000

    completed flash sampling api (fixed issue #1174)

--- std/flash/sampler/Api.hx
+++ std/flash/sampler/Api.hx
@@ -1,53 +1,73 @@
 package flash.sampler;
 
-class Api {
+extern class Api {
 
-	public static function clearSamples() {
+	public inline static function clearSamples() : Void {
 		untyped __global__["flash.sampler.clearSamples"]();
 	}
 
-	public static function getGetterInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
+	public inline static function getGetterInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
 		return untyped __global__["flash.sampler.getGetterInvocationCount"](obj,qname);
 	}
 
-	public static function getSetterInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
+	public inline static function getSetterInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
 		return untyped __global__["flash.sampler.getSetterInvocationCount"](obj,qname);
 	}
 
-	public static function getInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
+	public inline static function getInvocationCount( obj : Dynamic, qname : flash.utils.QName ) : Float {
 		return untyped __global__["flash.sampler.getInvocationCount"](obj,qname);
 	}
 
-	public static function getMemberNames( obj : Dynamic, ?instanceNames : Bool ) : Dynamic<flash.utils.QName> {
+	public inline static function getMemberNames( obj : Dynamic, instanceNames : Bool = false ) : Dynamic<flash.utils.QName> {
 		return untyped __global__["flash.sampler.getMemberNames"](obj,instanceNames);
 	}
 
-	public static function getSampleCount() : Float {
+	public inline static function getSampleCount() : Float {
 		return untyped __global__["flash.sampler.getSampleCount"]();
 	}
 
-	public static function getSamples() : Array<flash.sampler.Sample> {
+	public inline static function getSamples() : Array<flash.sampler.Sample> {
 		return untyped __foreach__(__global__["flash.sampler.getSamples"]());
 	}
 
-	public static function getSize( obj : Dynamic ) : Float {
+	public inline static function getSize( obj : Dynamic ) : Float {
 		return untyped __global__["flash.sampler.getSize"](obj);
 	}
 
-	public static function isGetterSetter( obj : Dynamic, qname : flash.utils.QName ) : Bool {
+	public inline static function isGetterSetter( obj : Dynamic, qname : flash.utils.QName ) : Bool {
 		return untyped __global__["flash.sampler.isGetterSetter"](obj,qname);
 	}
 
-	public static function pauseSampling() {
+	public inline static function pauseSampling() : Void {
 		untyped __global__["flash.sampler.pauseSampling"]();
 	}
 
-	public static function startSampling() {
+	public inline static function startSampling() : Void {
 		untyped __global__["flash.sampler.startSampling"]();
 	}
 
-	public static function stopSampling() {
+	public inline static function stopSampling() : Void {
 		untyped __global__["flash.sampler.stopSampling"]();
+	}
+
+	public inline static function getLexicalScopes( fun : Dynamic ) : Array<Dynamic> {
+		return untyped __global__["flash.sampler.getLexicalScopes"](fun);
+	}
+
+	public inline static function getMasterString( s : String ) : String {
+		return untyped __global__["flash.sampler.getMasterString"](s);
+	}
+
+	public inline static function getSavedThis( fun : Dynamic ) : Dynamic {
+		return untyped __global__["flash.sampler.getSavedThis"](fun);
+	}
+
+	public inline static function sampleInternalAllocs( b : Bool ) : Void {
+		untyped __global__["flash.sampler.sampleInternalAllocs"](b);
+	}
+
+	public inline static function setSamplerCallback( callb : Dynamic ) : Void {
+		untyped __global__["flash.sampler.setSamplerCallback"](callb);
 	}
 
 }
