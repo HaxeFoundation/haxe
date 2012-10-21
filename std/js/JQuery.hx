f8aeeb4befaceb3f9696e3252b8874f035d449cb
Nicolas Cannasse <ncannasse@gmail.com>
2012-10-21 10:35:15 +0000

    no longe embed JS automatically, enable with -D embedJs instead (fixed issue #698)

--- std/js/JQuery.hx
+++ std/js/JQuery.hx
@@ -345,7 +345,7 @@ extern class JQuery implements ArrayAccess<Dom.HtmlDom> {
 	}
 
 	private static function __init__() : Void untyped {
-		#if !noEmbedJS
+		#if !embedJs
 		if( untyped __js__("typeof($) == 'undefined'") )
 			haxe.macro.Tools.includeFile("js/jquery-latest.min.js");
 		#end
