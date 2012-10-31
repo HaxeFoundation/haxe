91c05c609e055822eb07eccfada38b72d28a995b
Nicolas Cannasse <ncannasse@gmail.com>
2012-10-31 14:37:09 +0000

    added

---
+++ tests/unit/TestStringTools.hx
@@ -0,0 +1,31 @@
+package unit;
+
+class TestStringTools extends Test
+{
+
+	function testHtmlEscape() {
+		eq( StringTools.htmlEscape("<>&\"'"), "&lt;&gt;&amp;&quot;&#039;");
+		eq( StringTools.htmlUnescape("&lt;&gt;&amp;&quot;&#039;"), "<>&\"'" );
+	}
+	
+	function testHex() {
+		eq( StringTools.hex(0xABCDEF,7), "0ABCDEF" );
+		eq( StringTools.hex(-1,8), "FFFFFFFF" );
+		eq( StringTools.hex(-481400000,8), "E34E6B40" );
+	}
+	
+	function testCCA() {
+		var str = "abc";
+		eq( StringTools.fastCodeAt(str, 0), "a".code );
+		eq( StringTools.fastCodeAt(str, 1), "b".code );
+		eq( StringTools.fastCodeAt(str, 2), "c".code );
+		eq(StringTools.fastCodeAt(String.fromCharCode(128), 0), 128);
+		eq(StringTools.fastCodeAt(String.fromCharCode(255), 0), 255);
+		f( StringTools.isEOF(StringTools.fastCodeAt(str, 2)) );
+		t( StringTools.isEOF(StringTools.fastCodeAt(str, 3)) );
+		
+		t( StringTools.isEOF(StringTools.fastCodeAt("", 0)) );
+	}
+		
+	
+}
