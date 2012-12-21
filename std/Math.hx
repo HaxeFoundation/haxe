8c75cb8705d584d40f9b3d8965f274ff088a5270
Simon Krajewski <simon@haxe.org>
2012-12-21 16:18:37 +0000

    [Cpp] ffloor/fceil/fround support

--- std/Math.hx
+++ std/Math.hx
@@ -49,7 +49,7 @@ extern class Math
 	static function pow(v:Float,exp:Float):Float;
 	static function random() : Float;
 	
-	#if flash9
+	#if (flash9 || cpp)
 	
 	static function ffloor( v : Float ) : Float;
 	static function fceil( v : Float ) : Float;
