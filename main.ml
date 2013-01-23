cd6402309b0afe2036ca26890e34184b3b1a8767
Nicolas Cannasse <ncannasse@gmail.com>
2013-01-23 11:46:47 +0000

    one more completion fix when having --next with different platforms

--- main.ml
+++ main.ml
@@ -1190,9 +1190,10 @@ with
 	| Parser.Error (m,p) ->
 		error ctx (Parser.error_msg m) p
 	| Typecore.Forbid_package ((pack,m,p),pl,pf)  ->
-		if !Common.display_default && ctx.has_next then
-			ctx.has_error <- false
-		else begin
+		if !Common.display_default && ctx.has_next then begin
+			ctx.has_error <- false;
+			ctx.messages <- [];
+		end else begin
 			error ctx (Printf.sprintf "You cannot access the %s package while %s (for %s)" pack (if pf = "macro" then "in a macro" else "targeting " ^ pf) (Ast.s_type_path m) ) p;
 			List.iter (error ctx "    referenced here") (List.rev pl);
 		end
