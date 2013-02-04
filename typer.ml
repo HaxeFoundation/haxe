02ab4a1245c6a793f961da6c078f9274426b0c9e
Simon Krajewski <simon@haxe.org>
2013-02-04 11:55:09 +0000

    raise Fatal_error if macro post-process fails (fixed issue #1351)

--- typer.ml
+++ typer.ml
@@ -3380,7 +3380,8 @@ and flush_macro_context mint ctx =
 		mint
 	end else mint in
 	(* we should maybe ensure that all filters in Main are applied. Not urgent atm *)
-	Interp.add_types mint types (Codegen.post_process [Codegen.handle_abstract_casts mctx; Codegen.captured_vars mctx.com; Codegen.rename_local_vars mctx.com]);
+	(try Interp.add_types mint types (Codegen.post_process [Codegen.handle_abstract_casts mctx; Codegen.captured_vars mctx.com; Codegen.rename_local_vars mctx.com])
+	with Error (e,p) -> display_error ctx (error_msg e) p; raise Fatal_error);
 	Codegen.post_process_end()
 
 let create_macro_interp ctx mctx =
