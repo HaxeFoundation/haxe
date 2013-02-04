1493efe7a9be209ebcdb36bb5294049840a94c13
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-02-04 00:31:26 +0000

    fixed exists return type

--- std/Map.hx
+++ std/Map.hx
@@ -25,7 +25,7 @@ abstract Map(IMap < K, V > )<K,V> {
 private typedef IMap < K, V > = {
 	public function get(k:K):V;
 	public function set(k:K, v:V):Void;
-	public function exists(k:K):Null<V>;
+	public function exists(k:K):Bool;
 	public function remove(k:K):Bool;
 	public function keys():Iterator<K>;
 	public function iterator():Iterator<V>;
