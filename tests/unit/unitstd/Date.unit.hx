27340fb2fcf7dd0d4ef9a888c7868a1f47f87f3e
Simon Krajewski <simon@haxe.org>
2013-02-10 21:59:23 +0000

    unspec (affected by timezone)

--- tests/unit/unitstd/Date.unit.hx
+++ tests/unit/unitstd/Date.unit.hx
@@ -7,5 +7,5 @@ date.getFullYear() == 1982;
 date.getMonth() == 10;
 date.getDate() == 10;
 date.getDay() == 3;
-date.getTime() == 405781340000.;
+//date.getTime() == 405781340000.;
 date.toString() == "1982-11-10 14:02:20";
