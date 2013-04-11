f80ccab7d70e87f89a56ae4dba6c136456c76a52
Tom Bebbington <tom@toms-laptop.(none)>
2013-04-11 22:58:31 +0100

    Fixed NativeArray

--- std/rust/NativeArray.hx
+++ std/rust/NativeArray.hx
@@ -2,4 +2,12 @@
 	public function new() {}
 	public function each(f:T -> Bool):Void {}
 	public function size_hint():Null<Int> {return null;}
+	public function capacity():Int;
+	public function len():Int;
+	public function filter(f:T -> Bool):NativeArray<T>;
+	public function from_elem(len:Int, d:T):NativeArray<T>;
+	public function grow(n:Int, initv:T):NativeArray<T>;
+	public function head():T;
+	public function is_empty():Bool;
+}
 }
