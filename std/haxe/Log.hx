7c7cdbe4fb46abc98ef38b6890da3fcedd0979a4
Simon Krajewski <simon@haxe.org>
2013-05-19 09:21:35 +0000

    [Php] handle extraParams (fixed issue #1643)

--- std/haxe/Log.hx
+++ std/haxe/Log.hx
@@ -45,44 +45,50 @@ class Log {
 	public static dynamic function trace( v : Dynamic, ?infos : PosInfos ) : Void {
 		#if flash
 			#if (fdb || native_trace)
-		var pstr = infos == null ? "(null)" : infos.fileName + ":" + infos.lineNumber;
-		var str = flash.Boot.__string_rec(v, "");
-		if( infos != null && infos.customParams != null ) for( v in infos.customParams ) str += "," + flash.Boot.__string_rec(v, "");
-		untyped #if flash9 __global__["trace"] #else __trace__ #end(pstr+": "+str);
+				var pstr = infos == null ? "(null)" : infos.fileName + ":" + infos.lineNumber;
+				var str = flash.Boot.__string_rec(v, "");
+				if( infos != null && infos.customParams != null ) for( v in infos.customParams ) str += "," + flash.Boot.__string_rec(v, "");
+				untyped #if flash9 __global__["trace"] #else __trace__ #end(pstr+": "+str);
 			#else
-		untyped flash.Boot.__trace(v,infos);
+				untyped flash.Boot.__trace(v,infos);
 			#end
 		#elseif neko
-		untyped {
-			$print(infos.fileName + ":" + infos.lineNumber + ": ", v);
-			if( infos.customParams != null ) for( v in infos.customParams ) $print(",", v);
-			$print("\n");
-		}
-		#elseif js
-		untyped js.Boot.__trace(v,infos);
-		#elseif php
-		untyped __call__('_hx_trace', v,infos);
-		#elseif cpp
-      if (infos!=null && infos.customParams!=null) {
-         var extra:String = "";
-         for( v in infos.customParams )
-            extra += "," + v;
-		   untyped __trace(v + extra,infos);
-      }
-      else
-		   untyped __trace(v,infos);
-		#elseif (cs || java)
-		var str:String = null;
-		if (infos != null)
-		{
-			str = infos.fileName + ":" + infos.lineNumber + ": " + v;
-			if (infos.customParams != null)
-			{
-				str += "," + infos.customParams.join(",");
+			untyped {
+				$print(infos.fileName + ":" + infos.lineNumber + ": ", v);
+				if( infos.customParams != null ) for( v in infos.customParams ) $print(",", v);
+				$print("\n");
 			}
-		} else {
-			str = v;
-		}
+		#elseif js
+			untyped js.Boot.__trace(v,infos);
+		#elseif php
+			if (infos!=null && infos.customParams!=null) {
+				var extra:String = "";
+				for( v in infos.customParams )
+					extra += "," + v;
+				untyped __call__('_hx_trace', v + extra, infos);
+			}
+			else
+				untyped __call__('_hx_trace', v, infos);		
+		#elseif cpp
+			if (infos!=null && infos.customParams!=null) {
+				var extra:String = "";
+				for( v in infos.customParams )
+					extra += "," + v;
+				untyped __trace(v + extra,infos);
+			}
+			else
+				untyped __trace(v,infos);
+		#elseif (cs || java)
+			var str:String = null;
+			if (infos != null) {
+				str = infos.fileName + ":" + infos.lineNumber + ": " + v;
+				if (infos.customParams != null)
+				{
+					str += "," + infos.customParams.join(",");
+				}
+			} else {
+				str = v;
+			}
 			#if cs
 			untyped __cs__("System.Console.WriteLine(str)");
 			#elseif java
