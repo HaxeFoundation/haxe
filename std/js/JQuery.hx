59cc6e10f53401341f64dcc575ca7d77a7ea22e2
Nicolas Cannasse <ncannasse@gmail.com>
2013-05-20 20:19:12 +0000

    added JQueryHelper to import J/JTHIS globals from

--- std/js/JQuery.hx
+++ std/js/JQuery.hx
@@ -62,6 +62,22 @@ typedef JqEvent = {
 	function stopPropagation() : Void;
 }
 
+extern class JQueryHelper {
+	@:overload(function(j:JQuery):JQuery{})
+	@:overload(function(j:DOMWindow):JQuery{})
+	@:overload(function(j:Element):JQuery{})
+	public static inline function J( html : String ) : JQuery {
+		return new JQuery(html);
+	}
+	
+	public static var JTHIS(get, null) : JQuery;
+
+	static inline function get_JTHIS() : JQuery {
+		return untyped __js__("$(this)");
+	}
+
+}
+
 @:initPackage
 extern class JQuery implements ArrayAccess<Element> {
 
