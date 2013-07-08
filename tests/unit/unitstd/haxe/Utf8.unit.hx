6356cb50eb939802ac8f3f2b275ff1b109be5679
Ryusei Yamaguchi <mandel59@gmail.com>
2013-07-08 15:19:39 +0900

    Add unit test for haxe.Utf8

---
+++ tests/unit/unitstd/haxe/Utf8.unit.hx
@@ -0,0 +1,10 @@
+var str = "あ𠀀い";
+haxe.Utf8.length(str) == 3;
+haxe.Utf8.charCodeAt(str, 0) == 0x3042;
+haxe.Utf8.charCodeAt(str, 1) == 0x20000;
+haxe.Utf8.charCodeAt(str, 2) == 0x3044;
+var buf = new haxe.Utf8();
+buf.addChar(0x3042);
+buf.addChar(0x20000);
+buf.addChar(0x3044);
+buf.toString() == str;
