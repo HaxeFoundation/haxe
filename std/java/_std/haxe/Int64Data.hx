09d7596edc4d923af63aab058c8db6ab1cab0cbb
Zachary Dremann <dremann@gmail.com>
2013-08-07 12:02:45 -0400

    Make haxe.Int64 into an abstract
    This allows Ints and Floats to automatically be cast to Int64, and
    allows for the use of operators (+, -, >>, etc.)

--- std/java/_std/haxe/Int64Data.hx
+++ std/java/_std/haxe/Int64Data.hx
@@ -20,124 +20,124 @@
  * DEALINGS IN THE SOFTWARE.
  */
 package haxe;
-using haxe.Int64;
+using haxe.Int64Data;
 private typedef NativeInt64 = Int;
 
 @:coreApi
-@:nativeGen class Int64
+@:nativeGen class Int64Data
 {
-	@:extern private static inline function asNative(i:Int64):NativeInt64 return untyped i;
-	@:extern private static inline function ofNative(i:NativeInt64):Int64 return untyped i;
+	@:extern private static inline function asNative(i:Int64Data):NativeInt64 return untyped i;
+	@:extern private static inline function ofNative(i:NativeInt64):Int64Data return untyped i;
 	@:extern private static inline function mkNative(i:Dynamic):NativeInt64 return i;
 
-	public static inline function make( high : Int, low : Int ) : Int64
+	public static inline function make( high : Int, low : Int ) : Int64Data
 	{
 		return ((cast(high, NativeInt64) << 32 ) | (low & untyped __java__('0xffffffffL'))).ofNative();
 	}
 
-	public static inline function getLow( x : Int64 ) : Int
+	public static inline function getLow( x : Int64Data ) : Int
 	{
 		return cast (x.asNative() & untyped __java__("0xFFFFFFFFL"));
 	}
 
-	public static inline function getHigh( x : Int64 ) : Int
+	public static inline function getHigh( x : Int64Data ) : Int
 	{
 		return cast(cast(x,NativeInt64) >>> cast(32,NativeInt64), Int);
 	}
 
-	public static inline function ofInt( x : Int ) : Int64 {
+	public static inline function ofInt( x : Int ) : Int64Data {
 		return cast x;
 	}
 
-	public static inline function toInt( x : Int64 ) : Int
+	public static inline function toInt( x : Int64Data ) : Int
 	{
 		return cast x;
 	}
 
-	public static inline function add( a : Int64, b : Int64 ) : Int64
+	public static inline function add( a : Int64Data, b : Int64Data ) : Int64Data
 	{
 		return (a.asNative() + b.asNative()).ofNative();
 	}
 
-	public static inline function sub( a : Int64, b : Int64 ) : Int64
+	public static inline function sub( a : Int64Data, b : Int64Data ) : Int64Data
 	{
 		return (a.asNative() - b.asNative()).ofNative();
 	}
 
-	public static inline function mul( a : Int64, b : Int64 ) : Int64 {
+	public static inline function mul( a : Int64Data, b : Int64Data ) : Int64Data {
 		return (a.asNative() * b.asNative()).ofNative();
 	}
 
-	static function divMod( modulus : Int64, divisor : Int64 ) : { quotient : Int64, modulus : Int64 }
+	static function divMod( modulus : Int64Data, divisor : Int64Data ) : { quotient : Int64Data, modulus : Int64Data }
 	{
-		var q:Int64 = (modulus.asNative() / divisor.asNative()).mkNative().ofNative();
-		var m:Int64 = (modulus.asNative() % divisor.asNative()).mkNative().ofNative();
+		var q:Int64Data = (modulus.asNative() / divisor.asNative()).mkNative().ofNative();
+		var m:Int64Data = (modulus.asNative() % divisor.asNative()).mkNative().ofNative();
 		return { quotient : q, modulus : m };
 	}
 
-	public static inline function div( a : Int64, b : Int64 ) : Int64 {
+	public static inline function div( a : Int64Data, b : Int64Data ) : Int64Data {
 		return (a.asNative() / b.asNative()).mkNative().ofNative();
 	}
 
-	public static inline function mod( a : Int64, b : Int64 ) : Int64 {
+	public static inline function mod( a : Int64Data, b : Int64Data ) : Int64Data {
 		return (a.asNative() % b.asNative()).mkNative().ofNative();
 	}
 
-	public static inline function shl( a : Int64, b : Int ) : Int64 {
+	public static inline function shl( a : Int64Data, b : Int ) : Int64Data {
 		return (a.asNative() << b).ofNative();
 	}
 
-	public static inline function shr( a : Int64, b : Int ) : Int64 {
+	public static inline function shr( a : Int64Data, b : Int ) : Int64Data {
 		return (a.asNative() >> b).ofNative();
 	}
 
-	public static inline function ushr( a : Int64, b : Int ) : Int64 {
+	public static inline function ushr( a : Int64Data, b : Int ) : Int64Data {
 		return (a.asNative() >>> b).ofNative();
 	}
 
-	public static inline function and( a : Int64, b : Int64 ) : Int64
+	public static inline function and( a : Int64Data, b : Int64Data ) : Int64Data
 	{
 		return (a.asNative() & b.asNative()).ofNative();
 	}
 
-	public static inline function or( a : Int64, b : Int64 ) : Int64
+	public static inline function or( a : Int64Data, b : Int64Data ) : Int64Data
 	{
 		return (a.asNative() | b.asNative()).ofNative();
 	}
 
-	public static inline function xor( a : Int64, b : Int64 ) : Int64
+	public static inline function xor( a : Int64Data, b : Int64Data ) : Int64Data
 	{
 		return (a.asNative() ^ b.asNative()).ofNative();
 	}
 
-	public static inline function neg( a : Int64 ) : Int64
+	public static inline function neg( a : Int64Data ) : Int64Data
 	{
 		return (~a.asNative()).ofNative();
 	}
 
-	public static inline function isNeg( a : Int64 ) : Bool
+	public static inline function isNeg( a : Int64Data ) : Bool
 	{
 		return (a.asNative() < 0.mkNative());
 	}
 
-	public static inline function isZero( a : Int64 ) : Bool
+	public static inline function isZero( a : Int64Data ) : Bool
 	{
 		return (a.asNative() == 0.mkNative());
 	}
 
-	public static inline function compare( a : Int64, b : Int64 ) : Int
+	public static inline function compare( a : Int64Data, b : Int64Data ) : Int
 	{
 		return cast(a.asNative() - b.asNative(), Int);
 	}
 
-	public static function ucompare( a : Int64, b : Int64 ) : Int
+	public static function ucompare( a : Int64Data, b : Int64Data ) : Int
 	{
 		if (a.asNative() < 0.mkNative())
 			return (b.asNative() < 0.mkNative()) ? compare( (~a.asNative()).ofNative(), (~b.asNative()).ofNative()) : 1;
 		return (b.asNative() < 0.mkNative()) ? -1 : compare(a, b);
 	}
 
-	public static inline function toStr( a : Int64 ) : String {
+	public static inline function toStr( a : Int64Data ) : String {
 		return a + "";
 	}
 }
