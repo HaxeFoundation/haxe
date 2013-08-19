a9c6ac0614e6e5563bdeab8fb5fc4c6e51b9e2e3
Hugh <gamehaxe@gmail.com>
2013-08-20 00:31:06 +0800

    Start in unsafe wrapper

---
+++ std/cpp/vm/Unsafe.hx
@@ -0,0 +1,10 @@
+package cpp.vm;
+
+class Unsafe
+{
+   public static inline function unsafeSet(outForced:Dynamic,inValue:Dynamic) : Void
+   {
+      untyped __global__.__hxcpp_unsafe_set(outForced,inValue);
+   }
+}
+
