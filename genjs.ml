5236b56af1e243ee006e2d23cf145c92b5658d74
Luca Deltodesco <luca@deltaluca.me.uk>
2013-09-15 14:02:59 +0100

    Removed 'is' and 'use' from keywords list.
    I cannot find a single source that specifies these as keywords.

--- genjs.ml
+++ genjs.ml
@@ -77,9 +77,9 @@ let kwds =
 		"abstract"; "as"; "boolean"; "break"; "byte"; "case"; "catch"; "char"; "class"; "continue"; "const";
 		"debugger"; "default"; "delete"; "do"; "double"; "else"; "enum"; "export"; "extends"; "false"; "final";
 		"finally"; "float"; "for"; "function"; "goto"; "if"; "implements"; "import"; "in"; "instanceof"; "int";
-		"interface"; "is"; "long"; "namespace"; "native"; "new"; "null"; "package"; "private"; "protected";
+		"interface"; "long"; "namespace"; "native"; "new"; "null"; "package"; "private"; "protected";
 		"public"; "return"; "short"; "static"; "super"; "switch"; "synchronized"; "this"; "throw"; "throws";
-		"transient"; "true"; "try"; "typeof"; "use"; "var"; "void"; "volatile"; "while"; "with"
+		"transient"; "true"; "try"; "typeof"; "var"; "void"; "volatile"; "while"; "with"
 	];
 	h
 
