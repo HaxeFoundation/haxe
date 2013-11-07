1273fda5acd31f58f22d58983020b503af0df69a
Nicolas Cannasse <ncannasse@gmail.com>
2013-11-07 14:30:03 +0100

    added

---
+++ std/haxe/crypto/Base64.hx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C)2005-2013 Haxe Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package haxe.crypto;
+
+/**
+	Allows to encode/decode String and bytes using Base64 encoding.
+**/
+class Base64 {
+
+	public static var CHARS(default,null) = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	public static var BYTES(default,null) = haxe.io.Bytes.ofString(CHARS);
+	
+	public static function encode( bytes : haxe.io.Bytes, complement = true ) : String {
+		var str = new BaseCode(BYTES).encodeBytes(bytes).toString();
+		if( complement )
+			for( i in 0...(bytes.length*4)%3 )
+				str += "=";
+		return str;
+	}
+	
+	public static function decode( str : String, complement = true ) : haxe.io.Bytes {
+		if( complement )
+			while( str.charCodeAt(str.length-1) == "=".code )
+				str = str.substr(0,-1);
+		return new BaseCode(BYTES).decodeBytes(haxe.io.Bytes.ofString(str));
+	}
+
+}
