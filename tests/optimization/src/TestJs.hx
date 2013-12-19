be6804e0b91e8185c84b06bc8bdec1ef805eb16a
Simon Krajewski <simon@haxe.org>
2013-12-19 11:23:30 +0100

    actually add TestJs

---
+++ tests/optimization/src/TestJs.hx
@@ -0,0 +1,15 @@
+class TestJs {
+	@:js('var x = 10;"" + x;var x1 = 10;"" + x1;var x2 = 10.0;"" + x2;var x3 = "10";x3;var x4 = true;"" + x4;')
+	static function testStdString() {
+        var x = 10;
+        Std.string(x);
+        var x:UInt = 10;
+        Std.string(x);
+        var x = 10.0;
+        Std.string(x);
+        var x = "10";
+        Std.string(x);
+        var x = true;
+        Std.string(x);
+	}
+}
