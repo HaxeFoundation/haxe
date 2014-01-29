b22b356219425ba6e8530ee3993659f095ae998e
Dan Korostelev <nadako@gmail.com>
2014-01-30 00:28:08 +0400

    support overriding pf_* settings

--- common.ml
+++ common.ml
@@ -836,9 +836,10 @@ let init_platform com pf =
 	let forbid acc p = if p = name || PMap.mem p acc then acc else PMap.add p Forbidden acc in
 	com.package_rules <- List.fold_left forbid com.package_rules (List.map platform_name platforms);
 	com.config <- get_config com;
-(*	if com.config.pf_static then define com "static"; *)
-	if com.config.pf_sys then define com Define.Sys else com.package_rules <- PMap.add "sys" Forbidden com.package_rules;
 	raw_define com name
+
+let apply_pf_config com =
+	if com.config.pf_sys then define com Define.Sys else com.package_rules <- PMap.add "sys" Forbidden com.package_rules
 
 let add_feature com f =
 	Hashtbl.replace com.features f true
