f1a52d8a16aefc659a51199459adf7e371e0a444
Simon Krajewski <simon@haxe.org>
2014-03-26 16:15:27 +0100

    move Sys to _std and add exit

--- std/python/_std/Sys.hx
+++ std/python/_std/Sys.hx
@@ -1,12 +1,13 @@
-
-package python;
-
 import python.lib.Time;
 
+@:preCode("import sys")
 class Sys {
 
 	public static function time () {
 		return Time.time()/1000;
 	}
 
+	public static function exit(code:Int) {
+		python.lib.Sys.exit(code);
+	}
 }
