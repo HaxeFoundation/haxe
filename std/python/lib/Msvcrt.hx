f9987e94a171f585b9c123624248ca4b0ddb4766
frabbit <frabbit77@googlemail.com>
2014-03-30 20:42:31 +0200

    typo fix

--- std/python/lib/Msvcrt.hx
+++ std/python/lib/Msvcrt.hx
@@ -8,7 +8,7 @@ extern class Msvcrt {
 	static function __init__ ():Void
 	{
 		try {
-			python.Macros.importAs("msvrt", "python.lib.Msvcrt");
+			python.Macros.importAs("msvcrt", "python.lib.Msvcrt");
 		} catch (e:Dynamic) {}
 	}
 
