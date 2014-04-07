1674d074d18c709b8be6c0ce7dd838c882145668
Simon Krajewski <simon@haxe.org>
2014-04-07 11:08:29 +0200

    always use the empty UInt for documentation generation

--- std/UInt.hx
+++ std/UInt.hx
@@ -20,7 +20,7 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#if ((flash9 || flash9doc || cs) && !doc_gen)
+#if (flash9 || flash9doc || cs || doc_gen)
 /**
 	The unsigned Int type is only defined for Flash9 and C#. It's currently
 	handled the same as a normal Int.
