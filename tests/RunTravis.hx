908376b5de703064278e100c52335dfde47eaffe
Simon Krajewski <simon@haxe.org>
2014-04-23 23:25:57 +0200

    also run macro tests in bytecode mode

--- tests/RunTravis.hx
+++ tests/RunTravis.hx
@@ -237,7 +237,7 @@ class RunTravis {
 
 		Sys.setCwd(unitDir);
 		switch (Sys.getEnv("TARGET")) {
-			case "macro", null:
+			case "macro", "bytecode", null:
 				runCommand("haxe", ["compile-macro.hxml"]);
 
 				//generate documentation
