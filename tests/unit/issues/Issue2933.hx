48b56ee03a667a7a950b1f9366273e8089cd71c5
Dan Korostelev <nadako@gmail.com>
2014-04-29 13:52:52 +0400

    Follow abstract underlying type when unifying with structure declaration (see #2933)

--- tests/unit/issues/Issue2933.hx
+++ tests/unit/issues/Issue2933.hx
@@ -8,10 +8,21 @@ private abstract B({a:A}) from {a:A} {
 	}
 }
 
+private typedef T = {a:A};
+
+private abstract C(T) from T {
+	public function getA() {
+		return this.a;
+	}
+}
+
 class Issue2933 extends Test {
 	function test() {
 		var a:A = 1;
 		var b:B = {a: 1};
 		eq(1, b.getA());
+
+		var c:C = {a: 1};
+		eq(1, c.getA());
 	}
 }
