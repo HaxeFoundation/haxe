c29a90d239b1f6a02d2b0ffa09014a384bda61a7
Dan Korostelev <nadako@gmail.com>
2014-05-14 02:03:35 +0400

    [python] clean up NativeIterable/NativeIterator stuff (nonsense type params, static @:to, inlines)

--- std/python/NativeIterator.hx
+++ std/python/NativeIterator.hx
@@ -6,14 +6,13 @@ import python.NativeIterable.NativeIterableRaw;
 abstract NativeIterator <T>(NativeIteratorRaw<T>) to NativeIteratorRaw<T> to NativeIterable<T> {
 	public inline function new (p:NativeIteratorRaw<T>) this = p;
 
-	@:to public static inline function toHaxeIterator <T>(p:NativeIteratorRaw<T>):HaxeIterator<T> return new HaxeIterator(p);
-	@:to public static inline function toNativeIterable <T>(p:NativeIteratorRaw<T>):NativeIterable<T> return p;
+	@:to public inline function toHaxeIterator():HaxeIterator<T> return new HaxeIterator(this);
+	@:to public inline function toNativeIterable():NativeIterable<T> return this;
 
-	public function getNativeIteratorRaw <T>():NativeIteratorRaw<T> return this;
+	public inline function getNativeIteratorRaw():NativeIteratorRaw<T> return this;
 }
 
 typedef NativeIteratorRaw<T> = {
 	> NativeIterableRaw<T>,
 	function __next__ ():T;
-	//function __iter__ ():NativeIterator<T>;
 }
