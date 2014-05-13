c0528467da3b1ff5697821d22d794ce48d104af2
Dan Korostelev <nadako@gmail.com>
2014-05-14 00:22:23 +0400

    [python] explicitly hint NativeIterable.iterator to avoid type inference problems (see #2979)

--- std/python/NativeIterable.hx
+++ std/python/NativeIterable.hx
@@ -9,7 +9,7 @@ abstract NativeIterable <T>(NativeIterableRaw<T>) to NativeIterableRaw<T> from N
 
 	//@:from public static inline function fromArray <T>(p:Array<T>):PyIterable<T> return cast p;
 
-	public inline function iterator <T>() return toHaxeIterable().iterator();
+	public inline function iterator <T>():HaxeIterator<T> return toHaxeIterable().iterator();
 
 	public function getNativeIterable <T>():NativeIterableRaw<T> return this;
 	public function getNativeIterator <T>():NativeIteratorRaw<T> return this.__iter__();
