17650eeed7df5d063dbd96d9f88bbb063a7b0e64
Simon Krajewski <simon@haxe.org>
2014-06-04 15:32:12 +0200

    add haxe.rtti.Rtti so we have something resembling an API (also add tests)

--- tests/unit/TestSpecification.hx
+++ tests/unit/TestSpecification.hx
@@ -122,6 +122,25 @@ enum EVMTest {
 	EVMF(a:Array<EVMTest>);
 }
 
+class NonRttiClass { }
+
+@:rtti
+@:keepSub
+class RttiClass1 {
+    static var v:String;
+    public function f() {
+        return 33.0;
+    }
+}
+
+class RttiClass2 extends RttiClass1 { }
+
+class RttiClass3 extends RttiClass1 {
+	override function f():Int {
+		return 33;
+	}
+}
+
 #if !macro
 @:build(unit.UnitBuilder.build("unitstd"))
 #end
