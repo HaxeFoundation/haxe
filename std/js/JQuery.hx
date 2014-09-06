c0d9c38ca89d95b28d2bc8a1f42adb94282b2599
Maximilian Ruta <mr@xtain.net>
2014-09-06 23:16:28 +0200

    using the $ sign is not safe when using jQuery.noConflict()

--- std/js/JQuery.hx
+++ std/js/JQuery.hx
@@ -390,7 +390,7 @@ extern class JQuery implements ArrayAccess<Element> {
 	//static function is*, makeArray, map, merge, noop, now, param, proxy, sub, trim, type, unique
 
 	private static inline function get_cur() : JQuery {
-		return untyped $(__js__("this"));
+		return untyped js.JQuery(__js__("this"));
 	}
 
 	private static function __init__() : Void untyped {
