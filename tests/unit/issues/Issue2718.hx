6b4f7bfcdafb2ce7808fe798918e0df2616de5b7
Simon Krajewski <simon@haxe.org>
2014-09-15 21:34:47 +0200

    add test (closes #2718)

--- tests/unit/issues/Issue2718.hx
+++ tests/unit/issues/Issue2718.hx
@@ -1,9 +1,8 @@
 package unit.issues;
-import unit.Test;
 
 class Issue2718 extends Test {
 	function test() {
-		var testMap = new Map();
+		var testMap = new Map<Int, String>();
 		var x0:UInt = 0;
 		var x1:UInt = 1;
 		var x2:UInt = 2;
