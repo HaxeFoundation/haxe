0829e1be9ebe568892b17cd9f9f0557162629897
Simon Krajewski <simon@haxe.org>
2014-09-17 10:51:15 +0200

    add array access order test

--- tests/unit/issues/Issue3347.hx
+++ tests/unit/issues/Issue3347.hx
@@ -12,6 +12,16 @@ private abstract IntMap2<V>(Dynamic<V>) {
     @:arrayAccess public function set<T:IntKey>(k:T, i:V):Void Reflect.setField(this, cast k, i);
 }
 
+abstract IntMap3<V>(Dynamic<V>) {
+    static public var called = false;
+	public function new() this = {};
+    @:arrayAccess function get(k:IntKey):V return {
+		called = true;
+		Reflect.field(this, k);
+	}
+    @:arrayAccess function getInt(k:Int):V return Reflect.field(this, Std.string(k));
+}
+
 private abstract IntKey(String) to String {
 	static public var fromList = "";
 	public inline function new(s) this = s;
@@ -44,5 +54,9 @@ class Issue3347 extends Test {
         var m2 = new IntMap2();
 		// should fail because constraints unify without casts
 		t(unit.TestType.typeError(m2[1]));
+
+        var m3 = new IntMap3();
+        var v = m3[0];
+		t(IntMap3.called);
 	}
 }
