1959617ace1b0431131069f1b5b9d05b3ce1f33c
filt3rek <filt3r@free.fr>
2014-10-01 18:29:54 +0200

    _multipart static var fixed - cacheModule

--- std/haxe/remoting/HttpConnection.hx
+++ std/haxe/remoting/HttpConnection.hx
@@ -38,7 +38,7 @@ class HttpConnection implements Connection implements Dynamic<Connection> {
 		c.__path.push(name);
 		return c;
 	}
-
+	
 	public function call( params : Array<Dynamic> ) : Dynamic {
 		var data = null;
 		var h = new haxe.Http(__url);
@@ -51,11 +51,40 @@ class HttpConnection implements Connection implements Dynamic<Connection> {
 		#if (neko || php || cpp)
 			h.cnxTimeout = TIMEOUT;
 		#end
+		var files	= new List();
+		function searchFile( o : Dynamic ) {
+			if ( Std.is( o, Array ) ) {
+				var a	: Array<Dynamic>	= cast o;
+				for ( i in 0...a.length ) {
+					var p	= a [ i ];
+					if ( p == null )	continue;
+					if ( p.param != null && p.filename != null && p.bytes != null  ) {
+						files.add( cast p );
+						a[ i ]	= '__file__${ p.param }';
+					}else if ( Std.is( p, Array ) || Reflect.isObject( p ) ) {
+						searchFile( p );
+					}
+				}
+			}else if ( Reflect.isObject( o ) ) {
+				for ( k in Reflect.fields( o ) ) {
+					var p	= Reflect.getProperty( o, k );
+					if ( p == null )	continue;
+					if ( p.param != null && p.filename != null && p.bytes != null  ) {
+						files.add( cast p );
+						Reflect.setProperty( o, k ,'__file__${ p.param }' );
+					}else if ( Std.is( p, Array ) || Reflect.isObject( p ) ) {
+						searchFile( p );
+					}
+				}
+			}
+		}
+		searchFile( params );
 		var s = new haxe.Serializer();
 		s.serialize(__path);
 		s.serialize(params);
 		h.setHeader("X-Haxe-Remoting","1");
-		h.setParameter("__x",s.toString());
+		h.setParameter("__x", s.toString());
+		for ( file in files )	h.addFileTransfer( file.param, file.filename, file.bytes, file.mimeType );
 		h.onData = function(d) { data = d; };
 		h.onError = function(e) { throw e; };
 		h.request(true);
@@ -75,27 +104,80 @@ class HttpConnection implements Connection implements Dynamic<Connection> {
 
 	#if neko
 	public static function handleRequest( ctx : Context ) {
-		var v = neko.Web.getParams().get("__x");
-		if( neko.Web.getClientHeader("X-Haxe-Remoting") == null || v == null )
-			return false;
-		neko.Lib.print(processRequest(v,ctx));
-		return true;
+		var v	= null;	
+		var mutlipartParams	= null;
+		if ( neko.Web.getClientHeader( "X-Haxe-Remoting" ) != null ) {
+			var ct	= neko.Web.getClientHeader( "Content-Type" );
+			if ( ct != null && ct.indexOf( "multipart/form-data" ) != -1 ) {
+				mutlipartParams	= neko.Web.getMultipartParams();
+				v	= mutlipartParams.get( "__x" );
+			}else
+				v	= neko.Web.getParams().get( "__x" );
+			if ( v != null ) {
+				neko.Lib.print( processRequest( v, ctx, mutlipartParams ) );
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	#elseif php
 	public static function handleRequest( ctx : Context ) {
-		var v = php.Web.getParams().get("__x");
-		if( php.Web.getClientHeader("X-Haxe-Remoting") == null || v == null )
-			return false;
-		php.Lib.print(processRequest(v,ctx));
-		return true;
+		var v	= null;		
+		var mutlipartParams	= null;
+		if ( php.Web.getClientHeader( "X-Haxe-Remoting" ) != null ) {
+			var ct	= php.Web.getClientHeader( "Content-Type" );
+			if ( ct != null && ct.indexOf( "multipart/form-data" ) != -1 ) {
+				mutlipartParams	= php.Web.getMultipartParams();
+				v	= mutlipartParams.get( "__x" );
+			}else
+				v	= php.Web.getParams().get( "__x" );
+			if ( v != null ) {
+				php.Lib.print( processRequest( v, ctx ) );
+				return true;
+			}
+		}
+		return false;
 	}
 	#end
-
-	public static function processRequest( requestData : String, ctx : Context ) : String {
+	
+	#if neko
+	public static function processRequest( requestData : String, ctx : Context, ?multipartParams : haxe.ds.StringMap<Dynamic> ) : String {
 		try {
 			var u = new haxe.Unserializer(requestData);
 			var path = u.unserialize();
-			var args = u.unserialize();
+			var args : Array<Dynamic> = cast u.unserialize();
+			
+			if( multipartParams != null ){					
+				function searchFile( o : Dynamic ) {
+					if ( Std.is( o, Array ) ) {
+						var a	: Array<Dynamic>	= cast o;
+						for ( i in 0...a.length ) {
+							var arg	= a [ i ];
+							if ( arg	== null )	continue;
+							if ( Std.is( arg, String ) && StringTools.startsWith( arg, "__file__" ) ) {
+								var s	: String	= cast arg;
+								a[ i ]	= multipartParams.get( s.substr( 8 ) );
+							}else if ( Std.is( arg, Array ) || ( Reflect.isObject( arg ) && !Std.is( arg, String ) ) ) {
+								searchFile( arg );
+							}
+						}
+					}else if ( Reflect.isObject( o ) && !Std.is( o, String ) ) {
+						for ( k in Reflect.fields( o ) ) {
+							var arg	= Reflect.getProperty( o, k );
+							if ( arg	== null )	continue;
+							if ( Std.is( arg, String ) && StringTools.startsWith( arg, "__file__" ) ) {
+								var s	: String	= cast arg;
+								Reflect.setProperty( o, k , multipartParams.get( s.substr( 8 ) ) );
+							}else if ( Std.is( arg, Array ) || ( Reflect.isObject( arg ) && !Std.is( arg, String ) ) ) {
+								searchFile( arg );
+							}
+						}
+					}
+				}
+				searchFile( args );
+			}
+			
 			var data = ctx.call(path,args);
 			var s = new haxe.Serializer();
 			s.serialize(data);
@@ -106,5 +188,53 @@ class HttpConnection implements Connection implements Dynamic<Connection> {
 			return "hxr" + s.toString();
 		}
 	}
+	#elseif php
+	public static function processRequest( requestData : String, ctx : Context, ?multipartParams : haxe.ds.StringMap<Dynamic> ) : String {
+		try {
+			var u = new haxe.Unserializer(requestData);
+			var path = u.unserialize();
+			var args : Array<Dynamic> = cast u.unserialize();
+			
+			if( multipartParams != null ){
+				function searchFile( o : Dynamic ) {
+					if ( Std.is( o, Array ) ) {
+						var a	: Array<Dynamic>	= cast o;
+						for ( i in 0...a.length ) {
+							var arg	= a [ i ];
+							if ( arg	== null )	continue;
+							if ( Std.is( arg, String ) && StringTools.startsWith( arg, "__file__" ) ) {
+								var s	: String	= cast arg;
+								a[ i ]	= multipartParams.get( s.substr( 8 ) );
+							}else if ( Std.is( arg, Array ) || ( Reflect.isObject( arg ) && !Std.is( arg, String ) ) ) {
+								searchFile( arg );
+							}
+						}
+					}else if ( Reflect.isObject( o ) && !Std.is( o, String ) ) {
+						for ( k in Reflect.fields( o ) ) {
+							var arg	= Reflect.getProperty( o, k );
+							if ( arg	== null )	continue;
+							if ( Std.is( arg, String ) && StringTools.startsWith( arg, "__file__" ) ) {
+								var s	: String	= cast arg;
+								Reflect.setProperty( o, k , multipartParams.get( s.substr( 8 ) ) );
+							}else if ( Std.is( arg, Array ) || ( Reflect.isObject( arg ) && !Std.is( arg, String ) ) ) {
+								searchFile( arg );
+							}
+						}
+					}
+				}
+				searchFile( args );
+			}
+			
+			var data = ctx.call(path,args);
+			var s = new haxe.Serializer();
+			s.serialize(data);
+			return "hxr" + s.toString();
+		} catch( e : Dynamic ) {
+			var s = new haxe.Serializer();
+			s.serializeException(e);
+			return "hxr" + s.toString();
+		}
+	}
+	#end
 
 }
