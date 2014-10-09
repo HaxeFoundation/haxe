9c11c1f5df7b34c5a65f615c7e22e5f504a90d4c
SmerkyG <SmerkyG@users.noreply.github.com>
2014-10-09 17:28:36 -0400

    Dramatically faster non-allocating version of StringMap which sanitizes keys via a special ReservedWordIndexer object for illegal keys, making UnsafeStringMap unnecessary

--- std/flash/_std/haxe/ds/StringMap.hx
+++ std/flash/_std/haxe/ds/StringMap.hx
@@ -22,40 +22,94 @@
 package haxe.ds;
 
 @:coreApi class StringMap<T> implements haxe.Constraints.IMap<String,T> {
-
-	private var h :flash.utils.Dictionary;
+	// reserved words that are not allowed in Dictionary include all non-static members of Dictionary
+	private static var reservedWords:Array<String> = ["constructor", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "setPropertyIsEnumerable", "toLocaleString", "toString", "valueOf", "toJSON"];
+	private static var reservedWordIndexers:Array<ReservedWordIndexer> = [new ReservedWordIndexer(0), new ReservedWordIndexer(1), new ReservedWordIndexer(2), new ReservedWordIndexer(3), new ReservedWordIndexer(4), new ReservedWordIndexer(5), new ReservedWordIndexer(6), new ReservedWordIndexer(7), new ReservedWordIndexer(8)];
+	private static inline var reservedWordCount:Int = 9;
+	
+	private var h : flash.utils.Dictionary;
 
 	public function new() : Void {
 		h = new flash.utils.Dictionary();
 	}
 
-	public function set( key : String, value : T ) : Void {
-		untyped h["$"+key] = value;
+	private inline function reservedWordIndex(key:String):Int {
+		var i:Int = reservedWordCount - 1;
+		while (i >= 0) {
+			if ( reservedWords[i] == key ) {
+				break;
+			}
+			i--;
+		}
+		return i;
+	}
+	public inline function set( key : String, value : T ) : Void {
+		var i:Int = reservedWordIndex(key);
+		if ( i >= 0 ) {
+			untyped h[reservedWordIndexers[i]] = value;
+		} else {
+			untyped h[key] = value;
+		}
 	}
 
-	public function get( key : String ) : Null<T> {
-		return untyped h["$"+key];
+	public inline function get( key : String ) : Null<T> {
+		var rv:Null<T>;
+		var i:Int = reservedWordIndex(key);
+		if ( i >= 0 ) {
+			rv = untyped h[reservedWordIndexers[i]];
+		} else {
+			rv = untyped h[key];
+		}
+		return rv == null ? null : rv;
 	}
 
 	public inline function exists( key : String ) : Bool {
-		return untyped __in__("$"+key,h);
+		var i:Int = reservedWordIndex(key);
+		if ( i >= 0 ) {
+			return untyped __in__(reservedWordIndexers[i], h);
+		} else {
+			return untyped __in__(key, h);
+		}
 	}
 
-	public function remove( key : String ) : Bool {
-		key = "$"+key;
-		if( untyped !__in__(key,h) ) return false;
-		untyped __delete__(h,key);
-		return true;
+	public inline function remove( key : String ) : Bool {
+		var i:Int = reservedWordIndex(key);
+		if ( i >= 0 ) {
+			if ( !(untyped __in__(reservedWordIndexers[i], h)) ) {
+				return false;
+			} else {
+				untyped __delete__(h, reservedWordIndexers[i]);
+				return true;
+			}
+		} else {
+			if ( !(untyped __in__(key, h)) ) {
+				return false;
+			} else {
+				untyped __delete__(h, key);
+				return true;
+			}
+		}
 	}
 
 	public function keys() : Iterator<String> {
-		return untyped (__hkeys__(h)).iterator();
+		var rv = untyped (__keys__(h));
+		var len:Int = untyped rv.length;
+		var i:Int = 0;
+		var reservedWordsCount:Int = reservedWords.length;
+		while (i < len) {
+			if ( Std.is(untyped rv[i], ReservedWordIndexer) ) {
+				var indexer:ReservedWordIndexer = untyped rv[i];
+				untyped rv[i] = reservedWords[indexer.i];
+			}
+			i++;
+		}
+		return rv.iterator();
 	}
 
 	public function iterator() : Iterator<T> {
 		return untyped {
 			ref : h,
-			it : __keys__(h).iterator(),
+			it : keys(),
 			hasNext : function() { return __this__.it.hasNext(); },
 			next : function() { var i : Dynamic = __this__.it.next(); return __this__.ref[i]; }
 		};
@@ -76,4 +130,12 @@ package haxe.ds;
 		return s.toString();
 	}
 
+}
+
+@:allow(haxe.ds.StringMap)
+class ReservedWordIndexer {
+	private var i:Int;
+	private function new(i:Int):Void {
+		this.i = i;
+	}
 }
