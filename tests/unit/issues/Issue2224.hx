eb79c97244f923f178cdf1c724b5d04d2695ea57
Simon Krajewski <simon@haxe.org>
2014-10-12 00:23:59 +0200

    add test (closes #2224)

---
+++ tests/unit/issues/Issue2224.hx
@@ -0,0 +1,28 @@
+package unit.issues;
+
+class Issue2224 extends unit.Test {
+
+	function test() {
+		eq("opt=null", getEF1());
+		eq("opt=null", getEF2());
+		eq("opt=null", getInt());
+		eq("opt=null", getClass());
+	}
+
+    static function getEF1( ?opt : Null<haxe.EnumFlags<MyEnum>> ){
+        return( 'opt=$opt' );
+    }
+
+    static function getEF2( ?opt : haxe.EnumFlags<MyEnum> ){
+        return( 'opt=$opt' );
+    }
+
+    static function getInt( ?opt : Int ){
+        return( 'opt=$opt' );
+    }
+
+    static function getClass( ?opt : MyClass ){
+        return( 'opt=$opt' );
+    }
+
+}
