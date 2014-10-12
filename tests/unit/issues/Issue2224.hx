4534642eb6640e9a1be263f7a347923d77907924
Simon Krajewski <simon@haxe.org>
2014-10-12 12:13:01 +0200

    disable test on Js for now

--- tests/unit/issues/Issue2224.hx
+++ tests/unit/issues/Issue2224.hx
@@ -3,10 +3,12 @@ package unit.issues;
 class Issue2224 extends unit.Test {
 
 	function test() {
+		#if !js
 		eq("opt=null", getEF1());
 		eq("opt=null", getEF2());
 		eq("opt=null", getInt());
 		eq("opt=null", getClass());
+		#end
 	}
 
     static function getEF1( ?opt : Null<haxe.EnumFlags<MyEnum>> ){
