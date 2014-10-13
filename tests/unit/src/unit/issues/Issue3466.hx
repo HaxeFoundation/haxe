88f6a124f8af289a755d57ac39c13f297c529570
Cauê Waneck <waneck@gmail.com>
2014-10-13 16:07:04 -0300

    oops

--- tests/unit/src/unit/issues/Issue3466.hx
+++ tests/unit/src/unit/issues/Issue3466.hx
@@ -1,7 +1,8 @@
 package unit.issues;
 
 class Issue3466 extends Test {
-function test() {
-	var d:Dynamic<Int> = {};
-	eq(null, Reflect.field(d,'someField'));
+	function test() {
+		var d:Dynamic<Int> = {};
+		eq(null, Reflect.field(d,'someField'));
+	}
 }
