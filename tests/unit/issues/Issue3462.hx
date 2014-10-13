4ab0f1eaa4c370e071e4fd4e49be5364caced0ae
SmerkyG <SmerkyG@users.noreply.github.com>
2014-10-13 12:36:31 -0400

    improved test cases applied to all StringMap types not just flash

--- tests/unit/issues/Issue3462.hx
+++ tests/unit/issues/Issue3462.hx
@@ -6,14 +6,13 @@ class Issue3462 extends Test
 {
 	function test()
 	{
-		#if flash
-
 		var d:StringMap<Int> = new StringMap<Int>();
 		var r:Dynamic;
 		
 		var s:String = "";
-		var i:Int = 1;
-		for (k in ["constructor", "sillyTest", "0", null, "1", "prototype", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "setPropertyIsEnumerable", "toLocaleString", "toString", "valueOf", "toJSON", "get", "set"]) {
+		var keys = ["__proto__", "__definegetter__", "__definesetter__", "__lookupgetter__", "__lookupsetter__", "constructor", "sillyTest", "0", "1", "prototype", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "setPropertyIsEnumerable", "toLocaleString", "toString", "valueOf", "toJSON", "get", "set"];
+		for (i in 0...keys.length) {
+			var k = keys[i];
 			r = d.exists(k);
 			f(r == true); // Entry should not exist before setting
 			r = d.get(k);
@@ -21,24 +20,52 @@ class Issue3462 extends Test
 			d.set(k, i);
 			r = d.exists(k);
 			f(r != true); // Entry missing after setting
+		}
+		
+		var keyCount:Int = 0;
+		for (k in d.keys()) {
+			f( keys.indexOf(k) == -1 ); // key missing from keys iterator
+			keyCount++;
+		}
+		f(keyCount != keys.length); // keys missing
+		var keyCount:Int = 0;
+		for (v in d) {
+			g(v < 0 || v >= keys.length); // bad value iterated
+			keyCount++;
+		}
+		f(keyCount != keys.length); // values missing
+		for (i in 0...keys.length) {
+			var k = keys[i];
 			r = d.get(k);
 			f(r != i); // Value not correct after setting
 			r = d.remove(k);
 			f(r != true); // Value unable to be removed
 			r = d.exists(k);
 			f(r == true); // Entry should not exist after removal
-			
-			i++;
 		}
+		keyCount = 0;
+		for (k in d.keys()) {			
+		}
+		f( keyCount != 0 ); // keys remaining after removal
 		
-		var keys = ["constructor", "sillyTest", "prototype", "hasOwnProperty", "isPrototypeOf", "propertyIsEnumerable", "setPropertyIsEnumerable", "toLocaleString", "toString", "valueOf", "toJSON"];
-		for (k in keys) {
-			d.set(k, i++);
+		// test fast iterators against normal keys
+		d = new StringMap();
+		keys = ["1", "2", "3"];
+		for (i in 0...keys.length) {
+			var k = keys[i];
+			d.set(k, i);
 		}
+		var keyCount:Int = 0;
 		for (k in d.keys()) {
 			f( keys.indexOf(k) == -1 ); // key missing from keys iterator
+			keyCount++;
 		}
-		
-		#end
+		f( keyCount != keys.length ); // keys missing
+		var keyCount:Int = 0;
+		for (v in d) {
+			f(v < 0 || v >= keys.length); // bad value iterated
+			keyCount++;
+		}
+		f(keyCount != keys.length); // values missing
 	}
 }
