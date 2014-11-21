08fce8c39a51eeccc5f5e8ebe0374963246e2180
Dan Korostelev <nadako@gmail.com>
2014-11-21 15:55:37 +0300

    [python] add total_seconds for TimeDelta (also clean up syntax a bit)

--- std/python/lib/datetime/TimeDelta.hx
+++ std/python/lib/datetime/TimeDelta.hx
@@ -1,18 +1,16 @@
-
 package python.lib.datetime;
 
 @:pythonImport("datetime", "timedelta")
 extern class TimeDelta {
 
-	public static var min : TimeDelta;
-	public static var max : TimeDelta;
-	public static var resolution : TimeDelta;
-
+	static var min : TimeDelta;
+	static var max : TimeDelta;
+	static var resolution : TimeDelta;
 
 	var days : Int;
-
 	var seconds : Int;
 	var microseconds : Int;
 
-	public function new(days:Int = 0, seconds:Int = 0, microseconds:Int = 0, milliseconds:Int = 0, minutes:Int = 0, hours:Int = 0, weeks:Int = 0):Void;
+	function new(days:Int = 0, seconds:Int = 0, microseconds:Int = 0, milliseconds:Int = 0, minutes:Int = 0, hours:Int = 0, weeks:Int = 0):Void;
+	function total_seconds():Float;
 }
