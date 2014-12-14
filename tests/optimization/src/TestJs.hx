2c78d5c287d153051eea87e45734205e3de6910c
Dan Korostelev <nadako@gmail.com>
2014-12-14 03:05:50 +0300

    add test for js inlining

--- tests/optimization/src/TestJs.hx
+++ tests/optimization/src/TestJs.hx
@@ -1,16 +1,16 @@
 class TestJs {
 	//@:js('var x = 10;"" + x;var x1 = 10;"" + x1;var x2 = 10.0;"" + x2;var x3 = "10";x3;var x4 = true;"" + x4;')
 	//static function testStdString() {
-        //var x = 10;
-        //Std.string(x);
-        //var x:UInt = 10;
-        //Std.string(x);
-        //var x = 10.0;
-        //Std.string(x);
-        //var x = "10";
-        //Std.string(x);
-        //var x = true;
-        //Std.string(x);
+	//var x = 10;
+	//Std.string(x);
+	//var x:UInt = 10;
+	//Std.string(x);
+	//var x = 10.0;
+	//Std.string(x);
+	//var x = "10";
+	//Std.string(x);
+	//var x = true;
+	//Std.string(x);
 	//}
 
 	@:js("var a = new List();var _g_head = a.h;var _g_val = null;while(_g_head != null) {var tmp;_g_val = _g_head[0];_g_head = _g_head[1];tmp = _g_val;tmp;}")
@@ -18,4 +18,16 @@ class TestJs {
 		var a = new List();
 		for (v in a) { }
 	}
+
+	@:js("var a = 1;var tmp;var v2 = a;tmp = a + v2;if(tmp > 0) {}")
+	@:analyzer(no_const_propagation)
+	static function testInline() {
+		var a = 1;
+		if (f(a) > 0) { }
+	}
+
+    inline static function f(v) {
+        var v2 = v;
+        return v + v2;
+    }
 }
