3004ddd8343263e8067fab73d6a0925ea997ce35
Simon Krajewski <simon@haxe.org>
2015-01-12 14:37:23 +0100

    remove conditional compilation from TestXML

--- tests/unit/src/unit/TestXML.hx
+++ tests/unit/src/unit/TestXML.hx
@@ -47,22 +47,9 @@ class TestXML extends Test {
 	}
 
 	function testFormat() {
-		#if flash8
-		// flash8 does not parse CDATA sections as PCDATA...
-		eq( Xml.parse("<a><b><c/> <d/> \n <e/><![CDATA[<x>]]></b></a>").toString(), "<a><b><c/> <d/> \n <e/>&lt;x&gt;</b></a>" );
-		#else
 		eq( Xml.parse("<a><b><c/> <d/> \n <e/><![CDATA[<x>]]></b></a>").toString(), "<a><b><c/> <d/> \n <e/><![CDATA[<x>]]></b></a>" );
-		#end
-		#if (flash8 || php)
-		eq( Xml.parse('"').toString(), '&quot;' ); // flash8 has bad habits of escaping entities
-		#else
 		eq( Xml.parse('"').toString(), '"' );
-		#end
-/*		#if flash9
-		eq( Xml.parse('&quot; &lt; &gt;').toString(), '" &lt; &gt;' ); // some entities are resolved but not escaped on printing
-		#else
-		eq( Xml.parse('&quot; &lt; &gt;').toString(), '&quot; &lt; &gt;' );
-		#end*/
+		//eq( Xml.parse('&quot; &lt; &gt;').toString(), '&quot; &lt; &gt;' );
 	}
 
 	function testComplex() {
@@ -73,19 +60,10 @@ class TestXML extends Test {
 		var comment = '<!--Comment-->';
 		var xml = '<html><body><![CDATA[<a href="CDATA"/>&lt;]]></body></html>';
 
-		#if !flash8
-
 		var x = Xml.parse(header + doctype + comment + xml);
-
-		#if flash
-		// doctype is well parsed but is not present in the parsed Xml (f8 and f9)
-		doctype = '';
-		#end
-
 		eq( x.toString(), header + doctype + comment + xml);
 
 		return; // too hard for him
-		#end
 	}
 
 	function testWhitespaces() {
@@ -104,13 +82,8 @@ class TestXML extends Test {
 		var a = el.next();
 		eq( a.firstChild().nodeValue, " ");
 		var b = el.next();
-		#if (flash || php)
-		eq( b.firstChild(), null);
-		eq( x.toString().split("\n").join("\\n"), '<a> </a><b/> \\n <c/>' );
-		#else
 		eq( b.firstChild().nodeValue, "");
 		eq( x.toString().split("\n").join("\\n"), '<a> </a><b></b> \\n <c/>' );
-		#end
 		var c = el.next();
 		eq( c.firstChild(), null);
 	}
@@ -118,20 +91,12 @@ class TestXML extends Test {
 	function testCreate() {
 		eq( Xml.createDocument().toString(), "");
 		eq( Xml.createPCData("Hello").toString(), "Hello" );
-		#if !flash8
-		// too hard for him
 
 		eq( Xml.createCData("<x>").toString(), "<![CDATA[<x>]]>" );
 		eq( Xml.createComment("Hello").toString(), "<!--Hello-->" );
 
-		#if flash9
-		eq( Xml.createProcessingInstruction("XHTML").toString(), "<?XHTML ?>");
-		// doctype is parsed but not printed
-		eq( Xml.createDocType("XHTML").toString(), "" );
-		#else
 		eq( Xml.createProcessingInstruction("XHTML").toString(), "<?XHTML?>");
 		eq( Xml.createDocType("XHTML").toString(), "<!DOCTYPE XHTML>" );
-		#end
 
 		eq( Xml.parse("<!--Hello-->").firstChild().nodeValue, "Hello" );
 		var c = Xml.createComment("Hello");
@@ -148,7 +113,6 @@ class TestXML extends Test {
 		eq( Xml.createPCData("Hello").nodeValue, "Hello" );
 
 		return;
-		#end
 	}
 
 	function testNS() {
@@ -179,7 +143,7 @@ class TestXML extends Test {
 	function testNodetype() {
 		var element = Xml.createElement("x");
 
-		var l = [Xml.createPCData("x"), Xml.createCData("x"), Xml.createDocType("x"), Xml.createProcessingInstruction("x") #if !flash8, Xml.createComment("x") #end];
+		var l = [Xml.createPCData("x"), Xml.createCData("x"), Xml.createDocType("x"), Xml.createProcessingInstruction("x")];
 		for (xml in l)
 		{
 			exc(function() xml.firstChild());
@@ -195,25 +159,7 @@ class TestXML extends Test {
 
 	function testEntities() {
 		var entities = ["&lt;", "&gt;", "&quot;", "&amp;", "&apos;", "&nbsp;", "&euro;", "&#64;", "&#244;", "&#x3F;", "&#xFF;"];
-		var values = entities.copy();
-		#if (flash || js || cs || java || python)
-		// flash parser does support XML + some HTML entities (nbsp only ?) + character codes entities
-		values = ['<', '>', '"', '&', "'", String.fromCharCode(160), '&euro;', '@', 'ô', '?', 'ÿ'];
-		#end
-
-		#if flash9
-		// for a very strange reason, flash9 uses a non standard charcode for non breaking space
-		values[5] = String.fromCharCode(65440);
-		#end
-
-		#if php
-		// &nbsp; and &euro; creates an invalid entity error (first time I see PHP being strict !)
-		entities[5] = "x";
-		entities[6] = "x";
-		// character codes entities are supported
-		values = ["&lt;", "&gt;", "&quot;", "&amp;", "&apos;", "x", "x", '@', 'ô', '?', 'ÿ'];
-		#end
-
+		var values = ['<', '>', '"', '&', "'", "&nbsp;", '&euro;', '@', String.fromCharCode(244), '?', String.fromCharCode(0xFF)];
 		for( i in 0...entities.length ) {
 			infos(entities[i]);
 			eq( Xml.parse(entities[i]).firstChild().nodeValue, values[i] );
