507bb72e73a6b8628d18eeddbaff05052c8e04af
frabbit <frabbit77@googlemail.com>
2015-02-21 15:16:25 +0100

    fix python conflicts with get_shortname (for very long class names)

--- codegen.ml
+++ codegen.ml
@@ -295,7 +295,7 @@ let get_short_name =
 	let i = ref (-1) in
 	(fun () ->
 		incr i;
-		Printf.sprintf "__hx_type_%i" !i
+		Printf.sprintf "Hx___short___hx_type_%i" !i
 	)
 
 let rec build_generic ctx c p tl =
@@ -461,8 +461,7 @@ let rec build_generic ctx c p tl =
 		(* In rare cases the class name can become too long, so let's shorten it (issue #3090). *)
 		if String.length (snd cg.cl_path) > 254 then begin
 			let n = get_short_name () in
-			let tp = fst cg.cl_path,n in
-			cg.cl_meta <- (Meta.Native,[EConst(String (s_type_path tp)),p],p) :: cg.cl_meta;
+			cg.cl_meta <- (Meta.Native,[EConst(String (n)),p],p) :: cg.cl_meta;
 		end;
 		TInst (cg,[])
 	end
