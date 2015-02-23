99ebf6419c48c69408b521d45e833722670d38be
Cauê Waneck <waneck@gmail.com>
2015-02-23 15:16:46 -0300

    [spod-tests] Disable spod tests for PHP by default

--- tests/unit/src/unit/Test.hx
+++ tests/unit/src/unit/Test.hx
@@ -300,7 +300,7 @@ class Test #if swf_mark implements mt.Protect #end {
 			//new TestRemoting(),
 		];
 		// SPOD tests
-		#if ( (neko || php || java || cpp || (cs && travis)) && !macro && !interp)
+		#if ( (neko || (php && (travis || php_sqlite)) || java || cpp || (cs && travis)) && !macro && !interp)
 		#if ( travis && !(cpp || cs) )
 		if (Sys.getEnv("CI") != null && Sys.systemName() == "Linux")
 		{
