74063437f58e511e0caebd5d0fac86dc8506bb21
Dan Korostelev <nadako@gmail.com>
2015-03-20 12:44:37 +0300

    add test for haxe.CallStack.exceptionStack inside a catch block

--- tests/unit/src/unitstd/haxe/CallStack.unit.hx
+++ tests/unit/src/unitstd/haxe/CallStack.unit.hx
@@ -3,3 +3,10 @@ Std.is(stack, Array) == true;
 
 var stack = haxe.CallStack.exceptionStack();
 Std.is(stack, Array) == true;
+
+try {
+    throw false;
+} catch (_:Dynamic) {
+    var stack = haxe.CallStack.exceptionStack();
+    Std.is(stack, Array) == true;
+}
