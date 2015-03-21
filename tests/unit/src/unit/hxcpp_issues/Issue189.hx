3e92340c54b2fe13603e7b7f761cdcfecc0cedd2
Hugh <gamehaxe@gmail.com>
2015-03-21 10:04:26 +0800

    [cpp] Correct version of test

--- tests/unit/src/unit/hxcpp_issues/Issue189.hx
+++ tests/unit/src/unit/hxcpp_issues/Issue189.hx
@@ -2,11 +2,12 @@ package unit.hxcpp_issues;
 
 
 class Issue189 extends Test {
-   function dynamicWidthDefaults(first=true, second=6, third=10.5) : Float {
+   dynamic function dynamicWidthDefaults(first=true, second=6, third=10.5) : Float {
       return first ? second : third;
    }
 	function test() {
-		eq(dynamicWidthDefaults(),10.5);
+		eq(dynamicWidthDefaults(),6);
+		eq(dynamicWidthDefaults(false),10.5);
 	}
 }
 
