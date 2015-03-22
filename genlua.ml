23f84bfaa2dcdffddff7f33fa2045e399ba89765
Justin Donaldson <jdonaldson@gmail.com>
2015-03-21 22:05:00 -0700

    Lua : reserved keywords/arbitrary strings in anoymous objects.
    In Haxe, if you want to set certain fields (reserved keywords, words
    with spaces, etc.), you need to enquote the field.  In lua, you need to
    also enclose it in brackets:
    
    ```lua
    local x = { ["some field name"] = 4 };
    ```
    
    Easy enough to fix.

--- genlua.ml
+++ genlua.ml
@@ -90,7 +90,7 @@ let valid_lua_ident s =
 let field s = if Hashtbl.mem kwds s || not (valid_lua_ident s) then "[\"" ^ s ^ "\"]" else "." ^ s
 let ident s = if Hashtbl.mem kwds s then "_" ^ s else s
 
-let anon_field s = if Hashtbl.mem kwds s || not (valid_lua_ident s) then "'" ^ s ^ "'" else s
+let anon_field s = if Hashtbl.mem kwds s || not (valid_lua_ident s) then "['" ^ s ^ "']" else s
 let static_field s =
 	match s with
 	| "length" | "name" -> ".$" ^ s
