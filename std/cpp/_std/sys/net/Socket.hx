9a17cb67d84047249727018e7c9a2374acdfe3b0
Bryan Ischo <bji@tivo.com>
2015-03-26 20:22:26 -0700

    Add nonblocking connect support neko and cpp socket implementations.

--- std/cpp/_std/sys/net/Socket.hx
+++ std/cpp/_std/sys/net/Socket.hx
@@ -99,6 +99,8 @@ private class SocketOutput extends haxe.io.Output {
 		} catch( e : Dynamic ) {
 			if( e == "Blocking" )
 				throw Blocked;
+			else if (e == "EOF")
+				throw new haxe.io.Eof();
 			else
 				throw Custom(e);
 		}
@@ -158,6 +160,10 @@ class Socket {
 		} catch( s : String ) {
 			if( s == "std@socket_connect" )
 				throw "Failed to connect on "+host.toString()+":"+port;
+			else if (s == "Blocking") {
+				// Do nothing, this is not a real error, it simply indicates
+				// that a non-blocking connect is in progress
+			}
 			else
 				cpp.Lib.rethrow(s);
 		}
@@ -186,6 +192,9 @@ class Socket {
 
 	public function peer() : { host : Host, port : Int } {
 		var a : Dynamic = socket_peer(__s);
+		if (a == null) {
+			return null;
+		}
 		var h = new Host("127.0.0.1");
 		untyped h.ip = a[0];
 		return { host : h, port : a[1] };
@@ -193,6 +202,9 @@ class Socket {
 
 	public function host() : { host : Host, port : Int } {
 		var a : Dynamic = socket_host(__s);
+		if (a == null) {
+			return null;
+		}
 		var h = new Host("127.0.0.1");
 		untyped h.ip = a[0];
 		return { host : h, port : a[1] };
