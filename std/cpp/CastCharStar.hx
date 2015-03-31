cca982ae2b9271efe6a6f7daa58c8661d08c2e43
Hugh <gamehaxe@gmail.com>
2015-03-31 21:47:50 +0800

    [cpp] Add explicit cast to CastCharStar

--- std/cpp/CastCharStar.hx
+++ std/cpp/CastCharStar.hx
@@ -2,7 +2,7 @@ package cpp;
 
 abstract CastCharStar( RawPointer<Char> ) to(RawPointer<Char>)
 {
-   inline function new(s:String) this = untyped s.__s;
+   inline function new(s:String) this = cast untyped s.__s;
 
    @:from
    static public inline function fromString(s:String) return new CastCharStar(s);
