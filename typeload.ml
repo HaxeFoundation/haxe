9e75bd4fcd264ac0812c5f16ee120f4ce95ce04d
Simon Krajewski <simon@haxe.org>
2015-05-04 10:19:07 +0200

    forward `@:native` to abstract implementation class (closes #4158)

--- typeload.ml
+++ typeload.ml
@@ -200,7 +200,7 @@ let make_module ctx mpath file tdecls loadp =
 				(match !decls with
 				| (TClassDecl c,_) :: _ ->
 					List.iter (fun m -> match m with
-						| ((Meta.Build | Meta.CoreApi | Meta.Allow | Meta.Access | Meta.Enum | Meta.Dce),_,_) ->
+						| ((Meta.Build | Meta.CoreApi | Meta.Allow | Meta.Access | Meta.Enum | Meta.Dce | Meta.Native),_,_) ->
 							c.cl_meta <- m :: c.cl_meta;
 						| _ ->
 							()
