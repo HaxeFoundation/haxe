f6cd97bed19459a100c57887740ce3a09cec2c8c
Simon Krajewski <simon@haxe.org>
2015-05-12 23:41:11 +0200

    remove abstract inline closure restriction (closes #4165)

--- typer.ml
+++ typer.ml
@@ -989,10 +989,6 @@ let rec acc_get ctx g p =
 		(* build a closure with first parameter applied *)
 		(match follow et.etype with
 		| TFun (_ :: args,ret) ->
-			begin match follow e.etype,cf.cf_kind with
-				| TAbstract _,Method MethInline -> error "Cannot create closure on abstract inline method" e.epos
-				| _ -> ()
-			end;
 			let tcallb = TFun (args,ret) in
 			let twrap = TFun ([("_e",false,e.etype)],tcallb) in
 			(* arguments might not have names in case of variable fields of function types, so we generate one (issue #2495) *)
