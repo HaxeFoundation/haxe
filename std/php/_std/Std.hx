fd78c126c0b6a59ad650b00ba36f6d855b32e1a6
Mark Knol <mark.knol@gmail.com>
2015-05-28 14:05:33 +0200

    Update Std.hx

--- std/php/_std/Std.hx
+++ std/php/_std/Std.hx
@@ -29,7 +29,7 @@
 		return Std.is(value, c) ? cast value : null;
 	}
 
-	public inline static function string( s : Dynamic ) : String {
+	public static function string( s : Dynamic ) : String {
 		return untyped __call__("_hx_string_rec", s, '');
 	}
 
