29dec24e1cb701afbbc2317a9a112d46ab53ec5e
Justin Donaldson <jdonaldson@gmail.com>
2015-06-15 20:48:31 -0500

    Lua: Update base lua stringtools extern

--- std/lua/StringTools.hx
+++ std/lua/StringTools.hx
@@ -7,14 +7,17 @@ package lua;
 extern class StringTools {
 	public static function len(str : String): Int;
 	public static function char(i: Int): String;
-	public static function sub(str : String, start : Int, end : Int): String;
+	public static function sub(str : String, start : Int, ?end : Int): String;
 	public static function charCodeAt(str : String, index : Int): Int;
-	public static function find(str : String, target : String, start : Int, end : Int, plain : Bool): Int;
+	public static function find(str : String, target : String, ?start : Int, ?plain : Bool): Int;
 	public static function byte(str : String, ?index : Int) : Int;
 	public static function format(str : String, ?e1 : String, ?e2 : String, ?e3 : String, ?e4 : String): String;
 
 	@:overload(   function     (str : String, pattern : String, replace : String->Void,   ?n : Int): String {})
 	@:overload(   function     (str : String, pattern : String, replace : String->String, ?n : Int): String {})
 	public static function gsub(str : String, pattern : String, replace : String,		  ?n : Int): String;
+
+	@:overload(   function     (str : String, pattern : String, match : Void->String,   ?n : Int): String->Void {})
+	public static function gmatch(str : String, pattern : String, ?n : Int): Void->String;
 }
 
