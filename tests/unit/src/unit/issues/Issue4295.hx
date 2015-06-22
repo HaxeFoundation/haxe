10b188edd437c4a47635a1042f637a9adf72c479
frabbit <frabbit77@googlemail.com>
2015-06-22 14:06:51 +0200

    minor, fix indents

--- tests/unit/src/unit/issues/Issue4295.hx
+++ tests/unit/src/unit/issues/Issue4295.hx
@@ -4,7 +4,7 @@ class Issue4295 extends Test {
 	function test() {
 
 		var dc:Dynamic<Array<Dynamic>> = {a: [0]};
-        eq(dc.a[0], 0);
-        eq(dc.a[0] = 1, 1);
+		eq(dc.a[0], 0);
+		eq(dc.a[0] = 1, 1);
 	}
 }
