dea162ea8ddfeb9a518fc9bf3e9fda717633b690
Justin Donaldson <jdonaldson@gmail.com>
2015-06-24 06:31:01 -0700

    Lua : More global method externs

--- std/lua/Lua.hx
+++ std/lua/Lua.hx
@@ -2,12 +2,14 @@ package lua;
 
 /**
   These are all global static methods within lua
-**/
+ **/
 
 @:native("_G")
 extern class Lua {
-	public static function unpack(x: Dynamic) : Table<Int,Dynamic>;
-	public static function setmetatable(tbl: Table<Dynamic,Dynamic>, mtbl: Table<Dynamic, Dynamic>) : Void;
-	public static function setfenv(i : Int, tbl: Table<Dynamic, Dynamic>) : Void;
-	public static function next<T>(k:Table<Dynamic,T>, i:Null<Int>) : T;
+	public static function setmetatable(tbl: Table<Dynamic,Dynamic>, mtbl: Table<Dynamic, Dynamic>): Void;
+	public static function setfenv(i: Int , tbl: Table<Dynamic, Dynamic>): Void;
+	public static function next<T>(k : Table<Dynamic, T>, ?i : Null<Int>): T;
+	public static function tostring(v: Dynamic): String;
+	public static function ipairs<T>(t: Table<Int,T>): Void->T;
+	public static function pairs<A,B>(t: Table<A,B>): Void->A;
 }
