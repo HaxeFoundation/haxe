bd3b4957a2f85f0c64d9faa2fae51f04bd477981
Andy Li <andy@onthewings.net>
2015-06-30 22:29:02 +0800

    put jQuery helper back to js.jquery and use `@:noPackageRestrict`

--- std/js/jquery/Helper.hx
+++ std/js/jquery/Helper.hx
@@ -1,3 +1,16 @@
 package js.jquery;
 
-typedef Helper = haxe.JQueryHelper;
+@:noPackageRestrict
+#if js extern #end class Helper {
+	macro public static function J(exprs:Array<haxe.macro.Expr>) {
+		return macro new js.jquery.JQuery($a{exprs});
+	}
+
+	#if !macro
+	public static var JTHIS(get, null) : js.jquery.JQuery;
+
+	static inline function get_JTHIS() : js.jquery.JQuery {
+		return new js.jquery.JQuery(js.Lib.nativeThis);
+	}
+	#end
+}
