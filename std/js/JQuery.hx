04b2f1ac71cb63cc97fdb0d6f3e60552e1ea8498
Andy Li <andy@onthewings.net>
2015-07-03 12:39:12 +0800

    deprecate old jQuery extern

--- std/js/JQuery.hx
+++ std/js/JQuery.hx
@@ -24,6 +24,7 @@ package js;
 import js.html.Window;
 import js.html.Element;
 
+@:deprecated("Use js.jquery.Event instead.")
 typedef JqEvent = {
 	var target : Element;
 	var currentTarget : Element;
@@ -62,6 +63,7 @@ typedef JqEvent = {
 	function stopPropagation() : Void;
 }
 
+@:deprecated("Use js.jquery.Helper instead.")
 extern class JQueryHelper {
 	@:overload(function(j:JQuery):JQuery{})
 	@:overload(function(j:Window):JQuery{})
@@ -79,6 +81,7 @@ extern class JQueryHelper {
 
 }
 
+@:deprecated("Use js.jquery.JQuery instead.")
 @:initPackage
 extern class JQuery implements ArrayAccess<Element> {
 
