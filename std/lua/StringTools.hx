8e590f7de4ee7f9431d3267001d3fe0b3263499d
Justin Donaldson <jdonaldson@gmail.com>
2015-07-15 21:38:42 -0700

    Lua : Stringtools.format should accept dynamic arguments

--- std/lua/StringTools.hx
+++ std/lua/StringTools.hx
@@ -11,7 +11,7 @@ extern class StringTools {
 	public static function charCodeAt(str : String, index : Int): Int;
 	public static function find(str : String, target : String, ?start : Int, ?plain : Bool): Int;
 	public static function byte(str : String, ?index : Int) : Int;
-	public static function format(str : String, ?e1 : String, ?e2 : String, ?e3 : String, ?e4 : String): String;
+	public static function format(str : String, ?e1 : Dynamic, ?e2 : Dynamic, ?e3 : Dynamic, ?e4 : Dynamic): String;
 
 	@:overload(   function     (str : String, pattern : String, replace : String->Void,   ?n : Int): String {})
 	@:overload(   function     (str : String, pattern : String, replace : String->String, ?n : Int): String {})
