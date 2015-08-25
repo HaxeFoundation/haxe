8e459b49aad25fe859da7a3f6e7a99fc4338630f
Dan Korostelev <nadako@gmail.com>
2015-08-25 11:46:39 +0300

    [cpp] add methods to get NULL pointer and address of a value in Raw(Const)Pointer

--- std/cpp/RawConstPointer.hx
+++ std/cpp/RawConstPointer.hx
@@ -3,4 +3,6 @@ package cpp;
 @:unreflective
 extern class RawConstPointer<T>
 {
+    @:extern inline static function nullptr<T>():RawConstPointer<T> return untyped __cpp__("NULL");
+    @:extern inline static function of<T>(v:T):RawConstPointer<T> return untyped __cpp__("&{0}", v);
 }
--- std/cpp/RawPointer.hx
+++ std/cpp/RawPointer.hx
@@ -3,4 +3,6 @@ package cpp;
 @:unreflective
 extern class RawPointer<T> extends RawConstPointer<T>
 {
+    @:extern inline static function nullptr<T>():RawPointer<T> return untyped __cpp__("NULL");
+    @:extern inline static function of<T>(v:T):RawPointer<T> return untyped __cpp__("&{0}", v);
 }
