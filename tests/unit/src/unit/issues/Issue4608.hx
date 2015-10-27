cf073db8a53a12dcd40de58b51c2079a95759748
Ryusei Yamaguchi <mandel59@gmail.com>
2015-10-27 23:03:18 +0900

    Add unit test for #4608

---
+++ tests/unit/src/unit/issues/Issue4608.hx
@@ -0,0 +1,14 @@
+package unit.issues;
+
+class Issue4608 extends Test {
+    function test() {
+        var s = "𩸽あëa";
+        var b = false;
+        switch s {
+        case "𩸽あëa":
+            b = true;
+        case _:
+        }
+        t(b);
+    }
+}
