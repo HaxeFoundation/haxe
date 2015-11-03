4f092f7d26a6effbc7b82a1ebed41e4cee477d7e
Justin Donaldson <jdonaldson@gmail.com>
2015-11-02 22:59:42 -0800

    Lua: lua Bit externs return Int, even though theyre really float under the hood

--- std/lua/Bit.hx
+++ std/lua/Bit.hx
@@ -5,12 +5,12 @@ package lua;
 
 @:native("_G.bit")
 extern class Bit {
-	public static function bnot(x:Float) : Float;
-	public static function band(a:Float, b:Float) : Float;
-	public static function bor(a:Float, b:Float) : Float;
-	public static function bxor(a:Float, b:Float) : Float;
-	public static function lshift(x:Float, places:Int) : Float;
-	public static function rshift(x:Float, places:Int) : Float;
-	public static function arshift(x:Float, places:Int) : Float;
-	public static function mod(numerator:Float, denominator:Float) : Float;
+	public static function bnot(x:Float) : Int;
+	public static function band(a:Float, b:Float) : Int;
+	public static function bor(a:Float, b:Float) : Int;
+	public static function bxor(a:Float, b:Float) : Int;
+	public static function lshift(x:Float, places:Int) : Int;
+	public static function rshift(x:Float, places:Int) : Int;
+	public static function arshift(x:Float, places:Int) : Int;
+	public static function mod(numerator:Float, denominator:Float) : Int;
 }
