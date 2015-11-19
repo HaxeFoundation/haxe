15b95e19f2cdc145de5e85099d093aee3ed26c29
Simon Krajewski <simon@haxe.org>
2015-11-19 19:51:46 +0100

    mention ArrayAccess restriction (closes #4632)

--- std/StdTypes.hx
+++ std/StdTypes.hx
@@ -115,5 +115,9 @@ typedef Iterable<T> = {
 /**
 	ArrayAccess is used to indicate a class that can be accessed using brackets.
 	The type parameter represents the type of the elements stored.
+
+	This interface should be used for externs only. Haxe does not support custom
+	array access on classes. However, array access can be implemented for
+	abstract types (see http://haxe.org/manual/types-abstract-array-access.html).
 **/
 extern interface ArrayAccess<T> { }
