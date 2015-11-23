2a5c5cb2204b69d91631cf4c75e568e269693123
Simon Krajewski <simon@haxe.org>
2015-11-23 12:17:32 +0100

    allow parsing postfix ! but fail during typing (closes #4284)

--- typer.ml
+++ typer.ml
@@ -2342,6 +2342,7 @@ and type_unop ctx op flag e p =
 		let make e =
 			let t = (match op with
 			| Not ->
+				if flag = Postfix then error "Postfix ! is not supported" p;
 				unify ctx e.etype ctx.t.tbool e.epos;
 				ctx.t.tbool
 			| Increment
