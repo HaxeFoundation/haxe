c89e1b2ef629d67bcd3f4ff0b0b22829ba32c801
Simon Krajewski <simon@haxe.org>
2015-11-24 14:15:00 +0100

    dodge C++ issue with `String` as value

--- tests/unit/src/unit/issues/misc/Issue4653Macro.hx
+++ tests/unit/src/unit/issues/misc/Issue4653Macro.hx
@@ -2,6 +2,10 @@ package unit.issues.misc;
 
 class Issue4653Macro {
 	static public function getType() {
+		return macro : NotString;
+	}
+
+	static public function getStringType() {
 		return macro : String;
 	}
 }
