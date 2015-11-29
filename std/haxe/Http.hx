fd5bf82fcb432f6bf525e7c6e0325003c178c6d4
Simon Krajewski <simon@haxe.org>
2015-11-29 13:10:15 +0100

    [js] add `Http.withCredentials` (closes #4442)

--- std/haxe/Http.hx
+++ std/haxe/Http.hx
@@ -65,6 +65,7 @@ class Http {
 	var file : { param : String, filename : String, io : haxe.io.Input, size : Int, mimeType : String };
 #elseif js
 	public var async : Bool;
+	public var withCredentials : Bool;
 #end
 	var postData : String;
 	var headers : List<{ header:String, value:String }>;
@@ -92,6 +93,7 @@ class Http {
 
 		#if js
 		async = true;
+		withCredentials = false;
 		#elseif sys
 		cnxTimeout = 10;
 		#end
@@ -197,6 +199,7 @@ class Http {
 	#if js
 		me.responseData = null;
 		var r = req = js.Browser.createXMLHttpRequest();
+		r.withCredentials = withCredentials;
 		var onreadystatechange = function(_) {
 			if( r.readyState != 4 )
 				return;
