e104d71549b51e38bc21edd56fc117232e71406b
Mockey <dev@medienpensionat.com>
2015-12-04 16:06:12 +0100

    added test for null and native array equality

---
+++ tests/unit/src/unit/issues/Issue4695.hx
@@ -0,0 +1,25 @@
+package unit.issues;
+
+class Issue4695 extends unit.Test {
+
+	function eqCheck<T>(v1:T, v2:T) {
+		return v1 == v2;
+	}
+	
+	public function testNull() {
+		f("" == null);
+		f(eqCheck("", null));
+		f(false == null);
+		f(eqCheck(false, null));
+	}
+	
+	#if php
+	public function testNativeArray() {
+		var a1 = (untyped __php__)('array("f1" => 1, "f2" => 2, "f3" => 3)');
+		var a2 = (untyped __php__)('array("f1" => true, "f2" => "2", "f3" => 3)');
+		f(a1 == a2);
+		f(eqCheck(a1, a2));
+	}
+	#end
+	
+}
