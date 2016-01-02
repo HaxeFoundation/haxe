1c4957ebd98cab0ea17ff48dd3c516821371d7f5
Justin Donaldson <jdonaldson@gmail.com>
2016-01-02 12:12:41 -0800

    Lua : use simple field length lookup in haxe.Serializer

--- std/haxe/Serializer.hx
+++ std/haxe/Serializer.hx
@@ -262,7 +262,7 @@ class Serializer {
 				#if (flash || python || hl)
 				var v : Array<Dynamic> = v;
 				#end
-				var l = #if (neko || flash || php || cs || java || python || hl) v.length #elseif cpp v.__length() #else __getField(v, "length") #end;
+				var l = #if (neko || flash || php || cs || java || python || hl || lua) v.length #elseif cpp v.__length() #else __getField(v, "length") #end;
 				for( i in 0...l ) {
 					if( v[i] == null )
 						ucount++;
