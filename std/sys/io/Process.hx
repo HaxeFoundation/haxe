f81a9f14f824812985d53e69e6753e4b2651feca
Andy Li <andy@onthewings.net>
2016-01-08 17:50:30 +0800

    improved doc for Sys.command and sys.io.Process regarding to passing arguments

--- std/sys/io/Process.hx
+++ std/sys/io/Process.hx
@@ -30,12 +30,12 @@ extern class Process {
 	/**
 		Construct a `Process` object, which run the given command immediately.
 
-		When `args` is not `null`, it will be used as the command arguments,
-		with shell meta-characters automatically escaped and quoted if needed.
+		Command arguments can be passed in two ways: 1. using `args`, 2. appending to `cmd` and leaving `args` as `null`.
+
+		 1. When using `args` to pass command arguments, each argument will be automatically quoted, and shell meta-characters will be escaped if needed.
 		`cmd` should be an executable name that can be located in the `PATH` environment variable, or a path to an executable.
-		
-		When `args` is not given or is `null`, no automatic escaping/quoting is performed.
-		`cmd` should include the command together with its arguments, formatted exactly as it would be when typed at the command line.
+
+		 2. When `args` is not given or is `null`, command arguments can be appended to `cmd`. No automatic quoting/escaping will be performed. `cmd` should be formatted exactly as it would be when typed at the command line.
 		It can run executables, as well as shell commands that are not executables (e.g. on Windows: `dir`, `cd`, `echo` etc).
 
 		`close()` should be called when the `Process` is no longer used.
