4144a009a6f7247766638e88e29f27256b27f3b0
Justin Donaldson <jdonaldson@gmail.com>
2016-01-24 14:04:58 -0800

    Lua: Ereg : matched group escapes must come before double dollar sign escapes

--- std/lua/_std/EReg.hx
+++ std/lua/_std/EReg.hx
@@ -114,8 +114,8 @@ class EReg {
 	}
 
 	public function replace( s : String, by : String ) : String {
-		by = Rex.gsub(by, "\\${2}", "$"); // escape double dollar signs
 		by = Rex.gsub(by, "\\$(\\d)", "%%1"); // convert dollar sign matched groups to Rex equivalent
+		by = Rex.gsub(by, "\\${2}", "$"); // escape double dollar signs
 		return Rex.gsub(s,r,by, global ? null : 1);
 	}
 
