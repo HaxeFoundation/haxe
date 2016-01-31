b8b1d4469a60763f5fdd9ae1f5d741adf5c0edb7
Justin Donaldson <jdonaldson@gmail.com>
2016-01-31 00:43:11 -0800

    Lua: base argument in Lua.tonumber is optional

--- std/lua/Lua.hx
+++ std/lua/Lua.hx
@@ -14,7 +14,7 @@ extern class Lua {
 	public static function tostring(v: Dynamic): String;
 	public static function ipairs<T>(t: Table<Int,T>): Void->T;
 	public static function pairs<A,B>(t: Table<A,B>): Void->A;
-	public static function tonumber(str:String, base:Int): Int;
+	public static function tonumber(str:String, ?base:Int): Int;
 	public static function type(v:Dynamic): String;
 	public static function print(v:Dynamic) : Void;
 	public static function select(n:Dynamic, rest:Rest<Dynamic>) : Dynamic;
