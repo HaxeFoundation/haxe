7efb76348b35e653a4cc3b361a999649d70e038a
Justin Donaldson <jdonaldson@gmail.com>
2016-02-15 10:32:57 -0800

    Lua: Travis tweaks

--- .travis/setenv_lua.sh
+++ .travis/setenv_lua.sh
@@ -1,3 +1,3 @@
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
-bash ${TRAVIS_BUILD_DIR}.travis/setup_lua.sh
+bash ${TRAVIS_BUILD_DIR}/.travis/setup_lua.sh
 eval `$HOME/.lua/luarocks path`
