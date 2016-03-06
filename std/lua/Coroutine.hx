a8a19180af336dc2b09d8991646c5820c9237b73
Justin Donaldson <jdonaldson@gmail.com>
2016-03-06 11:24:44 -0800

    Lua: reformat the messy coroutine extern a bit

--- std/lua/Coroutine.hx
+++ std/lua/Coroutine.hx
@@ -5,28 +5,28 @@ package lua;
  **/
 @:native("_G.coroutine")
 extern class Coroutine {
-	@:overload(function<A,B,C,D,E>(f : A->B->C->D->E->Void) : CoroutineInstance5<A,B,C,D,E> {})
-	@:overload(function<A,B,C,D>(f   : A->B->C->D->Void)    : CoroutineInstance4<A,B,C,D>   {})
-	@:overload(function<A,B,C>(f     : A->B->C->Void)       : CoroutineInstance3<A,B,C>     {})
-	@:overload(function<A,B>(f       : A->B->Void)          : CoroutineInstance2<A,B>       {})
-	@:overload(function<A>(f         : A->Void)             : CoroutineInstance1<A>         {})
-	@:overload(function(f            : Void->Void)             : CoroutineInstance0         {})
+	@:overload(function<A,B,C,D,E> (f:A->B->C->D->E->Void) : CoroutineInstance5<A,B,C,D,E>{})
+	@:overload(function<A,B,C,D>   (f:A->B->C->D->Void)    : CoroutineInstance4<A,B,C,D>{})
+	@:overload(function<A,B,C>     (f:A->B->C->Void)       : CoroutineInstance3<A,B,C>{})
+	@:overload(function<A,B>       (f:A->B->Void)          : CoroutineInstance2<A,B>{})
+	@:overload(function<A>         (f:A->Void)             : CoroutineInstance1<A>{})
+	@:overload(function            (f:Void->Void)          : CoroutineInstance0{})
 	public static function create(f : haxe.Constraints.Function) : CoroutineInstance;
 
-	@:overload(function<A,B,C,D,E>(c : CoroutineInstance5<A,B,C,D,E>, arg1 : A, arg2: B, arg3: C, arg4: C, arg5: C) : Void {})
-	@:overload(function<A,B,C,D>(c   : CoroutineInstance4<A,B,C,D>, arg1: A, arg2: B, arg3: C, arg4: C) : Void             {})
-	@:overload(function<A,B,C>(c     : CoroutineInstance3<A,B,C>, arg1: A, arg2: B, arg3: C) : Void                        {})
-	@:overload(function<A,B>(c       : CoroutineInstance2<A,B>, arg1: A, arg2: B) : Void                                   {})
-	@:overload(function<A>(c         : CoroutineInstance1<A>, arg1: A) : Void                                              {})
-	@:overload(function(c            : CoroutineInstance0) : Void                                                          {})
+	@:overload(function<A,B,C,D,E> (c:CoroutineInstance5<A,B,C,D,E>, arg1:A,arg2:B,arg3:C,arg4:C,arg5:C):Void{})
+	@:overload(function<A,B,C,D>   (c:CoroutineInstance4<A,B,C,D>,   arg1:A,arg2:B,arg3:C,arg4:C):Void{})
+	@:overload(function<A,B,C>     (c:CoroutineInstance3<A,B,C>,     arg1:A,arg2:B,arg3:C):Void{})
+	@:overload(function<A,B>       (c:CoroutineInstance2<A,B>,       arg1:A,arg2:B):Void{})
+	@:overload(function<A>         (c:CoroutineInstance1<A>,         arg1:A):Void{})
+	@:overload(function            (c:CoroutineInstance0):Void{})
 	public static function resume(c : CoroutineInstance, ?arg1:Dynamic, ?arg2:Dynamic, ?arg3:Dynamic, ?arg4:Dynamic, ?arg5:Dynamic) : Void;
 
-	@:overload(function<A,B,C,D,E>(c         : CoroutineInstance5<A,B,C,D,E>) : Void {})
-	@:overload(function<A,B,C,D>(c           : CoroutineInstance4<A,B,C,D>)   : Void {})
-	@:overload(function<A,B,C>(c             : CoroutineInstance3<A,B,C>)     : Void {})
-	@:overload(function<A,B>(c               : CoroutineInstance2<A,B>)       : Void {})
-	@:overload(function<A>(c                 : CoroutineInstance1<A>)         : Void {})
-	@:overload(function(c                    : CoroutineInstance0)            : Void {})
+	@:overload(function<A,B,C,D,E> (c:CoroutineInstance5<A,B,C,D,E>) : Void{})
+	@:overload(function<A,B,C,D>   (c:CoroutineInstance4<A,B,C,D>)   : Void{})
+	@:overload(function<A,B,C>     (c:CoroutineInstance3<A,B,C>)     : Void{})
+	@:overload(function<A,B>       (c:CoroutineInstance2<A,B>)       : Void{})
+	@:overload(function<A>         (c:CoroutineInstance1<A>)         : Void{})
+	@:overload(function            (c:CoroutineInstance0)            : Void{})
 	public static function yield(c : CoroutineInstance) : Void;
 }
 
