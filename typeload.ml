2bb512414d4689d1f596ea6762d4bd3e99f2e5ac
Johann Borck <ousado@tuta.io>
2016-03-15 21:44:08 +0100

    fix failing test for Issue4270

--- typeload.ml
+++ typeload.ml
@@ -3399,7 +3399,15 @@ let resolve_module_file com m remap p =
 			) in
 			String.concat "/" (x :: l) ^ "/" ^ name
 	) ^ ".hx" in
-	let file = if ExtString.String.ends_with (!Parser.resume_display).pfile file then (!Parser.resume_display).pfile else Common.find_file com file in
+	let file = (match com.display with
+		| DMNone -> Common.find_file com file
+		| _ ->  let revm   = (snd m ^ ".hx") :: List.rev (fst m) in
+				let pflist = List.rev (ExtString.String.nsplit (!Parser.resume_display).pfile "/") in
+				if fst (List.fold_left ( fun (a, p) m -> match (a,p) with
+					| false, _       -> false, p  (* once wrong, always wrong *)
+					| true,  []      -> false, p  (* module longer than pfile, so not what we're looking for *)
+					| true,  p :: pt -> m = p, pt (* actual test *)
+				) (true,pflist) revm) then (!Parser.resume_display).pfile else Common.find_file com file) in
 	let file = (match String.lowercase (snd m) with
 	| "con" | "aux" | "prn" | "nul" | "com1" | "com2" | "com3" | "lpt1" | "lpt2" | "lpt3" when Sys.os_type = "Win32" ->
 		(* these names are reserved by the OS - old DOS legacy, such files cannot be easily created but are reported as visible *)
