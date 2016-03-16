b3f616341f8de72f7460dfd878aa6fa1be77fbe9
Simon Krajewski <simon@haxe.org>
2016-03-16 09:40:51 +0100

    support 14 arguments for Type.createInstance on all targets (closes #4232)

--- std/python/_std/Type.hx
+++ std/python/_std/Type.hx
@@ -134,6 +134,18 @@ enum ValueType {
 				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6]);
 			case 8:
 				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7]);
+			case 9:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8]);
+			case 10:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9]);
+			case 11:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10]);
+			case 12:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11]);
+			case 13:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11],args[12]);
+			case 14:
+				return Syntax.newInstance(cl,args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11],args[12],args[13]);
 			default:
 				throw "Too many arguments";
 		}
