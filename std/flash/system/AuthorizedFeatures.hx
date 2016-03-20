608ad4ceacb9064ecf2b07fa7833122b926c9d54
Dan Korostelev <nadako@gmail.com>
2016-03-21 00:05:39 +0300

    "fix" for flash externs

--- std/flash/system/AuthorizedFeatures.hx
+++ std/flash/system/AuthorizedFeatures.hx
@@ -8,7 +8,7 @@ package flash.system;
 	function enableAVURLStream(stream : flash.media.AVURLStream) : Bool;
 	@:require(flash11_4) function enableDiskCache(stream : flash.net.URLStream) : Bool;
 	@:require(flash11_7) function enableHLSPlayback(stream : flash.media.AVStream) : Bool;
-	function enableMediaPlayerHLSPlayback(player : com.adobe.tvsdk.mediacore.MediaPlayer) : Bool;
+	function enableMediaPlayerHLSPlayback(player : Dynamic) : Bool;
 	@:require(flash11_4) function isFeatureEnabled(feature : String, ?data : String) : Bool;
 	@:require(flash11_4) function isNegativeToken() : Bool;
 }
