e35864f0aa90f887920c6264ae6f12c629571310
Justin Donaldson <jdonaldson@gmail.com>
2016-03-31 13:39:33 -0700

    Lua : Fix up Os extern, add note about multi-return

--- std/lua/Os.hx
+++ std/lua/Os.hx
@@ -9,20 +9,23 @@ extern class Os {
 	public static function date() : DateType;
 
 	public static function difftime(t2: Time, t1: Time) : Float;
-	public static function execute(command:String) : Int;
+
+	// TODO: multi-return
+	public static function execute(?command:String) : Bool;
+
 	public static function exit(code: Int) : Int;
 	public static function getenv(varname : String) : String;
 	public static function remove(filename : String) : Void;
 	public static function rename(oldname : String, newname : String) : Void;
 	public static function setlocale(locale : String, ?category : LocaleCategory ) : String;
-	public static function time(?arg : DateArg) : Time;
+	public static function time(?arg : TimeParam) : Time;
 	public static function tmpname() : String;
 }
 
 /**
   A typedef that matches the date parameter time() will accept.
  **/
-typedef DateArg = {
+typedef TimeParam = {
 	year   : Float,
 	month  : Float,
 	day    : Float,
