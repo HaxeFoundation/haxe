472b4da6f158cb52fa1267c36d682b65b922abd7
Mark Knol <mark@mediamonks.com>
2016-04-05 01:20:30 +0200

    EnumTools/EnumValueTools class descriptions

--- std/haxe/EnumTools.hx
+++ std/haxe/EnumTools.hx
@@ -22,6 +22,15 @@
 
 package haxe;
 
+/**
+	This class provides advanced methods on enums. It is ideally used with
+	`using EnumTools` and then acts as an 
+  [extension](http://haxe.org/manual/lf-static-extension.html) to the 
+  `enum` types.
+
+	If the first argument to any of the methods is null, the result is
+	unspecified.
+**/
 extern class EnumTools {
 	/**
 		Returns the name of enum `e`, including its path.
@@ -101,6 +110,15 @@ extern class EnumTools {
 	}
 }
 
+/**
+	This class provides advanced methods on enum values. It is ideally used with
+	`using EnumValueTools` and then acts as an 
+  [extension](http://haxe.org/manual/lf-static-extension.html) to the 
+  `EnumValue` types.
+
+	If the first argument to any of the methods is null, the result is
+	unspecified.
+**/
 extern class EnumValueTools {
 
 	/**
