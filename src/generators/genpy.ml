177cd494818d25e91cdadc723693af69fb99f895
Dan Korostelev <nadako@gmail.com>
2016-04-05 17:51:58 +0300

    [python] generate saner imports when imported name is the same as type name (e.g. `import A` instead of `import A as A`)

--- src/generators/genpy.ml
+++ src/generators/genpy.ml
@@ -2344,7 +2344,10 @@ module Generator = struct
 				let import = match import_type with
 					| IModule module_name ->
 						(* importing whole module *)
-						"import " ^ module_name ^ " as " ^ class_name
+						if module_name = class_name then
+							"import " ^ module_name
+						else
+							"import " ^ module_name ^ " as " ^ class_name
 
 					| IObject (module_name,object_name) ->
 						if String.contains object_name '.' then
@@ -2352,7 +2355,10 @@ module Generator = struct
 							"import " ^ module_name ^ " as _hx_temp_import; " ^ class_name ^ " = _hx_temp_import." ^ object_name ^ "; del _hx_temp_import"
 						else
 							(* importing a class from a module *)
-							"from " ^ module_name ^ " import " ^ object_name ^ " as " ^ class_name
+							if object_name = class_name then
+								"from " ^ module_name ^ " import " ^ object_name
+							else
+								"from " ^ module_name ^ " import " ^ object_name ^ " as " ^ class_name
 				in
 				newline ctx;
 				if ignore_error then begin
