c0764c6642410ce9dbb859e7762bee40e8c7e9b0
Pascal Peridont <pperidont@motion-twin.com>
2016-04-05 11:56:44 +0200

    Fix sys.ssl.DigestAlgorithm

--- std/cpp/_std/sys/ssl/Digest.hx
+++ std/cpp/_std/sys/ssl/Digest.hx
@@ -1,17 +1,6 @@
 package sys.ssl;
 import cpp.NativeSsl;
 
-@:enum
-abstract DigestAlgorithm(String) to String {
-	var MD5 = "MD5";
-	var SHA1 = "SHA1";
-	var SHA224 = "SHA224";
-	var SHA256 = "SHA256";
-	var SHA384 = "SHA384";
-	var SHA512 = "SHA512";
-	var RIPEMD160 = "RIPEMD160";
-}
-
 @:coreApi
 class Digest {
 
