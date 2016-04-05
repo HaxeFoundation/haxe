c4f2b1587455169cf785ced40d929ddf4627b5ab
Gama11 <jensfischer95@gmail.com>
2016-04-05 13:05:33 +0200

    Disable #4988 test on PHP for now

--- tests/unit/src/unit/issues/Issue4988.hx
+++ tests/unit/src/unit/issues/Issue4988.hx
@@ -2,10 +2,12 @@ package unit.issues;
 
 class Issue4988 extends Test {
 	function test() {
+		#if !php
 		try {
 			var d:{i:Null<Int>} = null;
 			foo(d.i > 0);
-		} catch(e:Dynamic) {}
+		} catch (e:Dynamic) {}
+		#end
 	}
 
 	function foo(_) {}
