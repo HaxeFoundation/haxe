9427990420beaf4c3d39b1dfc1f2943195b220cf
Mockey <dev@medienpensionat.com>
2016-04-07 14:34:57 +0100

    fix test

--- tests/unit/src/unit/issues/Issue4973.hx
+++ tests/unit/src/unit/issues/Issue4973.hx
@@ -1,13 +1,11 @@
 package unit.issues;
 
-#if php
 class Issue4973 extends Test {
-
+	#if php
 	function test() {
 		try sys.io.File.getContent("not-existant")
 		catch(exc:php.Exception) t(untyped __php__("{0} instanceof Exception", exc))
 		catch(exc:Dynamic) t(untyped __php__("{0} instanceof Exception", exc));
 	}
-	
+	#end
 }
-#end
