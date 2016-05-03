e091dc20090b754c9f97ebe300c88a99a371e6ca
Cauê Waneck <waneck@gmail.com>
2016-05-03 04:12:33 -0300

    Fix macro problem with the issues directory and Issue5192_Test module

--- tests/unit/src/unit/issues/Issue5192_Test.hx
+++ tests/unit/src/unit/issues/Issue5192_Test.hx
@@ -1,6 +1,9 @@
 package unit.issues;
 
-abstract Issue5192_Test(Int) {
+class Issue5192_Test extends Test {
+}
+
+abstract Issue5192_Test_Abstract(Int) {
   @:generic public static function doSomething<T>():T {
     return cast null;
   }
