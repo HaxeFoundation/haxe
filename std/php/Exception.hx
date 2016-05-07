c84f04fa77878667cfdb2e0b882756ea8166f4b9
Théo SABATTIE (ilicos) <fondateur.ilicos@gmail.com>
2016-05-07 17:32:18 +0200

    Fix type (see php doc)

--- std/php/Exception.hx
+++ std/php/Exception.hx
@@ -31,7 +31,7 @@ extern class Exception {
 
   public function getMessage() : String;       // message of the exception
   public function getPrevious() : Exception;   // last exception
-  public function getCode() : Int;             // code of the exception
+  public function getCode() : Dynamic;         // code of the exception
   public function getFile() : String;          // source filename
   public function getLine() : Int;             // source line
   public function getTrace() : Array<String>;  // an array of the backtrace()
