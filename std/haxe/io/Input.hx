60dbc416f6c2c20d06f7c845ddbabfa0418604aa
Justin Donaldson <jdonaldson@gmail.com>
2016-05-20 16:21:11 -0700

    Fix haxe.io.Input.readAll

--- std/haxe/io/Input.hx
+++ std/haxe/io/Input.hx
@@ -77,7 +77,10 @@ class Input {
 			    pos++;
 			    k--;
 			}
-		} catch (eof: haxe.io.Eof){}
+		} catch (eof : Eof){
+			if (k == len)
+				throw eof;
+		}
 		return len-k;
 	}
 
@@ -112,14 +115,13 @@ class Input {
 
 		var buf = Bytes.alloc(bufsize);
 		var total = new haxe.io.BytesBuffer();
-		try {
+		var first = true;
+		try{
 			while( true ) {
 				var len = readBytes(buf,0,bufsize);
-				if( len == 0 )
-					throw Error.Blocked;
 				total.addBytes(buf,0,len);
 			}
-		} catch( e : Eof ) { }
+		} catch(e : Eof){}
 		return total.getBytes();
 	}
 
@@ -131,8 +133,6 @@ class Input {
 	public function readFullBytes( s : Bytes, pos : Int, len : Int ) : Void {
 		while( len > 0 ) {
 			var k = readBytes(s,pos,len);
-			if (k == 0)
-				throw Error.Blocked;
 			pos += k;
 			len -= k;
 		}
