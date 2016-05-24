2da004afc10a7f20c78a804fa929c7ade5a5279f
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-05-25 01:40:04 +0200

    Fix package.

--- std/php/_std/sys/db/Mysql.hx
+++ std/php/_std/sys/db/Mysql.hx
@@ -42,7 +42,7 @@ import sys.db.Connection;
 				dsn+='port='+params.port+";";
 		}
 		dsn+="dbname="+params.database;
-		return php.db.PDO.open(dsn,params.user,params.pass);
+		return php.PDO.open(dsn,params.user,params.pass);
 	}
 
 }
