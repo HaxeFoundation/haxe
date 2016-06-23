82af58c0c6b13af72d44d0ecec3b6542c3a2b426
Mark Knol <mark.knol@gmail.com>
2016-06-23 17:02:54 +0200

    haxe.unit documentation

--- std/haxe/unit/TestResult.hx
+++ std/haxe/unit/TestResult.hx
@@ -21,10 +21,16 @@
  */
  package haxe.unit;
 
+/**
+	TestResult contains the result of the executed unit tests.
+**/
 class TestResult {
-
 	var m_tests : List<TestStatus>;
-	public var success(default,null) : Bool;
+
+	/**
+		`true` if the unit test succesfully executed the test cases.
+	**/
+	public var success(default, null) : Bool;
 
 	public function new() {
 		m_tests = new List();
@@ -37,6 +43,9 @@ class TestResult {
 			success = false;
 	}
 
+	/**
+		String representation from the result of the unit test.
+	**/
 	public function toString() : String 	{
 		var buf = new StringBuf();
 		var failures = 0;
