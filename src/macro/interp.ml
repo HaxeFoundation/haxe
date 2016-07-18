HaxeFoundation/haxe

commit 52aeb180fd233f400d997e1d57bfb13c6e878ada

some fixes for neko Socket

std/neko/_std/sys/net/Socket.hx
@@ -144,7 +144,7 @@ class Socket {
 	}
 
 	public function read() : String {
-		return socket_read(__s);
+		return new String(socket_read(__s));
 	}
 
 	public function write( content : String ) : Void {
@@ -232,7 +232,7 @@ class Socket {
 				var i = 0;
 				while( i < a.length ){
 					r[i] = a[i].__s;
-					__dollar__hadd(c,a[i].__s,a[i]);
+					__dollar__hadd(c,a[i].__s,a[i],null);
 					i += 1;
 				}
 				return r;

commit 2e80842218b7e1cc0c761e38844406d1702ba90f

[neko] fix sys.net.Socket (broken by 52aeb180fd233f400d997e1d57bfb13c6e878ada)

src/macro/interp.ml
@@ -993,8 +993,7 @@ let builtins =
 			Hashtbl.replace h k v;
 			VBool (not old);
 		);
-		"hadd", Fun4 (fun h k v cmp ->
-			if cmp <> VNull then assert false;
+		"hadd", Fun3 (fun h k v ->
 			let h = vhash h in
 			let old = Hashtbl.mem h k in
 			Hashtbl.add h k v;

std/neko/_std/sys/net/Socket.hx
@@ -232,7 +232,7 @@ class Socket {
 				var i = 0;
 				while( i < a.length ){
 					r[i] = a[i].__s;
-					__dollar__hadd(c,a[i].__s,a[i],null);
+					__dollar__hadd(c,a[i].__s,a[i]);
 					i += 1;
 				}
 				return r;