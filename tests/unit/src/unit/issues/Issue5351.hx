8ecd14fb04a5d0020a18262ad59fc4845104755a
Cauê Waneck <waneck@gmail.com>
2016-08-03 21:17:42 -0300

    Workaround for as3

--- tests/unit/src/unit/issues/Issue5351.hx
+++ tests/unit/src/unit/issues/Issue5351.hx
@@ -2,12 +2,14 @@ package unit.issues;
 import scripthost.Issue5351;
 
 class Issue5351 extends Test {
+#if !as3
 	public function test() {
 		var t3:Issue5351_2 = Type.createInstance(Type.resolveClass('unit.issues.Issue5351_3'), []);
 		eq(t3.doTest1(), 'doTest1 override');
 		eq(t3.doTest2(), 'doTest2');
 		eq(t3.doTest3(), 'doTest3');
 	}
+#end
 }
 
 @:keep class Issue5351_3 extends Issue5351_2 {
