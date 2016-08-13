303a190463ff3d8a88f9ffc0c8abcee4239fdfca
Alexander Kuzmenko <alex@stablex.ru>
2016-08-13 05:57:12 +0300

    [php7] create filesystem tree for namespaces

--- src/generators/genphp7.ml
+++ src/generators/genphp7.ml
@@ -1,43 +1,48 @@
-(*
-	The Haxe Compiler
-	Copyright (C) 2005-2016  Haxe Foundation
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*)
 
 open Ast
 open Type
 open Common
 
-(** 
+(*
+	@param path Something like [ "/some/path/first_dir_to_create"; "nested_level1"; "nested_level2" ]
+*)
+let create_dir_recursive path =
+	let rec create dir nested_dirs =
+		if not (Sys.file_exists dir) then (Unix.mkdir dir 0o755);
+		match nested_dirs with
+			| [] -> ();
+			| next :: rest -> create (dir ^ "/" ^ next) rest
+	in
+	match path with
+		| [] -> ();
+		| root :: rest -> create root rest
+
+(**
 	@param php7_path Output path specified by `-php7` compiler flag
 	@param php_lib_path Path specified by `--php-lib` compiler flag
+	@return Full path of a directory to generate php classes into.
 *)
-let create_output_dir php7_path php_lib_path = 
-	let lib_path = 
+let create_output_dir php7_path php_lib_path =
+	let lib_path =
 		match php_lib_path with
 			| None -> ["lib"];
 			| Some path -> (Str.split (Str.regexp "/")  path)
 	in
-	let rec create_recursively dir nested_dirs = 
-		if not (Sys.file_exists dir) then (Unix.mkdir dir 0o755);
-		match nested_dirs with
-			| [] -> ();
-			| next :: rest -> create_recursively (dir ^ "/" ^ next) rest
-	in
-	create_recursively php7_path lib_path
+	let build_path = (php7_path :: lib_path) in
+	create_dir_recursive build_path;
+	String.concat "/" build_path
+
+let generate_class build_dir cls =
+	let namespace = (match cls.cl_path with (module_path, _) -> module_path) in
+	create_dir_recursive (build_dir :: namespace)
+
+let generate_type build_dir type_declaration =
+	match type_declaration with
+		| TClassDecl cls -> generate_class build_dir cls;
+		| TEnumDecl cenum -> ();
+		| TTypeDecl ctypedef -> ();
+		| TAbstractDecl cabstract -> ()
 
 let generate com =
- 	create_output_dir com.file com.php_lib
+ 	let build_dir = create_output_dir com.file com.php_lib in
+ 	List.iter (generate_type build_dir) com.types
