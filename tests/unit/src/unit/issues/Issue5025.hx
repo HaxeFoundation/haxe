7abf6e687aeae87667f38b5b89a6008b56d9c9d0
Gama11 <jensfischer95@gmail.com>
2016-08-31 17:31:42 +0200

    Fix a cpp runtime crash in Issue5025.hx

--- tests/unit/src/unit/issues/Issue5025.hx
+++ tests/unit/src/unit/issues/Issue5025.hx
@@ -2,6 +2,10 @@ package unit.issues;
 
 class Issue5025 extends Test {
 	function test() {
+		// nothing to do here, we just want to see if the switch (null) compiles
+	}
+
+	function shouldCompile() {
 		#if !(java || cs || as3 || lua)
 		try {
 			switch (null) {
