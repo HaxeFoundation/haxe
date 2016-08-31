72fa53e215b8a80fb318cf559fbc5085c37302ba
Alexander Kuzmenko <alex@stablex.ru>
2016-08-31 09:29:29 +0300

    [php7] truncate exception stack to the place where exception was caught

--- std/php7/stdimpl/haxe/CallStack.hx
+++ std/php7/stdimpl/haxe/CallStack.hx
@@ -8,57 +8,71 @@ private typedef NativeTrace = NativeIndexedArray<NativeAssocArray<Dynamic>>;
 @:dox(hide)
 @:noCompletion
 class CallStack {
-    static var lastExceptionTrace : NativeTrace;
+	static var lastExceptionTrace : NativeTrace;
 
-    /**
+	/**
 		Return the call stack elements, or an empty array if not available.
 	**/
-    public static inline function callStack() : Array<StackItem> {
-        return makeStack(Global.debug_backtrace(Const.DEBUG_BACKTRACE_IGNORE_ARGS));
-    }
+	public static inline function callStack() : Array<StackItem> {
+		return makeStack(Global.debug_backtrace(Const.DEBUG_BACKTRACE_IGNORE_ARGS));
+	}
 
-    /**
+	/**
 		Return the exception stack : this is the stack elements between
 		the place the last exception was thrown and the place it was
 		caught, or an empty array if not available.
 	**/
 	public static inline function exceptionStack() : Array<StackItem> {
-        return makeStack(lastExceptionTrace == null ? new NativeIndexedArray() : lastExceptionTrace);
-    }
+		return makeStack(lastExceptionTrace == null ? new NativeIndexedArray() : lastExceptionTrace);
+	}
 
-    @:keep
+	@:keep
 	static function saveExceptionTrace( e:Throwable ) : Void {
 		lastExceptionTrace = e.getTrace();
-		var count = php7.Global.count(lastExceptionTrace);
+
+		//Reduce exception stack to the place where exception was caught
+		var currentTrace = Global.debug_backtrace(Const.DEBUG_BACKTRACE_IGNORE_ARGS);
+		var count = Global.count(currentTrace);
+		for (i in -(count - 1)...1) {
+			var exceptionEntry:NativeAssocArray<Dynamic> = Global.end(lastExceptionTrace);
+			if (currentTrace[-i]['file'] == exceptionEntry['file'] && currentTrace[-i]['line'] == exceptionEntry['line']) {
+				Global.array_pop(lastExceptionTrace);
+			} else {
+				break;
+			}
+		}
+
+		//Remove arguments from trace to avoid blocking some objects from GC
+		var count = Global.count(lastExceptionTrace);
 		for (i in 0...count) {
 			lastExceptionTrace[i]['args'] = new NativeArray();
 		}
 
-        var thrownAt = new NativeAssocArray<Dynamic>();
-        thrownAt['function'] = '';
-        thrownAt['line'] = e.getLine();
-        thrownAt['file'] = e.getFile();
-        thrownAt['class'] = '';
-        thrownAt['args'] = new NativeArray();
-        Global.array_unshift(lastExceptionTrace, thrownAt);
+		var thrownAt = new NativeAssocArray<Dynamic>();
+		thrownAt['function'] = '';
+		thrownAt['line'] = e.getLine();
+		thrownAt['file'] = e.getFile();
+		thrownAt['class'] = '';
+		thrownAt['args'] = new NativeArray();
+		Global.array_unshift(lastExceptionTrace, thrownAt);
 	}
 
-    static function makeStack (native:NativeTrace) : Array<StackItem> {
-        var result = [];
-        var count = Global.count(native);
-        for (i in 0...count) {
-            var entry = native[i];
-            var next = (i + 1 < count ? native[i + 1] : null);
-            var item = null;
-            if ((next['function']:String).indexOf('{closure}') >= 0) {
-                item = LocalFunction();
-            } else if ((next['class']:String).length > 0 && (next['function']:String).length > 0) {
-                var cls = Boot.getClass(next['class']);
-                item = Method(cls.getName(), next['function']);
-            }
-            result.push(FilePos(item, entry['file'], entry['line']));
-        }
+	static function makeStack (native:NativeTrace) : Array<StackItem> {
+		var result = [];
+		var count = Global.count(native);
+		for (i in 0...count) {
+			var entry = native[i];
+			var next = (i + 1 < count ? native[i + 1] : null);
+			var item = null;
+			if ((next['function']:String).indexOf('{closure}') >= 0) {
+				item = LocalFunction();
+			} else if ((next['class']:String).length > 0 && (next['function']:String).length > 0) {
+				var cls = Boot.getClass(next['class']);
+				item = Method(cls.getName(), next['function']);
+			}
+			result.push(FilePos(item, entry['file'], entry['line']));
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
