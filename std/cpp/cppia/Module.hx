c4825bed472b4bebdbe83f14d7cae273bdbc1155
Hugh <gamehaxe@gmail.com>
2016-09-07 14:25:00 +0800

    [cpp] Add a bit more control over cppia script running order

--- std/cpp/cppia/Module.hx
+++ std/cpp/cppia/Module.hx
@@ -19,41 +19,15 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
- package cpp.cppia;
+package cpp.cppia;
 
 
-@:build(cpp.cppia.HostClasses.include())
-class Host
+@:native("hx::CppiaLoadedModule")
+extern class Module
 {
-   public static function run(source:String)
-   {
-      var module = Module.fromString(source);
-      module.boot();
-      module.run();
-   }
-
-   public static function runFile(filename:String)
-   {
-      run( sys.io.File.getContent(filename) );
-   }
-
-
-   public static function main()
-   {
-      var script = Sys.args()[0];
-      #if (!scriptable && !doc_gen)
-      #error "Please define scriptable to use cppia"
-      #end
-      if (script==null)
-      {
-         Sys.println("Usage : Cppia scriptname");
-      }
-      else
-      {
-         var source = sys.io.File.getContent(script);
-         var module = Module.fromString(source);
-         module.boot();
-         module.run();
-      }
-   }
+   @:native("__scriptable_cppia_from_string")
+   public static function fromString(sourceCode:String) : Module;
+   public function boot():Void;
+   public function run():Void;
 }
+
