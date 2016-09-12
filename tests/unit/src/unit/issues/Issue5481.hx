8872b4402a8269c5ab35a5e7458b3f0004ea2a2a
Dan Korostelev <nadako@gmail.com>
2016-09-12 23:36:07 +0300

    [cs] add null check in Runtime.slowSetField (closes #5481)

---
+++ tests/unit/src/unit/issues/Issue5481.hx
@@ -0,0 +1,15 @@
+package unit.issues;
+
+@:nativeGen
+private class Some {
+	public var s:String;
+	public function new() s = "hello";
+}
+
+class Issue5481 extends Test {
+	function test() {
+		var o = new Some();
+		Reflect.setField(o, "s", null);
+		eq(o.s, null);
+	}
+}
