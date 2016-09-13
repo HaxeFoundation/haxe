b549c876f991ddd66a57a5e3a9a1e11aeeb2a125
Nicolas Cannasse <ncannasse@gmail.com>
2016-09-14 01:49:34 +0200

    added hl inflate support, added $unref

--- src/generators/genhl.ml
+++ src/generators/genhl.ml
@@ -1822,6 +1822,17 @@ and eval_expr ctx e =
 			let rv = eval_to ctx e2 (match rtype ctx r with HRef t -> t | _ -> invalid()) in
 			op ctx (OSetref (r,rv));
 			r
+		| "$unref", [e1] ->
+			let rec loop e = match e.eexpr with
+				| TParenthesis e1 | TMeta(_,e1) | TCast(e1,None) -> loop e1
+				| TLocal v -> v
+				| _ -> invalid()
+			in
+			let v = loop e1 in
+			let r = alloc_reg ctx v in
+			let out = alloc_tmp ctx (match rtype ctx r with HRef t -> t | _ -> invalid()) in
+			op ctx (OUnref (out,r));
+			out
 		| "$ttype", [v] ->
 			let r = alloc_tmp ctx HType in
 			op ctx (OType (r,to_type ctx v.etype));
