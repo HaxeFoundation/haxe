136450d5538fb63f01a4dac556d986c31ab61c59
Justin Donaldson <jdonaldson@gmail.com>
2016-09-22 18:15:08 -0700

    add new unit test for input/output encoding

---
+++ tests/unit/src/unit/issues/Issue5693.hx
@@ -0,0 +1,16 @@
+package unit.issues;
+import haxe.io.*;
+class Issue5693 extends Test{
+	function test(){
+		var original:Float = 0.012755102040816;
+		var btOutput:BytesOutput = new BytesOutput();
+		btOutput.writeDouble(original);
+
+		var byteArray:Bytes = btOutput.getBytes();
+
+		var btInput:BytesInput = new BytesInput(byteArray);
+		var copyVal:Float = btInput.readDouble();
+
+		eq(original, copyVal);
+	}
+}
