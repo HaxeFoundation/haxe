f98ace3deffba8398aa818dcbd222783e4dadd36
Justin Donaldson <jdonaldson@gmail.com>
2016-09-22 20:33:00 -0700

    [lua] make the TableTool addfeature a no-op in the output

--- std/lua/TableTools.hx
+++ std/lua/TableTools.hx
@@ -10,6 +10,6 @@ extern class TableTools {
 	public static function unpack<Int,V>(args:lua.Table<Int,V>, ?min : Int, ?max : Int) : Dynamic;
 	public static function maxn(t:Table.AnyTable) : Int;
 	public static function __init__() : Void {
-	  untyped _hx_table = __define_feature__("use._hx_table", _hx_table);
+	   untyped __define_feature__("use._hx_table", null);
 	}
 }
