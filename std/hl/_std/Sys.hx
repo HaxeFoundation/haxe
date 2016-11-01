d32dd16f1b14bbdfc8c1ccc0896f30981b00e5f2
Nicolas Cannasse <ncannasse@gmail.com>
2016-11-01 20:19:21 +0100

    fix Sys.getEnv (close #5782)

--- std/hl/_std/Sys.hx
+++ std/hl/_std/Sys.hx
@@ -71,7 +71,7 @@ class Sys {
 	}
 
 	public static function getEnv( s : String ) : String {
-		var v = get_env(s.bytes);
+		var v = get_env(getPath(s.bytes));
 		if( v == null )
 			return null;
 		return makePath(v);
