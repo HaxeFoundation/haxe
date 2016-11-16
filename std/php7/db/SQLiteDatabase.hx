59df121f8b4ac9c1429b0aa371a83591c0b5174a
Alexander Kuzmenko <alex@stablex.ru>
2016-11-17 01:24:49 +0300

    [php7] SQLite externs

--- std/php7/db/SQLiteDatabase.hx
+++ std/php7/db/SQLiteDatabase.hx
@@ -1,6 +1,7 @@
 package php7.db;
 
 import php7.*;
+import haxe.Constraints;
 
 /**
     @see http://php.net/manual/en/ref.sqlite.php
@@ -12,4 +13,12 @@ extern class SQLiteDatabase {
     function queryExec( query:String, ?error_msg:Ref<String> ) : Bool;
     function arrayQuery( query:String, ?result_type:Int, decode_binary:Bool = true) : NativeArray;
     function singleQuery( query:String, ?first_row_only:Bool, ?decode_binary:Bool ) : NativeArray;
+    // function unbufferedQuery( query:String, ?result_type:Int, ?error_msg:Ref<String> ) : SQLiteUnbuffered;
+    function lastInsertRowid() : Int;
+    function changes() : Int;
+    function createAggregate( function_name:String, step_func:Function, finalize_func:Function, num_args:Int = -1 ) : Void;
+    function createFunction( function_name:String, callback:Function, num_args:Int = -1 ) : Void;
+    function busyTimeout( milliseconds:Int ) : Void;
+    function lastError() : Int;
+    function fetchColumnTypes( table_name:String, ?result_type:Int ) : NativeArray;
 }
