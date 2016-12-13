a9f40fe6103d1c63e1be98cec5c99cdc7c2ffc9d
Simon Krajewski <simon@haxe.org>
2016-12-13 12:05:29 +0100

    fix metadata completion showing up in trace arguments (closes #5775)

--- src/typing/typer.ml
+++ src/typing/typer.ml
@@ -4064,7 +4064,7 @@ and type_call ctx e el (with_type:with_type) p =
 		if Common.defined ctx.com Define.NoTraces then
 			null ctx.t.tvoid p
 		else
-		let mk_to_string_meta e = EMeta((Meta.ToString,[],pos e),e),pos e in
+		let mk_to_string_meta e = EMeta((Meta.ToString,[],null_pos),e),pos e in
 		let params = (match el with [] -> [] | _ -> [("customParams",null_pos),(EArrayDecl (List.map mk_to_string_meta el) , p)]) in
 		let infos = mk_infos ctx p params in
 		if (platform ctx.com Js || platform ctx.com Python) && el = [] && has_dce ctx.com then
