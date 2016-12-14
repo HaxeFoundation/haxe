2fc83d6b0aa234d10fc04266c0d8376f727ab581
Alexander Kuzmenko <alex@stablex.ru>
2016-12-14 23:47:52 +0300

    Fixed exception thrown by `Reflect.fields(o)` when `o` is `Class<T>` (fixes #5608) (#5884)
    * [php] test case for #5608
    
    * [php7] do not throw on Reflect.fields(o) when o is a Class (fixes #5608)

---
+++ tests/unit/src/unit/issues/Issue5608.hx
@@ -0,0 +1,11 @@
+package unit.issues;
+
+class Issue5608 extends unit.Test {
+	var fields:Array<String>;
+
+	function test() {
+		//should not throw
+		fields = Reflect.fields(haxe.remoting.HttpConnection);
+		t(true);
+	}
+}
