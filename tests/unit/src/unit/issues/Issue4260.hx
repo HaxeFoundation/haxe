f173b2c6abd37c790f1924491a9d184e2f573946
Simon Krajewski <simon@haxe.org>
2016-12-15 10:41:05 +0100

    fix indentation (skip ci)

--- tests/unit/src/unit/issues/Issue4260.hx
+++ tests/unit/src/unit/issues/Issue4260.hx
@@ -6,7 +6,7 @@ class Issue4260 extends unit.Test {
 	var nullValue:Null<Float>;
 
 	function test() {
-        t(intValue == floatValue);
+		t(intValue == floatValue);
 		f(intValue == nullValue);
 		f(floatValue == nullValue);
 	}
