bffc775f14a8542763102633134a551fb7730d74
Nicolas Cannasse <ncannasse@gmail.com>
2016-12-15 10:02:57 +0100

    use another class which does not reference Http (fix HL interpreter not supporting Socket for now)

--- tests/unit/src/unit/issues/Issue5608.hx
+++ tests/unit/src/unit/issues/Issue5608.hx
@@ -5,7 +5,7 @@ class Issue5608 extends unit.Test {
 
 	function test() {
 		//should not throw
-		fields = Reflect.fields(haxe.remoting.HttpConnection);
+		fields = Reflect.fields(haxe.Serializer);
 		t(true);
 	}
 }
