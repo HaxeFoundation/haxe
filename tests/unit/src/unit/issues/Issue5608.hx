2a8d80aa26079f9c262ba7dd3ca7c6d1a5f8deb0
Alexander Kuzmenko <alex@stablex.ru>
2016-12-15 22:08:31 +0300

    [php] reduce test case for #5608

--- tests/unit/src/unit/issues/Issue5608.hx
+++ tests/unit/src/unit/issues/Issue5608.hx
@@ -1,11 +1,13 @@
 package unit.issues;
 
-class Issue5608 extends unit.Test {
+class Issue5608 extends unit.Test implements Dynamic<Bool> {
 	var fields:Array<String>;
 
 	function test() {
 		//should not throw
-		fields = Reflect.fields(haxe.Serializer);
+		fields = Reflect.fields(Issue5608);
 		t(true);
 	}
+
+	public function resolve( name ) return true;
 }
