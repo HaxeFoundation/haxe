82dbba78a894ca1162a59f082cb13bf2b7edc287
Alexander Kuzmenko <alex@stablex.ru>
2016-12-15 12:38:47 +0300

    fix Issue4260 for static targets

--- tests/unit/src/unit/issues/Issue4260.hx
+++ tests/unit/src/unit/issues/Issue4260.hx
@@ -3,7 +3,7 @@ package unit.issues;
 class Issue4260 extends unit.Test {
 	var floatValue:Float = 0.0;
 	var intValue:Float = 0;
-	var nullValue:Float;
+	var nullValue:Null<Float>;
 
 	function test() {
         t(intValue == floatValue);
