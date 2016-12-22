d8acad664241993e7555820f7e84208b22145e77
Alexander Kuzmenko <alex@stablex.ru>
2016-12-23 01:41:07 +0300

    [php7] non-blocking Process.exitCode(false)

--- std/php7/_std/sys/io/Process.hx
+++ std/php7/_std/sys/io/Process.hx
@@ -157,13 +157,11 @@ class Process {
 		return pid;
 	}
 
-	/**
-		Block until the process exits and return the exit code of the process.
-		If the process has already exited, return the exit code immediately.
-	*/
-	public function exitCode( block : Bool = true ) : Int {
-		if( block == false ) throw "Non blocking exitCode() not supported on this platform";
-
+	public function exitCode( block : Bool = true ) : Null<Int> {
+		if(!block) {
+			updateStatus();
+			return (running ? null : _exitCode);
+		}
 		while (running) {
 			var arr = Syntax.arrayDecl(process);
 			Syntax.suppress(Global.stream_select(arr, arr, arr, null));
