e2b7ad62eb5d7fba685967a63935359e18c8a937
Mark Knol <mark.knol@gmail.com>
2017-01-09 14:53:14 +0100

    set copyright to 2017

--- src/generators/gencpp.ml
+++ src/generators/gencpp.ml
@@ -1,6 +1,6 @@
 (*
    The Haxe Compiler
-   Copyright (C) 2005-2016  Haxe Foundation
+   Copyright (C) 2005-2017  Haxe Foundation
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
