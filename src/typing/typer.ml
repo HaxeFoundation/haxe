d3955c6796ab8e400dc7a36e3713b9a80d60261a
Simon Krajewski <simon@haxe.org>
2017-01-30 13:30:33 +0100

    don't whine about arrays of mixed types in display mode (closes #5972)

--- src/typing/typer.ml
+++ src/typing/typer.ml
@@ -3315,7 +3315,7 @@ and type_array_decl ctx el with_type p =
 		let t = try
 			unify_min_raise ctx el
 		with Error (Unify l,p) ->
-			if ctx.untyped then t_dynamic else begin
+			if ctx.untyped || ctx.com.display.dms_error_policy = EPIgnore then t_dynamic else begin
 				display_error ctx "Arrays of mixed types are only allowed if the type is forced to Array<Dynamic>" p;
 				raise (Error (Unify l, p))
 			end
