a34a00a17887a431d4d8445ae412fb1c1009f9eb
Justin Donaldson <jdonaldson@gmail.com>
2017-02-05 23:32:46 -0800

    fix Issue6007 for other platforms

--- tests/unit/src/unit/issues/Issue6007.hx
+++ tests/unit/src/unit/issues/Issue6007.hx
@@ -3,11 +3,9 @@ package unit.issues;
 class Issue6007 extends Test {
 	function test() {
 		// test anonymous type with borrowed iterator
-        var h = new haxe.ds.IntMap<Null<Int>>();
-        h.set(0, -1);
-        h.set(-4815, 8546);
-        var k = Lambda.array({iterator : h.keys});
-        eq("[0,-4815]", Std.string(k));
+        var a = [0,4];
+        var k = Lambda.array({iterator : a.iterator});
+        eq("[0,4]", Std.string(k));
 
 		// make sure instance body isn't executed twice
 		var k = [];
