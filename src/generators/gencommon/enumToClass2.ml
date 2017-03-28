961dce773d538ba27b32da00ecb58d94bb2005d5
Dan Korostelev <nadako@gmail.com>
2017-03-28 12:14:17 +0300

    some cleanup for EnumToClass2 in preparation for java support (and extracting from gencommon)

--- src/generators/gencommon/enumToClass2.ml
+++ src/generators/gencommon/enumToClass2.ml
@@ -22,28 +22,30 @@ open Codegen
 open Type
 open Gencommon
 
+let add_static c cf =
+	c.cl_statics <- PMap.add cf.cf_name cf c.cl_statics;
+	c.cl_ordered_statics <- cf :: c.cl_ordered_statics
+
+let add_field c cf override =
+	c.cl_fields <- PMap.add cf.cf_name cf c.cl_fields;
+	c.cl_ordered_fields <- cf :: c.cl_ordered_fields;
+	if override then c.cl_overrides <- cf :: c.cl_overrides
+
+let add_meta com en cl_enum =
+	Option.may (fun expr ->
+		let cf_meta = mk_field "__meta__" expr.etype expr.epos expr.epos in
+		cf_meta.cf_expr <- Some expr;
+		add_static cl_enum cf_meta;
+	) (Codegen.build_metadata com (TEnumDecl en));
+
 type enclasses = {
 	base : tclass;
 	ctors : (string, tclass) PMap.t;
 }
 
-let name = "enum_to_class2"
-let priority = solve_deps name []
-
-module EnumToClass2Modf =
-struct
-
+module EnumToClass2Modf = struct
 	let name = "enum_to_class2_mod"
 	let priority = solve_deps name [DBefore ReflectionCFs.priority; DBefore RealTypeParams.RealTypeParamsModf.priority]
-
-	let add_static c cf =
-		c.cl_statics <- PMap.add cf.cf_name cf c.cl_statics;
-		c.cl_ordered_statics <- cf :: c.cl_ordered_statics
-
-	let add_field c cf override =
-		c.cl_fields <- PMap.add cf.cf_name cf c.cl_fields;
-		c.cl_ordered_fields <- cf :: c.cl_ordered_fields;
-		if override then c.cl_overrides <- cf :: c.cl_overrides
 
 	let convert gen ec_tbl base_class en =
 		let pos = en.e_pos in
@@ -52,26 +54,24 @@ struct
 		let cl_enum = mk_class en.e_module en.e_path pos in
 		cl_enum.cl_super <- Some (base_class,[]);
 		cl_enum.cl_extern <- en.e_extern;
-		cl_enum.cl_module <- en.e_module;
 		cl_enum.cl_meta <- [(Meta.Enum,[],pos); (Meta.NativeGen,[],pos)] @ cl_enum.cl_meta;
 
 		(* mark the enum that it's generated as a class *)
 		en.e_meta <- (Meta.Class,[],pos) :: en.e_meta;
 
 		(* add metadata *)
-		Option.may (fun expr ->
-			let cf_meta = mk_class_field "__meta__" expr.etype false expr.epos (Var { v_read = AccNormal; v_write = AccNormal }) [] in
-			cf_meta.cf_expr <- Some expr;
-			add_static cl_enum cf_meta;
-		) (Codegen.build_metadata gen.gcon (TEnumDecl en));
+		add_meta gen.gcon en cl_enum;
 
 		let basic = gen.gcon.basic in
+		let mk_array_decl t el p = mk_nativearray_decl gen t el p in
 
 		(* add constructs field (for reflection) *)
 		if has_feature gen.gcon "Type.getEnumConstructs" then begin
-			let cf_constructs = mk_class_field "__hx_constructs" (gen.gclasses.nativearray basic.tstring) true pos (Var { v_read = AccNormal; v_write = AccNever }) [] in
+			let e_constructs = mk_array_decl basic.tstring (List.map (fun s -> ExprBuilder.make_string gen.gcon s pos) en.e_names) pos in
+			let cf_constructs = mk_field "__hx_constructs" e_constructs.etype pos pos in
+			cf_constructs.cf_kind <- Var { v_read = AccNormal; v_write = AccNever };
 			cf_constructs.cf_meta <- (Meta.ReadOnly,[],pos) :: (Meta.Protected,[],pos) :: cf_constructs.cf_meta;
-			cf_constructs.cf_expr <- Some (mk_nativearray_decl gen basic.tstring (List.map (fun s -> ExprBuilder.make_string gen.gcon s pos) en.e_names) pos);
+			cf_constructs.cf_expr <- Some e_constructs;
 			add_static cl_enum cf_constructs
 		end;
 
@@ -115,10 +115,10 @@ struct
 					in
 					let params, ret = get_fun ef_type in
 
-					let cl_ctor_t = TInst(cl_ctor,[]) in
+					let cl_ctor_t = TInst (cl_ctor,[]) in
 					let other_en_v = alloc_var "en" cl_ctor_t in
-					let other_en_local = mk (TLocal other_en_v) cl_ctor_t pos in
-					let mk_bool b = mk (TConst(TBool b)) basic.tbool pos in
+					let other_en_local = mk_local other_en_v pos in
+					let mk_bool b = ExprBuilder.make_bool gen.gcon b pos in
 					let enumeq = mk_static_field_access_infer (get_cl (get_type gen ([],"Type"))) "enumEq" pos [t_dynamic] in
 					let refeq = mk_static_field_access_infer (get_cl (get_type gen (["System"],"Object"))) "ReferenceEquals" pos [] in
 
@@ -126,7 +126,7 @@ struct
 					let assigns = ref [] in
 					let ctor_args = ref [] in
 					let static_ctor_args = ref [] in
-					let ethis = mk (TConst TThis) (TInst(cl_ctor, [])) pos in
+					let ethis = mk (TConst TThis) cl_ctor_t pos in
 					List.iter (fun (n,_,t) ->
 						let cf_param = mk_class_field n t true pos (Var { v_read = AccNormal; v_write = AccNever }) [] in
 						cf_param.cf_meta <- (Meta.ReadOnly,[],pos) :: cf_param.cf_meta;
@@ -135,7 +135,7 @@ struct
 						let ctor_arg_v = alloc_var n t in
 						let static_ctor_arg_v = alloc_var n t in
 
-						let ctor_arg_local = mk (TLocal ctor_arg_v) t pos in
+						let ctor_arg_local = mk_local ctor_arg_v pos in
 						let efield = mk (TField (ethis, FInstance(cl_ctor, [], cf_param))) t pos in
 						let assign = Codegen.binop OpAssign efield ctor_arg_local t pos in
 
@@ -156,7 +156,7 @@ struct
 
 					assigns := (mk (TCall(esuper,[ExprBuilder.make_int gen.gcon index pos])) basic.tvoid pos) :: !assigns;
 
-					let cf_ctor_t = TFun(params,basic.tvoid) in
+					let cf_ctor_t = TFun (params, basic.tvoid) in
 					let cf_ctor = mk_class_field "new" cf_ctor_t true pos (Method MethNormal) [] in
 					cf_ctor.cf_expr <- Some {
 						eexpr = TFunction {
@@ -169,16 +169,16 @@ struct
 					};
 					cl_ctor.cl_constructor <- Some cf_ctor;
 
-					let cf_toString_t = TFun([],basic.tstring) in
+					let cf_toString_t = TFun ([],basic.tstring) in
 					let cf_toString = mk_class_field "toString" cf_toString_t true pos (Method MethNormal) [] in
 
-					let etoString_args = mk_nativearray_decl gen t_dynamic !param_arr pos in
+					let etoString_args = mk_array_decl t_dynamic !param_arr pos in
 					cf_toString.cf_expr <- Some {
 						eexpr = TFunction {
 							tf_args = [];
 							tf_type = basic.tstring;
 							tf_expr = mk_block (mk_return (
-								mk (TCall(eparamsToString, [etag;etoString_args])) basic.tstring pos
+								mk (TCall (eparamsToString, [etag; etoString_args])) basic.tstring pos
 							));
 						};
 						etype = cf_toString_t;
@@ -201,8 +201,8 @@ struct
 					(* add Equals field *)
 					begin
 						let other_v = alloc_var "other" t_dynamic in
-						let eother_local = mk (TLocal other_v) t_dynamic pos in
-						let eas = mk (TLocal(alloc_var "__as__" t_dynamic)) t_dynamic pos in
+						let eother_local = mk_local other_v pos in
+						let eas = mk_local (alloc_var "__as__" t_dynamic) pos in
 						let ecast = mk (TCall(eas,[eother_local])) cl_ctor_t pos in
 
 						let equals_exprs = ref (List.rev [
@@ -330,10 +330,9 @@ struct
 end;;
 
 
-module EnumToClass2Exprf =
-struct
+module EnumToClass2Exprf = struct
 	let name = "enum_to_class2_exprf"
-	let priority = solve_deps name [DBefore TArrayTransform.priority]
+	let priority = solve_deps name []
 
 	let configure gen ec_tbl =
 		let rec run e =
@@ -362,7 +361,7 @@ struct
 
 				let cl_ctor = PMap.find ef.ef_name classes.ctors in
 				let cl_ctor_t = TInst(cl_ctor, []) in
-				let eas = mk (TLocal(alloc_var "__as__" t_dynamic)) t_dynamic f.epos in
+				let eas = mk_local (alloc_var "__as__" t_dynamic) f.epos in
 				let ecast = mk (TCall(eas,[f])) cl_ctor_t f.epos in
 
 				(match ef.ef_type with
