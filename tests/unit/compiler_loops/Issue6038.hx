5d633f120f0a889c110918c58da5ebc8df05fe5c
Nicolas Cannasse <ncannasse@gmail.com>
2017-04-24 16:35:02 +0200

    added

---
+++ tests/unit/compiler_loops/Issue6038.hx
@@ -0,0 +1,14 @@
+class Promise<T> {
+    function then<TOut>():Promise<TOut> { return null; }
+}
+
+typedef Thenable<T> = {
+    function then():Thenable<T>;
+}
+
+class Issue6038 {
+    static function main() {
+        var p:Promise<Int> = null;
+        var t:Thenable<Int> = p;
+    }
+}
