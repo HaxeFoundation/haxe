315b26b177af8cb4650f9344ee37db9976791003
Johann Borck <ousado@tuta.io>
2017-04-27 17:32:49 +0200

    add some display tests for arrow functions

---
+++ tests/display/src/cases/ArrowFunctions.hx
@@ -0,0 +1,37 @@
+package cases;
+
+class ArrowFunctions extends DisplayTestCase {
+
+    /**
+    var obj = { foo : 1 };
+    var f = () -> obj.{-1-}
+    var f = () -> {
+        [1].{-2-}
+    }
+    **/
+    @:funcCode function testBodyCompletion1(){
+        eq(true, hasField(fields(pos(1)), "foo", "Int"));
+        eq(true, hasField(fields(pos(2)), "copy", "Void -> Array<Int>"));
+    }
+
+    /**
+    enum E { EA; EB; EC; }
+    class SomeClass {
+        static function sf(){
+            (e:E) -> e.{-1-}
+        }
+    }
+    **/
+    function testBodyCompletion2(){
+        eq(true, hasField(fields(pos(1)), "getName", "Void -> String"));
+    }
+
+    /**
+    var arr = [1,2,3,4,5];
+    arr.map( a{-1-} -> a{-2-} + 1 )
+    **/
+    @:funcCode function testHover(){
+        eq("Int", type(pos(1)));
+        eq("Int", type(pos(2)));
+    }
+}
