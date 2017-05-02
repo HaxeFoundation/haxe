1861c5579a3bd1d851ff1418cce85561ec1478d8
Mario Carbajal <mario.e.carbajal@gmail.com>
2017-05-02 13:16:19 -0300

    Flatten blocks in inline constructors. Fixes #6229 (#6230)
    * When unwrapping a block, also unwrap child blocks
    
    * Test for #6229

---
+++ tests/optimization/src/issues/Issue6229.hx
@@ -0,0 +1,27 @@
+package issues;
+
+class Vec {
+    public var x:Float;
+	public var y:Float;
+    public inline function new(x) {
+        this.x = x;
+		this.y = x;
+    }
+}
+
+class Issue6229 {
+    static inline function mkVec(x) {
+		{
+			{
+				return new Vec(x);
+			}
+		}
+	}
+
+	@:js('var a = 1 + 1;')
+	@:analyzer(no_local_dce)
+    static function test() {
+        var v = mkVec(1);
+		var a = v.x + v.y;
+    }
+}
