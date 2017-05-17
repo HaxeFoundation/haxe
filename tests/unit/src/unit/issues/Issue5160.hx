123688fc38591e4e3e2609458c446977557626b8
Simon Krajewski <simon@haxe.org>
2017-05-17 15:10:28 +0200

    don't comment out what you want to test

--- tests/unit/src/unit/issues/Issue5160.hx
+++ tests/unit/src/unit/issues/Issue5160.hx
@@ -3,7 +3,7 @@ package unit.issues;
 class Issue5160 extends unit.Test {
 	function test() {
         var bytes = haxe.io.Bytes.alloc(256);
-        // bytes.setInt32(0, -1241956892);
-		// eq(-1241956892, bytes.getInt32(0));
+        bytes.setInt32(0, -1241956892);
+		eq(-1241956892, bytes.getInt32(0));
 	}
 }
