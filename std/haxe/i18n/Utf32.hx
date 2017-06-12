bc5d55e1713cd96a0ee7243634f75c61841d867d
frabbit <frabbit77@googlemail.com>
2017-06-12 12:34:07 +0200

    use native string for utf32 impl on python

--- std/haxe/i18n/Utf32.hx
+++ std/haxe/i18n/Utf32.hx
@@ -23,6 +23,185 @@ package haxe.i18n;
 
 import haxe.i18n.Tools;
 
+#if python
+
+@:allow(haxe.i18n)
+abstract Utf32(String) {
+
+	public var length(get,never) : Int;
+
+	public inline function new(str:String) : Void {
+		this = str;
+	}
+
+	inline function get_length():Int {
+		return this.length;
+	}
+
+	public inline function toUpperCase() : Utf32 {
+		return new Utf32(this.toUpperCase());
+	}
+
+	public inline function toLowerCase() : Utf32 {
+		return new Utf32(this.toLowerCase());
+	}
+
+	public inline function charAt(index : Int) : Utf32 {
+		return new Utf32(this.charAt(index));
+	}
+
+	public inline function charCodeAt( index : Int) : Null<Int> {
+		return this.charCodeAt(index);
+	}
+
+	public inline function fastCodeAt( index : Int) : Int {
+		return StringTools.fastCodeAt(this, index);
+	}
+
+	public inline function indexOf( str : Utf32, ?startIndex : Int ) : Int {
+		return this.indexOf(str.impl(),startIndex);
+	}
+
+	public inline function lastIndexOf( str : Utf32, ?startIndex : Int ) : Int {
+		if (startIndex == null) { // required for flash
+			return this.lastIndexOf(str.impl());
+		}
+		return this.lastIndexOf(str.impl(), startIndex);
+	}
+
+	public inline function split( delimiter : Utf32 ) : Array<Utf32> {
+		return cast this.split(delimiter.impl());
+	}
+
+	public function substr( pos : Int, ?len : Int ) : Utf32 {
+		if (len == null) return new Utf32(this.substr(pos)); // required for flash
+		return new Utf32(this.substr(pos,len));
+	}
+
+	public function substring( startIndex : Int, ?endIndex : Int ) : Utf32 {
+		if (endIndex == null) return new Utf32(this.substring(startIndex));
+		return new Utf32(this.substring(startIndex,endIndex));
+	}
+
+	public inline function toNativeString() : String {
+		return this;
+	}
+
+	public static inline function fromCharCode( code : Int ) : Utf32 {
+		return new Utf32(nativeStringfromCharCode(code));
+	}
+
+	static inline function nativeStringfromCharCode( code : Int ) : String {
+		return String.fromCharCode(code);
+	}
+
+	public function toBytes(  ) : haxe.io.Bytes {
+		var res = haxe.io.Bytes.alloc(this.length << 2);
+		for (i in 0...this.length) {
+			res.setInt32(i << 2, fastCodeAt(i));
+		}
+		return res;
+	}
+
+	public static inline function fromBytes( bytes : haxe.io.Bytes ) : Utf32 {
+		return fromByteAccess(ByteAccess.fromBytes(bytes));
+	}
+
+	public static function fromByteAccess (ba:ByteAccess):Utf32 {
+		var res = "";
+		var i = 0;
+		while (i < ba.length) {
+			res += String.fromCharCode(ba.getInt32(i));
+			i+=4;
+		}
+		return new Utf32(res);
+	}
+
+	@:op(A == B) inline function opEq (other:Utf32) {
+		return this == other.impl();
+	}
+
+	@:op(A + B) inline function opAdd (other:Utf32) {
+		return fromImpl(this + other.impl());
+	}
+
+	@:op(A != B) inline function opNotEq (other:Utf32) {
+		return !opEq(other);
+	}
+
+	public inline function toUtf8() : Utf8 {
+		return toUtf16().toUtf8();
+	}
+
+	public inline function toUtf16() : Utf16 {
+		var ba = NativeStringTools.toUtf16ByteAccess(this);
+		return Utf16.fromByteAccess(ba);
+	}
+
+	public inline function toUcs2() : Ucs2 {
+		return toUtf16().toUcs2();
+	}
+
+	static inline function fromImpl (str:String):Utf32 {
+		return cast str;
+	}
+
+	inline function impl() : String {
+		return this;
+	}
+
+	inline function eachCode ( f : Int -> Void) {
+		for (i in 0...length) {
+			var code = fastCodeAt(i);
+			f(code);
+		}
+	}
+
+	public inline function getReader ():Utf32Reader {
+		return new Utf32Reader(this);
+	}
+
+	public function toCodeArray ():Array<Int> {
+		var res = [];
+		eachCode(function (c) res.push(c));
+		return res;
+	}
+
+	@:op(A > B) inline function opGreaterThan (other:Utf32) {
+		return this > other.impl();
+	}
+
+	@:op(A < B) inline function opLessThan (other:Utf32) {
+		return this < other.impl();
+	}
+
+	@:op(A <= B) inline function opLessThanOrEq (other:Utf32) {
+		return this <= other.impl();
+	}
+
+	@:op(A >= B) inline function opGreaterThanOrEq (other:Utf32) {
+		return this >= other.impl();
+	}
+}
+
+abstract Utf32Reader(String) {
+	public inline function new (s:String) {
+		this = s;
+	}
+
+	public var length(get, never):Int;
+
+	inline function get_length () {
+		return this.length;
+	}
+
+	public inline function getInt32 (pos:Int) {
+		return StringTools.fastCodeAt(this, pos);
+	}
+}
+
+#else
+
 import haxe.i18n.ByteAccess;
 
 private typedef Utf32Impl = haxe.ds.Vector<Int>;
@@ -511,3 +690,5 @@ private class Utf32Tools {
 		return true;
 	}
 }
+
+#end
