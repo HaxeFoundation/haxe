ae7463daf912835f4aab46f7623671fa0c020a7c
Dan Korostelev <nadako@gmail.com>
2017-06-18 13:56:31 +0300

    add test

---
+++ tests/unit/src/unit/issues/Issue6375.hx
@@ -0,0 +1,32 @@
+package unit.issues;
+
+class Issue6375 extends Test {
+	var memberField(default,set):Int;
+	static var staticField(default,set):Int;
+
+	static var rollback:Void->Void;
+
+	function set_memberField(_) {
+		memberField = 1;
+		rollback = () -> memberField = 3;
+		return memberField;
+	}
+
+	static function set_staticField(_) {
+		staticField = 2;
+		rollback = () -> staticField = 4;
+		return staticField;
+	}
+
+	function test() {
+		memberField = 42;
+		eq(memberField, 1);
+		rollback();
+		eq(memberField, 3);
+
+		staticField = 42;
+		eq(staticField, 2);
+		rollback();
+		eq(staticField, 4);
+	}
+}
