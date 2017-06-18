d0f118d3206bd4d102f1f5f9de15e8825b1ec7c6
Cauê Waneck <waneck@gmail.com>
2017-06-18 01:12:29 -0300

    Fix test

--- tests/unit/src/unit/issues/Issue5751.hx
+++ tests/unit/src/unit/issues/Issue5751.hx
@@ -1,13 +1,9 @@
 package unit.issues;
 
 class Issue5751 extends Test {
-#if (java || cs)
+#if cs
   public function test() {
-#if java
-    var arr = @:privateAccess (new Array(new java.NativeArray(10)) : Array<Null<Int>>);
-#elseif cs
     var arr = @:privateAccess (new Array(new cs.NativeArray(10)) : Array<Null<Int>>);
-#end
     eq(arr[0], null);
     arr[0] = 100;
     eq(arr[0], 100);
