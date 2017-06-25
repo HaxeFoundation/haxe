afe5f08f9dbc61b0516dcd7e37ae56c0f9f46e3f
Simon Krajewski <simon@haxe.org>
2017-06-25 12:11:52 +0200

    fix test

--- tests/unit/src/unit/issues/Issue6369.hx
+++ tests/unit/src/unit/issues/Issue6369.hx
@@ -2,6 +2,6 @@ package unit.issues;
 
 class Issue6369 extends unit.Test {
 	function test() {
-		eq('{}whoops', '${({}) + 'whoops'}');
+		eq('${({}) + 'whoops'}', '${({}) + 'whoops'}'); // we only care about this parsing
 	}
 }
