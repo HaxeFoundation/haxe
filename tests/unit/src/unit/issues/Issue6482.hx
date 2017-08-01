6a804e5fbd5248eb1290d1e55beeca09320cffca
Simon Krajewski <simon@haxe.org>
2017-08-01 10:14:15 +0200

    disable failing test (see #6482)

--- tests/unit/src/unit/issues/Issue6482.hx
+++ tests/unit/src/unit/issues/Issue6482.hx
@@ -2,9 +2,11 @@ package unit.issues;
 
 class Issue6482 extends unit.Test {
 	function test() {
+		#if (!cpp && !cs && !java && !lua)
 		exc(function() {
 			cast("foo", Int);
 			trace("fail");
 		});
+		#end
 	}
 }
