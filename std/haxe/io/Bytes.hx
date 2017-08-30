74fe2cae4c61d6a403f88c3022518ede51b6e12b
Nicolas Cannasse <ncannasse@gmail.com>
2017-08-30 18:21:29 +0200

    fixed generic getFloat to use FPHelper

--- std/haxe/io/Bytes.hx
+++ std/haxe/io/Bytes.hx
@@ -239,8 +239,7 @@ class Bytes {
 		if( pos < 0 || pos + 4 > length ) throw Error.OutsideBounds;
 		return untyped __global__.__hxcpp_memory_get_float(b,pos);
 		#else
-		var b = new haxe.io.BytesInput(this,pos,4);
-		return b.readFloat();
+		return FPHelper.i32ToFloat(getInt32(pos));
 		#end
 	}
 
