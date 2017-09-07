1cd4a7c1f73e7b64ecf916fad881120deb02cb58
Simon Krajewski <simon@haxe.org>
2017-09-07 11:04:12 +0200

    [tests] dodge C# issue (see #6561)

--- tests/unit/src/unit/issues/Issue6561.hx
+++ tests/unit/src/unit/issues/Issue6561.hx
@@ -1,18 +1,18 @@
 package unit.issues;
 
 private enum Log<A> {
-	Log(msg:String):Log<String>;
+	NotLog(msg:String):Log<String>;
 }
 
 
 class Issue6561 extends unit.Test {
 	function test() {
-		eq("hello", apply(Log("hello")));
+		eq("hello", apply(NotLog("hello")));
 	}
 
   	static function apply<A>(f:Log<A>):A {
 		return switch f {
-			case Log(msg):
+			case NotLog(msg):
       			msg;
 		}
   	}
