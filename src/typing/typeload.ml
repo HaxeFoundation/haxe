3e07b0bf957fe5b1865695ca911407b473fbd2af
Nicolas Cannasse <ncannasse@gmail.com>
2017-09-08 17:13:28 +0200

    better fix for #6505, do not flush if we are in module build (causes assert in typeload)

--- src/typing/typeload.ml
+++ src/typing/typeload.ml
@@ -4035,6 +4035,7 @@ let build_instance ctx mtype p =
 				let tf = (f()) in
 				unify_raise ctx tf t p;
 				link_dynamic t tf;
+				if ctx.pass >= PBuildClass then flush_pass ctx PBuildClass "after_build_instance";
 				t
 			) s in
 			TLazy r
