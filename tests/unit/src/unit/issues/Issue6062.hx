93270b0789db7a2e1d2991c3b64343589b73bebb
Alexander Kuzmenko <alex@stablex.ru>
2017-10-20 16:58:40 +0300

    improved test case (#6062)

--- tests/unit/src/unit/issues/Issue6062.hx
+++ tests/unit/src/unit/issues/Issue6062.hx
@@ -4,12 +4,22 @@ class Issue6062 extends unit.Test {
 	function test() {
 		var child = Type.createInstance(Child, []);
 		t(child.parentFn());
+		t(Parent.tmp);
 	}
 }
 
 private class Parent {
+	static public var tmp:Bool = false;
+
 	public var parentFn = function() return true;
-	function new() {}
+
+	function new() {
+		tmp = OnlyParentNewReferencesMe.field;
+	}
 }
 
 private class Child extends Parent {}
+
+private class OnlyParentNewReferencesMe {
+	static public var field = true;
+}
