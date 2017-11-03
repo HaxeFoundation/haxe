7fec12b8744f227507784ac31acc9a94ca43caca
frabbit <frabbit77@googlemail.com>
2017-11-03 01:01:48 +0100

    add custom anon to Issue6259, should also work

--- tests/unit/src/unit/issues/Issue6259.hx
+++ tests/unit/src/unit/issues/Issue6259.hx
@@ -4,5 +4,8 @@ class Issue6259 extends Test{
 		function f(a) return a.b;
 		var res = f({b: (s) -> s.length})("6259");
 		eq(res,4);
+
+		var res = f({b: (s) -> s.length})({ length : 10});
+		eq(res,10);
 	}
 }
