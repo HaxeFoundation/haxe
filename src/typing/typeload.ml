4867ac405bcda05393fc33bae601340abf5cdbb5
frabbit <frabbit77@googlemail.com>
2017-11-20 12:01:04 +0100

    minor

--- src/typing/typeload.ml
+++ src/typing/typeload.ml
@@ -1969,7 +1969,6 @@ let is_java_native_function meta = try
 	with | Not_found -> false
 
 let build_module_def ctx mt meta fvars context_init fbuild =
-
 	let loop (f_build,f_enum) = function
 		| Meta.Build,args,p -> (fun () ->
 				if ctx.in_macro then error "You cannot use @:build inside a macro : make sure that your type is not used in macro" p;
