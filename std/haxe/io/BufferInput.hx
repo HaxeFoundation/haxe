a2df8ed99da03a33f77bc206785141ffa2e0de79
Alexander Kuzmenko <alex@stablex.ru>
2018-01-24 14:03:37 +0300

    fixed signature of haxe.io.BufferInput.readByte()

--- std/haxe/io/BufferInput.hx
+++ std/haxe/io/BufferInput.hx
@@ -43,7 +43,7 @@ class BufferInput extends haxe.io.Input {
 		available += i.readBytes(buf, available, buf.length - available);
 	}
 
-	override function readByte() {
+	override function readByte():Int {
 		if( available == 0 ) refill();
 		var c = buf.get(pos);
 		pos++;
