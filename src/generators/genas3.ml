ccf321f8d4ad09b2f7545586c5a8655cfc3e0a36
Alexander Kuzmenko <alex@stablex.ru>
2018-02-05 17:58:10 +0300

    [as3] added "object" to keywords list

--- src/generators/genas3.ml
+++ src/generators/genas3.ml
@@ -123,7 +123,7 @@ let reserved =
 	(* we don't include get+set since they are not 'real' keywords, but they can't be used as method names *)
 	"function";"class";"var";"if";"else";"while";"do";"for";"break";"continue";"return";"extends";"implements";
 	"import";"switch";"case";"default";"static";"public";"private";"try";"catch";"new";"this";"throw";"interface";
-	"override";"package";"null";"true";"false";"void"
+	"override";"package";"null";"true";"false";"void";"object"
 	];
 	h
 
