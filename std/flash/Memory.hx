bfd210446aa7cb71926e0c2a765e5c96bad8a144
r32 <56782055@qq.com>
2018-02-14 23:10:57 +0800

    Reduce temp variables caused by "flash.Memory"

--- std/flash/Memory.hx
+++ std/flash/Memory.hx
@@ -27,56 +27,29 @@ extern class Memory {
 		flash.system.ApplicationDomain.currentDomain.domainMemory = b;
 	}
 
-	public static inline function setByte( addr : Int, v : Int ) : Void {
-		untyped __vmem_set__(0,addr,v);
-	}
+	public static function setByte( addr : Int, v : Int ) : Void;
 
-	public static inline function setI16( addr : Int, v : Int ) : Void {
-		untyped __vmem_set__(1,addr,v);
-	}
+	public static function setI16( addr : Int, v : Int ) : Void;
 
-	public static inline function setI32( addr : Int, v : Int ) : Void {
-		untyped __vmem_set__(2,addr,v);
-	}
+	public static function setI32( addr : Int, v : Int ) : Void;
 
-	public static inline function setFloat( addr : Int, v : Float ) : Void {
-		untyped __vmem_set__(3,addr,v);
-	}
+	public static function setFloat( addr : Int, v : Float ) : Void;
 
-	public static inline function setDouble( addr : Int, v : Float ) : Void {
-		untyped __vmem_set__(4,addr,v);
-	}
+	public static function setDouble( addr : Int, v : Float ) : Void;
 
-	public static inline function getByte( addr : Int ) : Int {
-		return untyped __vmem_get__(0,addr);
-	}
+	public static function getByte( addr : Int ) : Int;
 
-	public static inline function getUI16( addr : Int ) : Int {
-		return untyped __vmem_get__(1,addr);
-	}
+	public static function getUI16( addr : Int ) : Int;
 
-	public static inline function getI32( addr : Int ) : Int {
-		return untyped __vmem_get__(2,addr);
-	}
+	public static function getI32( addr : Int ) : Int;
 
-	public static inline function getFloat( addr : Int ) : Float {
-		return untyped __vmem_get__(3,addr);
-	}
+	public static function getFloat( addr : Int ) : Float;
 
-	public static inline function getDouble( addr : Int ) : Float {
-		return untyped __vmem_get__(4,addr);
-	}
+	public static function getDouble( addr : Int ) : Float;
 
-	public static inline function signExtend1( v : Int ) : Int {
-		return untyped __vmem_sign__(0,v);
-	}
+	public static function signExtend1( v : Int ) : Int;
 
-	public static inline function signExtend8( v : Int ) : Int {
-		return untyped __vmem_sign__(1,v);
-	}
+	public static function signExtend8( v : Int ) : Int;
 
-	public static inline function signExtend16( v : Int ) : Int {
-		return untyped __vmem_sign__(2,v);
-	}
-
+	public static function signExtend16( v : Int ) : Int;
 }
