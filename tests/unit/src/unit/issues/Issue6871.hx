90272e4b1ac3977f8820b0475d8572ae0686a058
Alexander Kuzmenko <alex@stablex.ru>
2018-02-26 22:47:36 +0300

    [flash] don't swallow exceptions thrown in getters/setters when invoked with Reflect methods (fixes #6871, fixes #5460)

---
+++ tests/unit/src/unit/issues/Issue6871.hx
@@ -0,0 +1,26 @@
+package unit.issues;
+
+class Issue6871 extends unit.Test {
+	static inline var GETTER_ERROR = 'getter exception';
+	static inline var SETTER_ERROR = 'setter exception';
+
+	@:keep static public var field(get,set):Int;
+	static function get_field():Int throw GETTER_ERROR;
+	static function set_field(v:Int):Int throw SETTER_ERROR;
+
+	function test() {
+		try {
+			Reflect.getProperty(Issue6871, 'field');
+			t(false);
+		} catch(e:Dynamic) {
+			eq(GETTER_ERROR, e);
+		}
+
+		try {
+			Reflect.setProperty(Issue6871, 'field', 123);
+			t(false);
+		} catch(e:Dynamic) {
+			eq(SETTER_ERROR, e);
+		}
+	}
+}
