a016cc85ada89c95bfb38e595f5dfb98fb494816
Simon Krajewski <simon@haxe.org>
2018-05-11 23:10:34 +0200

    [display] properly deal with macro context
    * remove special `#if macro` handling
    * if we come across a macro field which happens to be the display field (or if we simply want to type macros in general), type the macro (but don't call it)
    * recurse into macro context when collecting statistics
    
    closes #6094
    closes #6405

--- src/context/displayTypes.ml
+++ src/context/displayTypes.ml
@@ -232,6 +232,7 @@ module DisplayMode = struct
 		| DMDefault | DMDefinition | DMResolve _ | DMPackage | DMHover | DMSignature -> settings
 		| DMUsage _ -> { settings with
 				dms_full_typing = true;
+				dms_force_macro_typing = true;
 				dms_collect_data = true;
 				dms_display_file_policy = DFPAlso;
 				dms_exit_during_typing = false
@@ -246,6 +247,7 @@ module DisplayMode = struct
 				dms_error_policy = EPCollect;
 				dms_collect_data = true;
 				dms_inline = true;
+				dms_force_macro_typing = true;
 				dms_display_file_policy = if global then DFPNo else DFPAlso;
 				dms_exit_during_typing = false;
 			}
@@ -254,7 +256,8 @@ module DisplayMode = struct
 				dms_collect_data = true;
 				dms_inline = false;
 				dms_display_file_policy = DFPAlso;
-				dms_exit_during_typing = false
+				dms_exit_during_typing = false;
+				dms_force_macro_typing = true;
 			}
 
 	let to_string = function
