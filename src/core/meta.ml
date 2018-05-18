0da32b5f44b2f61c02a9cb6bfeb65f29c27046d6
Jens Fischer <jensfischer95@gmail.com>
2018-05-18 11:14:32 +0200

    Update meta docs

--- src/core/meta.ml
+++ src/core/meta.ml
@@ -245,7 +245,7 @@ let get_info = function
 	| FakeEnum -> ":fakeEnum",("Treat enum as collection of values of the specified type",[HasParam "Type name";UsedOn TEnum])
 	| File -> ":file",("Includes a given binary file into the target Swf and associates it with the class (must extend flash.utils.ByteArray)",[HasParam "File path";UsedOn TClass;Platform Flash])
 	| FileXml -> ":fileXml",("Include xml attribute snippet in Build.xml entry for file",[UsedOn TClass;Platform Cpp])
-	| Final -> ":final",("Prevents a class from being extended",[UsedOn TClass])
+	| Final -> ":final",("Prevents a class or interface from being extended",[UsedOn TClass])
 	| Fixed -> ":fixed",("Delcares an anonymous object to have fixed fields",[ (*UsedOn TObjectDecl(_)*)])
 	| FlatEnum -> ":flatEnum",("Internally used to mark an enum as being flat, i.e. having no function constructors",[UsedOn TEnum; UsedInternally])
 	| Font -> ":font",("Embeds the given TrueType font into the class (must extend flash.text.Font)",[HasParam "TTF path";HasParam "Range String";UsedOn TClass])
