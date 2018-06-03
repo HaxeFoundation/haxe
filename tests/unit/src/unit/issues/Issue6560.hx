976ffbe42f3f512a23eb45c305aa20584f8b070f
Caue Waneck <waneck@gmail.com>
2018-06-03 16:06:01 -0300

    [java/cs] Fix recursive local functions with type parameters
    * Do not set the functions as captured because the type parameters
    matter for Java and C#, so we cannot reuse the local function
    * Make sure we rerun the closuresToClass filter again on all the
    generated classes. This ensures that we can deal with the recursive
    functions on this second pass
    * Closes #7118

--- tests/unit/src/unit/issues/Issue6560.hx
+++ tests/unit/src/unit/issues/Issue6560.hx
@@ -2,13 +2,11 @@ package unit.issues;
 
 class Issue6560 extends unit.Test {
 	function test() {
-		#if (!cs && !java)
         function foo<F>(a:F):Array<F> {
 			if (false) foo(1);
             return if (a == null) [] else foo(null);
 		}
 		var bar:Array<Int> = foo(1);
 		eq(0, bar.length);
-		#end
 	}
 }
