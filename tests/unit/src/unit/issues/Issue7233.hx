1d0f0b7e531b5ea00af119f9123fe243428f311f
Andreas <ciscoheat@gmail.com>
2018-06-29 14:23:38 +0200

    [java] Type.createInstance with anonymous structure (fixes #7233)

---
+++ tests/unit/src/unit/issues/Issue7233.hx
@@ -0,0 +1,18 @@
+package unit.issues;
+
+class Issue7233 extends unit.Test {
+	function test() {
+		var data = {name: "Test"};
+		var obj = Type.createInstance(TestClass, [data]);
+
+		eq("Test", obj.name);
+	}
+}
+
+private class TestClass {
+    var name : String;
+
+    public function new(data) {
+        this.name = data.name;
+    }
+}
