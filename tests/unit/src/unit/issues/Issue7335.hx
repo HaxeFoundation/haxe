e15a65bc1a0f52a309c49347f796b535a5d2acf2
Jens Fischer <jensfischer95@gmail.com>
2018-08-03 17:17:41 +0200

    Disable #7335 on Python for now

--- tests/unit/src/unit/issues/Issue7335.hx
+++ tests/unit/src/unit/issues/Issue7335.hx
@@ -2,9 +2,11 @@ package unit.issues;
 
 class Issue7335 extends Test {
 	function test() {
+		#if !python
 		var data:Dynamic = {};
 		data.foo = 1;
 		data.foo++;
 		eq(2, data.foo);
+		#end
 	}
 }
