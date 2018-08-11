3ef9abffdd130ff8e1f199b1d36f4b1a1e370157
flashultra <flashultra@users.noreply.github.com>
2018-08-11 16:44:55 +0300

    Create Mode.hx

---
+++ std/haxe/crypto/mode/Mode.hx
@@ -0,0 +1,11 @@
+package haxe.crypto.mode;
+
+class Mode 
+{
+    public static inline var CBC:String = "cbc";
+    public static inline var CFB:String = "cfb";
+    public static inline var CTR:String = "ctr";
+    public static inline var ECB:String = "ecb";
+    public static inline var OFB:String = "ofb";
+    public static inline var PCBC:String = "pcbc";
+}
