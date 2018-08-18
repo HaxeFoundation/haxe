066b0ae6586c6f4955d24c5a0f2a992a4fce425d
flashultra <flashultra@users.noreply.github.com>
2018-08-18 17:49:53 +0300

    Update CTR.hx

--- std/haxe/crypto/mode/CTR.hx
+++ std/haxe/crypto/mode/CTR.hx
@@ -5,21 +5,21 @@ import haxe.io.Bytes;
 class CTR
 {
     public static function encrypt( src : Bytes, iv : Bytes, blockSize : Int, encryptBlock : Bytes->Int->Bytes->Int->Void) : Void
-	{
-		var vector:Bytes;
+    {
+	var vector:Bytes;
         var vkey : Bytes = iv.sub(0,iv.length); 
         var i : Int = 0;
         var len : Int = src.length;
-		while (i < len)
-		{
+	while (i < len)
+	{
             vector = vkey.sub(0,vkey.length);
             encryptBlock(vector, 0, vector , 0);
             
             var block:Int = (i+blockSize)>len?len-i:blockSize;
-			for (j in 0...block)
-			{
-				src.set(i + j, src.get(i + j) ^ vector.get(j) );
-			}
+	    for (j in 0...block)
+	    {
+		src.set(i + j, src.get(i + j) ^ vector.get(j) );
+	    }
 
             var j = blockSize;
             while (j-- >= 0) {
@@ -27,12 +27,12 @@ class CTR
                 if (vkey.get(j) != 0) break;  
             }
 
-			i += blockSize;
-		}
+	    i += blockSize;
 	}
+    }
 
-	public static function decrypt( src : Bytes, iv : Bytes, blockSize : Int, encryptBlock : Bytes->Int->Bytes->Int->Void) : Void
+    public static function decrypt( src : Bytes, iv : Bytes, blockSize : Int, decryptBlock : Bytes->Int->Bytes->Int->Void) : Void
     {
-       encrypt(src,iv,blockSize,encryptBlock);
+	encrypt(src,iv,blockSize,decryptBlock);
     }
 }
