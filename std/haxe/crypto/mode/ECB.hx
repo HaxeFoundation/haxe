5a640c16b37af31c17bfe2446b70cfd72235f3d5
flashultra <flashultra@users.noreply.github.com>
2018-08-18 17:55:56 +0300

    Update ECB.hx

--- std/haxe/crypto/mode/ECB.hx
+++ std/haxe/crypto/mode/ECB.hx
@@ -5,24 +5,24 @@ import haxe.io.Bytes;
 class ECB
 {
     public static function encrypt( src : Bytes, blockSize : Int, encryptBlock : Bytes->Int->Bytes->Int->Void) : Void
-	{
-        var i : Int = 0;
-        var len : Int = src.length;
-		while (i < len)
-		{
-			encryptBlock(src, i, src , i);
-			i += blockSize;
-		}
-	}
-
-	public static function decrypt( src : Bytes, blockSize : Int, decryptBlock : Bytes->Int->Bytes->Int->Void) : Void
     {
         var i : Int = 0;
         var len : Int = src.length;
-		while (i < len)
-		{
-			decryptBlock(src, i, src , i);
-			i += blockSize;
-		}
+	while (i < len)
+	{
+	  encryptBlock(src, i, src , i);
+	  i += blockSize;
+	}
+    }
+
+    public static function decrypt( src : Bytes, blockSize : Int, decryptBlock : Bytes->Int->Bytes->Int->Void) : Void
+    {
+        var i : Int = 0;
+        var len : Int = src.length;
+	while (i < len)
+	{
+	   decryptBlock(src, i, src , i);
+	   i += blockSize;
+	}
     }
 }
