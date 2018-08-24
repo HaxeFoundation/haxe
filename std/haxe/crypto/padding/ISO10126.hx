a08ec9b9de65e41baa4fde08ca1f9aafba3db5d2
flashultra <flashultra@users.noreply.github.com>
2018-08-24 10:14:46 +0300

    Create ISO10126.hx

---
+++ std/haxe/crypto/padding/ISO10126.hx
@@ -0,0 +1,25 @@
+package haxe.crypto.padding;
+
+import haxe.io.BytesBuffer;
+import haxe.io.Bytes;
+
+class ISO10126
+{
+    public static function pad(ciphertext:Bytes, blockSize:Int):Bytes
+    {
+        var buffer: BytesBuffer = new BytesBuffer();
+        buffer.addBytes(ciphertext,0,ciphertext.length);
+        var padding:Int = blockSize - ciphertext.length % blockSize -1;
+        for(i in 0...padding) {
+          buffer.addByte(Std.random(255)); 
+        }
+        buffer.addByte(padding+1);
+        return buffer.getBytes();
+    }
+
+    public static function unpad(encrypt:Bytes):Bytes
+    {
+      var padding : Int = encrypt.get(encrypt.length-1);
+      return encrypt.sub(0,encrypt.length - padding);
+    }
+}
