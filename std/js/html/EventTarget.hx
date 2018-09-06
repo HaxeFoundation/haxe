4fd0c29ed21c357574e66de7785f459adc29dcad
George Corney <haxiomic@gmail.com>
2018-09-06 20:04:48 +0100

    [js] Minor formatting

--- std/js/html/EventTarget.hx
+++ std/js/html/EventTarget.hx
@@ -44,14 +44,12 @@ extern class EventTarget
 	@:overload( function( type : String, listener : EventListener, ?options : haxe.extern.EitherType<AddEventListenerOptions,Bool>, ?wantsUntrusted : Bool ) : Void {} )
 	function addEventListener( type: String, listener: haxe.Constraints.Function, ?options : haxe.extern.EitherType<AddEventListenerOptions,Bool>, ?wantsUntrusted : Bool ) : Void;
 	
-	
 	/**
 		Removes an event listener from the `EventTarget`.
 		@throws DOMError
 	**/
 	@:overload( function( type : String, listener : EventListener, ?options : haxe.extern.EitherType<EventListenerOptions,Bool>) : Void {} )
 	function removeEventListener( type : String, listener : haxe.Constraints.Function, ?options : haxe.extern.EitherType<EventListenerOptions,Bool> ) : Void;
-	
 	
 	/**
 		Dispatch an event to this `EventTarget`.
