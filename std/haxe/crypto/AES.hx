29d3f82109d00b8d4c6d69d8fd8dff1fe91c2a32
flashultra <flashultra@users.noreply.github.com>
2018-09-07 08:48:19 +0300

    Update AES.hx

--- std/haxe/crypto/AES.hx
+++ std/haxe/crypto/AES.hx
@@ -7,26 +7,26 @@ import haxe.crypto.padding.*;
 
 class AES
 {
-	static var SBOX:Array<Int> =  [
+    static var SBOX:Array<Int> =  [
 			0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-            0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-            0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-            0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
-            0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
-            0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
-            0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
-            0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
-            0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
-            0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
-            0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
-            0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
-            0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-            0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
-            0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-            0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+            		0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+            		0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+            		0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+            		0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+            		0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+            		0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+            		0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+            		0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+            		0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+            		0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+            		0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+            		0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+            		0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+            		0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+            		0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
 		];
 
-	static var RSBOX:Array<Int> =  [
+    static var RSBOX:Array<Int> =  [
 			0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
   			0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
   			0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
@@ -45,7 +45,7 @@ class AES
   			0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
 		];
 	
-	static var POWER3:Array<Int>=[
+    static var POWER3:Array<Int>=[
 			0x01, 0x03, 0x05, 0x0f, 0x11, 0x33, 0x55, 0xff, 0x1a, 0x2e, 0x72, 0x96, 0xa1, 0xf8, 0x13, 0x35,
 			0x5f, 0xe1, 0x38, 0x48, 0xd8, 0x73, 0x95, 0xa4, 0xf7, 0x02, 0x06, 0x0a, 0x1e, 0x22, 0x66, 0xaa,
 			0xe5, 0x34, 0x5c, 0xe4, 0x37, 0x59, 0xeb, 0x26, 0x6a, 0xbe, 0xd9, 0x70, 0x90, 0xab, 0xe6, 0x31,
@@ -64,7 +64,7 @@ class AES
 			0x39, 0x4b, 0xdd, 0x7c, 0x84, 0x97, 0xa2, 0xfd, 0x1c, 0x24, 0x6c, 0xb4, 0xc7, 0x52, 0xf6
 		];
 
-	static var LOG3:Array<Int> = [
+    static var LOG3:Array<Int> = [
 			  0,   0,  25,   1,  50,   2,  26, 198,  75, 199,  27, 104,  51, 238, 223,   3,
 			100,   4, 224,  14,  52, 141, 129, 239,  76, 113,   8, 200, 248, 105,  28, 193,
 			125, 194,  29, 181, 249, 185,  39, 106,  77, 228, 166, 114, 154, 201,   9, 120,
@@ -83,48 +83,48 @@ class AES
 			103,  74, 237, 222, 197,  49, 254,  24,  13,  99, 140, 128, 192, 247, 112,   7
 		];
 
-	static var RCON:Array<Int> = [0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 ]; 
+    static var RCON:Array<Int> = [0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 ]; 
 
-	static inline var NB:Int = 4;
-	static inline var BLOCK_SIZE : Int = 16;
+    static inline var NB:Int = 4;
+    static inline var BLOCK_SIZE : Int = 16;
 
-	private var roundKey:Array<Int>;
-	private var state:Array<Array<Int>>;
+    private var roundKey:Array<Int>;
+    private var state:Array<Array<Int>>;
 
-	public var Nk(default, null):Int;
-	public var Nr(default, null):Int;
+    public var Nk(default, null):Int;
+    public var Nr(default, null):Int;
 
-	public var iv(default, set):Bytes;
+    public var iv(default, set):Bytes;
 	
-	function set_iv(vector) 
-	{
-        iv = vector;
-        if (iv == null) 
-        {
-        	iv = Bytes.alloc(BLOCK_SIZE);
-           	iv.fill(0,BLOCK_SIZE,0x00);
-        }
-        return iv;
-	}
-
-	public function new(?key:Bytes, ?iv:Bytes)
+    function set_iv(vector) 
     {
-		state = new Array<Array<Int>>();
-       	if ( key != null ) init(key,iv);
+      	iv = vector;
+      	if (iv == null) 
+      	{
+      	      	iv = Bytes.alloc(BLOCK_SIZE);
+      	      	iv.fill(0,BLOCK_SIZE,0x00);
+      	}
+      	return iv;
     }
 
-	public function init(key:Bytes, ?iv:Bytes):Void
+    public function new(?key:Bytes, ?iv:Bytes)
     {
-		var keyLength = key.length;
-
-		Nk = keyLength >> 2;
-		Nr = Nk + 6; 
-
-        this.iv = iv;
-        roundKey = keyExpansion(key);
+      	state = new Array<Array<Int>>();
+      	if ( key != null ) init(key,iv);
     }
 
-	public function getBlockSize():Int
+    public function init(key:Bytes, ?iv:Bytes):Void
+    {
+      	var keyLength = key.length;
+
+      	Nk = keyLength >> 2;
+      	Nr = Nk + 6; 
+
+      	this.iv = iv;
+      	roundKey = keyExpansion(key);
+    }
+
+    public function getBlockSize():Int
     {
        	return BLOCK_SIZE;
     }
@@ -134,8 +134,8 @@ class AES
        	var out:Bytes;
         
        	switch(padding)  {
-			//CBC, ECB  and PCBC requires padding
-           	case Padding.NoPadding:
+           //CBC, ECB  and PCBC requires padding
+           case Padding.NoPadding:
             	out = NoPadding.pad(data,BLOCK_SIZE); 
             case Padding.PKCS7:
             	out = PKCS7.pad(data,BLOCK_SIZE);
@@ -212,187 +212,186 @@ class AES
         return out;
     }
 
-	private function encryptBlock( src:Bytes, srcIndex:Int, dst:Bytes, dstIndex:Int):Void
+    private function encryptBlock( src:Bytes, srcIndex:Int, dst:Bytes, dstIndex:Int):Void
     {
-		for(i in 0...4) {
-      		state[i] = [ src.get(4*i+srcIndex) , src.get(4*i+srcIndex+1) , src.get(4*i+srcIndex+2) , src.get(4*i+srcIndex+3) ];
-    	}
+       	for(i in 0...4) {
+      	      	state[i] = [ src.get(4*i+srcIndex) , src.get(4*i+srcIndex+1) , src.get(4*i+srcIndex+2) , src.get(4*i+srcIndex+3) ];
+       	}
 
-		addRoundKey(0); 
+       	addRoundKey(0); 
 
-  		for (round  in 1...Nr)
-  		{
-    		subBytes();
+      	for (round  in 1...Nr)
+       	{
+       	       	subBytes();
     		shiftRows();
     		mixColumns();
     		addRoundKey(round);
-  		}
+       	}
   
-  		subBytes();
-  		shiftRows();
-  		addRoundKey(Nr);
+       	subBytes();
+       	shiftRows();
+       	addRoundKey(Nr);
 
-		for(i in 0...4) {
-			for(j in 0...4) {
-				dst.set(4*i+j+dstIndex,state[i][j]);
-			}
+       	for(i in 0...4) {
+       	       	for(j in 0...4) {
+       	       	       	dst.set(4*i+j+dstIndex,state[i][j]);
 		}
+	}
     }
 
-	private function decryptBlock( src:Bytes, srcIndex:Int, dst:Bytes, dstIndex:Int):Void
+    private function decryptBlock( src:Bytes, srcIndex:Int, dst:Bytes, dstIndex:Int):Void
     {
-		for(i in 0...4) {
-      		state[i] = [ src.get(4*i+srcIndex) , src.get(4*i+srcIndex+1) , src.get(4*i+srcIndex+2) , src.get(4*i+srcIndex+3) ];
+       	for(i in 0...4) {
+       	       	state[i] = [ src.get(4*i+srcIndex) , src.get(4*i+srcIndex+1) , src.get(4*i+srcIndex+2) , src.get(4*i+srcIndex+3) ];
     	}
 
         addRoundKey(Nr);
 
-  		var round : Int = Nr -1;
-		while ( round > 0)
-  		{
-    		invShiftRows();
+       	var round : Int = Nr -1;
+       	while ( round > 0)
+       	{
+        	invShiftRows();
     		invSubBytes();
     		addRoundKey(round);
     		invMixColumns();
-			round--;
-  		}
+		round--;
+  	}
   
-  		invShiftRows();
-  		invSubBytes();
-  		addRoundKey(0);
+  	invShiftRows();
+  	invSubBytes();
+  	addRoundKey(0);
 
-		for(i in 0...4) {
-			for(j in 0...4) {
-				dst.set(4*i+j+dstIndex,state[i][j]);
-			}
+	for(i in 0...4) {
+		for(j in 0...4) {
+			dst.set(4*i+j+dstIndex,state[i][j]);
 		}
+	}
     }
 			 
-	private function rotWord(w:Array<Int>):Array<Int>
-	{
-		var tmp:Int = w[0];
-		for(i in 0...3)  w[i] = w[i+1];
-		w[3] = tmp;
-		return w;
-	};
+    private function rotWord(w:Array<Int>):Array<Int>
+    {
+	var tmp:Int = w[0];
+	for(i in 0...3)  w[i] = w[i+1];
+	w[3] = tmp;
+	return w;
+    }
 	
-	private function subWord(w:Array<Int>):Array<Int> 
-	{
-		for(i in 0...4) w[i] = SBOX[w[i]];
-		return w;
-	};
+    private function subWord(w:Array<Int>):Array<Int> 
+    {
+	for(i in 0...4) w[i] = SBOX[w[i]];
+	return w;
+    }
 
-	private function keyExpansion(key:Bytes) : Array<Int>
-	{
+    private function keyExpansion(key:Bytes) : Array<Int>
+    {
 
-		var roundKey:Array<Int> = new Array<Int>();
-		var temp:Array<Int> = new Array<Int>();
+	var roundKey:Array<Int> = new Array<Int>();
+	var temp:Array<Int> = new Array<Int>();
 		
-		for (i in 0...Nk) {
-			for (j in 0...4) {
-				roundKey[4*i+j] = key.get(4*i+j);
-			}
+	for (i in 0...Nk) {
+		for (j in 0...4) {
+			roundKey[4*i+j] = key.get(4*i+j);
 		}
-
-		for (i in Nk...(NB*(Nr+1))) {
-			for ( j in 0...NB) temp[j] = roundKey[4*(i-1)+j];
-			if (i % Nk == 0) {
-				temp = subWord(rotWord(temp));
-				var k = Std.int(i/Nk);
-				temp[0]  ^= RCON[k];
-			} else if (Nk > 6 && (i % Nk == 4) ) {
-				temp = subWord(temp);
-			}
-			var k = i * 4;
-			var m = (i - Nk) * 4;
-			for (j in 0...NB) roundKey[k + j] = roundKey[m + j] ^ temp[j];
-		}
-
-		return roundKey;
-	};
-
-	private function addRoundKey(round:Int):Void
-	{
-		round <<= 2;
-		for (i in 0...4)
-		{
-			for(j in 0...NB) state[i][j] ^= roundKey[ round*4 + i*NB +j ];
-		}
-	};
-
-	private function subBytes():Void
-	{
-		for (i in 0...4)
-		{
-			for (j in 0...4) {
-				state[i][j] = SBOX[state[i][j]];
-			}
-		}
-	};
-
-	private  function shiftRows():Void
-	{
-		var t:Array<Int> = new Array<Int>();
-		for (i in 1...4) {
-			for (j in 0...4) 
-				t[j] = state[(j+i)%4][i];
-			for (j in 0...4) 
-				state[j][i] = t[j];
-		}
-	};
-
-	private function mixColumns():Void 
-	{
-		var t:Array<Int> = new Array<Int>();
-	 	for (i in 0...4) {
-			for (j in 0...4) {
-		  		t[j] = state[i][j];
-			}
-			for (j in 0...4) {
-				state[i][j] = mul(0x02, t[j])
-							^ mul(0x03, t[(j+1)%4])
-							^ mul(0x01, t[(j+2)%4])
-							^ mul(0x01, t[(j+3)%4]);
-			}
-  	 	}
 	}
 
-	private function mul(a:Int, b:Int):Int
-	{
-		return (a != 0 && b != 0)?POWER3[(LOG3[a]+LOG3[b])%255]:0;
+	for (i in Nk...(NB*(Nr+1))) {
+		for ( j in 0...NB) temp[j] = roundKey[4*(i-1)+j];
+		if (i % Nk == 0) {
+			temp = subWord(rotWord(temp));
+			var k = Std.int(i/Nk);
+			temp[0]  ^= RCON[k];
+		} else if (Nk > 6 && (i % Nk == 4) ) {
+			temp = subWord(temp);
+		}
+		var k = i * 4;
+		var m = (i - Nk) * 4;
+		for (j in 0...NB) roundKey[k + j] = roundKey[m + j] ^ temp[j];
 	}
+	return roundKey;
+    }
+
+    private function addRoundKey(round:Int):Void
+    {
+	round <<= 2;
+	for (i in 0...4)
+	{
+		for(j in 0...NB) state[i][j] ^= roundKey[ round*4 + i*NB +j ];
+	}
+    }
+
+    private function subBytes():Void
+    {
+	for (i in 0...4)
+	{
+		for (j in 0...4) {
+			state[i][j] = SBOX[state[i][j]];
+		}
+	}
+    }
+
+    private  function shiftRows():Void
+    {
+	var t:Array<Int> = new Array<Int>();
+	for (i in 1...4) {
+		for (j in 0...4) 
+			t[j] = state[(j+i)%4][i];
+		for (j in 0...4) 
+			state[j][i] = t[j];
+	}
+    }
+
+    private function mixColumns():Void 
+    {
+	var t:Array<Int> = new Array<Int>();
+ 	for (i in 0...4) {
+		for (j in 0...4) {
+	  		t[j] = state[i][j];
+		}
+		for (j in 0...4) {
+			state[i][j] = mul(0x02, t[j])
+				    ^ mul(0x03, t[(j+1)%4])
+				    ^ mul(0x01, t[(j+2)%4])
+				    ^ mul(0x01, t[(j+3)%4]);
+		}
+ 	}
+    }
+
+    private function mul(a:Int, b:Int):Int
+    {
+	return (a != 0 && b != 0)?POWER3[(LOG3[a]+LOG3[b])%255]:0;
+    }
 	
-	private function invMixColumns():Void
-	{
-		var t:Array<Int> = new Array<Int>();
-		for (i in 0...4) {
-			for (j in 0...4)
-				t[j] = state[i][j];
-			for (j in 0...4) {
-				state[i][j] = mul(0x0e, t[j])
-							^ mul(0x0b, t[(j+1)%4])
-							^ mul(0x0d, t[(j+2)%4])
-							^ mul(0x09, t[(j+3)%4]);
-			}
+    private function invMixColumns():Void
+    {
+	var t:Array<Int> = new Array<Int>();
+	for (i in 0...4) {
+		for (j in 0...4)
+			t[j] = state[i][j];
+		for (j in 0...4) {
+			state[i][j] = mul(0x0e, t[j])
+				    ^ mul(0x0b, t[(j+1)%4])
+				    ^ mul(0x0d, t[(j+2)%4])
+				    ^ mul(0x09, t[(j+3)%4]);
 		}
 	}
+    }
 
-	private function invSubBytes():Void
-	{
-		for(i in 0...4) {
-			for(j in 0...4) 
-				state[j][i] = RSBOX[state[j][i]];
-		}
+    private function invSubBytes():Void
+    {
+	for(i in 0...4) {
+		for(j in 0...4) 
+			state[j][i] = RSBOX[state[j][i]];
 	}
+    }
 
-	private function invShiftRows():Void
-	{
-		var t:Array<Int> = new Array<Int>();
-		for (i in 1...4) {
-			for (j in 0...4) 
-				t[j] = state[(j-i+4)%4][i];
-			for (j in 0...4) 
-				state[j][i] = t[j];
-		}
+    private function invShiftRows():Void
+    {
+	var t:Array<Int> = new Array<Int>();
+	for (i in 1...4) {
+		for (j in 0...4) 
+			t[j] = state[(j-i+4)%4][i];
+		for (j in 0...4) 
+			state[j][i] = t[j];
 	}
+    }
 }
