ae84ea9839de7d9a47db6729925fcdffff682aa8
Simon Krajewski <simon@haxe.org>
2018-09-09 10:30:22 +0200

    [java/cs] Disable `implements Dynamic` support (#7403)

--- std/cs/internal/HxObject.hx
+++ std/cs/internal/HxObject.hx
@@ -22,11 +22,16 @@
 package cs.internal;
 import cs.system.Type;
 import haxe.ds.Vector;
+import cs.internal.FieldLookup;
 private typedef StdType = std.Type;
 
 @:keep @:native('haxe.lang.HxObject')
 class HxObject implements IHxObject
 {
+	public function __hx_deleteField(field:String, hash:Int):Bool
+	{
+		return false;
+	}
 }
 
 @:keep @:native('haxe.lang.IHxObject')
@@ -38,8 +43,204 @@ interface IHxObject
 @:meta(System.Serializable)
 #end
 @:keep @:native('haxe.lang.DynamicObject')
-class DynamicObject extends HxObject implements Dynamic
+class DynamicObject extends HxObject
 {
+	@:skipReflection var __hx_hashes:NativeArray<Int>;
+	@:skipReflection var __hx_dynamics:NativeArray<Dynamic>;
+
+	@:skipReflection var __hx_hashes_f:NativeArray<Int>;
+	@:skipReflection var __hx_dynamics_f:NativeArray<Float>;
+
+	@:skipReflection var __hx_length:Int;
+	@:skipReflection var __hx_length_f:Int;
+	@:skipReflection var __hx_conflicts:FieldHashConflict;
+
+	@:overload public function new()
+	{
+		this.__hx_hashes = new NativeArray(0);
+		this.__hx_dynamics = new NativeArray(0);
+		this.__hx_hashes_f = new NativeArray(0);
+		this.__hx_dynamics_f = new NativeArray(0);
+		this.__hx_conflicts = null;
+	}
+
+	@:overload public function new(hashes:NativeArray<Int>, dynamics:NativeArray<Dynamic>, hashes_f:NativeArray<Int>, dynamics_f:NativeArray<Float>)
+	{
+		this.__hx_hashes = hashes;
+		this.__hx_dynamics = dynamics;
+		this.__hx_hashes_f = hashes_f;
+		this.__hx_dynamics_f = dynamics_f;
+		this.__hx_length = hashes.length;
+		this.__hx_length_f = hashes_f.length;
+		this.__hx_conflicts = null;
+	}
+
+	override public function __hx_deleteField(field:String, hash:Int):Bool
+	{
+		if (hash < 0)
+		{
+			return FieldLookup.deleteHashConflict(this.__hx_conflicts, hash, field);
+		}
+
+		var res = FieldLookup.findHash(hash, this.__hx_hashes, this.__hx_length);
+		if (res >= 0)
+		{
+			FieldLookup.removeInt(this.__hx_hashes, this.__hx_length, res);
+			FieldLookup.removeDynamic(this.__hx_dynamics, this.__hx_length, res);
+			this.__hx_length--;
+			return true;
+		}
+		res = FieldLookup.findHash(hash, this.__hx_hashes_f, this.__hx_length_f);
+		if (res >= 0)
+		{
+			FieldLookup.removeInt(this.__hx_hashes_f, this.__hx_length_f, res);
+			FieldLookup.removeFloat(this.__hx_dynamics_f, this.__hx_length_f, res);
+			this.__hx_length_f--;
+			return true;
+		}
+		return false;
+	}
+
+	public function __hx_getField(field:String, hash:Int, throwErrors:Bool, isCheck:Bool, handleProperties:Bool):Dynamic
+	{
+		if (hash < 0)
+		{
+			var conflict = FieldLookup.getHashConflict(this.__hx_conflicts, hash, field);
+			if (conflict != null)
+			{
+				return conflict.value;
+			}
+		}
+
+		var res = FieldLookup.findHash(hash, this.__hx_hashes, this.__hx_length);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics[res];
+		}
+		res = FieldLookup.findHash(hash, this.__hx_hashes_f, this.__hx_length_f);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics_f[res];
+		}
+
+		return isCheck ? Runtime.undefined : null;
+	}
+
+	public function __hx_setField(field:String, hash:Int, value:Dynamic, handleProperties:Bool):Dynamic
+	{
+		if (hash < 0)
+		{
+			FieldLookup.setHashConflict(this.__hx_conflicts, hash, field, value);
+			return value;
+		}
+
+		var res = FieldLookup.findHash(hash, this.__hx_hashes, this.__hx_length);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics[res] = value;
+		} else {
+			var res = FieldLookup.findHash(hash, this.__hx_hashes_f, this.__hx_length_f);
+			if (res >= 0)
+			{
+				if (Std.is(value, Float))
+				{
+					return this.__hx_dynamics_f[res] = value;
+				}
+
+				FieldLookup.removeInt(this.__hx_hashes_f, this.__hx_length_f, res);
+				FieldLookup.removeFloat(this.__hx_dynamics_f, this.__hx_length_f, res);
+				this.__hx_length_f--;
+			}
+		}
+
+		this.__hx_hashes = FieldLookup.insertInt(this.__hx_hashes, this.__hx_length,  ~(res) , hash);
+		this.__hx_dynamics = FieldLookup.insertDynamic(this.__hx_dynamics, this.__hx_length,  ~(res) , value);
+		this.__hx_length++;
+		return value;
+	}
+
+	public function __hx_getField_f(field:String, hash:Int, throwErrors:Bool, handleProperties:Bool):Float
+	{
+		if (hash < 0)
+		{
+			var conflict = FieldLookup.getHashConflict(this.__hx_conflicts, hash, field);
+			if (conflict != null)
+			{
+				return conflict.value;
+			}
+		}
+
+		var res = FieldLookup.findHash(hash, this.__hx_hashes_f, this.__hx_length_f);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics_f[res];
+		}
+		res = FieldLookup.findHash(hash, this.__hx_hashes, this.__hx_length);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics[res];
+		}
+
+		return 0.0;
+	}
+
+	public function __hx_setField_f(field:String, hash:Int, value:Float, handleProperties:Bool):Float
+	{
+		if (hash < 0)
+		{
+			FieldLookup.setHashConflict(this.__hx_conflicts, hash, field, value);
+			return value;
+		}
+
+		var res = FieldLookup.findHash(hash, this.__hx_hashes_f, this.__hx_length_f);
+		if (res >= 0)
+		{
+			return this.__hx_dynamics_f[res] = value;
+		} else {
+			var res = FieldLookup.findHash(hash, this.__hx_hashes, this.__hx_length);
+			if (res >= 0)
+			{
+				// return this.__hx_dynamics[res] = value;
+				FieldLookup.removeInt(this.__hx_hashes, this.__hx_length, res);
+				FieldLookup.removeDynamic(this.__hx_dynamics, this.__hx_length, res);
+				this.__hx_length--;
+			}
+		}
+
+		this.__hx_hashes_f = FieldLookup.insertInt(this.__hx_hashes_f, this.__hx_length_f,  ~(res) , hash);
+		this.__hx_dynamics_f = FieldLookup.insertFloat(this.__hx_dynamics_f, this.__hx_length_f,  ~(res) , value);
+		this.__hx_length_f++;
+		return value;
+	}
+
+	public function __hx_getFields(baseArr:Array<String>):Void
+	{
+		for (i in 0...this.__hx_length)
+		{
+			baseArr.push(FieldLookup.lookupHash(this.__hx_hashes[i]));
+		}
+		for (i in 0...this.__hx_length_f)
+		{
+			baseArr.push(FieldLookup.lookupHash(this.__hx_hashes_f[i]));
+		}
+		FieldLookup.addHashConflictNames(this.__hx_conflicts, baseArr);
+	}
+
+	public function __hx_invokeField(field:String, hash:Int, dynargs:NativeArray<Dynamic>):Dynamic
+	{
+		if (field == "toString")
+		{
+			return this.toString();
+		}
+		var fn:Function = this.__hx_getField(field, hash, false, false, false);
+		if (fn == null)
+		{
+			throw 'Cannot invoke field $field: It does not exist';
+		}
+
+		return untyped fn.__hx_invokeDynamic(dynargs);
+	}
+
 	@:skipReflection public function toString():String
 	{
 		var ts = Reflect.field(this, "toString");
