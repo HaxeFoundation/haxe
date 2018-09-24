97895be4fdf594bdddd138532b65f7f8c1816be8
Kevin Leung <kevinresol@users.noreply.github.com>
2018-09-24 23:40:18 +0800

    Update ReadOnlyArray.hx

--- std/haxe/ds/ReadOnlyArray.hx
+++ std/haxe/ds/ReadOnlyArray.hx
@@ -24,7 +24,7 @@ package haxe.ds;
 
 @:forward(concat, copy, filter, indexOf, iterator, join, lastIndexOf, map, slice, toString)
 abstract ReadOnlyArray<T>(Array<T>) from Array<T> {
-  var length(get,never):Int;
+  public var length(get,never):Int;
   inline function get_length() return this.length;
   @:arrayAccess inline function get(i:Int) return this[i];
 } 
