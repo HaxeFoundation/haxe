f2dd99f40b74b16f3107b8a8138c412291c3e18f
George Corney <haxiomic@gmail.com>
2018-09-29 20:53:12 +0100

    [tests] remove jquery dependence

--- tests/misc/projects/Issue4803/Main.hx
+++ tests/misc/projects/Issue4803/Main.hx
@@ -1,6 +1,18 @@
+@:native("$.Event") extern class Event extends js.html.Event {
+	public var data : Dynamic;	
+} 
+
+@:native("$") extern class JQuery implements ArrayAccess<js.html.Element> {
+	public function new():Void;
+	
+	@:overload(function(handler:Event -> Void):JQuery { })	
+	@:overload(function(?eventData:Dynamic, handler:Event -> Void):JQuery { })	
+	public function click():JQuery;
+}
+
 class Main  {
 	static function main() {
-		var j = new js.jquery.JQuery();
+		var j = new JQuery();
 		j.click(function(e) {
 			var p = { y : 3 };
 			p = { x : 5 };
