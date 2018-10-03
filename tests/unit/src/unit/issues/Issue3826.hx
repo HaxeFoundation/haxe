2c17333197cd9736d191bcf4e960971dc264c9a3
ncannasse <ncannasse@gmail.com>
2018-10-03 12:17:52 +0200

    allow hl to skip not nullable args (still an issue with .bind)

--- tests/unit/src/unit/issues/Issue3826.hx
+++ tests/unit/src/unit/issues/Issue3826.hx
@@ -13,19 +13,30 @@ class Issue3826 extends Test {
 		eq( get(), "2/4.25" );
 		eq( get(5), "5/4.25" );
 		eq( get(5,8.5), "5/8.5" );
-
+		
+		#if !flash
+		eq( get(8.5), "2/8.5" );
+		#end
 
 		#if static
 		eq( get(inull), "0/4.25" );
 		eq( get(inull,fnull), "0/0" );
+
+		#if !flash
+		eq( get(fnull), "2/0" );
+		#end
+		
 		#else
 		eq( get(inull), "2/4.25" );
 		eq( get(inull,fnull), "2/4.25" );
-
-		// skipping only allowed on non static targets
-		eq( get(8.5), "2/8.5" );
+		
+		#if !flash
 		eq( get(fnull), "2/4.25" );
 		#end
+		
+		#end
+
+	
 	}
 
 	function getOpt( ?a = 2, ?b = 4.25 ) {
