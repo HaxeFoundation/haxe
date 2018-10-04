6dc01c7965808432210455acf8afa9b50f0f8198
r32 <56782055@qq.com>
2018-10-04 15:09:49 +0800

    unit test

---
+++ tests/unit/src/unit/issues/Issue7497.hx
@@ -0,0 +1,82 @@
+package unit.issues;
+
+import haxe.io.FPHelper;
+import Math.isNaN;
+
+class Issue7497 extends unit.Test {
+#if js
+	static function pot(n) return Math.pow(2, n);
+	static function hexI32(b: Int) return StringTools.hex(b, 8);
+
+	static function i2f(i) return @:privateAccess FPHelper._i32ToFloat(i);
+	static function f2i(f) return @:privateAccess FPHelper._floatToI32(f);
+
+	static function d2ii(d) return @:privateAccess FPHelper._doubleToI64(d);
+	static function ii2d(lo, hi) return @:privateAccess FPHelper._i64ToDouble(lo, hi);
+
+	function rtsingle(i:Int, n:Float, name:String) {
+		var ai = f2i(n);
+		var af = i2f(ai);
+		var bi = FPHelper.floatToI32(af);
+		var ci = FPHelper.floatToI32(n);
+		if (!(isNaN(n) && isNaN(af)))
+			eq(n, af);
+		eq(ai, bi);
+		eq(ai, ci);
+	}
+
+	function rtdouble(i:Int, n:Float, name:String){
+		var ai = d2ii(n);
+		var ad = ii2d(ai.low, ai.high);
+		var bi = FPHelper.doubleToI64(ad);
+		var ci = FPHelper.doubleToI64(n);
+		if (!(isNaN(n) && isNaN(ad)))
+			eq(n, ad);
+		eq(ai, bi);
+		eq(ai, ci);
+	}
+
+	function test() {
+		var NA = -9999;
+		// float32 to int32
+		rtsingle(NA, 0, "s 0");
+		rtsingle(NA, -0.0, "s -0");
+		rtsingle(NA, pot(-149), "s min");
+		rtsingle(NA, -pot(-149), "s -min");
+		rtsingle(NA, pot(127) * (2 - pot(-23)), "s max");
+		rtsingle(NA, -pot(127) * (2 - pot(-23)), "s -max");
+		rtsingle(NA, Math.NEGATIVE_INFINITY, "-infinity");
+		rtsingle(NA, Math.POSITIVE_INFINITY, "infinity");
+		rtsingle(NA, Math.NaN, "nan");
+		for(i in -149...128){ // min denormalized float to max normalized pot float
+			var n = pot(i);
+			var uulp = if(i < -126) pot(-149) else pot(i - 23);
+			var dulp = if(i <= -126) pot(-149) else pot(i - 24);
+			rtsingle(i, n, "s pot");
+			rtsingle(i, n + uulp, "s pot+");
+			rtsingle(i, n - dulp, "s pot-");
+			rtsingle(i, n - 2 * dulp, "s pot--");
+		}
+
+		// float64 to int64
+		rtdouble(NA, 0, "s 0");
+		rtdouble(NA, -0.0, "s -0");
+		rtdouble(NA, pot(-1075), "d min");
+		rtdouble(NA, -pot(-1075), "d -min");
+		rtdouble(NA, pot(1023) * (2 - pot( -52)), "d max");
+		rtdouble(NA, -pot(1023) * (2 - pot( -52)), "d -max");
+		rtdouble(NA, Math.POSITIVE_INFINITY, "d Inf");
+		rtdouble(NA, Math.NEGATIVE_INFINITY, "d -Inf");
+		rtdouble(NA, Math.NaN, "d NaN");
+		for(i in -1075...1024){ // min denormalized double to max normalized pot double
+			var n = pot(i);
+			var uulp = if(i < -1022) pot(-1075) else pot(i - 52);
+			var dulp = if(i <= -1022) pot(-1075) else pot(i - 53);
+			rtdouble(i, n, "d pot");
+			rtdouble(i, n + 2 * uulp, "d pot+");
+			rtdouble(i, n - dulp, "d pot-");
+			rtdouble(i, n - 2 * dulp, "d pot--");
+		}
+	}
+#end
+}
