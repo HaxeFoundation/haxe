0d423126fa391d6950bea7bbb858213b4bde9d18
Mark Knol <mark.knol@gmail.com>
2018-10-04 11:51:45 +0200

    IntMap order is undefined, use sorting

--- tests/unit/src/unitstd/haxe/ds/IntMap.unit.hx
+++ tests/unit/src/unitstd/haxe/ds/IntMap.unit.hx
@@ -6,13 +6,28 @@ map1.set(3, 6);
 map1.get(1) == 2;
 map1.get(2) == 4; 
 map1.get(3) != 8;
+
 // iterator
-[for (k in map1.keys()) k] == [1,2,3];
-[for (v in map1) v] == [2,4,6];
+var keys1a = [for (k in map1.keys()) k];
+keys1a.sort(Reflect.compare); // Order is undefined
+keys1a == [1,2,3];
+
+var values1a = [for (v in map1) v];
+values1a.sort(Reflect.compare); 
+values1a == [2,4,6];
+
 // key value iterator
-[for (k=>v in map1) k] == [1,2,3];
-[for (k=>v in map1) v] == [2,4,6];
-[for (k=>v in map1) k*v] == [2,8,18];
+var keys1b = [for (k=>v in map1) k];
+keys1b.sort(Reflect.compare); 
+keys1b == [1,2,3];
+
+var values1b = [for (k=>v in map1) v];
+values1b.sort(Reflect.compare); 
+values1b == [2,4,6];
+
+var values1c = [for (k=>v in map1) k*v];
+values1c.sort(Reflect.compare); 
+values1c == [2,8,18];
 
 
 var map2 = new haxe.ds.IntMap();
@@ -23,9 +38,20 @@ map2.set(3, "6");
 map2.get(1) == "2";
 map2.get(2) == "4"; 
 map2.get(3) != "8";
+
 // iterator
-[for (k in map2.keys()) k] == [1,2,3];
-[for (v in map2) v] == ["2","4","6"];
+var keys2a = [for (k in map2.keys()) k];
+keys2a.sort(Reflect.compare); 
+keys2a == [1,2,3];
+
+var values2a = [for (v in map2) v];
+values2a.sort(Reflect.compare); 
+values2a == ["2","4","6"];
 // key value iterator
-[for (k=>v in map2) k] == [1,2,3];
-[for (k=>v in map2) v] == ["2","4","6"];
+var keys2b = [for (k=>v in map2) k];
+keys2b.sort(Reflect.compare); 
+keys2b == [1,2,3];
+
+var values2b = [for (k=>v in map2) v];
+values2b.sort(Reflect.compare); 
+values2b == ["2","4","6"];
