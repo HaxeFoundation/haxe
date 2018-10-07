53374d4feb4e9e7a8a17181036d34d22d3213f6d
Alexander Kuzmenko <alex@stablex.ru>
2018-10-07 13:33:25 +0300

    cleanup

--- std/js/_std/Reflect.hx
+++ std/js/_std/Reflect.hx
@@ -19,12 +19,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
-import js.Syntax;
-import js.Lib;
-import haxe.iterators.StringIterator;
-import haxe.iterators.StringKeyValueIterator;
-
 @:coreApi class Reflect {
 
 	@:pure
