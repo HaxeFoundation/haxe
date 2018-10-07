25dd400548d5a53efe2de5aad67e60288083b10c
Nicolas Cannasse <ncannasse@gmail.com>
2018-10-07 11:38:23 +0200

    unify static/dynamic platforms (except flash)

--- tests/unit/src/unit/issues/Issue3826.hx
+++ tests/unit/src/unit/issues/Issue3826.hx
@@ -10,33 +10,19 @@ class Issue3826 extends Test {
 	}
 
 	public function test() {
+		#if !flash
+		// flash does have two problems wrt these tests
+		// - not skippable nullable arg
+		// - explicit null args are cast to 0
 		eq( get(), "2/4.25" );
 		eq( get(5), "5/4.25" );
 		eq( get(5,8.5), "5/8.5" );
-		
-		#if !flash
 		eq( get(8.5), "2/8.5" );
-		#end
 
-		#if static
-		eq( get(inull), "0/4.25" );
-		eq( get(inull,fnull), "0/0" );
-
-		#if !flash
-		eq( get(fnull), "2/0" );
-		#end
-		
-		#else
 		eq( get(inull), "2/4.25" );
-		eq( get(inull,fnull), "2/4.25" );
-		
-		#if !flash
+		eq( get(inull,fnull), "2/4.25" );		
 		eq( get(fnull), "2/4.25" );
 		#end
-		
-		#end
-
-	
 	}
 
 	function getOpt( ?a = 2, ?b = 4.25 ) {
