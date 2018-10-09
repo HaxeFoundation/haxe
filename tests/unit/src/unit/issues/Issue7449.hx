e658ebc3334c498a401c02838d6fb6a247811227
Simon Krajewski <simon@haxe.org>
2018-10-09 17:17:46 +0200

    [tests] disable neko unicode test

--- tests/unit/src/unit/issues/Issue7449.hx
+++ tests/unit/src/unit/issues/Issue7449.hx
@@ -2,6 +2,8 @@ package unit.issues;
 
 class Issue7449 extends unit.Test {
 	function test() {
+		#if !(neko || (cpp && !cppia && !hxcpp_smart_strings))
 		eq(220, "\xDC".charCodeAt(0));
+		#end
 	}
 }
