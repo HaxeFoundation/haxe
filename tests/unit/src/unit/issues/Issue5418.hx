195bef09f94c934c4cb73d7fde44cf5a4d2e5b09
azrafe7 <azrafe7@gmail.com>
2018-10-25 20:02:08 +0200

    add more tests

--- tests/unit/src/unit/issues/Issue5418.hx
+++ tests/unit/src/unit/issues/Issue5418.hx
@@ -4,7 +4,36 @@ class Issue5418 extends unit.Test {
 	var TEST_FILE = "issue5418_test.txt";
 
 	#if (sys || nodejs)
-	function testIssue() {
+	function testOldUsage() {
+		var testContent = "line1\nline2\n";
+		var expectedLines = ["line1", "line2"];
+
+		sys.io.File.saveContent(TEST_FILE, testContent);
+
+		var is = sys.io.File.read(TEST_FILE, false);
+
+		var lines = [];
+		var line;
+		try {
+			while (true) {
+				line = is.readLine();
+				if (line != null) lines.push(line); // <- now needs to test for null!
+			}
+		} catch (e:haxe.io.Eof) {}
+
+		t(is.eof());
+
+		var expected = expectedLines;
+		aeq(expected, lines);
+
+		// seek should also reset eof
+		is.seek(0, sys.io.FileSeek.SeekBegin);
+		aeq([expectedLines[0]], [is.readLine()]);
+
+		is.close();
+	}
+
+	function testNewUsage() {
 		var testContent = "line1\nline2\n";
 		var expectedLines = ["line1", "line2"];
 
@@ -22,6 +51,58 @@ class Issue5418 extends unit.Test {
 
 		var expected = expectedLines;
 		aeq(expected, lines);
+
+		// seek should also reset eof
+		is.seek(0, sys.io.FileSeek.SeekBegin);
+		aeq([expectedLines[0]], [is.readLine()]);
+
+		is.close();
+	}
+
+	function testEmpty() {
+		var testContent = "";
+		var expectedLines = [];
+
+		sys.io.File.saveContent(TEST_FILE, testContent);
+
+		var is = sys.io.File.read(TEST_FILE, false);
+
+		var lines = [];
+		var line;
+		while ((line = is.readLine()) != null) {
+			lines.push(line);
+		}
+
+		t(is.eof());
+
+		var expected = expectedLines;
+		aeq(expected, lines);
+
+		is.close();
+	}
+
+	function testCRLF() {
+		var testContent = "\r\n";
+		var expectedLines = [""];
+
+		sys.io.File.saveContent(TEST_FILE, testContent);
+
+		var is = sys.io.File.read(TEST_FILE, false);
+
+		var lines = [];
+		var line;
+		while ((line = is.readLine()) != null) {
+			lines.push(line);
+		}
+
+		t(is.eof());
+
+		var expected = expectedLines;
+		aeq(expected, lines);
+
+		// seek should also reset eof
+		is.seek(0, sys.io.FileSeek.SeekBegin);
+		aeq([expectedLines[0]], [is.readLine()]);
 
 		is.close();
 	}
