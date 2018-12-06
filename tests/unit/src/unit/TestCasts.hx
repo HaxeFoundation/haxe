a1ff6ff83d44e9a3c8ff4199e7af72442fcc525d
Simon Krajewski <simon@haxe.org>
2018-12-06 11:02:08 +0100

    don't test null

--- tests/unit/src/unit/TestCasts.hx
+++ tests/unit/src/unit/TestCasts.hx
@@ -12,34 +12,31 @@ class TestCasts extends Test  {
 		var f : Int -> Float = make(foo);
 		eq( f(123), 123);
 	}
-	
+
 	#if !as3
 	function testInterfaces() {
-		
-		var v : MyClass.CI1 = null;
-		eq( cast(v,MyClass.I1), null );
-		
+
 		var v = new MyClass.CI1();
 		eq( cast(v,MyClass.I1), v );
 		exc( function() cast(v,MyClass.I2) );
-		
+
 		var v = new MyClass.ClassI2();
 		eq( cast(v,MyClass.I1), v );
 		eq( cast(v,MyClass.I2), v );
-		
+
 		var v = cast(v,MyClass.I2);
 		eq( cast(v,MyClass.I1), v );
-		exc( function() cast(v,MyClass.CovI) );		
+		exc( function() cast(v,MyClass.CovI) );
 
-		var v : MyClass.CovI = null;
-		eq( cast(v,MyClass.CovI), null );
-		
+		var v : MyClass.CovI = new MyClass.Cov2();
+		eq( cast(v,MyClass.CovI), v );
+
 		var v = new MyClass.Cov3();
 		eq( cast(v,MyClass.CovI), v );
 		eq( cast(v,MyClass.CovI2), v );
-		
+
 		exc(function() { cast(new MyClass.Cov1(), MyClass.CovI); });
-		
+
 	}
 	#end
 
