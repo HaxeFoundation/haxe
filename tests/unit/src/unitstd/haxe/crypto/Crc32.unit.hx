5333a5817f3c5625c550758eb092164af70f22c0
Dan Korostelev <nadako@gmail.com>
2019-02-18 22:40:01 +0100

    fix Crc32 tests?

--- tests/unit/src/unitstd/haxe/crypto/Crc32.unit.hx
+++ tests/unit/src/unitstd/haxe/crypto/Crc32.unit.hx
@@ -1,2 +1,2 @@
-haxe.crypto.Crc32.make(haxe.io.Bytes.fromString("")) == 0;
-haxe.crypto.Crc32.make(haxe.io.Bytes.fromString("The quick brown fox jumps over the lazy dog")) == 0x414FA339;
+haxe.crypto.Crc32.make(haxe.io.Bytes.ofString("")) == 0;
+haxe.crypto.Crc32.make(haxe.io.Bytes.ofString("The quick brown fox jumps over the lazy dog")) == 0x414FA339;
