2daf49b2404b5e4f1c574f90fe1fd2870be8dc49
Simon Krajewski <simon@haxe.org>
2019-03-01 12:07:42 +0100

    [tests] get the display tests to STFU

--- tests/display/src/Main.hx
+++ tests/display/src/Main.hx
@@ -1,5 +1,15 @@
+import utest.Runner;
+import utest.ui.Report;
+
 class Main {
 	static function main() {
-		utest.UTest.run(Macro.getCases("cases"));
+		var runner = new Runner();
+		for (c in Macro.getCases("cases")) {
+			runner.addCase(c);
+		}
+		var report = Report.create(runner);
+		report.displayHeader = AlwaysShowHeader;
+		report.displaySuccessResults = NeverShowSuccessResults;
+		runner.run();
 	}
 }
