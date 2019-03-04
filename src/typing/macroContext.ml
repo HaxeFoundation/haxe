368893d9c0b4e2e3471eafd9ebbf0769ee3693ba
Simon Krajewski <simon@haxe.org>
2019-03-04 12:33:08 +0100

    [parser] adjust Context.parse(InlineString)

--- src/typing/macroContext.ml
+++ src/typing/macroContext.ml
@@ -128,7 +128,9 @@ let make_macro_api ctx p =
 		typing_timer ctx false (fun() ->
 			try
 				begin match ParserEntry.parse_expr_string ctx.com.defines s p error inl with
-					| ParseSuccess data | ParseDisplayFile(data,_) -> data (* PARSERTODO *)
+					| ParseSuccess data -> data
+					| ParseDisplayFile(data,_) when inl -> data (* ignore errors when inline-parsing in display file *)
+					| ParseDisplayFile _ -> assert false (* cannot happen because ParserEntry.parse_string sets `display_position := null_pos;` *)
 					| ParseError _ -> raise MacroApi.Invalid_expr
 				end
 			with Exit ->
