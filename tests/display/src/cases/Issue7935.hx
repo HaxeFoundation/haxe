80fb8c56d17f888ccd1f251780018b1e08c08a59
Jens Fischer <jensfischer95@gmail.com>
2019-03-06 21:03:02 +0100

    [tests] add more tests for #7935

--- tests/display/src/cases/Issue7935.hx
+++ tests/display/src/cases/Issue7935.hx
@@ -8,13 +8,39 @@ class Issue7935 extends DisplayTestCase {
 			}
 		}
 	**/
-	function test() {
+	function test1() {
+		hasParserError("Expected expression or )");
+	}
+
+	/**
+		class Main {
+			public static function main() {
+				trace(0
+			{-1-}}{-2-}
+		}
+	**/
+	function test2() {
+		hasParserError("Expected , or )");
+	}
+
+	/**
+		class Main {
+			public static function main() {
+				trace(0,
+			{-1-}}{-2-}
+		}
+	**/
+	function test3() {
+		hasParserError("Expected expression");
+	}
+
+	function hasParserError(message:String) {
 		arrayEq([
 			{
 				kind: DKParserError,
 				range: diagnosticsRange(pos(1), pos(2)),
 				severity: Error,
-				args: "Expected expression or )"
+				args: message
 			}
 		], diagnostics());
 	}
---
+++ tests/misc/projects/Issue7935/Main3.hx
@@ -0,0 +1,5 @@
+class Main {
+	public static function main() {
+		trace(0,
+	}
+}
