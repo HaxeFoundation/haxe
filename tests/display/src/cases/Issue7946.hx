23ca866a7ae3b25006535ac1787fa7b7a0e6da7c
Jens Fischer <jensfischer95@gmail.com>
2019-03-07 12:01:51 +0100

    [tests] next batch of parser error diagnostics tests
    closes #7943
    closes #7945
    closes #7946
    see #7947

---
+++ tests/display/src/cases/Issue7946.hx
@@ -0,0 +1,31 @@
+package cases;
+
+class Issue7946 extends DisplayTestCase {
+	/**
+		{-1-}open{-2-} haxe.Json;
+	**/
+	function test1() {
+		arrayEq([
+			{
+				kind: DKParserError,
+				severity: Error,
+				range: diagnosticsRange(pos(1), pos(2)),
+				args: "Unexpected open"
+			}
+		], diagnostics());
+	}
+
+	/**
+		{-1-}clas{-2-} Test {}
+	**/
+	function test() {
+		arrayEq([
+			{
+				kind: DKParserError,
+				severity: Error,
+				range: diagnosticsRange(pos(1), pos(2)),
+				args: "Unexpected clas"
+			}
+		], diagnostics());
+	}
+}
