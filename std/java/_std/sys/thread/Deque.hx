c27c714d574075d992ca6a6393efbe0fcc798a29
Simon Krajewski <simon@haxe.org>
2019-03-17 11:30:52 +0100

    [java] use LinkedBlockingDeque instead of waneck-Deque
    closes #7996

--- std/java/_std/sys/thread/Deque.hx
+++ std/java/_std/sys/thread/Deque.hx
@@ -19,74 +19,33 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
+
 package sys.thread;
+
 import java.Lib;
 
 @:coreApi
 @:native('haxe.java.vm.Deque')
-@:nativeGen class Deque<T>
-{
-	@:private var head:Node<T>;
-	@:private var tail:Node<T>;
+@:nativeGen class Deque<T> {
+	var lbd:java.util.concurrent.LinkedBlockingDeque<T>;
 
-	public function new()
-	{
-		this.head = this.tail = new Node(null);
+	public function new() {
+		lbd = new java.util.concurrent.LinkedBlockingDeque<T>();
 	}
 
-	public function add(i : T):Void
-	{
-		var n = new Node(i);
-		untyped __lock__(this,
-		{
-			tail.next = n;
-			tail = n;
-			try { untyped this.notify(); } catch(e:Dynamic) { throw e; }
-		});
+	public function add(i:T):Void {
+		lbd.add(i);
 	}
 
-	public function push(i : T):Void
-	{
-		var n = new Node(i);
-		untyped __lock__(this,
-		{
-			n.next = head.next;
-			head.next = n;
-			try { untyped this.notify(); } catch(e:Dynamic) { throw e; }
-		});
+	public function push(i:T):Void {
+		lbd.push(i);
 	}
 
-	public function pop(block : Bool) : Null<T>
-	{
-		var ret = null;
-		untyped __lock__(this, {
-			var n = null;
-			do {
-				n = head.next;
-				if (n != null)
-				{
-					ret = n.value;
-					n.value = null;
-					head = n;
-				} else if (block) {
-					//block
-					try { untyped this.wait(); } catch(e:Dynamic) { throw e; }
-				}
-			} while( block && n == null );
-		});
-		return ret;
-	}
-}
-
-@:native('haxe.java.vm.DequeNode')
-@:nativeGen
-class Node<T>
-{
-	public var value:T;
-	public var next:Node<T>;
-
-	public function new(val)
-	{
-		this.value = val;
+	public inline function pop(block:Bool):Null<T> {
+		return if (block) {
+			lbd.take();
+		} else {
+			lbd.poll();
+		}
 	}
 }
