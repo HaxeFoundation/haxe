f04b0ca92e898422159435fff70285e62c818f46
Alexander Kuzmenko <alex@stablex.ru>
2019-04-05 17:09:35 +0300

    fix test for #8079

--- tests/unit/src/unit/issues/Issue8079.hx
+++ tests/unit/src/unit/issues/Issue8079.hx
@@ -2,9 +2,10 @@ package unit.issues;
 
 class Issue8079 extends unit.Test {
 	function test() {
-		var instance = ({}:Dynamic).instance;
+		var instance = ({}:Dynamic);
 		if(instance.timeout != null) {
 			instance.timeout = null;
 		}
+		utest.Assert.pass();
 	}
 }
