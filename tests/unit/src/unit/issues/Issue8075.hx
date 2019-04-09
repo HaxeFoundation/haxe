16cbcad1ffce68743085ddc43ab4e804b3705d6b
Alexander Kuzmenko <alex@stablex.ru>
2019-04-09 11:19:11 +0300

    [as3] fixed a test for #8075 (closes #8120)

--- tests/unit/src/unit/issues/Issue8075.hx
+++ tests/unit/src/unit/issues/Issue8075.hx
@@ -2,7 +2,7 @@ package unit.issues;
 
 class Issue8075 extends unit.Test {
 	function test() {
-		var expect = #if static 0 #else null #end;
+		var expect = #if (static && !as3) 0 #else null #end;
 		var a = [];
 		a.push(1);
 		a.pop();
