9f7428fee208fcfe4fc20a6727f39ed85409965b
Alexander Kuzmenko <alex@stablex.ru>
2019-04-11 23:50:32 +0300

    Restrict `\x` escape sequence  to values less than `\x80` (#8141)
    * restrict \x to values less than \x80 (closes #8119)
    
    * minor tweak
    
    * again
    
    * disabled test of #7449

--- tests/unit/src/unit/issues/Issue7449.hx
+++ tests/unit/src/unit/issues/Issue7449.hx
@@ -1,9 +1,12 @@
 package unit.issues;
 
 class Issue7449 extends unit.Test {
-	#if !(neko || (cpp && !cppia && !hxcpp_smart_strings))
-	function test() {
-		eq(220, "\xDC".charCodeAt(0));
-	}
-	#end
+	/**
+	 * Not happens since https://github.com/HaxeFoundation/haxe/pull/8141
+	 */
+	// #if !(neko || (cpp && !cppia && !hxcpp_smart_strings))
+	// function test() {
+	// 	eq(220, "\xDC".charCodeAt(0));
+	// }
+	// #end
 }
