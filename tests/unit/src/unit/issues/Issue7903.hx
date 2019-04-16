fbb9a3f6138ec112e16babd1a53653f0099ad19a
Alexander Kuzmenko <alex@stablex.ru>
2019-04-16 11:33:18 +0300

    added a test for recursive enums; [php] fixed stringification of recursive enums

--- tests/unit/src/unit/issues/Issue7903.hx
+++ tests/unit/src/unit/issues/Issue7903.hx
@@ -5,14 +5,29 @@ import utest.Assert;
 class Issue7903 extends unit.Test {
 	static var tmp:String;
 	function test() {
+		//Test recursive objects
 		var o = {rec:(null:Dynamic)};
 		o.rec = o;
 		try {
 			tmp = Std.string(o);
 			Assert.pass();
 		} catch(e:Dynamic) {
-			trace(e);
-			Assert.fail('Failed to stringify an object with recursive reference');
+			Assert.fail('Failed to stringify an object with recursive reference: $e');
+		}
+
+		//Test recursive enums
+		var o = {rec:(null:RecursiveEnum)};
+		var e = Ctor(o);
+		o.rec = e;
+		try {
+			tmp = Std.string(e);
+			Assert.pass();
+		} catch(e:Dynamic) {
+			Assert.fail('Failed to stringify an enum instance with recursive reference: $e');
 		}
 	}
+}
+
+private enum RecursiveEnum {
+	Ctor(o:{rec:Null<RecursiveEnum>});
 }
