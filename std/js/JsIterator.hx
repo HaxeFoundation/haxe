265174d24892bca37f944073bf88c960d7bd9e9a
Alexander Kuzmenko <alex@stablex.ru>
2019-04-17 12:39:54 +0300

    [js] fixed JsIteratorStep

--- std/js/JsIterator.hx
+++ std/js/JsIterator.hx
@@ -1,4 +1,4 @@
 package js;
 
 @:deprecated typedef JsIterator<T> = js.lib.Iterator<T>;
-@:deprecated typedef JsIteratorStep<T> = js.lib.Iterator.JsIteratorStep<T>;
+@:deprecated typedef JsIteratorStep<T> = js.lib.Iterator.IteratorStep<T>;
