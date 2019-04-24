7ac27b7c74b38221e436489755ba39a6a38aa240
Aurel Bílý <aurel.bily@gmail.com>
2019-04-24 22:52:25 +0100

    fix #8212 test better

--- tests/unit/src/unit/issues/Issue8212.hx
+++ tests/unit/src/unit/issues/Issue8212.hx
@@ -6,7 +6,7 @@ class Issue8212 extends unit.Test {
 	function test() {
 		var hex = "010a7f0ac2800adfbf0ae0a0800aed9fbf0aee80800aefbfbd0af09f9882f09f9884f09f98990ac8a70a";
 		var bytes = Bytes.ofHex(hex);
-		eq(hex.length, bytes.length);
+		eq(hex.length, bytes.length * 2);
 		eq(hex, bytes.toHex());
 	}
 }
