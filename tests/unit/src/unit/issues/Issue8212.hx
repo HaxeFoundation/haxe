c96195f55afb3d926e06af8ef844c243fc84dc45
Aurel Bílý <aurel.bily@gmail.com>
2019-04-24 22:44:35 +0100

    fix #8212 test

--- tests/unit/src/unit/issues/Issue8212.hx
+++ tests/unit/src/unit/issues/Issue8212.hx
@@ -4,7 +4,7 @@ import haxe.io.Bytes;
 
 class Issue8212 extends unit.Test {
 	function test() {
-		var hex = "010A7F0AC2800ADFBF0AE0A0800AED9FBF0AEE80800AEFBFBD0AF09F9882F09F9884F09F98990AC8A70A";
+		var hex = "010a7f0ac2800adfbf0ae0a0800aed9fbf0aee80800aefbfbd0af09f9882f09f9884f09f98990ac8a70a";
 		var bytes = Bytes.ofHex(hex);
 		eq(hex.length, bytes.length);
 		eq(hex, bytes.toHex());
