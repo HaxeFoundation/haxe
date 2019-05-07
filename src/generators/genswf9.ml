05a3c878a9b31f67b1e95bb0b54a4d0e5a9f1a06
Dan Korostelev <nadako@gmail.com>
2019-05-07 21:55:41 +0200

    make as3 tests pass
    should be good enough

--- src/generators/genswf9.ml
+++ src/generators/genswf9.ml
@@ -23,6 +23,7 @@ open Type
 open As3
 open As3hl
 open Common
+open FlashProps
 
 type read = Read
 type write = Unused__ | Write
@@ -315,47 +316,6 @@ let ns_access cf =
 			Some (HMName (cf.cf_name, HNInternal (Some ns)))
 		| _ -> assert false
 	with Not_found ->
-		None
-
-let is_getter_name name = ExtString.String.starts_with name "get_"
-let is_setter_name name = ExtString.String.starts_with name "set_"
-let get_property_name accessor_name = String.sub accessor_name 4 (String.length accessor_name - 4)
-let is_flash_property cf = Meta.has Meta.FlashProperty cf.cf_meta
-
-let find_property_for_accessor ~isget cl tl accessor_name =
-	let prop_name = get_property_name accessor_name in
-	try
-		match Type.class_field cl tl prop_name with
-		| Some (prop_cl, prop_tl), _, prop_cf ->
-			(match prop_cf.cf_kind with
-			| Var { v_read = AccCall; v_write = AccCall | AccNever } when isget && is_flash_property prop_cf -> Some (prop_cl, prop_tl, prop_cf)
-			| Var { v_read = AccCall | AccNever; v_write = AccCall } when not isget && is_flash_property prop_cf -> Some (prop_cl, prop_tl, prop_cf)
-			| _ -> None)
-		| _ -> None
-	with Not_found ->
-		None
-
-let is_extern_instance_accessor ~isget cl tl cf =
-	if cl.cl_extern && (if isget then is_getter_name cf.cf_name else is_setter_name cf.cf_name) then
-		find_property_for_accessor ~isget cl tl cf.cf_name
-	else
-		None
-
-let find_static_property_for_accessor ~isget cl accessor_name =
-	let prop_name = get_property_name accessor_name in
-	try
-		let prop_cf = PMap.find prop_name cl.cl_statics in
-		(match prop_cf.cf_kind with
-		| Var { v_read = AccCall; v_write = AccCall | AccNever } when isget && is_flash_property prop_cf -> Some prop_cf
-		| Var { v_read = AccCall | AccNever; v_write = AccCall } when not isget && is_flash_property prop_cf -> Some prop_cf
-		| _ -> None)
-	with Not_found ->
-		None
-
-let is_extern_static_accessor ~isget cl cf =
-	if cl.cl_extern && (if isget then is_getter_name cf.cf_name else is_setter_name cf.cf_name) then
-		find_static_property_for_accessor ~isget cl cf.cf_name
-	else
 		None
 
 let property ctx fa t =
