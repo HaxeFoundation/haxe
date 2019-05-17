84711c810ec51eefd19491b8e6334da457bf1179
Simon Krajewski <simon@haxe.org>
2019-05-17 10:17:58 +0200

    [inliner] fix inline_constructors logic wrt expression-inline
    closes #8240

---
+++ tests/unit/src/unit/issues/Issue8240.hx
@@ -0,0 +1,14 @@
+package unit.issues;
+
+private class Suite {
+	public function new() {}
+}
+
+class Issue8240 extends unit.Test {
+	@:analyzer(ignore)
+	function test() {
+		var suite = new Suite();
+		var f = function() (for (c in inline new haxe.iterators.StringIteratorUnicode("")) {});
+		utest.Assert.pass();
+	}
+}
