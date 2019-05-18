1b5e363e1923914d29fd699074376d9bde565df1
Aleksandr Kuzmenko <alex@stablex.ru>
2019-05-18 10:37:38 +0300

    fix cs

--- std/cs/_std/Std.hx
+++ std/cs/_std/Std.hx
@@ -191,7 +191,7 @@ import cs.internal.Exceptions;
 			Math.NaN;
 	}
 
-	extern public static function downcast<T:{},S:T>( value : T, c : Class<S> ) : S {
+	extern inline public static function downcast<T:{},S:T>( value : T, c : Class<S> ) : S {
 		return cs.Lib.as(value,c);
 	}
 
