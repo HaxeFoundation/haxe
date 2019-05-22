951cfdd4432f26a44ec1728780cc04cb0772fb35
Aleksandr Kuzmenko <alex@stablex.ru>
2019-05-22 11:54:03 +0300

    test Float too

--- tests/unit/src/unit/issues/Issue6942.hx
+++ tests/unit/src/unit/issues/Issue6942.hx
@@ -1,12 +1,29 @@
 package unit.issues;
 
 class Issue6942 extends unit.Test {
+	static inline var FLOAT_INLINE = -1.0;
+	static inline var INT_INLINE = -1;
+
 	function test() {
-		eq(1, -First);
-		eq(2, 1 - First);
+		eq(1, -IntEnum);
+		eq(2, 1 - IntEnum);
+
+		eq(1, -INT_INLINE);
+		eq(2, 1 - INT_INLINE);
+
+		// TODO: fix https://github.com/HaxeFoundation/haxe/issues/8321
+		// eq(1.0, -FloatEnum);
+		// eq(2.0, 1 - FloatEnum);
+
+		eq(1.0, -FLOAT_INLINE);
+		eq(2.0, 1 - FLOAT_INLINE);
 	}
 }
 
-private enum abstract Test(Int) from Int to Int {
-	var First = -1;
+enum abstract FloatTest(Float) from Float to Float {
+	var FloatEnum = -1.0;
+}
+
+enum abstract IntTest(UInt) from UInt to UInt {
+ var IntEnum = -1;
 }
