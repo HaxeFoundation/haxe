c16d2a69c3d1f79c5d98a39702413b5d05c963a6
Aleksandr Kuzmenko <alex@stablex.ru>
2019-06-27 11:55:00 +0300

    [cpp] disable test #4649 (see #8485)

--- tests/unit/src/unit/issues/Issue4649.hx
+++ tests/unit/src/unit/issues/Issue4649.hx
@@ -2,14 +2,15 @@ package unit.issues;
 
 class Issue4649 extends unit.Test {
 	static var v:Val<Float> = 10.5;
-
+#if !cpp
 	function test() {
 		v.incr();
 		eq(11.5, v);
 	}
+#end
 }
 
-abstract Val<T:Float>(T) from T to T {
+private abstract Val<T:Float>(T) from T to T {
 	public inline function incr():Void
 		this++;
 }
