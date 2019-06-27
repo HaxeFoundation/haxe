c53201dc3fdbe1ab8a58391174f6e9d3ba8ca887
Aleksandr Kuzmenko <alex@stablex.ru>
2019-06-27 20:49:33 +0300

    Added a typer pass between PBuildClass and PTypeField (#8456)
    * added a typer pass between PBuildClass and PTypeField to handle property-accessors relations
    
    * its 6226
    
    * no need for delay_late in `TypeloadFields.create_property`

--- src/context/typecore.ml
+++ src/context/typecore.ml
@@ -47,6 +47,7 @@ type macro_mode =
 type typer_pass =
 	| PBuildModule			(* build the module structure and setup module type parameters *)
 	| PBuildClass			(* build the class structure *)
+	| PConnectField			(* handle associated fields, which may affect each other. E.g. a property and its getter *)
 	| PTypeField			(* type the class field, allow access to types structures *)
 	| PCheckConstraint		(* perform late constraint checks with inferred types *)
 	| PForce				(* usually ensure that lazy have been evaluated *)
