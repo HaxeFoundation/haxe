ad4c88aa53ed3f03a60f1ee609203bdeb7ee0b89
Aleksandr Kuzmenko <alex@stablex.ru>
2019-06-28 23:57:17 +0300

    fix misc/cs tests

--- tests/misc/cs/projects/Issue4623/Run.hx
+++ tests/misc/cs/projects/Issue4623/Run.hx
@@ -1,11 +1,10 @@
 class Run {
 	static public function run() {
-		var exe = 'bin/bin/Main.exe';
 		var exitCode = switch (Sys.systemName()) {
 			case 'Windows':
-				Sys.command(exe);
+				Sys.command('bin\\bin\\Main.exe');
 			case _:
-				Sys.command('mono', [exe]);
+				Sys.command('mono', ['bin/bin/Main.exe']);
 		}
 		Sys.exit(exitCode);
 	}
