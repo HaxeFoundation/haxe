19167a20e9daa6060e5c5b7e7852b482fb1a5c43
Aleksandr Kuzmenko <alex@stablex.ru>
2019-06-29 12:34:53 +0300

    disable test #4481 for jvm

--- tests/unit/src/unit/issues/Issue4481.hx
+++ tests/unit/src/unit/issues/Issue4481.hx
@@ -2,11 +2,12 @@ package unit.issues;
 
 class Issue4481 extends unit.Test {
 	static var s:String = null;
-
+#if !jvm
 	function test() {
 		switch s {
 			case 'a': assert();
 			case _: eq(null, s);
 		}
 	}
+#end
 }
