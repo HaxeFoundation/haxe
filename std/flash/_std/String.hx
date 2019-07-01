52795e878c37c5e810df478c872a7c3d44e187b4
Aurel Bílý <aurel.bily@gmail.com>
2019-07-01 15:57:22 +0100

    fix 4 ...

--- std/flash/_std/String.hx
+++ std/flash/_std/String.hx
@@ -34,19 +34,6 @@ extern class String {
 	function substring(startIndex:Int, ?endIndex:Int):String;
 	function toString():String;
 
-	var length(default, null):Int;
-	function new(string:String):Void;
-	function toUpperCase():String;
-	function toLowerCase():String;
-	function charAt(index:Int):String;
-	function charCodeAt(index:Int):Null<Int>;
-	function indexOf(str:String, ?startIndex:Int):Int;
-	function lastIndexOf(str:String, ?startIndex:Int):Int;
-	function split(delimiter:String):Array<String>;
-	function substr(pos:Int, ?len:Int):String;
-	function substring(startIndex:Int, ?endIndex:Int):String;
-	function toString():String;
-
 	@:pure static inline function fromCharCode(code:Int):String
 		untyped {
 			return code < 0x10000 ? String["fromCharCode"](code) : flash.Boot.fromCodePoint(code);
