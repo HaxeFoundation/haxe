f69bf5c48b53fe6d3e8c02b4229b6b3af4c22eda
Aurel Bílý <aurel.bily@gmail.com>
2019-07-05 15:45:41 +0200

    test fromTime

--- tests/unit/src/unitstd/Date.unit.hx
+++ tests/unit/src/unitstd/Date.unit.hx
@@ -25,16 +25,20 @@ date.getTime() == 405781340000;
 // timezone issues
 var date1 = Date.fromTime(1455555555 * 1000.); // 15 Feb 2016 16:59:15 GMT
 var date2 = new Date(2016, 1, 15, 16, 59, 15);
+#if azure
 date1.getTime() == date2.getTime(); // depends on Azure timezone setting!
+#end
 
 var referenceDate = new Date(1970, 0, 1, 2, 0, 0);
 referenceDate.toString() == "1970-01-01 02:00:00";
+#if azure
 referenceDate.getTime() == 7200000.; // depends on Azure timezone setting!
+#end
 
 var date = new Date(1970, 0, 1, 1, 59, 59);
 date.getTime() < referenceDate.getTime();
 
-// < 1970
+// < 1970 (negative timestamp)
 var date = new Date(1904, 11, 12, 1, 4, 1);
 date.getHours() == 1;
 date.getMinutes() == 4;
@@ -45,7 +49,12 @@ date.getDate() == 12;
 date.getDay() == 1;
 date.getTime() < referenceDate.getTime();
 
-// < 1902
+var date = Date.fromTime(-2052910800.0);
+date.getFullYear() == 1904;
+date.getMonth() == 11;
+date.getDate() == 12; // could fail on very large UTC offsets
+
+// < 1902 (negative timestamp, outside of signed 32-bit integer range)
 var date = new Date(1888, 0, 1, 15, 4, 2);
 date.getHours() == 15;
 date.getMinutes() == 4;
@@ -56,7 +65,12 @@ date.getDate() == 1;
 date.getDay() == 0;
 date.getTime() < referenceDate.getTime();
 
-// Y2038
+var date = Date.fromTime(-2587294800.0);
+date.getFullYear() == 1888;
+date.getMonth() == 0;
+date.getDate() == 5; // could fail on very large UTC offsets
+
+// Y2038 (outside of signed 32-bit integer range)
 var date = new Date(2039, 0, 1, 1, 59, 59);
 date.getHours() == 1;
 date.getMinutes() == 59;
@@ -66,6 +80,27 @@ date.getMonth() == 0;
 date.getDate() == 1;
 date.getDay() == 6;
 date.getTime() > referenceDate.getTime();
+
+var date = Date.fromTime(2177838000.0);
+date.getFullYear() == 2039;
+date.getMonth() == 0;
+date.getDate() == 5; // could fail on very large UTC offsets
+
+// Y2112 (outside of unsigned 32-bit integer range)
+var date = new Date(2112, 0, 1, 1, 59, 59);
+date.getHours() == 1;
+date.getMinutes() == 59;
+date.getSeconds() == 59;
+date.getFullYear() == 2112;
+date.getMonth() == 0;
+date.getDate() == 1;
+date.getDay() == 5;
+date.getTime() > referenceDate.getTime();
+
+var date = Date.fromTime(4481434800.0);
+date.getFullYear() == 2039;
+date.getMonth() == 0;
+date.getDate() == 5; // could fail on very large UTC offsets
 
 // weekdays
 (new Date(2019, 6, 1, 12, 0, 0)).getDay() == 1;
