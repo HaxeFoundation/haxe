c6f9190b0b99039fbc2eacfb7a37ffb767e98ea0
Aurel Bílý <aurel.bily@gmail.com>
2019-07-05 11:58:45 +0200

    pre-1902 dates

--- tests/unit/src/unitstd/Date.unit.hx
+++ tests/unit/src/unitstd/Date.unit.hx
@@ -7,7 +7,6 @@ date.getFullYear() == 1982;
 date.getMonth() == 10;
 date.getDate() == 10;
 date.getDay() == 3;
-//date.getTime() == 405781340000.;
 date.toString() == "1982-11-10 14:02:20";
 
 var date = Date.fromTime(date.getTime());
@@ -18,7 +17,6 @@ date.getFullYear() == 1982;
 date.getMonth() == 10;
 date.getDate() == 10;
 date.getDay() == 3;
-//date.getTime() == 405781340000.;
 date.toString() == "1982-11-10 14:02:20";
 
 var date = Date.fromTime(405781340000);
@@ -27,21 +25,44 @@ date.getTime() == 405781340000;
 // timezone issues
 var date1 = Date.fromTime(1455555555 * 1000.); // 15 Feb 2016 16:59:15 GMT
 var date2 = new Date(2016, 1, 15, 16, 59, 15);
-date1.getTime() == date2.getTime();
+date1.getTime() == date2.getTime(); // depends on Azure timezone setting!
 
 var referenceDate = new Date(1970, 0, 1, 2, 0, 0);
 referenceDate.toString() == "1970-01-01 02:00:00";
-referenceDate.getTime() == 7200000.;
+referenceDate.getTime() == 7200000.; // depends on Azure timezone setting!
 
 var date = new Date(1970, 0, 1, 1, 59, 59);
 date.getTime() < referenceDate.getTime();
 
-// < 1970 problems
+// < 1970
 var date = new Date(1904, 11, 12, 1, 4, 1);
-date.getFullYear() == 1904;
 date.getHours() == 1;
+date.getMinutes() == 4;
+date.getSeconds() == 1;
+date.getFullYear() == 1904;
+date.getMonth() == 10;
+date.getDate() == 12;
+date.getDay() == 1;
+date.getTime() < referenceDate.getTime();
 
-// Y2038 problems
+// < 1902
+var date = new Date(1888, 0, 1, 15, 4, 2);
+date.getHours() == 15;
+date.getMinutes() == 4;
+date.getSeconds() == 2;
+date.getFullYear() == 1888;
+date.getMonth() == 0;
+date.getDate() == 1;
+date.getDay() == 0;
+date.getTime() < referenceDate.getTime();
+
+// Y2038
 var date = new Date(2039, 0, 1, 1, 59, 59);
+date.getHours() == 1;
+date.getMinutes() == 59;
+date.getSeconds() == 59;
 date.getFullYear() == 2039;
+date.getMonth() == 0;
+date.getDate() == 1;
+date.getDay() == 6;
 date.getTime() > referenceDate.getTime();
