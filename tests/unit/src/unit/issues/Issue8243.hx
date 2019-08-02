c6ede59cada98cf75510db0917ab1bf9d0086309
Aleksandr Kuzmenko <alex@stablex.ru>
2019-08-02 19:58:01 +0300

    [hl] fix stack overflow on recursive abstract (closes #8243)

--- tests/unit/src/unit/issues/Issue8243.hx
+++ tests/unit/src/unit/issues/Issue8243.hx
@@ -1,14 +1,11 @@
 package unit.issues;
 
 class Issue8243 extends unit.Test {
-#if !hl // TODO https://github.com/HaxeFoundation/haxe/issues/8243
-
 	var involveRecursiveAbstractTyping:Null<Rec> = null;
 
 	function test() {
 		t(involveRecursiveAbstractTyping == null);
 	}
-#end
 }
 
 private abstract Rec(Array<Rec>) from Array<Rec> {}
