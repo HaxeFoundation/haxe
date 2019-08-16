3811361d88b9587a1aa98985eb5a3e41f8b3bd2f
Aurel Bílý <aurel.bily@gmail.com>
2019-08-16 20:15:48 +0100

    remove std comments in python

--- std/python/_std/sys/net/Socket.hx
+++ std/python/_std/sys/net/Socket.hx
@@ -107,31 +107,15 @@ private class SocketOutput extends haxe.io.Output {
 	}
 }
 
-/**
-	A TCP socket class : allow you to both connect to a given server and exchange messages or start your own server and wait for connections.
-**/
 @:coreApi class Socket {
 	var __s:PSocket;
 
-	/**
-		The stream on which you can read available data. By default the stream is blocking until the requested data is available,
-		use `setBlocking(false)` or `setTimeout` to prevent infinite waiting.
-	**/
 	public var input(default, null):haxe.io.Input;
 
-	/**
-		The stream on which you can send data. Please note that in case the output buffer you will block while writing the data, use [setBlocking(false)] or [setTimeout] to prevent that.
-	**/
 	public var output(default, null):haxe.io.Output;
 
-	/**
-			A custom value that can be associated with the socket. Can be used to retrieve your custom infos after a `select`.
-	***/
 	public var custom:Dynamic;
 
-	/**
-		Creates a new unconnected socket.
-	**/
 	public function new():Void {
 		__initSocket();
 		input = new SocketInput(__s);
@@ -142,59 +126,35 @@ private class SocketOutput extends haxe.io.Output {
 		__s = new PSocket();
 	}
 
-	/**
-		Closes the socket : make sure to properly close all your sockets or you will crash when you run out of file descriptors.
-	**/
 	public function close():Void {
 		__s.close();
 	}
 
-	/**
-		Read the whole data available on the socket.
-	**/
 	public function read():String {
 		return input.readAll().toString();
 	}
 
-	/**
-		Write the whole data to the socket output.
-	**/
 	public function write(content:String):Void {
 		output.writeString(content);
 	}
 
-	/**
-		Connect to the given server host/port. Throw an exception in case we couldn't successfully connect.
-	**/
 	public function connect(host:Host, port:Int):Void {
 		var host_str = host.toString();
 		__s.connect(Tuple2.make(host_str, port));
 	}
 
-	/**
-		Allow the socket to listen for incoming questions. The parameter tells how many pending connections we can have until they get refused. Use `accept()` to accept incoming connections.
-	**/
 	public function listen(connections:Int):Void {
 		__s.listen(connections);
 	}
 
-	/**
-		Shutdown the socket, either for reading or writing.
-	**/
 	public function shutdown(read:Bool, write:Bool):Void
 		__s.shutdown((read && write) ? PSocketModule.SHUT_RDWR : read ? PSocketModule.SHUT_RD : PSocketModule.SHUT_WR);
 
-	/**
-		Bind the socket to the given host/port so it can afterwards listen for connections there.
-	**/
 	public function bind(host:Host, port:Int):Void {
 		var host_str = host.toString();
 		__s.bind(Tuple2.make(host_str, port));
 	}
 
-	/**
-		Accept a new connected client. This will return a connected socket on which you can read/write some data.
-	**/
 	public function accept():Socket {
 		var tp2:Tuple2<PSocket, PAddress> = __s.accept();
 		var s = new Socket();
@@ -204,46 +164,28 @@ private class SocketOutput extends haxe.io.Output {
 		return s;
 	}
 
-	/**
-		Return the information about the other side of a connected socket.
-	**/
 	public function peer():{host:Host, port:Int} {
 		var pn = __s.getpeername();
 		return {host: new Host(pn._1), port: pn._2}
 	}
 
-	/**
-		Return the information about our side of a connected socket.
-	**/
 	public function host():{host:Host, port:Int} {
 		var pn = __s.getsockname();
 		return {host: new Host(pn._1), port: pn._2};
 	}
 
-	/**
-		Gives a timeout after which blocking socket operations (such as reading and writing) will abort and throw an exception.
-	**/
 	public function setTimeout(timeout:Float):Void {
 		__s.settimeout(timeout);
 	}
 
-	/**
-		Block until some data is available for read on the socket.
-	**/
 	public function waitForRead():Void {
 		Select.select([this], [], []);
 	}
 
-	/**
-		Change the blocking mode of the socket. A blocking socket is the default behavior. A non-blocking socket will abort blocking operations immediately by throwing a haxe.io.Error.Blocked value.
-	**/
 	public function setBlocking(b:Bool):Void {
 		__s.setblocking(b);
 	}
 
-	/**
-		Allows the socket to immediately send the data when written to its output : this will cause less ping but might increase the number of packets / data size, especially when doing a lot of small writes.
-	**/
 	public function setFastSend(b:Bool):Void {
 		__s.setsockopt(PSocketModule.SOL_TCP, PSocketModule.TCP_NODELAY, b);
 	}
@@ -251,14 +193,6 @@ private class SocketOutput extends haxe.io.Output {
 	@:keep function fileno():Int
 		return __s.fileno();
 
-	/**
-		Wait until one of the sockets groups is ready for the given operation :
-		- `read` contains sockets on which we want to wait for available data to be read,
-		- `write` contains sockets on which we want to wait until we are allowed to write some data to their output buffers,
-		- `others` contains sockets on which we want to wait for exceptional conditions.
-		- `select` will block until one of the condition is met, in which case it will return the sockets for which the condition was true.
-		In case a `timeout` (in seconds) is specified, select might wait at worse until the timeout expires.
-	**/
 	public static function select(read:Array<Socket>, write:Array<Socket>, others:Array<Socket>,
 			?timeout:Float):{read:Array<Socket>, write:Array<Socket>, others:Array<Socket>} {
 		var t3 = Select.select(read, write, others, timeout);
