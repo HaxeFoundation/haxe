011f25109e6f9e4ff60d89ed699381c0ebf37844
ConstNW <constnw@gmail.com>
2019-09-03 09:31:31 +0300

    remove the docstrings

--- std/hl/_std/sys/thread/Lock.hx
+++ std/hl/_std/sys/thread/Lock.hx
@@ -31,60 +31,16 @@ extern class Lock {
 }
 #else
 
-/**
-	A Lock allows blocking execution until it has been unlocked. It keeps track
-	of how often `release` has been called, and blocks exactly as many `wait`
-	calls.
-
-	The order of the `release` and `wait` calls is irrelevant. That is, a Lock
-	can be released before anyone waits for it. In that case, the `wait` call
-	will execute immediately.
-
-	Usage example:
-
-	```
-		var lock = new Lock();
-		var elements = [1, 2, 3];
-		for (element in elements) {
-			// Create one thread per element
-			Thread.create(function() {
-				trace(element);
-				Sys.sleep(1);
-				// Release once per thread = 3 times
-				lock.release();
-			});
-		}
-		for (_ in elements) {
-			// Wait 3 times
-			lock.wait();
-		}
-		trace("All threads finished");
-	```
-**/
 abstract Lock(hl.Abstract<"hl_lock">) {
-	/**
-		Creates a new Lock which is initially locked.
-	**/
+
 	public function new() {
 		this = create();
 	}
 
-	/**
-		Waits for the lock to be released, or `timeout` (in seconds)
-		to expire. Returns `true` if the lock is released and `false`
-		if a time-out occurs.
-	**/
 	@:hlNative("std", "lock_wait") public function wait( ?timeout : Float ) : Bool {
 		return false;
 	}
 
-	/**
-		Releases the lock once.
-
-		The thread does not need to own the lock in order to release
-		it. Each call to `release` allows exactly one call to `wait`
-		to execute.
-	**/
 	@:hlNative("std", "lock_release") public function release( ) : Void {}
 
 	@:hlNative("std", "lock_create") public static function create( ) {
