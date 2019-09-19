133950b6be200db05e18759d6b3635b44a906aed
k <k@klabz.org>
2019-09-19 06:36:13 +0200

    Actually test the getter

--- tests/unit/src/unit/issues/Issue6195.hx
+++ tests/unit/src/unit/issues/Issue6195.hx
@@ -2,10 +2,10 @@ package unit.issues;
 
 class Issue6195 extends Test {
 	static var foo(get, default):Int;
-	static function get_foo() return foo;
+	static function get_foo() return foo + 1;
 
 	function test() {
 		foo = 10;
-		eq(foo, 10);
+		eq(11, foo);
 	}
 }
