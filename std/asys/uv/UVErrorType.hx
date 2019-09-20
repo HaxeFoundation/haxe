2d54ddc8d4007274415243742ea9c5f732042b5a
Aurel Bílý <aurel.bily@gmail.com>
2019-09-20 15:22:00 +0100

    error code docstrings

--- std/asys/uv/UVErrorType.hx
+++ std/asys/uv/UVErrorType.hx
@@ -1,81 +1,388 @@
 package asys.uv;
 
 extern enum abstract UVErrorType(Int) {
-	var E2BIG; // "argument list too long"
-	var EACCES; // "permission denied"
-	var EADDRINUSE; // "address already in use"
-	var EADDRNOTAVAIL; // "address not available"
-	var EAFNOSUPPORT; // "address family not supported"
-	var EAGAIN; // "resource temporarily unavailable"
-	var EAI_ADDRFAMILY; // "address family not supported"
-	var EAI_AGAIN; // "temporary failure"
-	var EAI_BADFLAGS; // "bad ai_flags value"
-	var EAI_BADHINTS; // "invalid value for hints"
-	var EAI_CANCELED; // "request canceled"
-	var EAI_FAIL; // "permanent failure"
-	var EAI_FAMILY; // "ai_family not supported"
-	var EAI_MEMORY; // "out of memory"
-	var EAI_NODATA; // "no address"
-	var EAI_NONAME; // "unknown node or service"
-	var EAI_OVERFLOW; // "argument buffer overflow"
-	var EAI_PROTOCOL; // "resolved protocol is unknown"
-	var EAI_SERVICE; // "service not available for socket type"
-	var EAI_SOCKTYPE; // "socket type not supported"
-	var EALREADY; // "connection already in progress"
-	var EBADF; // "bad file descriptor"
-	var EBUSY; // "resource busy or locked"
-	var ECANCELED; // "operation canceled"
-	var ECHARSET; // "invalid Unicode character"
-	var ECONNABORTED; // "software caused connection abort"
-	var ECONNREFUSED; // "connection refused"
-	var ECONNRESET; // "connection reset by peer"
-	var EDESTADDRREQ; // "destination address required"
-	var EEXIST; // "file already exists"
-	var EFAULT; // "bad address in system call argument"
-	var EFBIG; // "file too large"
-	var EHOSTUNREACH; // "host is unreachable"
-	var EINTR; // "interrupted system call"
-	var EINVAL; // "invalid argument"
-	var EIO; // "i/o error"
-	var EISCONN; // "socket is already connected"
-	var EISDIR; // "illegal operation on a directory"
-	var ELOOP; // "too many symbolic links encountered"
-	var EMFILE; // "too many open files"
-	var EMSGSIZE; // "message too long"
-	var ENAMETOOLONG; // "name too long"
-	var ENETDOWN; // "network is down"
-	var ENETUNREACH; // "network is unreachable"
-	var ENFILE; // "file table overflow"
-	var ENOBUFS; // "no buffer space available"
-	var ENODEV; // "no such device"
-	var ENOENT; // "no such file or directory"
-	var ENOMEM; // "not enough memory"
-	var ENONET; // "machine is not on the network"
-	var ENOPROTOOPT; // "protocol not available"
-	var ENOSPC; // "no space left on device"
-	var ENOSYS; // "function not implemented"
-	var ENOTCONN; // "socket is not connected"
-	var ENOTDIR; // "not a directory"
-	var ENOTEMPTY; // "directory not empty"
-	var ENOTSOCK; // "socket operation on non-socket"
-	var ENOTSUP; // "operation not supported on socket"
-	var EPERM; // "operation not permitted"
-	var EPIPE; // "broken pipe"
-	var EPROTO; // "protocol error"
-	var EPROTONOSUPPORT; // "protocol not supported"
-	var EPROTOTYPE; // "protocol wrong type for socket"
-	var ERANGE; // "result too large"
-	var EROFS; // "read-only file system"
-	var ESHUTDOWN; // "cannot send after transport endpoint shutdown"
-	var ESPIPE; // "invalid seek"
-	var ESRCH; // "no such process"
-	var ETIMEDOUT; // "connection timed out"
-	var ETXTBSY; // "text file is busy"
-	var EXDEV; // "cross-device link not permitted"
-	var UNKNOWN; // "unknown error"
-	var EOF; // "end of file"
-	var ENXIO; // "no such device or address"
-	var EMLINK; // "too many links"
-	var EHOSTDOWN; // "host is down"
+	/**
+		Argument list too long.
+	**/
+	var E2BIG;
+
+	/**
+		Permission denied.
+	**/
+	var EACCES;
+
+	/**
+		Address already in use.
+	**/
+	var EADDRINUSE;
+
+	/**
+		Address not available.
+	**/
+	var EADDRNOTAVAIL;
+
+	/**
+		Address family not supported.
+	**/
+	var EAFNOSUPPORT;
+
+	/**
+		Resource temporarily unavailable.
+	**/
+	var EAGAIN;
+
+	/**
+		Address family not supported.
+	**/
+	var EAI_ADDRFAMILY;
+
+	/**
+		Temporary failure.
+	**/
+	var EAI_AGAIN;
+
+	/**
+		Bad ai_flags value.
+	**/
+	var EAI_BADFLAGS;
+
+	/**
+		Invalid value for hints.
+	**/
+	var EAI_BADHINTS;
+
+	/**
+		Request canceled.
+	**/
+	var EAI_CANCELED;
+
+	/**
+		Permanent failure.
+	**/
+	var EAI_FAIL;
+
+	/**
+		Ai_family not supported.
+	**/
+	var EAI_FAMILY;
+
+	/**
+		Out of memory.
+	**/
+	var EAI_MEMORY;
+
+	/**
+		No address.
+	**/
+	var EAI_NODATA;
+
+	/**
+		Unknown node or service.
+	**/
+	var EAI_NONAME;
+
+	/**
+		Argument buffer overflow.
+	**/
+	var EAI_OVERFLOW;
+
+	/**
+		Resolved protocol is unknown.
+	**/
+	var EAI_PROTOCOL;
+
+	/**
+		Service not available for socket type.
+	**/
+	var EAI_SERVICE;
+
+	/**
+		Socket type not supported.
+	**/
+	var EAI_SOCKTYPE;
+
+	/**
+		Connection already in progress.
+	**/
+	var EALREADY;
+
+	/**
+		Bad file descriptor.
+	**/
+	var EBADF;
+
+	/**
+		Resource busy or locked.
+	**/
+	var EBUSY;
+
+	/**
+		Operation canceled.
+	**/
+	var ECANCELED;
+
+	/**
+		Invalid Unicode character.
+	**/
+	var ECHARSET;
+
+	/**
+		Software caused connection abort.
+	**/
+	var ECONNABORTED;
+
+	/**
+		Connection refused.
+	**/
+	var ECONNREFUSED;
+
+	/**
+		Connection reset by peer.
+	**/
+	var ECONNRESET;
+
+	/**
+		Destination address required.
+	**/
+	var EDESTADDRREQ;
+
+	/**
+		File already exists.
+	**/
+	var EEXIST;
+
+	/**
+		Bad address in system call argument.
+	**/
+	var EFAULT;
+
+	/**
+		File too large.
+	**/
+	var EFBIG;
+
+	/**
+		Host is unreachable.
+	**/
+	var EHOSTUNREACH;
+
+	/**
+		Interrupted system call.
+	**/
+	var EINTR;
+
+	/**
+		Invalid argument.
+	**/
+	var EINVAL;
+
+	/**
+		I/o error.
+	**/
+	var EIO;
+
+	/**
+		Socket is already connected.
+	**/
+	var EISCONN;
+
+	/**
+		Illegal operation on a directory.
+	**/
+	var EISDIR;
+
+	/**
+		Too many symbolic links encountered.
+	**/
+	var ELOOP;
+
+	/**
+		Too many open files.
+	**/
+	var EMFILE;
+
+	/**
+		Message too long.
+	**/
+	var EMSGSIZE;
+
+	/**
+		Name too long.
+	**/
+	var ENAMETOOLONG;
+
+	/**
+		Network is down.
+	**/
+	var ENETDOWN;
+
+	/**
+		Network is unreachable.
+	**/
+	var ENETUNREACH;
+
+	/**
+		File table overflow.
+	**/
+	var ENFILE;
+
+	/**
+		No buffer space available.
+	**/
+	var ENOBUFS;
+
+	/**
+		No such device.
+	**/
+	var ENODEV;
+
+	/**
+		No such file or directory.
+	**/
+	var ENOENT;
+
+	/**
+		Not enough memory.
+	**/
+	var ENOMEM;
+
+	/**
+		Machine is not on the network.
+	**/
+	var ENONET;
+
+	/**
+		Protocol not available.
+	**/
+	var ENOPROTOOPT;
+
+	/**
+		No space left on device.
+	**/
+	var ENOSPC;
+
+	/**
+		Function not implemented.
+	**/
+	var ENOSYS;
+
+	/**
+		Socket is not connected.
+	**/
+	var ENOTCONN;
+
+	/**
+		Not a directory.
+	**/
+	var ENOTDIR;
+
+	/**
+		Directory not empty.
+	**/
+	var ENOTEMPTY;
+
+	/**
+		Socket operation on non-socket.
+	**/
+	var ENOTSOCK;
+
+	/**
+		Operation not supported on socket.
+	**/
+	var ENOTSUP;
+
+	/**
+		Operation not permitted.
+	**/
+	var EPERM;
+
+	/**
+		Broken pipe.
+	**/
+	var EPIPE;
+
+	/**
+		Protocol error.
+	**/
+	var EPROTO;
+
+	/**
+		Protocol not supported.
+	**/
+	var EPROTONOSUPPORT;
+
+	/**
+		Protocol wrong type for socket.
+	**/
+	var EPROTOTYPE;
+
+	/**
+		Result too large.
+	**/
+	var ERANGE;
+
+	/**
+		Read-only file system.
+	**/
+	var EROFS;
+
+	/**
+		Cannot send after transport endpoint shutdown.
+	**/
+	var ESHUTDOWN;
+
+	/**
+		Invalid seek.
+	**/
+	var ESPIPE;
+
+	/**
+		No such process.
+	**/
+	var ESRCH;
+
+	/**
+		Connection timed out.
+	**/
+	var ETIMEDOUT;
+
+	/**
+		Text file is busy.
+	**/
+	var ETXTBSY;
+
+	/**
+		Cross-device link not permitted.
+	**/
+	var EXDEV;
+
+	/**
+		Unknown error.
+	**/
+	var UNKNOWN;
+
+	/**
+		End of file.
+	**/
+	var EOF;
+
+	/**
+		No such device or address.
+	**/
+	var ENXIO;
+
+	/**
+		Too many links.
+	**/
+	var EMLINK;
+
+	/**
+		Host is down.
+	**/
+	var EHOSTDOWN;
+
+	/**
+		Unknown error within libuv or libuv glue code.
+	**/
 	var EOTHER;
 }
