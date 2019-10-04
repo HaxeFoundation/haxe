7a5a114013012da912080bafcfa2022bb35f43fa
George Corney <haxiomic@gmail.com>
2019-10-04 08:11:09 +0100

    [printer] don't print `var` with `final` accessor. Closes #8859 (#8863)
    * [printer] don't print `var` with `final` accessor. Closes #8859
    
    * [test] add regression test for #8859

--- std/haxe/macro/Printer.hx
+++ std/haxe/macro/Printer.hx
@@ -165,7 +165,7 @@ class Printer {
 			+ (field.meta != null && field.meta.length > 0 ? field.meta.map(printMetadata).join('\n$tabs') + '\n$tabs' : "")
 			+ (field.access != null && field.access.length > 0 ? field.access.map(printAccess).join(" ") + " " : "")
 			+ switch (field.kind) {
-				case FVar(t, eo): 'var ${field.name}' + opt(t, printComplexType, " : ") + opt(eo, printExpr, " = ");
+				case FVar(t, eo): ((field.access != null && field.access.has(AFinal)) ? '' : 'var ') + '${field.name}' + opt(t, printComplexType, " : ") + opt(eo, printExpr, " = ");
 				case FProp(get, set, t, eo): 'var ${field.name}($get, $set)' + opt(t, printComplexType, " : ") + opt(eo, printExpr, " = ");
 				case FFun(func): 'function ${field.name}' + printFunction(func);
 			}
