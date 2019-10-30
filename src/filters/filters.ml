02bc22628c554365ed7eb23d23f2286f218eca19
Aleksandr Kuzmenko <alex@stablex.ru>
2019-10-30 15:44:44 +0300

    local named functions

--- src/filters/filters.ml
+++ src/filters/filters.ml
@@ -809,11 +809,23 @@ module ForRemap = struct
 end
 
 module Tre = struct
+	let null_var = {
+		v_id = -1;
+		v_name = "null_var";
+		v_type = t_dynamic;
+		v_kind = VGenerated;
+		v_capture = false;
+		v_final = false;
+		v_extra = None;
+		v_meta = [];
+		v_pos = null_pos;
+	}
+
 	let rec collect_new_args_values ctx args declarations values n =
 		match args with
 		| [] -> declarations, values
 		| arg :: rest ->
-			let v = alloc_var VGenerated ("`tre" ^ (string_of_int n)) arg.etype arg.epos in
+			let v = alloc_var VGenerated ("`tmp" ^ (string_of_int n)) arg.etype arg.epos in
 			let decl = { eexpr = TVar (v, Some arg); etype = ctx.t.tvoid; epos = v.v_pos }
 			and value = { arg with eexpr = TLocal v } in
 			collect_new_args_values ctx rest (decl :: declarations) (value :: values) (n + 1)
@@ -827,7 +839,7 @@ module Tre = struct
 			{ e with eexpr = TBinop (OpAssign, arg, e) } :: assign_args rest_vars rest_exprs
 		| _ -> assert false
 
-	let return_replacement ctx fn args p =
+	let replacement_for_TReturn ctx fn args p =
 		let temps_rev, args_rev = collect_new_args_values ctx args [] [] 0
 		and continue = mk TContinue ctx.t.tvoid Globals.null_pos in
 		{
@@ -836,34 +848,84 @@ module Tre = struct
 			epos = p;
 		}
 
+	let wrap_loop ctx body =
+		let cond = mk (TConst (TBool true)) ctx.t.tbool Globals.null_pos in
+		{ body with eexpr = TWhile (cond, body, Ast.NormalWhile) }
+
+	let rec transform_named_local_function ctx e fn_var fn =
+		let add_loop = ref false in
+		let rec transform e =
+			match e.eexpr with
+			(* named local function *)
+			| TBinop (OpAssign, { eexpr = TLocal ({ v_kind = VUser TVOLocalFunction } as v) }, { eexpr = TFunction fn }) ->
+				transform_named_local_function ctx e v fn
+			(* anonymous function *)
+			| TFunction _ ->
+				e
+			(* return a call to this local function *)
+			| TReturn (Some { eexpr = TCall ({ eexpr = TLocal v }, args) }) when v == fn_var ->
+				add_loop := true;
+				replacement_for_TReturn ctx fn args e.epos
+			| _ ->
+				map_expr transform e
+		in
+		let body = transform fn.tf_expr in
+		let body = if !add_loop then wrap_loop ctx body else body in
+		{ e with
+			eexpr = TBinop (
+				OpAssign,
+				{ e with eexpr = TLocal fn_var },
+				{ e with eexpr = TFunction { fn with tf_expr = body } }
+			)
+		}
+
+	let transform_method ctx fn field_expr =
+		let field = ctx.curfield in
+		let add_loop = ref false in
+		let rec transform e =
+			match e.eexpr with
+			(* named local function *)
+			| TBinop (OpAssign, { eexpr = TLocal ({ v_kind = VUser TVOLocalFunction } as v) }, { eexpr = TFunction fn }) ->
+				transform_named_local_function ctx e v fn
+			(* anonymous function *)
+			| TFunction _ ->
+				e
+			(* return a call to an instance method *)
+			| TReturn (Some { eexpr = TCall ({ eexpr = TField ({ eexpr = TConst TThis }, FInstance (_, _, f)) }, args) })
+			(* return a call to a static method *)
+			| TReturn (Some { eexpr = TCall ({ eexpr = TField (_, FStatic (_, f)) }, args) })
+			when f == field ->
+				add_loop := true;
+				replacement_for_TReturn ctx fn args e.epos
+			| _ ->
+				map_expr transform e
+		in
+		let body = transform fn.tf_expr in
+		let body = if !add_loop then wrap_loop ctx body else body in
+		{ field_expr with eexpr = TFunction { fn with tf_expr = body } }
+
+	let rec has_tail_recursion ctx field fn_var e =
+		match e.eexpr with
+		(* named local function *)
+		| TBinop (OpAssign, { eexpr = TLocal ({ v_kind = VUser TVOLocalFunction } as v) }, { eexpr = TFunction fn }) ->
+			has_tail_recursion ctx null_field v fn.tf_expr
+		(* anonymous function *)
+		| TFunction _ ->
+			false
+		(* return a call to a named local function*)
+		| TReturn (Some { eexpr = TCall ({ eexpr = TLocal v }, _) }) -> v == fn_var
+		(* return a call to an instance method *)
+		| TReturn (Some { eexpr = TCall ({ eexpr = TField ({ eexpr = TConst TThis }, FInstance (_, _, f)) }, args) })
+		(* return a call to a static method *)
+		| TReturn (Some { eexpr = TCall ({ eexpr = TField (_, FStatic (_, f)) }, args) }) ->
+			f == field
+		| _ ->
+			check_expr (has_tail_recursion ctx field fn_var) e
+
 	let run ctx e =
 		match e.eexpr with
-		| TFunction fn ->
-			let field = ctx.curfield in
-			let add_loop = ref false in
-			let rec transform e =
-				match e.eexpr with
-				| TFunction _ ->
-					e
-				(* instance methods *)
-				| TReturn (Some { eexpr = TCall ({ eexpr = TField ({ eexpr = TConst TThis }, FInstance (_, _, f)) }, args) })
-				(* static methods *)
-				| TReturn (Some { eexpr = TCall ({ eexpr = TField (_, FStatic (_, f)) }, args) })
-				when f == field ->
-					add_loop := true;
-					return_replacement ctx fn args e.epos
-				| _ ->
-					map_expr transform e
-			in
-			let body = transform fn.tf_expr in
-			if !add_loop then
-				let cond = mk (TConst (TBool true)) ctx.t.tbool Globals.null_pos in
-				{ e with
-					eexpr = TFunction { fn with
-						tf_expr = { body with eexpr = TWhile (cond, body, Ast.NormalWhile) }
-					}
-				}
-			else e
+		| TFunction fn when has_tail_recursion ctx ctx.curfield null_var fn.tf_expr ->
+			transform_method ctx fn e
 		| _ -> e
 end
 
