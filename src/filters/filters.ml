27c349a525cf46ecdf6879c8544b9545ab857b63
Aleksandr Kuzmenko <alex@stablex.ru>
2019-11-09 16:30:02 +0300

    [wip] Tail recursion elimination (#8908)
    * draft tre
    
    * don't descend into anon functions
    
    * local named functions
    
    * tests
    
    * don't eliminate tail recursion in user-defined loops
    
    * fix abstract member methods
    
    * fix it even more
    
    * handle arguments captured in closures
    
    * "internal" flag for @:tailRecursion in meta.json
    
    * wip
    
    * tre for void functions
    
    * fix tre for void functions
    
    * cleanup
    
    * don't apply tre to overridden methods

--- src/filters/filters.ml
+++ src/filters/filters.ml
@@ -820,6 +820,7 @@ let run com tctx main =
 		fix_return_dynamic_from_void_function tctx true;
 		check_local_vars_init;
 		check_abstract_as_value;
+		if defined com Define.AnalyzerOptimize then Tre.run tctx else (fun e -> e);
 		Optimizer.reduce_expression tctx;
 		if Common.defined com Define.OldConstructorInline then Optimizer.inline_constructors tctx else InlineConstructors.inline_constructors tctx;
 		CapturedVars.captured_vars com;
---
+++ tests/optimization/src/TestTreGeneration.hx
@@ -0,0 +1,129 @@
+class TestTreGeneration {
+	@:js('
+		if(b == null) {
+			b = 10;
+		}
+		while(true) {
+			if(Std.random(2) == 0) {
+				var _gtmp1 = a;
+				a = b + a;
+				b = _gtmp1;
+				s += "?";
+				continue;
+			}
+			if(s == null) {
+				return a;
+			} else {
+				return b;
+			}
+		}
+	')
+	static function testStaticMethod(a:Int, b:Int = 10, ?s:String):Int {
+		if(Std.random(2) == 0) {
+			return testStaticMethod(b + a, a, s + '?');
+		}
+		return s == null ? a : b;
+	}
+
+	@:js('
+		if(b == null) {
+			b = 10;
+		}
+		while(true) {
+			if(Std.random(2) == 0) {
+				var _gtmp1 = a;
+				a = b + a;
+				b = _gtmp1;
+				s += "?";
+				continue;
+			}
+			if(s == null) {
+				return a;
+			} else {
+				return b;
+			}
+		}
+	')
+	function testInstanceMethod(a:Int, b:Int = 10, ?s:String):Int {
+		if(Std.random(2) == 0) {
+			return testInstanceMethod(b + a, a, s + '?');
+		}
+		return s == null ? a : b;
+	}
+
+	@:js('
+		var local = null;
+		local = function(a,b,s) {
+			if(b == null) {
+				b = 10;
+			}
+			while(true) {
+				if(Std.random(2) == 0) {
+					var _gtmp1 = a;
+					a = b + a;
+					b = _gtmp1;
+					s += "?";
+					continue;
+				}
+				if(s == null) {
+					return a;
+				} else {
+					return b;
+				}
+			}
+		};
+		local(1,2);
+	')
+	static function testLocalNamedFunction() {
+		function local(a:Int, b:Int = 10, ?s:String):Int {
+			if(Std.random(2) == 0) {
+				return local(b + a, a, s + '?');
+			}
+			return s == null ? a : b;
+		}
+		local(1, 2);
+	}
+
+	@:js('
+		var _g = 0;
+		var _g1 = Std.random(10);
+		while(_g < _g1) {
+			++_g;
+			if(Std.random(2) == 0) {
+				return TestTreGeneration.testTailRecursionInsideLoop();
+			}
+		}
+		return Std.random(10);
+	')
+	static function testTailRecursionInsideLoop():Int {
+		for(i in 0...Std.random(10)) {
+			if(Std.random(2) == 0) {
+				return testTailRecursionInsideLoop();
+			}
+		}
+		return Std.random(10);
+	}
+
+	@:js('
+		while(true) {
+			if(Std.random(2) == 0) {
+				a -= 1;
+				continue;
+			}
+			if(a < 10) {
+				a += 1;
+				continue;
+			}
+			return;
+		}
+	')
+	static function testVoid(a:Int):Void {
+		if(Std.random(2) == 0) {
+			testVoid(a - 1);
+			return;
+		}
+		if(a < 10) {
+			testVoid(a + 1);
+		}
+	}
+}
