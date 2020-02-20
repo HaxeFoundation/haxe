9e468f9a9bfd639b73e77f1c5d2c1feb819e6031
George Corney <haxiomic@gmail.com>
2020-02-20 17:49:23 +0000

    [js] Use higher precision time-stamp when available

--- std/js/_std/HxOverrides.hx
+++ std/js/_std/HxOverrides.hx
@@ -138,6 +138,9 @@ class HxOverrides {
 			};
 		}
 
+	@:pure
+	static function now(): Float return js.lib.Date.now();
+
 	static function __init__()
 		untyped {
 			#if (js_es < 5)
@@ -146,5 +149,11 @@ class HxOverrides {
 			__feature__('HxOverrides.lastIndexOf',
 				if (Array.prototype.lastIndexOf) __js__("HxOverrides").lastIndexOf = function(a, o, i) return Array.prototype.lastIndexOf.call(a, o, i));
 			#end
+
+			__feature__('HxOverrides.now',
+				if (js.Syntax.typeof(performance) != 'undefined' && js.Syntax.typeof(performance.now) == 'function') {
+					HxOverrides.now = performance.now.bind(performance);
+				}
+			);
 		}
 }
