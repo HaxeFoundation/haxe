d8d2161431d0b8adea5b1d8c009fcd46fb8ecf46
Aleksandr Kuzmenko <alex@stablex.ru>
2020-02-27 15:51:18 +0300

    [hl] adjust stacks

--- std/hl/_std/haxe/Exception.hx
+++ std/hl/_std/haxe/Exception.hx
@@ -10,6 +10,7 @@ class Exception {
 	@:noCompletion var __exceptionMessage:String;
 	@:noCompletion var __exceptionStack:Null<CallStack>;
 	@:noCompletion var __nativeStack:hl.NativeArray<hl.Bytes>;
+	@:noCompletion var __skipStack:Int = 0;
 	@:noCompletion var __nativeException:Any;
 	@:noCompletion var __previousException:Null<Exception>;
 
@@ -25,7 +26,9 @@ class Exception {
 		if(Std.isOfType(value, Exception)) {
 			return (value:Exception).native;
 		} else {
-			return new ValueException(value);
+			var e = new ValueException(value);
+			e.__shiftStack();
+			return e;
 		}
 	}
 
@@ -49,6 +52,10 @@ class Exception {
 		return inline CallStack.exceptionToString(this);
 	}
 
+	@:noCompletion inline function __shiftStack():Void {
+		__skipStack++;
+	}
+
 	function get_message():String {
 		return __exceptionMessage;
 	}
@@ -63,7 +70,7 @@ class Exception {
 
 	function get_stack():CallStack {
 		return switch __exceptionStack {
-			case null: __exceptionStack = NativeStackTrace.toHaxe(__nativeStack);
+			case null: __exceptionStack = NativeStackTrace.toHaxe(__nativeStack, __skipStack);
 			case s: s;
 		}
 	}
