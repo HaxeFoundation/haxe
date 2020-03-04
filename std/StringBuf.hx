f020ef12d1b538adb6e3466f318ef30ccc034a38
aszasz <aszasz@users.noreply.github.com>
2020-03-04 08:52:48 -0300

    Clarify wording of StringBuf doc (#9201)
    * Clarify wording of StringBuf doc
    
    It was not clear what `internal buffer` was referring to.
    
    * Remove mentions of internals from StringBuf doc
    
    as proposed in https://github.com/HaxeFoundation/haxe/pull/9201

--- std/StringBuf.hx
+++ std/StringBuf.hx
@@ -24,12 +24,9 @@
 	A String buffer is an efficient way to build a big string by appending small
 	elements together.
 
-	Its cross-platform implementation uses String concatenation internally, but
-	StringBuf may be optimized for different targets.
-
 	Unlike String, an instance of StringBuf is not immutable in the sense that
 	it can be passed as argument to functions which modify it by appending more
-	values. However, the internal buffer cannot be modified.
+	values.
 **/
 class StringBuf {
 	var b:String;
