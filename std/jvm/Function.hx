dd9091883387be2401cd8ac8f70585d01e0c8f35
Simon Krajewski <simon@haxe.org>
2020-03-11 20:59:53 +0100

    [jvm] make jvm-specific implementation of Thread and Lock

--- std/jvm/Function.hx
+++ std/jvm/Function.hx
@@ -4,9 +4,10 @@ import java.NativeArray;
 
 @:native("haxe.jvm.Function")
 @:nativeGen
-extern class Function {
+extern class Function implements java.lang.Runnable {
 	public function new():Void;
 	public function invokeDynamic(args:NativeArray<Dynamic>):Dynamic;
 	public function equals(other:java.lang.Object):Bool;
 	public function invokeObject(arg1:java.lang.Object):java.lang.Object;
+	public function run():Void;
 }
