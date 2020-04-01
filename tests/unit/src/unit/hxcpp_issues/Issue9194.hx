f1bc0f35aeaf6920eb216a1cafd18a89a4fcc407
George Corney <haxiomic@gmail.com>
2020-04-01 14:02:22 +0100

    [test] Provide an empty test to keep the test-runner happy

--- tests/unit/src/unit/hxcpp_issues/Issue9194.hx
+++ tests/unit/src/unit/hxcpp_issues/Issue9194.hx
@@ -1,23 +1,24 @@
 package unit.hxcpp_issues;
 
-#if (cpp && !cppia)
-import cpp.*;
-#end
 
 class Issue9194 extends Test {
 
-	#if (cpp && !cppia)
 	@:analyzer(no_optimize)
 	function test() {
-		var buffer: RawPointer<cpp.Void> = null;
-		var floatBuffer: RawPointer<Float32> = cast buffer;
+		#if (cpp && !cppia)
+		// will fail during C++ compile
+		var buffer: cpp.RawPointer<cpp.Void> = null;
+		var floatBuffer: cpp.RawPointer<cpp.Float32> = cast buffer;
 		// generates incorrect: float* floatBuffer = buffer
 		// the lack of native casting means the compiler throws an error here
 
-		var buffer: Star<cpp.Void> = null;
-		var floatBuffer: Star<Float32> = cast buffer;
+		var buffer: cpp.Star<cpp.Void> = null;
+		var floatBuffer: cpp.Star<cpp.Float32> = cast buffer;
 		// generates correct: float* floatBuffer = ( (float*) buffer ) 
+		#end
+
+		// empty test to keep the test-runner happy
+		t(true);
 	}
-	#end
 
 }
