d6d4d3036adff61b50de780fe93b95d74339c68f
Jens Fischer <jensfischer95@gmail.com>
2020-04-05 17:40:32 +0200

    Document -D hl-ver in define.json

--- src/generators/genhl.ml
+++ src/generators/genhl.ml
@@ -3309,7 +3309,7 @@ let generate_static ctx c f =
 			| (Meta.HlNative,[(EConst(String(lib,_)),_)] ,_ ) :: _ ->
 				add_native lib f.cf_name
 			| (Meta.HlNative,[(EConst(Float(ver)),_)] ,_ ) :: _ ->
-				let cur_ver = (try Common.raw_defined_value ctx.com "hl-ver" with Not_found -> "") in
+				let cur_ver = (try Common.defined_value ctx.com Define.HlVer with Not_found -> "") in
 				if cur_ver < ver then
 					let gen_content() =
 						op ctx (OThrow (make_string ctx ("Requires compiling with -D hl-ver=" ^ ver ^ ".0 or higher") null_pos));
