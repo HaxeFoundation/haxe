839ae4012b319dd9c0c867fabc89c55bfc6e4ea6
Aleksandr Kuzmenko <alex@stablex.ru>
2020-04-08 19:11:13 +0300

    update the test

--- tests/misc/projects/Issue9294/Main.hx
+++ tests/misc/projects/Issue9294/Main.hx
@@ -1,10 +1,16 @@
+import haxe.macro.Context;
+
 class Main {
 	static function main() {
 		test();
 	}
 
 	macro static public function test() {
-		haxe.macro.Context.getType("");
+		try {
+			Context.getType("");
+		} catch(e:Dynamic) {
+			Context.error(Std.string(e), Context.currentPos());
+		}
 		return macro {};
 	}
 }
