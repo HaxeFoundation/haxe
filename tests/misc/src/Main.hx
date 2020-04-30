f1e1fa7100c11117de642e46f9c5b10f476e39cd
Aleksandr Kuzmenko <alex@stablex.ru>
2020-04-30 13:44:18 +0300

    update tests

--- tests/misc/src/Main.hx
+++ tests/misc/src/Main.hx
@@ -74,13 +74,11 @@ class Main {
 		return new haxe.Template(s).execute(context, macros);
 	}
 
-	static function normPath(_, p:String, properCase = false):String {
+	static function normPath(_, p:String):String {
 		if (Sys.systemName() == "Windows") {
 			// on windows, haxe returns lowercase paths with backslashes, drive letter uppercased
 			p = p.substr(0, 1).toUpperCase() + p.substr(1);
 			p = p.replace("/", "\\");
-			if (!properCase)
-				p = p.toLowerCase();
 		}
 		return p;
 	}
