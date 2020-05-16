ca81472bc8e8c61c5ab3659879990f8f78122132
Dan Korostelev <nadako@gmail.com>
2020-05-16 23:03:06 +0200

    add hyphen field to the test case

--- tests/unit/src/unit/issues/Issue7722.hx
+++ tests/unit/src/unit/issues/Issue7722.hx
@@ -4,6 +4,7 @@ private typedef Schema = {
 	final "$ref":String;
 	var ?"enum":Int;
 	var "default":{"$id":Int, "class":String};
+	var "aria-details":String;
 }
 
 class Issue7722 extends unit.Test {
@@ -11,20 +12,24 @@ class Issue7722 extends unit.Test {
 		var a:Schema = {
 			"$ref": "hi",
 			"enum": 42,
-			"default": {"$id": 10, "class": "bye"}
+			"default": {"$id": 10, "class": "bye"},
+			"aria-details": "yep",
 		};
 		var b = {
 			"$ref": "hi",
 			"enum": 42,
-			"default": {"$id": 10, "class": "bye"}
+			"default": {"$id": 10, "class": "bye"},
+			"aria-details": "yep",
 		};
 		eq("hi", a."$ref");
 		eq(42, a."enum");
 		eq(10, a."default"."$id");
 		eq("bye", a."default"."class");
+		eq("yep", a."aria-details");
 		eq("hi", b."$ref");
 		eq(42, b."enum");
 		eq(10, b."default"."$id");
 		eq("bye", b."default"."class");
+		eq("yep", b."aria-details");
 	}
 }
