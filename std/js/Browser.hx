f5126b1dcf36d03ba9861eff2d22c3fb65dabe13
George Corney <haxiomic@gmail.com>
2020-05-24 19:47:11 +0100

    Remove inline from get_supported

--- std/js/Browser.hx
+++ std/js/Browser.hx
@@ -64,7 +64,7 @@ class Browser {
 	 */
 	public static var supported(get, never):Bool;
 
-	extern inline static function get_supported()
+	extern static function get_supported()
 		return 
 			js.Syntax.typeof(window) != "undefined" &&
 			js.Syntax.typeof(window.location) != "undefined" &&
