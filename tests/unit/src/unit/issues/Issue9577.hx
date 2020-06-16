14704cc90c00fa9d5d65b3dd4523b8f8a78ccd95
Dmitry Maganov <vonagam@gmail.com>
2020-06-16 10:32:36 +0300

    extension should not shadow single pattern match for abstracted enum

--- tests/unit/src/unit/issues/Issue9577.hx
+++ tests/unit/src/unit/issues/Issue9577.hx
@@ -6,8 +6,14 @@ class Issue9577 extends Test {
   function test() {
     var foo: Foo = Some(9577);
     var bar: Bar = Some(9577);
+    var baz: Baz = Some(9577);
     eq(true, foo.match(Some(9577)));
     eq(9578, bar.match(9577));
+    eq(false, baz.match(None));
+  }
+
+  public static function match(baz: Baz, value: Int) {
+    return value + 1;
   }
 }
 
@@ -18,3 +24,6 @@ class Issue9577 extends Test {
     return value + 1;
   }
 }
+
+@:using(unit.issues.Issue9577)
+@:forward private abstract Baz(Option<Int>) from Option<Int> {}
