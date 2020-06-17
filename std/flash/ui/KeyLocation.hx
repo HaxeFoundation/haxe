70c42f36f6fced8359b9635ef8171e95740a2f89
nanjizal <nanjizal@users.noreply.github.com>
2020-06-17 09:28:41 +0100

    this is the file I intended to modify

--- std/flash/ui/KeyLocation.hx
+++ std/flash/ui/KeyLocation.hx
@@ -1,6 +1,6 @@
 package flash.ui;
 
-@:native("flash.ui.KeyLocation") extern enum abstract KeyLocation(UInt) {
+@:native("flash.ui.KeyLocation") extern enum abstract KeyLocation(UInt) from UInt to UInt {
 	var D_PAD;
 	var LEFT;
 	var NUM_PAD;
