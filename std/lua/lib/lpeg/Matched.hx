db40d96ee080fdb054bf729e118323082f604a62
Justin Donaldson <jdonaldson@gmail.com>
2020-07-07 08:35:33 -0700

    [lua] more lpeg components

---
+++ std/lua/lib/lpeg/Matched.hx
@@ -0,0 +1,24 @@
+
+abstract Matched(Int) {
+    inline public function new(d:Dynamic) {
+        this = d;
+    }
+    inline public function matched() : Bool {
+        return Std.isOfType(this, Int) || Lua.next(cast this) != null;
+    }
+    inline public function captures() : Table<String,String> {
+        if (Std.isOfType(this,Table)) {
+            return cast this;
+        } else {
+            return Table.create();
+        }
+    }
+    inline public function position() : Int {
+        if (Std.isOfType(this, Int)) {
+            return this;
+        } else {
+            return null;
+        }
+    }
+}
+
