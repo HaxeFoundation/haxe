17c4af3b451cb4f3f4ec11be7aea3a5072cc7afa
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-07-17 10:57:42 -0600

    Simplify python Lock using Semaphore
    Co-authored-by: Jonas Malaco <me@jonasmalaco.com>

--- std/python/_std/sys/thread/Lock.hx
+++ std/python/_std/sys/thread/Lock.hx
@@ -22,95 +22,21 @@
 
 package sys.thread;
 
-import python.Exceptions.InterruptedError;
-
-class Lock {
-	var releasedCount = 0;
-	var waitLock: NativeLock;
-	var releaseLock: NativeLock;
-	var condition: NativeCondition;
-
-	public function new() {
-		waitLock = new NativeLock();
-		releaseLock = new NativeLock();
-		condition = new NativeCondition();
+@:forward(release)
+abstract Lock(NativeSemaphore) {
+	public inline function new() {
+		this = new NativeSemaphore(0);
 	}
 
-	// TODO use python Condition object to do the wait()/notify() thing that
-	// the Java lock implementation does.
-
-	public function wait(?timeout:Float):Bool {
-		trace('wait $timeout called from ${haxe.CallStack.callStack().toString()}');
-		var ret = false;
-		waitLock.acquire(true);
-		if (--releasedCount < 0) {
-			if (timeout == null) {
-				while (releasedCount < 0) {
-					try {
-						trace(releasedCount);
-						condition.acquire();
-						condition.wait();
-						condition.release();
-					} catch (e: InterruptedError) {}
-				}
-			} else {
-				var timeout = timeout * 1000;
-				var cur = Sys.time(),
-					max = cur + timeout;
-				while (releasedCount < 0 && cur < max) {
-					try {
-						var t = max - cur;
-						condition.acquire();
-						condition.wait(t / 1000.);
-						condition.release();
-						cur = Sys.time();
-					} catch (e: InterruptedError) {}
-				}
- 			}
-		}
-
-		ret = releasedCount >= 0;
-		if (!ret)
-			releasedCount++;
-		waitLock.release();
-		return ret;
-	}
-
-	/**
-		Releases the lock once.
-
-		The thread does not need to own the lock in order to release
-		it. Each call to `release` allows exactly one call to `wait`
-		to execute.
-	**/
-	public function release():Void {
-		trace('release called from ${haxe.CallStack.callStack().toString()}');
-		releaseLock.acquire(true);
-		++releasedCount;
-		trace(releasedCount);
-		if (releasedCount == 0) {
-			condition.acquire();
-			condition.notify();
-			condition.release();
-		}
-		releaseLock.release();
+	public inline function wait(?timeout:Float):Bool {
+		return this.acquire(true, timeout);
 	}
 }
 
-@:pythonImport("threading", "Lock")
+@:pythonImport("threading", "Semaphore")
 @:native("Lock")
-private extern class NativeLock {
-	function new();
-	function acquire(blocking: Bool=true, ?timeout: Float=-1):Bool;
+private extern class NativeSemaphore {
+	function new(value:Int);
+	function acquire(blocking:Bool = true, ?timeout:Float):Bool;
 	function release():Void;
-}
-
-@:pythonImport("threading", "Condition")
-@:native("Condition")
-private extern class NativeCondition {
-	function new();
-	function acquire():Void;
-	function release():Void;
-	function wait(?timeout: Float):Void;
-	function notify():Void;
 }
