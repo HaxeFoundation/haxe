8388ad2eb5103ba82bcc3396bb32273563d98d4a
Simon Krajewski <simon@haxe.org>
2020-07-26 07:07:48 +0200

    Refactor operator handling (#9751)
    * [typer] start messing with `type_binop` and friends
    
    * [typer] restructure more
    
    * [typer] add BinopResult module
    
    * [typer] banish operator handling into its own module
    
    * [tests] add
    
    closes #9746
    
    * [typer] restructure a bit
    
    * [tests] C# has issues with -D fat-cat or whatever
    
    * [typer] consistently check abstract field casts when assigning
    
    see #8427

--- src/typing/typerBase.ml
+++ src/typing/typerBase.ml
@@ -56,6 +56,7 @@ type object_decl_kind =
 	| ODKPlain
 
 let type_call_target_ref : (typer -> expr -> expr list -> WithType.t -> bool -> pos -> access_kind) ref = ref (fun _ _ _ _ _ -> die "" __LOC__)
+let type_access_ref : (typer -> expr_def -> pos -> access_mode -> WithType.t -> access_kind) ref = ref (fun _ _ _ _ _ -> assert false)
 
 let relative_path ctx file =
 	let slashes path = String.concat "/" (ExtString.String.nsplit path "\\") in
