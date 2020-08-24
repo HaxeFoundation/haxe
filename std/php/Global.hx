7949dc829e3f7f803b467dfb217c8a6eca4ac782
Cédric Belin <cedric@belin.io>
2020-08-24 10:41:31 +0200

    Fix the overload

--- std/php/Global.hx
+++ std/php/Global.hx
@@ -1579,6 +1579,6 @@ extern class Global {
 	/**
 		@see http://php.net/manual/en/function.version-compare.php
 	**/
-	@:overload(function(version1:String, version2:String, ?comparisonOperator:String):Bool {})
+	@:overload(function(version1:String, version2:String, comparisonOperator:String):Bool {})
 	static function version_compare(version1:String, version2:String):Int;
 }
