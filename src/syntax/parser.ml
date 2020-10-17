dadbb453e0a1201ddb6868164588cf42be64428d
Dan Korostelev <nadako@gmail.com>
2020-10-17 12:36:51 +0200

    [parser] fix is operator priority vs unops (see #9899)

--- src/syntax/parser.ml
+++ src/syntax/parser.ml
@@ -299,6 +299,7 @@ let rec make_unop op ((v,p2) as e) p1 =
 	match v with
 	| EBinop (bop,e,e2) -> EBinop (bop, make_unop op e p1 , e2) , (punion p1 p2)
 	| ETernary (e1,e2,e3) -> ETernary (make_unop op e1 p1 , e2, e3), punion p1 p2
+	| EIs (e, t) -> EIs (make_unop op e p1, t), punion p1 p2
 	| EConst (Int i) when op = Neg -> EConst (Int (neg i)),punion p1 p2
 	| EConst (Float j) when op = Neg -> EConst (Float (neg j)),punion p1 p2
 	| _ -> EUnop (op,Prefix,e), punion p1 p2
