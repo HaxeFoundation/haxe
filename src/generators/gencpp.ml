ef0c4e9a7b5532302b54f3e694886e962be2261c
Aidan63 <aidan.lee63@gmail.com>
2020-10-25 09:41:06 +0000

    Disallow type checks using the haxe.Int64 abstract

--- src/generators/gencpp.ml
+++ src/generators/gencpp.ml
@@ -3777,7 +3777,6 @@ let gen_cpp_ast_expression_tree ctx class_name func_name function_args function_
          let path = match path with
          | "::Int" -> "int"
          | "::Bool" -> "bool"
-         | "::haxe::_Int64::Int64_Impl_" -> "::cpp::Int64"
          | x -> x in
          if (native) then
             out "null()"
