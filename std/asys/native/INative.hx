ce835c2d5ca68fdebbf83468c8ee077cf3268c84
Aleksandr Kuzmenko <alex@stablex.ru>
2020-11-12 13:39:07 +0300

    [api] dropped IJobExecutor idea

--- std/asys/native/INative.hx
+++
@@ -1,9 +0,0 @@
-package asys.native;
-
-import asys.native.filesystem.IFileSystem;
-
-@:inheritDoc(asys.native.Native)
-interface INative {
-	@:inheritDoc(asys.native.Native.filesystem)
-	var filesystem(get,never):IFileSystem;
-}
