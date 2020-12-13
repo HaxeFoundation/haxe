ebe65a9141ae68f7992c492945d4b3b3031fde10
Aleksandr Kuzmenko <alex@stablex.ru>
2020-12-13 17:51:57 +0300

    cleanup

--- src/typing/operators.ml
+++ src/typing/operators.ml
@@ -801,6 +801,9 @@ let type_unop ctx op flag e with_type p =
 		| _ ->
 			raise Not_found
 	in
+	let unexpected_spread p =
+		error "Spread unary operator is only allowed for unpacking the last argument in a call with rest arguments" p
+	in
 	let make e =
 		let check_int () =
 			match classify e.etype with
@@ -825,7 +828,8 @@ let type_unop ctx op flag e with_type p =
 				check_int()
 			| Neg ->
 				check_int()
-			| Spread -> die "todo" __LOC__
+			| Spread ->
+				unexpected_spread p
 		in
 		mk (TUnop (op,flag,e)) t p
 	in
@@ -837,7 +841,7 @@ let type_unop ctx op flag e with_type p =
 	in
 	match op with
 	| Spread ->
-		error "Spread unary operator is only allowed for unpacking the last argument in a call with rest arguments" p
+		unexpected_spread p
 	| Not | Neg | NegBits ->
 		let access_get = !type_access_ref ctx (fst e) (snd e) MGet WithType.value (* WITHTYPETODO *) in
 		let e = acc_get ctx access_get p in
