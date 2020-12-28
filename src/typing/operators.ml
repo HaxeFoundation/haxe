312cffb1e408481a6922a3f9e80de3e3f5def5e5
Aleksandr Kuzmenko <alex@stablex.ru>
2020-12-28 21:41:38 +0300

    minor

--- src/typing/operators.ml
+++ src/typing/operators.ml
@@ -235,7 +235,7 @@ let unify_int ctx e k =
 		unify ctx e.etype ctx.t.tint e.epos;
 		true
 
-let compare_functions_with_reflect ctx op e1 e2 p needs_isFunction_checks =
+let compare_functions_with_reflect ctx op e1 e2 p =
 	let reflect_cls =
 		match Typeload.load_type_raise ctx ([],"Reflect") "Reflect" p with
 		| TClassDecl cls -> cls
@@ -264,7 +264,10 @@ let compare_functions_with_reflect ctx op e1 e2 p needs_isFunction_checks =
 		make_static_call ctx reflect_cls compareMethods_field (fun t -> t) [tmp_e1; tmp_e2] ctx.t.tbool p
 	in
 	let comparison =
-		if needs_isFunction_checks then
+		match follow e1.etype, follow e2.etype with
+		| TFun _, TFun _ ->
+			compareMethods_e1_e2
+		| _ ->
 			let isFunction_e1, isFunction_e2 =
 				make_static_call ctx reflect_cls isFunction_field (fun t -> t) [tmp_e1] ctx.t.tbool p,
 				make_static_call ctx reflect_cls isFunction_field (fun t -> t) [tmp_e2] ctx.t.tbool p
@@ -272,8 +275,6 @@ let compare_functions_with_reflect ctx op e1 e2 p needs_isFunction_checks =
 			let are_functions = mk (TBinop (OpBoolAnd,isFunction_e1,isFunction_e2)) ctx.t.tbool p
 			and compare_as_not_functions = mk (TBinop (op,tmp_e1,tmp_e2)) ctx.t.tbool p in
 			mk (TIf (are_functions,compareMethods_e1_e2,Some compare_as_not_functions)) ctx.t.tbool p
-		else
-			compareMethods_e1_e2
 	in
 	let e =
 		match declare_tmp_e1, declare_tmp_e2 with
@@ -442,11 +443,9 @@ let make_binop ctx op e1 e2 is_assign_op with_type p =
 			| TConst TNull , _ | _ , TConst TNull -> default()
 			| _ ->
 				match follow e1.etype, follow e2.etype with
-				| TFun _, TFun _ ->
-					compare_functions_with_reflect ctx op e1 e2 p false
 				| TFun _ , _ | _, TFun _
 				| TInst ({ cl_kind = KTypeParameter _ },_), _ | _, TInst ({ cl_kind = KTypeParameter _ },_) ->
-					compare_functions_with_reflect ctx op e1 e2 p true
+					compare_functions_with_reflect ctx op e1 e2 p
 				| _ ->
 					default()
 		end
