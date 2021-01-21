3dd7fd40e50e9cbf85c26c4dbceb7dfe1c5c043e
k <k@klabz.org>
2021-01-21 19:47:42 +0100

    Add test for #10083

---
+++ tests/unit/src/unit/issues/Issue10083.hx
@@ -0,0 +1,26 @@
+package unit.issues;
+
+private enum FooBar {
+  Foo;
+  Bar;
+}
+
+class Issue10083 extends Test {
+  function test() {
+    var hasFoo = false;
+    var tasks:Array<Void->Void> = [];
+    var flags = ["A" => Bar, "B" => Foo, "C" => Bar];
+
+    for (flag in flags) {
+      switch (flag) {
+        case Foo:
+          hasFoo = true;
+
+        case Bar:
+          tasks.push(() -> eq(hasFoo, true));
+      }
+    }
+
+    while (tasks.length > 0) tasks.shift()();
+  }
+}
