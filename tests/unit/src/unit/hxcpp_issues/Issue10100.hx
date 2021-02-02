5a1d34b1cc3a96e2acb10cd5bebc0780a732e02b
George Corney <haxiomic@gmail.com>
2021-02-02 15:35:24 +0000

    [test] add regression test for Int64 roundtrip #10100

---
+++ tests/unit/src/unit/hxcpp_issues/Issue10100.hx
@@ -0,0 +1,24 @@
+package unit.hxcpp_issues;
+
+class Issue10100 extends Test {
+
+	@:analyzer(no_optimize)
+	function test() {
+		var cpp64: cpp.Int64 = untyped __cpp__('12345678944444');
+		// https://repl.it/join/wtqnxkxc-haxiomic
+		// low 1942935740
+		// high 2874
+		var hx64: haxe.Int64 = cpp64;
+		eq(hx64.low, 1942935740);
+		eq(hx64.high, 2874);
+
+		// there and back again
+		var cpp64: cpp.Int64 = hx64;
+		eq(Std.string(cpp64), '12345678944444');
+
+		var hx64: haxe.Int64 = cpp64;
+		eq(hx64.low, 1942935740);
+		eq(hx64.high, 2874);
+	}
+
+}
