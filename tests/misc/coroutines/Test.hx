1f7765cce21485151c84a3bb94e64268de70ee87
Dan Korostelev <nadako@gmail.com>
2021-02-19 21:50:17 +0100

    start some simple testing (js-only for now)

---
+++ tests/misc/coroutines/Test.hx
@@ -0,0 +1,29 @@
+import utest.Assert;
+import utest.Async;
+
+class TestStaticFields extends utest.Test {
+	function testSimpleStart(async:Async) {
+		simple.start(42, result -> {
+			Assert.equals(42, result);
+			async.done();
+		});
+	}
+
+	function testSimpleCreate(async:Async) {
+		var cont = simple.create(42, result -> {
+			Assert.equals(42, result);
+			async.done();
+		});
+		cont(null);
+	}
+
+	@:coroutine static function simple(arg:Int):Int {
+		return arg;
+	}
+}
+
+function main() {
+	utest.UTest.run([
+		new TestStaticFields(),
+	]);
+}
