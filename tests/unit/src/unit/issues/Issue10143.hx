dbf314abe6476ceffe4d2db69ca8b1037f4a7785
Aleksandr Kuzmenko <alex@stablex.ru>
2021-02-28 13:50:02 +0300

    use Type.unify for rest args after all. +test (#10143)

--- tests/unit/src/unit/issues/Issue10143.hx
+++ tests/unit/src/unit/issues/Issue10143.hx
@@ -8,12 +8,26 @@ class Issue10143 extends Test {
 		noAssert();
 		eq('String', HelperMacros.typeString(Win.test('hello')));
 		eq('Bool', HelperMacros.typeString(Win.test({field:'world'})));
+		eq('String', HelperMacros.typeString(Win.test(('hello':AString))));
+		eq('Bool', HelperMacros.typeString(Win.test(({field:'world'}:AObj))));
 	}
 }
 
 private extern class Win {
 	overload static function test<T:{field:String}>(items:...T):Bool;
 	overload static function test(items:...String):String;
+}
+
+private abstract AString(String) from String {
+	@:to public function toString():String {
+		return this;
+	}
+}
+
+private abstract AObj({field:String}) from {field:String} {
+	@:to public function toObj():{field:String} {
+		return this;
+	}
 }
 
 #else
