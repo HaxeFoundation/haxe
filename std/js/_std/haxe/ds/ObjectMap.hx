b621c5270979ffd24d21e9e61561eae8ef6f3400
Aleksandr Kuzmenko <alex@stablex.ru>
2021-03-04 21:24:57 +0300

    [js] removed ObjectMap.count as it's not used anymore (see #6792, #8084)

--- std/js/_std/haxe/ds/ObjectMap.hx
+++ std/js/_std/haxe/ds/ObjectMap.hx
@@ -27,13 +27,6 @@ import js.Lib;
 
 @:coreApi
 class ObjectMap<K:{}, V> implements haxe.Constraints.IMap<K, V> {
-	static var count:Int;
-
-	// initialize count through __init__ magic, because these are generated
-	// before normal static initializations for which ObjectMap should be ready to use
-	// see https://github.com/HaxeFoundation/haxe/issues/6792
-	static inline function __init__():Void
-		count = 0;
 
 	static inline function assignId(obj:{}):Int {
 		return Syntax.code('({0}.__id__ = {1})', obj, Lib.getNextHaxeUID());
