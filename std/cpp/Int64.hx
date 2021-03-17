612daa7747516d765c6a4e29b411663160d10b94
George Corney <haxiomic@gmail.com>
2021-03-17 09:53:16 +0000

    [cpp,hl] disable lossy implicit cast of int 64 to 32 (#10133)
    * [cpp] disable lossy implicit cast of int 64 to 32
    
    * [hl] disable lossy implicit cast of int 64 to 32
    
    * [cpp,hl] add explicit toInt() to Int64 types
    
    * [cpp,hl] add toInt() doc
    
    * deprecate implicit cast of Int64 -> Int32

--- std/cpp/Int64.hx
+++ std/cpp/Int64.hx
@@ -22,7 +22,21 @@
 
 package cpp;
 
-@:coreType @:notNull @:runtimeValue abstract Int64 from Int to Int {
+@:coreType @:notNull @:runtimeValue abstract Int64 from Int {
+
+	/**
+		Destructively cast to Int
+	**/
+	public inline function toInt():Int {
+		return cast this;
+	}
+
+	@:to
+	@:deprecated("Implicit cast from Int64 to Int (32 bits) is deprecated. Use .toInt() or explicitly cast instead.")
+	inline function implicitToInt(): Int {
+		return toInt();
+	}
+
 	@:to
 	#if !cppia inline #end function toInt64():haxe.Int64 {
 		return cast this;
