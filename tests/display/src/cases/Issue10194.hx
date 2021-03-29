adf74211ff77bd4085911aa2ba3cb932ef565ec0
Simon Krajewski <simon@haxe.org>
2021-03-29 08:40:21 +0200

    [display] don't diagnostics so much if there are errors
    closes #10194

---
+++ tests/display/src/cases/Issue10194.hx
@@ -0,0 +1,22 @@
+package cases;
+
+class Issue10194 extends DisplayTestCase {
+	/**
+		function f(a:Int, b:Int) {}
+
+		function main() {
+			var v = 10; // reported as "unused variable"
+			f({-1-}""{-2-}, v);
+		}
+	**/
+	function test() {
+		arrayEq([
+			{
+				kind: DKCompilerError,
+				severity: Error,
+				range: diagnosticsRange(pos(1), pos(2)),
+				args: "String should be Int\nFor function argument 'a'"
+			}
+		], diagnostics());
+	}
+}
