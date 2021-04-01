1eedbadc41770d0cdb11267158fa4de6a1c3ddd1
Aleksandr Kuzmenko <alex@stablex.ru>
2021-04-01 21:42:49 +0300

    update tests

--- tests/asys/src/cases/asys/native/filesystem/TestDirectory.hx
+++ tests/asys/src/cases/asys/native/filesystem/TestDirectory.hx
@@ -15,50 +15,18 @@ class TestDirectory extends FsTest {
 		});
 	}
 
-	function testNextEntry(async:Async) {
-		var contents = [];
-		function read(dir:Directory, callback:()->Void) {
-			dir.nextEntry((e, r) -> {
-				if(noException(e))
-					switch r {
-						case null:
-							callback();
-						case entry:
-							contents.push(entry.toString());
-							read(dir, callback);
-
-					}
-				else
-					callback();
-			});
-		}
-
-		asyncAll(async,
-			FileSystem.openDirectory('test-data', (e, dir) -> {
-				if(noException(e))
-					read(dir, () -> {
-						var expected = ['sub', 'symlink-dir', 'temp', 'bytes.bin', 'symlink'];
-						expected.sort(Reflect.compare);
-						contents.sort(Reflect.compare);
-						same(expected, contents);
-						dir.close((e, _) -> noException(e));
-					});
-			})
-		);
-	}
-
-	function testNextBatch(async:Async) {
+	function testNext(async:Async) {
 		var expected = ['sub', 'symlink-dir', 'temp', 'bytes.bin', 'symlink'];
 		var batchSize = 4;
 		var actual = [];
 		asyncAll(async,
-			FileSystem.openDirectory('test-data', (e, dir) -> {
+			FileSystem.openDirectory('test-data', batchSize, (e, dir) -> {
 				if(noException(e))
-					dir.nextBatch(batchSize, (e, r) -> {
+					dir.next((e, r) -> {
 						if(noException(e)) {
 							equals(batchSize, r.length);
 							for(f in r) actual.push(f.toString());
-							dir.nextBatch(batchSize, (e, r) -> {
+							dir.next((e, r) -> {
 								if(noException(e)) {
 									for(f in r) actual.push(f.toString());
 									expected.sort(Reflect.compare);
