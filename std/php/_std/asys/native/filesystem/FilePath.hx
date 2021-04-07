327fb85e066a9f60d6f544baabf80db0fa4af191
Aleksandr Kuzmenko <alex@stablex.ru>
2021-04-08 00:26:28 +0300

    [php] update FilePath

--- std/php/_std/asys/native/filesystem/FilePath.hx
+++ std/php/_std/asys/native/filesystem/FilePath.hx
@@ -7,6 +7,7 @@ import php.Const.*;
 import php.Global.*;
 import php.Syntax.*;
 import php.NativeArray;
+import haxe.exceptions.ArgumentException;
 
 private typedef NativeFilePath = NativeString;
 
@@ -16,36 +17,38 @@ private typedef NativeFilePath = NativeString;
 		return DIRECTORY_SEPARATOR;
 	}
 
-	@:allow(asys.native.filesystem)
-	function new(s:NativeString) {
-		this = switch s {
-			case null: null;
-			case '': '.';
-			case _:
-				if(SEPARATOR == '\\') {
-					var trimmed:NativeString = rtrim(s, '\\/');
-					var length = strlen(s);
-					var lengthTrimmed = strlen(trimmed);
-					//current disk root. E.g. `\`
-					if(trimmed == '') {
-						SEPARATOR;
-					//specific disk root. E.g. `C:\`
-					} else if(lengthTrimmed == 2 && trimmed[1] == ':' && length >= 3 && isSeparator(s[2])) {
-						trimmed + SEPARATOR;
-					} else {
-						trimmed;
-					}
-				} else {
-					switch rtrim(s, SEPARATOR) {
-						case '': s[0];
-						case s: s;
-					}
-				}
-		}
+	overload extern static public inline function createPath(path:String, ...appendices:String):FilePath
+		return createPathImpl(path, ...appendices);
+
+	@:native('createPath')
+	static function createPathImpl(path:String, ...appendices:String):FilePath {
+		var path = ofString(path);
+		for(p in appendices)
+			path = path.add(p);
+		return path;
 	}
 
+	overload extern static public inline function createPath(parts:Array<String>):FilePath {
+		return ofArray(parts);
+	}
+
+	@:noUsing
 	@:from public static inline function ofString(path:String):FilePath {
 		return new FilePath(path);
+	}
+
+	@:from static function ofArray(parts:Array<String>):FilePath {
+		if(parts.length == 0)
+			throw new ArgumentException('parts');
+		var path = ofString(parts[0]);
+		for(i in 1...parts.length)
+			path = path.add(parts[i]);
+		return path;
+	}
+
+	@:allow(asys.native.filesystem)
+	inline function new(s:NativeString) {
+		this = s;
 	}
 
 	@:to public inline function toString():String {
@@ -73,7 +76,7 @@ private typedef NativeFilePath = NativeString;
 				throw new FsException(CustomError('Unable to get current working directory'), this);
 			return result;
 		}
-		var fullPath = if(this == '') {
+		return if(this == '') {
 			cwd();
 		} else if(this[0] == '/') {
 			this;
@@ -92,31 +95,38 @@ private typedef NativeFilePath = NativeString;
 					}
 				}
 			} else {
-				rtrim(cwd() + SEPARATOR + this, '\\/');
+				cwd() + SEPARATOR + this;
 			}
 		} else {
-			rtrim(cwd() + SEPARATOR + this, '/');
+			cwd() + SEPARATOR + this;
 		}
+	}
 
+	public function normalize():FilePath {
 		var parts:NativeIndexedArray<String> = if(SEPARATOR == '\\') {
-			(preg_split('#\\\\|/#', fullPath):NativeArray);
+			(preg_split('#\\\\|/#', this):NativeArray);
 		} else {
-			explode('/', fullPath);
+			explode('/', this);
 		}
-		var i = 1;
+		var i = count(parts) - 1;
 		var result = new NativeIndexedArray();
-		while(i < count(parts)) {
+		var skip = 0;
+		while(i >= 0) {
 			switch parts[i] {
 				case '.' | '':
 				case '..':
-					array_pop(result);
+					++skip;
+				case _ if(skip > 0):
+					--skip;
 				case part:
-					result.push(part);
+					array_unshift(result, part);
 			}
-			i++;
+			--i;
 		}
-		array_unshift(result, parts[0]);
-		return implode(SEPARATOR, result);
+		for(i in 0...skip)
+			array_unshift(result, '..');
+		var result = ofString(implode(SEPARATOR, result));
+		return isAbsolute() && !result.isAbsolute() ? SEPARATOR + result : result;
 	}
 
 	public function parent():Null<FilePath> {
@@ -126,7 +136,7 @@ private typedef NativeFilePath = NativeString;
 			case path:
 				if(path == this) {
 					null;
-				//relative to current drive. E.g. `C:.\relative\path`
+				//relative to current drive with a dot. E.g. `C:.\relative\path`
 				} else if(SEPARATOR == '\\' && strlen(path) == 3 && preg_match('/^[a-zA-Z]:\\./', path)) {
 					strlen(this) >= 4 && this[2] == '.' && isSeparator(this[3]) ? path : null;
 				} else {
@@ -136,7 +146,46 @@ private typedef NativeFilePath = NativeString;
 		return new FilePath(path);
 	}
 
+	public function add(path:FilePath):FilePath {
+		if(path.isAbsolute() || this == '')
+			return path;
+		if(path == '')
+			return this;
+		if(SEPARATOR == '\\') {
+			var s = (path:NativeString);
+			if(strlen(s) >= 2 && s[1] == ':') {
+				if(strlen(this) >= 2 && this[1] == ':') {
+					if(strtolower(s[0]) != strtolower(this[0])) {
+						throw new ArgumentException('path', 'Cannot combine paths on different drives');
+					}
+					return trimSlashes(this) + SEPARATOR + (substr(s, 2):String);
+				} else if(isSeparator(this[0])) {
+					return (substr(s, 0, 2):String) + trimSlashes(this) + SEPARATOR + (substr(s, 2):String);
+				}
+			}
+		}
+		return trimSlashes(this) + SEPARATOR + path;
+	}
+
 	static inline function isSeparator(char:String):Bool {
 		return char == '/' || (char == SEPARATOR);
+	}
+
+	static function trimSlashes(s:NativeString):NativeString {
+		return if(SEPARATOR == '\\') {
+			var result:NativeString = rtrim(s, '/\\');
+			switch strlen(result) {
+				// \
+				case 0 if(strlen(s) > 0):
+					s[0];
+				// C:\
+				case 2 if(result[1] == ':' && strlen(s) > 2):
+					substr(s, 0, 3);
+				case _:
+					result;
+			}
+		} else {
+			rtrim(s, '/');
+		}
 	}
 }
