145ec67fb3a3f24f36b3a6e54409175a96b93bf9
Simon Krajewski <simon@haxe.org>
2021-06-17 08:25:41 +0200

    [jvm] still keep member synthetics around
    closes #10279

--- src/codegen/javaModern.ml
+++ src/codegen/javaModern.ml
@@ -923,7 +923,8 @@ module Converter = struct
 		let known_sigs = Hashtbl.create 0 in
 		let should_generate jf =
 			not (AccessFlags.has_flag jf.jf_flags MPrivate)
-			&& not (AccessFlags.has_flag jf.jf_flags MSynthetic)
+			(* We might need member synthetics for proper call resolution, but we should never need static ones (issue #10279). *)
+			&& (not (AccessFlags.has_flag jf.jf_flags MSynthetic) || not (AccessFlags.has_flag jf.jf_flags MStatic))
 			&& jf.jf_name <> "<clinit>"
 		in
 		if jc.jc_path <> (["java";"lang"], "CharSequence") then begin
