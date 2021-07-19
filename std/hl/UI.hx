96e6846812c6e1698de29224c31d984b78c21f7b
Aleksandr Kuzmenko <alex@stablex.ru>
2021-07-19 23:19:24 +0300

    Add clipboard support in hl 1.12 (#10320)

--- std/hl/UI.hx
+++ std/hl/UI.hx
@@ -214,4 +214,37 @@ class UI {
 	static function _chooseFile(forSave:Bool, obj:Dynamic):hl.Bytes {
 		return null;
 	}
+	
+	#if (hl_ver >= version("1.12.0"))
+	public static function setClipboardText(text:String):Bool {
+		if(text == null)
+			return false;
+		return @:privateAccess _setClipboardText(text.toUtf8());
+	}
+
+	@:hlNative("?ui", "ui_set_clipboard_text")
+	static function _setClipboardText(text:hl.Bytes):Bool {
+		return false;
+	}
+
+	public static function getClipboardText():String {
+		var t = _getClipboardText();
+		if( t == null )
+			return null;
+		return @:privateAccess String.fromUTF8(t);
+	}
+
+	@:hlNative("?ui", "ui_get_clipboard_text")
+	static function _getClipboardText():hl.Bytes {
+		return null;
+	}
+	#else
+	public static function setClipboardText(text:String):Bool {
+		return false;
+	}
+	public static function getClipboardText():String {
+		return null;
+	}
+	#end
+	
 }
