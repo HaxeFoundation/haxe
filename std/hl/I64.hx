8d8af880c1c9711a683a3f26e549f4d31c2603d2
Aleksandr Kuzmenko <alex@stablex.ru>
2021-08-09 12:59:40 +0300

    wip

--- std/hl/I64.hx
+++ std/hl/I64.hx
@@ -22,7 +22,18 @@
 
 package hl;
 
+import haxe.Int64;
+
 @:coreType @:notNull @:runtimeValue abstract I64 from Int {
+	/** The greatest representable I64 value. */
+	static public var MAX(get,never):I64;
+	@:hlNative("std", "num_i64_max") static function get_MAX():I64
+		return 0;
+
+	/** The smallest representable I64 value. */
+	static public var MIN(get,never):I64;
+	@:hlNative("std", "num_i64_min") static function get_MIN():I64
+		return 0;
 
 	/**
 		Destructively cast to Int
@@ -36,5 +47,43 @@ package hl;
 	inline function implicitToInt(): Int {
 		return toInt();
 	}
+
+	/**
+		Parse the given string value to I64.
+	**/
+	@:hlNative("std", "num_i64_of_string")
+	static public function ofString(s:String):I64;
+
+	/**
+		Convert `haxe.Int64` to `hl.uv.I64`
+	**/
+	@:from static public function ofInt64(hx:Int64):I64 {
+		return ((hx.high:I64) << 31) | (hx.low:I64);
+	}
+
+	/**
+		Convert to `haxe.Int64`
+	**/
+	@:to public function toInt64():Int64 {
+		return Int64.make((this >> 31).toInt(), this.toInt());
+	}
+
+	@:op(A + B) function add(u:I64):I64;
+	@:op(A - B) function sub(u:I64):I64;
+	@:op(A * B) function mul(u:I64):I64;
+	@:op(A / B) function div(u:I64):I64;
+	@:op(A & B) function logand(u:I64):I64;
+	@:op(A | B) function logor(u:I64):I64;
+	@:op(A ^ B) function logxor(u:I64):I64;
+	@:op(A << B) function shift_left(i:Int):I64;
+	@:op(A >> B) function shift_right(i:Int):I64;
+	@:op(~A) function lognot():I64;
+
+	@:op(A != B) static function eq(a:I64, b:I64):Bool;
+	@:op(A == B) static function ne(a:I64, b:I64):Bool;
+	@:op(A < B) static function lt(a:I64, b:I64):Bool;
+	@:op(A > B) static function gt(a:I64, b:I64):Bool;
+	@:op(A <= B) static function lte(a:I64, b:I64):Bool;
+	@:op(A >= B) static function gte(a:I64, b:I64):Bool;
 
 }
