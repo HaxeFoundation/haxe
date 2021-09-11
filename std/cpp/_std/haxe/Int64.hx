552df781a9f651ba358f8ece14e59b87196535ea
Aidan63 <aidan.lee63@gmail.com>
2021-09-11 17:34:10 +0100

    use extern functions for Int64 operators

--- std/cpp/_std/haxe/Int64.hx
+++ std/cpp/_std/haxe/Int64.hx
@@ -25,46 +25,106 @@ package haxe;
 @:include("cpp/Int64.h")
 private extern class NativeInt64Helper {
 	@:native("_hx_int64_make")
-	static function make(high:Int32, low:Int32):Int64;
+	static function make(high:Int32, low:Int32):__Int64;
+
+	@:native(" ::cpp::Int64Struct")
+	static function ofInt(value:Int):__Int64;
+
+	@:native(" ::cpp::Int64Struct::is")
+	static function isInt64(d:Dynamic):Bool;
+
+	@:native("_hx_int64_is_neg")
+	static function isNeg(a:__Int64):Bool;
+
+	@:native("_hx_int64_is_zero")
+	static function isZero(a:__Int64):Bool;
 
 	@:native("_hx_int64_compare")
-	static function compare(a:Int64, b:Int64):Int;
+	static function compare(a:__Int64, b:__Int64):Int;
 
 	@:native("_hx_int64_ucompare")
-	static function ucompare(a:Int64, b:Int64):Int;
+	static function ucompare(a:__Int64, b:__Int64):Int;
+
+	@:native("_hx_int64_to_string")
+	static function toString(a:__Int64):String;
+
+	@:native("_hx_int64_neg")
+	static function neg(a:__Int64):__Int64;
+
+	@:native("_hx_int64_pre_increment")
+	static function preIncrement(a:__Int64):__Int64;
+
+	@:native("_hx_int64_post_increment")
+	static function postIncrement(a:__Int64):__Int64;
+
+	@:native("_hx_int64_pre_decrement")
+	static function preDecrement(a:__Int64):__Int64;
+
+	@:native("_hx_int64_post_decrement")
+	static function postDecrement(a:__Int64):__Int64;
+
+	@:native("_hx_int64_add")
+	static function add(a:__Int64, b:__Int64):__Int64;
+
+	@:native("_hx_int64_add")
+	static function addInt(a:__Int64, b:Int):__Int64;
+
+	@:native("_hx_int64_sub")
+	static function sub(a:__Int64, b:__Int64):__Int64;
+
+	@:native("_hx_int64_sub")
+	static function subInt(a:__Int64, b:Int):__Int64;
+
+	@:native("_hx_int64_sub")
+	static function intSub(a:Int, b:__Int64):__Int64;
+
+	@:native("_hx_int64_mul")
+	static function mul(a:__Int64, b:__Int64):__Int64;
 
 	@:native("_hx_int64_div")
-	static function div(a:Int64, b:Int64):Int64;
+	static function div(a:__Int64, b:__Int64):__Int64;
 
 	@:native("_hx_int64_mod")
-	static function mod(a:Int64, b:Int64):Int64;
+	static function mod(a:__Int64, b:__Int64):__Int64;
 
-	@:native("_hx_int64_shl")
-	static function shl(a:Int64, b:Int):Int64;
+	@:native("_hx_int64_eq")
+	static function eq(a:__Int64, b:__Int64):Bool;
 
-	@:native("_hx_int64_shr")
-	static function shr(a:Int64, b:Int):Int64;
+	@:native("_hx_int64_eq")
+	static function eqInt(a:__Int64, b:Int):Bool;
 
-	@:native("_hx_int64_ushr")
-	static function ushr(a:Int64, b:Int):Int64;
+	@:native("_hx_int64_neq")
+	static function neq(a:__Int64, b:__Int64):Bool;
 
-	@:native("_hx_int64_and")
-	static function and(a:Int64, b:Int64):Int64;
-
-	@:native("_hx_int64_or")
-	static function or(a:Int64, b:Int64):Int64;
-
-	@:native("_hx_int64_xor")
-	static function xor(a:Int64, b:Int64):Int64;
+	@:native("_hx_int64_neq")
+	static function neqInt(a:__Int64, b:Int):Bool;
 
 	@:native("_hx_int64_complement")
-	static function complement(a:Int64):Int64;
+	static function complement(a:__Int64):__Int64;
+
+	@:native("_hx_int64_and")
+	static function bitAnd(a:__Int64, b:__Int64):__Int64;
+
+	@:native("_hx_int64_or")
+	static function bitOr(a:__Int64, b:__Int64):__Int64;
+
+	@:native("_hx_int64_xor")
+	static function bitXor(a:__Int64, b:__Int64):__Int64;
+
+	@:native("_hx_int64_shl")
+	static function shl(a:__Int64, b:Int):__Int64;
+
+	@:native("_hx_int64_shr")
+	static function shr(a:__Int64, b:Int):__Int64;
+
+	@:native("_hx_int64_ushr")
+	static function ushr(a:__Int64, b:Int):__Int64;
 
 	@:native("_hx_int64_high")
-	static function high(a:Int64):Int32;
+	static function high(a:__Int64):Int32;
 
 	@:native("_hx_int64_low")
-	static function low(a:Int64):Int32;
+	static function low(a:__Int64):Int32;
 }
 
 private typedef __Int64 = cpp.Int64;
@@ -108,10 +168,10 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 		return x.low;
 
 	public static #if !cppia inline #end function isNeg(x:Int64):Bool
-		return x.val < 0;
+		return NativeInt64Helper.isNeg(x);
 
 	public static #if !cppia inline #end function isZero(x:Int64):Bool
-		return x == 0;
+		return NativeInt64Helper.isZero(x);
 
 	public static #if !cppia inline #end function compare(a:Int64, b:Int64):Int
 		return NativeInt64Helper.compare(a, b);
@@ -144,16 +204,16 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 		return {quotient: q, modulus: m};
 	}
 
-	@:op(-A)
+@:op(-A)
 	public static #if !cppia inline #end function neg(x:Int64):Int64
-		return -x.val;
+		return NativeInt64Helper.neg(x);
 
 	@:op(++A) private inline function preIncrement():Int64 {
 		#if cppia
 		this = this + make(0, 1);
 		return this;
 		#else
-		return ++this;
+		return NativeInt64Helper.preIncrement(this);
 		#end
 	}
 
@@ -163,7 +223,7 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 		this = this + make(0, 1);
 		return result;
 		#else
-		return this++;
+		return NativeInt64Helper.postIncrement(this);
 		#end
 	}
 
@@ -172,7 +232,7 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 		untyped this = this - make(0, 1);
 		return this;
 		#else
-		return --this;
+		return NativeInt64Helper.preDecrement(this);
 		#end
 	}
 
@@ -182,46 +242,46 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 		this = this - make(0, 1);
 		return result;
 		#else
-		return this--;
+		return NativeInt64Helper.postDecrement(this);
 		#end
 	}
 
 	@:op(A + B)
 	public static #if !cppia inline #end function add(a:Int64, b:Int64):Int64
-		return a.val + b.val;
+		return NativeInt64Helper.add(a, b);
 
 	@:op(A + B)
 	@:commutative
 	private static #if !cppia inline #end function addInt(a:Int64, b:Int):Int64
-		return a.val + b;
+		return NativeInt64Helper.addInt(a, b);
 
 	@:op(A - B)
 	public static #if !cppia inline #end function sub(a:Int64, b:Int64):Int64 {
-		return a.val - b.val;
+		return NativeInt64Helper.sub(a, b);
 	}
 
 	@:op(A - B)
 	private static #if !cppia inline #end function subInt(a:Int64, b:Int):Int64
-		return a.val - b;
+		return NativeInt64Helper.subInt(a, b);
 
 	@:op(A - B)
 	private static #if !cppia inline #end function intSub(a:Int, b:Int64):Int64
-		return a - b.val;
+		return NativeInt64Helper.intSub(a, b);
 
 	@:op(A * B)
 	public static #if !cppia inline #end function mul(a:Int64, b:Int64):Int64
-		return a.val * b.val;
+		return NativeInt64Helper.mul(a, b);
 
 	@:op(A * B)
 	@:commutative
 	private static #if !cppia inline #end function mulInt(a:Int64, b:Int):Int64
-		return a.val * b;
+		return mul(a, b);
 
 	@:op(A / B)
 	public static #if !cppia inline #end function div(a:Int64, b:Int64):Int64 {
-		if (b == 0)
+		if (NativeInt64Helper.isZero(b))
 			throw "divide by zero";
-		return NativeInt64Helper.div(a.val, b.val);
+		return NativeInt64Helper.div(a, b);
 	}
 
 	@:op(A / B)
@@ -234,7 +294,7 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 
 	@:op(A % B)
 	public static #if !cppia inline #end function mod(a:Int64, b:Int64):Int64 {
-		if (b == 0)
+		if (NativeInt64Helper.isZero(b))
 			throw "divide by zero";
 		return NativeInt64Helper.mod(a, b);
 	}
@@ -249,69 +309,69 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 
 	@:op(A == B)
 	public static #if !cppia inline #end function eq(a:Int64, b:Int64):Bool
-		return a.val == b.val;
+		return NativeInt64Helper.eq(a, b);
 
 	@:op(A == B)
 	@:commutative
 	private static #if !cppia inline #end function eqInt(a:Int64, b:Int):Bool
-		return a.val == b;
+		return NativeInt64Helper.eqInt(a, b);
 
 	@:op(A != B)
 	public static #if !cppia inline #end function neq(a:Int64, b:Int64):Bool
-		return a.val != b.val;
+		return NativeInt64Helper.neq(a, b);
 
 	@:op(A != B)
 	@:commutative
 	private static #if !cppia inline #end function neqInt(a:Int64, b:Int):Bool
-		return a.val != b;
+		return neq(a, b);
 
 	@:op(A < B)
 	private static #if !cppia inline #end function lt(a:Int64, b:Int64):Bool
-		return a.val < b.val;
+		return compare(a, b) < 0;
 
 	@:op(A < B)
 	private static #if !cppia inline #end function ltInt(a:Int64, b:Int):Bool
-		return a.val < b;
+		return lt(a, b);
 
 	@:op(A < B)
 	private static #if !cppia inline #end function intLt(a:Int, b:Int64):Bool
-		return a < b.val;
+		return lt(a, b);
 
 	@:op(A <= B)
 	private static #if !cppia inline #end function lte(a:Int64, b:Int64):Bool
-		return a.val <= b.val;
+		return compare(a, b) <= 0;
 
 	@:op(A <= B)
 	private static #if !cppia inline #end function lteInt(a:Int64, b:Int):Bool
-		return a.val <= b;
+		return lte(a, b);
 
 	@:op(A <= B)
 	private static #if !cppia inline #end function intLte(a:Int, b:Int64):Bool
-		return a <= b.val;
+		return lte(a, b);
 
 	@:op(A > B)
 	private static #if !cppia inline #end function gt(a:Int64, b:Int64):Bool
-		return a.val > b.val;
+		return compare(a, b) > 0;
 
 	@:op(A > B)
 	private static #if !cppia inline #end function gtInt(a:Int64, b:Int):Bool
-		return a.val > b;
+		return gt(a, b);
 
 	@:op(A > B)
 	private static #if !cppia inline #end function intGt(a:Int, b:Int64):Bool
-		return a > b.val;
+		return gt(a, b);
 
 	@:op(A >= B)
 	private static #if !cppia inline #end function gte(a:Int64, b:Int64):Bool
-		return a.val >= b.val;
+		return compare(a, b) >= 0;
 
 	@:op(A >= B)
 	private static #if !cppia inline #end function gteInt(a:Int64, b:Int):Bool
-		return a.val >= b;
+		return gte(a, b);
 
 	@:op(A >= B)
 	private static #if !cppia inline #end function intGte(a:Int, b:Int64):Bool
-		return a > b.val;
+		return gte(a, b);
 
 	@:op(~A)
 	private static #if !cppia inline #end function complement(a:Int64):Int64
@@ -319,15 +379,15 @@ abstract Int64(__Int64) from __Int64 from Int to __Int64 {
 
 	@:op(A & B)
 	public static #if !cppia inline #end function and(a:Int64, b:Int64):Int64
-		return NativeInt64Helper.and(a, b);
+		return NativeInt64Helper.bitAnd(a, b);
 
 	@:op(A | B)
 	public static #if !cppia inline #end function or(a:Int64, b:Int64):Int64
-		return NativeInt64Helper.or(a, b);
+		return NativeInt64Helper.bitOr(a, b);
 
 	@:op(A ^ B)
 	public static #if !cppia inline #end function xor(a:Int64, b:Int64):Int64
-		return NativeInt64Helper.xor(a, b);
+		return NativeInt64Helper.bitXor(a, b);
 
 	@:op(A << B)
 	public static #if !cppia inline #end function shl(a:Int64, b:Int):Int64
