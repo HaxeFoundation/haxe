59c66dbdde585f291e9b8d36e647c611401790d1
Aleksandr Kuzmenko <alex@stablex.ru>
2021-09-14 20:54:11 +0300

    Loop

--- std/cpp/uv/Loop.hx
+++ std/cpp/uv/Loop.hx
@@ -24,16 +24,157 @@ package cpp.uv;
 
 using cpp.uv.UV;
 
+enum LoopOption {
+	/**
+		Block a signal when polling for new events.
+
+		This operation is currently only implemented for SIGPROF signals.
+		Requesting other signals will fail with `UV_EINVAL`.
+
+		TODO: change Int to `enum abstract SigNum` when `cpp.uv.Signal` is implemented.
+	**/
+	BlockSignal(sigNum:Int);
+	/** Accumulate the amount of idle time the event loop spends in the event provider. */
+	MetricsIdleTime;
+}
+
+/**
+	Mode used to run the loop with `Loop.run`
+**/
+enum abstract LoopRunMode(Int) {
+	/**
+		Runs the event loop until there are no more active and referenced handles
+		or requests.
+		`loop.run(DEFAULT)` returns `true` if `loop.stop()` was called and there
+		are still active handles or requests.
+	**/
+	var DEFAULT;
+	/**
+		Poll for i/o once. Note that this function blocks if there are no pending
+		callbacks.
+		`loop.run(ONCE)` returns `false` when done (no active handles or requests
+		left), or `true` if more callbacks are expected (meaning you should run
+		the event loop again sometime in the future).
+	**/
+	var ONCE;
+	/**
+		Poll for i/o once but don’t block if there are no pending callbacks.
+		`loop.run(NOWAIT)` returns `false` if done (no active handles or requests
+		left), or `true` if more callbacks are expected (meaning you should run
+		the event loop again sometime in the future).
+	**/
+	var NOWAIT;
+}
+
+/**
+	Event loop.
+
+	@see http://docs.libuv.org/en/v1.x/loop.html
+**/
 @:headerCode('#include "uv.h"')
 class Loop {
-	var handle:Star<UvLoopT>;
+	var ref:Star<UvLoopT>;
 
-	function new() {}
+	function new() {
+		ref = UvLoopT.create();
+		cpp.vm.Gc.setFinalizer(this, Function.fromStaticFunction(finalizer));
+	}
 
+	static function finalizer(loop:Loop) {
+		Native.free(loop.ref);
+	}
+
+	/**
+		Creates and initializes a loop.
+	**/
 	static public function init():Loop {
 		var loop = new Loop();
-		loop.handle = UvLoopT.create();
-		trace(UV.loop_init(loop.handle));
+		UV.loop_init(loop.ref).resolve();
 		return loop;
+	}
+
+	/**
+		Set additional loop options.
+
+		You should normally call this before the first call to `loop.run()` unless
+		mentioned otherwise.
+	**/
+	public function configure(option:LoopOption):Void {
+		var result = switch option {
+			case BlockSignal(sigNum):
+				UV.loop_configure(ref, UV_LOOP_BLOCK_SIGNAL, sigNum);
+			case MetricsIdleTime:
+				UV.loop_configure(ref, UV_METRICS_IDLE_TIME);
+		}
+		result.resolve();
+	}
+
+	/**
+		Releases all internal loop resources.
+
+		Call this function only when the loop has finished executing and all open
+		handles and requests have been closed.
+	**/
+	public function close():Void {
+		UV.loop_close(ref).resolve();
+	}
+
+	/**
+		This function runs the event loop.
+	**/
+	public function run(mode:LoopRunMode = DEFAULT):Bool {
+		var result = switch mode {
+			case DEFAULT: UV.run(ref, UV_RUN_DEFAULT);
+			case ONCE: UV.run(ref, UV_RUN_ONCE);
+			case NOWAIT: UV.run(ref, UV_RUN_NOWAIT);
+		}
+		return 0 != result.resolve();
+	}
+
+	/**
+		Returns `true` if there are referenced active handles, active requests or
+		closing handles in the loop.
+	**/
+	public function alive():Bool {
+		return 0 != UV.loop_alive(ref).resolve();
+	}
+
+	/**
+		Stop the event loop, causing `loop.run()` to end as soon as possible.
+	**/
+	public function stop() {
+		return UV.stop(ref);
+	}
+
+	/**
+		Get backend file descriptor.
+
+		Only kqueue, epoll and event ports are supported.
+	**/
+	public function backendFd():Int {
+		return UV.backend_fd(ref);
+	}
+
+	/**
+		Get the poll timeout.
+
+		The return value is in milliseconds, or -1 for no timeout.
+	**/
+	public function backendTimeout():Int {
+		return UV.backend_timeout(ref);
+	}
+
+	/**
+		Return the current timestamp in milliseconds.
+	**/
+	public function now():UInt64 {
+		return UV.now(ref);
+	}
+
+	/**
+		Update the event loop’s concept of “now”.
+	**/
+	public function updateTime() {
+		UV.update_time(ref);
 	}
 }
