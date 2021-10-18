94043aba886f632b17dcf714a8db3fdd7d036f51
RblSb <msrblsb@gmail.com>
2021-10-18 18:08:35 +0300

    enum abstract test

--- tests/misc/projects/Issue6938/Main1.hx
+++ tests/misc/projects/Issue6938/Main1.hx
@@ -2,13 +2,23 @@ enum ScreenMode<T> {
 	AAA;
 	AAB;
 }
+enum abstract ScreenRatio(Int) {
+	var ZZA;
+	var ZZB;
+}
 
 class Main {
 	static final AAF = 0;
+	static final ZZF = 0;
 	static function main() {
 		var mode = ScreenMode.AAA;
 		switch(mode) {
 			case ScreenMoze.AAZ:
+			case _:
+		}
+		var mode2 = ScreenRatio.ZZA;
+		switch(mode2) {
+			case ScreenRazio.ZZZ:
 			case _:
 		}
 	}
