df8b4d9479debd1feb79e8f71aa7cea3f366bbbe
RblSb <msrblsb@gmail.com>
2021-10-18 18:00:43 +0300

    Detect field typos in switch

---
+++ tests/misc/projects/Issue6938/Main1.hx
@@ -0,0 +1,15 @@
+enum ScreenMode<T> {
+	AAA;
+	AAB;
+}
+
+class Main {
+	static final AAF = 0;
+	static function main() {
+		var mode = ScreenMode.AAA;
+		switch(mode) {
+			case ScreenMoze.AAZ:
+			case _:
+		}
+	}
+}
