f337243b01bcf6c7a0df76a80582e6c9f92c82e9
RblSb <msrblsb@gmail.com>
2021-10-25 22:03:16 +0300

    Make test run

--- tests/optimization/src/issues/Issue9943.hx
+++ tests/optimization/src/issues/Issue9943.hx
@@ -16,23 +16,18 @@ private extern class CSSStyleDeclaration {
 }
 
 class Issue9943 {
-	static function main() new Issue9943();
-	final foo = Std.random(0);
-	public function new() {
-		test();
-		test2();
-	}
+	static final foo = Std.random(0);
 
 	@:js('
 		var el = issues._Issue9943.Element.get();
-		var a = el.offset + this.foo;
+		var a = el.offset + issues_Issue9943.foo;
 		el.style.verticalAlign = 1;
-		var b = el.offset + this.foo;
+		var b = el.offset + issues_Issue9943.foo;
 		el.style.verticalAlign = 2;
-		var c = el.offset + this.foo;
-		this.values(a,b,c);
+		var c = el.offset + issues_Issue9943.foo;
+		issues_Issue9943.values(a,b,c);
 	')
-	function test() {
+	static function test() {
 		final el = Element.get();
 		final a = (el.offset + foo);
 		el.style.verticalAlign = 1;
@@ -45,9 +40,9 @@ class Issue9943 {
 		var el = issues._Issue9943.PureElement.get();
 		el.style.verticalAlign = 1;
 		el.style.verticalAlign = 2;
-		this.values(el.offset + this.foo,el.offset + this.foo,el.offset + this.foo);
+		issues_Issue9943.values(el.offset + issues_Issue9943.foo,el.offset + issues_Issue9943.foo,el.offset + issues_Issue9943.foo);
 	')
-	function test2() {
+	static function test2() {
 		final el = PureElement.get();
 		final a = (el.offset + foo);
 		el.style.verticalAlign = 1;
@@ -56,5 +51,5 @@ class Issue9943 {
 		final c = (el.offset + foo);
 		values(a, b, c);
 	}
-	function values(a, b, c) trace(a, b, c);
+	static function values(a, b, c) trace(a, b, c);
 }
