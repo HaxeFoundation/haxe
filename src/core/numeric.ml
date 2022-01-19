13ee0754fa59008c55a37a7f14400ba0b8369278
zagortenej <zagortenej@xbotdepot.com>
2022-01-19 06:30:45 -0600

    Add test cases for issues #10544 & #10545 (#10546)
    * Add test cases for issues #10544 & #10545
    
    * [eval] ignore tabs in parseFloat
    
    * ignore neko
    
    * [lua] Std.parseFloat: ignore any leading whitespace chars
    
    Co-authored-by: Simon Krajewski <simon@haxe.org>
    Co-authored-by: Aleksandr Kuzmenko <alex@stablex.ru>

--- src/core/numeric.ml
+++ src/core/numeric.ml
@@ -49,7 +49,7 @@ let parse_float s =
 	let rec loop sp i =
 		if i = String.length s then (if sp = 0 then s else String.sub s sp (i - sp)) else
 		match String.unsafe_get s i with
-		| ' ' when sp = i -> loop (sp + 1) (i + 1)
+		| ' ' | '\t' when sp = i -> loop (sp + 1) (i + 1)
 		| '0'..'9' | '-' | '+' | 'e' | 'E' | '.' -> loop sp (i + 1)
 		| _ -> String.sub s sp (i - sp)
 	in
