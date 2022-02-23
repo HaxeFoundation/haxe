154ee60135919559497b7207d2c0c6205fed3a7c
Simon Krajewski <simon@haxe.org>
2022-02-23 12:15:58 +0100

    add test

---
+++ tests/unit/src/unit/issues/Issue10537.hx
@@ -0,0 +1,40 @@
+package unit.issues;
+
+import haxe.macro.Context;
+
+class Issue10537 extends Test {
+	macro static function m() {
+		var imports = [
+			"haxe.ds.StringMap",
+			"StringTools.endsWith",
+			"Sys.*",
+			"StringTools.startsWith as sw"
+		];
+		var usings = ["StringTools"];
+		Context.withImports(imports, usings, () -> {
+			Context.typeExpr(macro {
+				StringMap;
+				endsWith("foo", "o");
+				println("print");
+				sw("foo", "f");
+				"foo".endsWith("o");
+			});
+		});
+
+		// nested
+		Context.withImports(["haxe.ds.StringMap"], [], () -> {
+			Context.withImports(["StringTools.endsWith"], [], () -> {
+				Context.typeExpr(macro {
+					StringMap;
+					endsWith("foo", "o");
+				});
+			});
+		});
+		return macro null;
+	}
+
+	function test() {
+		m();
+		utest.Assert.pass();
+	}
+}
