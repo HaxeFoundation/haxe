e1ced88e13ebdde36209b8af8e92d388793d91db
Jaime Marquínez Ferrándiz <jregistros+github@fastmail.net>
2022-03-08 09:25:26 +0100

    lua: Minimal implementation of ssl (#10593)
    * lua: Minimal implementation of ssl
    
    Depends on luasec.
    The initial implementation is enough to access a webpage over https.
    
    * Install luasec in test environment
    
    * Install openssl when testing the lua target on Mac OS
    
    Required by luasec
    
    * Don't try to link openssl on Mac OS
    
    brew doesn't allow it. Instead we pass the OPENSSL_DIR option to luarocks.
    
    * Don't require luasec when using lua-vanilla

--- tests/runci/targets/Lua.hx
+++ tests/runci/targets/Lua.hx
@@ -23,6 +23,7 @@ class Lua {
 
 				attemptCommand("brew", ["install", "pcre"]);
 				runCommand("pip3", ["install", "hererocks"]);
+				runCommand("brew", ["install", "openssl"]);
 			}
 		}
 	}
@@ -30,6 +31,9 @@ class Lua {
 	static function installLib(lib : String, version : String, ?server :String){
 		if (!commandSucceed("luarocks", ["show", lib, version])) {
             final args = ["install", lib, version];
+			if (systemName == "Mac") {
+				args.push('OPENSSL_DIR=/usr/local/opt/openssl@3');
+			}
             if (server != null){
                 final server_arg = '--server=$server';
                 args.push(server_arg);
@@ -67,6 +71,7 @@ class Lua {
 			// attemptCommand("luarocks", ["config", "--user-config"]);
 
 			installLib("haxe-deps", "0.0.1-6");
+			installLib("luasec", "1.0.2-1");
 
 			changeDirectory(unitDir);
 			runCommand("haxe", ["compile-lua.hxml"].concat(args));
