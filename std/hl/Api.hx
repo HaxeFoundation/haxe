c8dcde22c20b96847070d5eb9af9a16db9119b44
Nicolas Cannasse <ncannasse@gmail.com>
2022-04-28 17:02:59 +0200

    added checkReload()

--- std/hl/Api.hx
+++ std/hl/Api.hx
@@ -44,5 +44,6 @@ extern class Api {
 	#if (hl_ver >= version("1.12.0"))
 	@:hlNative("std", "is_prim_loaded") static function isPrimLoaded(f:haxe.Constraints.Function):Bool;
 	@:hlNative("?std", "mem_compact") static function compact<T>( v : T, exclude : hl.NativeArray<Dynamic>, flags : Int, outCount : hl.Ref<Int> ) : T;
+	@:hlNative("?std", "sys_check_reload") static function checkReload( ?debugFile : hl.Bytes ) : Bool;
 	#end
 }
