4ba3186e2ec06ce0cea7e5f45fb3892bde574432
Dan Korostelev <nadako@gmail.com>
2022-07-01 14:32:09 +0200

    test throw too (works already, just in case we break it in future)

--- tests/unit/src/unit/issues/Issue10744.hx
+++ tests/unit/src/unit/issues/Issue10744.hx
@@ -6,6 +6,7 @@ class Issue10744 extends Test {
 	function test() {
 		var v:Null<Int> = 10;
 		eq("Null<Int>", typeString(v ?? return));
+		eq("Null<Int>", typeString(v ?? throw true));
 		for (i in 0...1) {
 			eq("Null<Int>", typeString(v ?? break));
 			eq("Null<Int>", typeString(v ?? continue));
