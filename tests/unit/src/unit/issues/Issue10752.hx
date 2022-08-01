45db4d93b5f23c4c74f2ac6717e9c6ae68c6dda2
Simon Krajewski <simon@haxe.org>
2022-08-01 10:08:57 +0200

    [eval] deal with bit shifts > 31
    closes #10752

---
+++ tests/unit/src/unit/issues/Issue10752.hx
@@ -0,0 +1,14 @@
+package unit.issues;
+
+class Issue10752 extends Test {
+	function shl(x, y)
+		return x << y;
+
+	function test() {
+		eq(2, shl(1, 33));
+		eq(2, 1 << 33);
+
+		eq(1 >> 1, 1 >> 33);
+		eq(1 >>> 1, 1 >>> 33);
+	}
+}
