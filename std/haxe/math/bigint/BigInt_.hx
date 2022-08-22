b15c9f01c732e15b850834c7d423f0aaaa319983
flashultra <m_mail01@abv.bg>
2022-08-22 10:53:19 +0300

    Debug

--- std/haxe/math/bigint/BigInt_.hx
+++ std/haxe/math/bigint/BigInt_.hx
@@ -128,15 +128,25 @@ class BigInt_
 		var x:Int32, m:Int32;
 		for (n in 0...this.m_count) {
 			x = this.m_data.get(n);
+			#if cpp trace("x:"+x); #end
 			m = (x >> 1) & 0x77777777;
+			#if cpp trace("m:"+m); #end
 			x = x - m;
+			#if cpp trace("x:"+x); #end
 			m = (m >> 1) & 0x77777777;
+			#if cpp trace("m:"+m); #end
 			x = x - m;
+			#if cpp trace("x:"+x); #end
 			m = (m >> 1) & 0x77777777;
+			#if cpp trace("m:"+m); #end
 			x = x - m;
+			#if cpp trace("x:"+x); #end
 			x = (x + (x >> 4)) & 0x0F0F0F0F;
+			#if cpp trace("x:"+x); #end
 			x = x * 0x01010101;
+			#if cpp trace("x:"+x); #end
 			totalBits += x >> 24;
+			#if cpp trace("totalBits:"+totalBits); #end
 		}
 		return totalBits;
 	}
