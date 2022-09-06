e090b424d0c5a7d7320afdd79aa7a3ff2dacc821
RblSb <msrblsb@gmail.com>
2022-09-06 03:41:18 +0300

    Early returns in NullCoal

--- src/typing/typer.ml
+++ src/typing/typer.ml
@@ -1852,7 +1852,28 @@ and type_expr ?(mode=MGet) ctx (e,p) (with_type:WithType.t) =
 		let e1 = vr#as_var "tmp" {e1 with etype = ctx.t.tnull e1.etype} in
 		let e_null = Builder.make_null e1.etype e1.epos in
 		let e_cond = mk (TBinop(OpNotEq,e1,e_null)) ctx.t.tbool e1.epos in
-		let iftype = WithType.WithType(e2.etype,None) in
+		let rec is_dead_end e = match e.eexpr with
+			| TParenthesis e -> is_dead_end e
+			| TThrow _ -> true
+			| TReturn _ -> true
+			| TBreak -> true
+			| TContinue -> true
+			| TWhile (_, body, DoWhile) -> is_dead_end body
+			| TIf (_, if_body, Some else_body) -> is_dead_end if_body && is_dead_end else_body
+			| TBlock exprs -> List.exists is_dead_end exprs
+			| TMeta (_, e) -> is_dead_end e
+			| TCast (e, _) -> is_dead_end e
+			| _ -> false in
+		let follow_null_once t =
+			match t with
+			| TAbstract({a_path = [],"Null"},[t]) -> t
+			| _ -> t
+		in
+		let iftype = if is_dead_end e2 then
+			WithType.with_type (follow_null_once e1.etype)
+		else
+			WithType.WithType(e2.etype,None)
+		in
 		let e_if = make_if_then_else ctx e_cond e1 e2 iftype p in
 		vr#to_texpr e_if
 	| EBinop (OpAssignOp OpNullCoal,e1,e2) ->
--- tests/unit/src/unit/issues/Issue10744.hx
+++ tests/unit/src/unit/issues/Issue10744.hx
@@ -5,11 +5,20 @@ import unit.HelperMacros.typeString;
 class Issue10744 extends Test {
 	function test() {
 		var v:Null<Int> = 10;
-		eq("Null<Int>", typeString(v ?? return));
-		eq("Null<Int>", typeString(v ?? throw true));
+		eq("Int", typeString(v ?? return));
+		eq("Int", typeString(v ?? throw true));
 		for (i in 0...1) {
-			eq("Null<Int>", typeString(v ?? break));
-			eq("Null<Int>", typeString(v ?? continue));
+			eq("Int", typeString(v ?? break));
+			eq("Int", typeString(v ?? continue));
 		}
+		eq("Int", typeString(v ?? {
+			(throw "nope");
+		}));
+		eq("Null<Int>", typeString(v ?? {
+			if (Std.random(0) == 0)
+				return;
+			else
+				v;
+		}));
 	}
 }
