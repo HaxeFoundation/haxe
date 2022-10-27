39d85bd94c9c66694775397398146cebc669230a
Aidan Lee <aidan.lee63@gmail.com>
2022-10-27 16:17:59 +0100

    Fix change where I didn't notice case change

--- src/generators/gencpp.ml
+++ src/generators/gencpp.ml
@@ -2099,7 +2099,8 @@ let cpp_cast_variant_type_of t = match t with
 let enum_getter_type t =
    match cpp_variant_type_of t with
    | TCppString -> "String"
-   | TCppScalar "int" -> "Int"
+   | TCppScalar "int"  -> "Int"
+   | TCppScalar "bool"  -> "Bool"
    | TCppScalar x  -> x
    | _  -> "Object"
 ;;
