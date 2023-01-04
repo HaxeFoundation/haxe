1d17d55ef5cc10de30701147f410dd240c3d6e1b
inklit <inklit@users.noreply.github.com>
2023-01-04 09:57:30 -0800

    Update Lua.hx

--- tests/runci/targets/Lua.hx
+++ tests/runci/targets/Lua.hx
@@ -14,8 +14,8 @@ class Lua {
 				//choco install openssl
 				runCommand("choco", ["install", "openssl"]);
 				
-				attemptCommand("cp", ["C:\\Program Files\\OpenSSL-Win64\\libssl-1_1.dll", "C:\\Program Files\\OpenSSL-Win64\\libssl32MD.dll"]);
-				attemptCommand("cp", ["C:\\Program Files\\OpenSSL-Win64\\libcrypto-1_1.dll", "C:\\Program Files\\OpenSSL-Win64\\libcrypto32MD.dll"]);
+				attemptCommand("cp", ["C:\\Program Files\\OpenSSL-Win64\\lib\\libssl-1_1.dll", "C:\\Program Files\\OpenSSL-Win64\\lib\\libssl32MD.dll"]);
+				attemptCommand("cp", ["C:\\Program Files\\OpenSSL-Win64\\lib\\libcrypto-1_1.dll", "C:\\Program Files\\OpenSSL-Win64\\lib\\libcrypto32MD.dll"]);
 			case "Linux":
 				Linux.requireAptPackages(["libpcre3-dev", "libssl-dev", "libreadline-dev"]);
 				runCommand("pip", ["install", "--user", "hererocks"]);
