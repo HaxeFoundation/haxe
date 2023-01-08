0e1072ba0f5345a7cea1a8ff2b0233b65dbc33a5
inklit <inklit@users.noreply.github.com>
2023-01-08 15:57:06 -0800

    Update _hx_bit.lua
    Modify default lua native bitops library from bit to bit32

--- std/lua/_lua/_hx_bit.lua
+++ std/lua/_lua/_hx_bit.lua
@@ -1,15 +1,16 @@
--- require this for lua 5.1
-pcall(require, 'bit')
-if bit then
-  _hx_bit_raw = bit
-  _hx_bit = setmetatable({}, { __index = _hx_bit_raw });
-else
+if pcall(require, 'bit32') then --if we are on Lua 5.1, bit32 will be the default. 
   _hx_bit_raw = _G.require('bit32')
   _hx_bit = setmetatable({}, { __index = _hx_bit_raw });
   -- lua 5.2 weirdness
   _hx_bit.bnot = function(...) return _hx_bit_clamp(_hx_bit_raw.bnot(...)) end;
   _hx_bit.bxor = function(...) return _hx_bit_clamp(_hx_bit_raw.bxor(...)) end;
+else
+  --If we do not have bit32, fallback to 'bit'
+  pcall(require, 'bit')
+  _hx_bit_raw = bit
+  _hx_bit = setmetatable({}, { __index = _hx_bit_raw });
 end
+
 -- see https://github.com/HaxeFoundation/haxe/issues/8849
 _hx_bit.bor = function(...) return _hx_bit_clamp(_hx_bit_raw.bor(...)) end;
 _hx_bit.band = function(...) return _hx_bit_clamp(_hx_bit_raw.band(...)) end;
