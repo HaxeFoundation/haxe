dc16c83ae3bf8b46671dad8f51e89a6a2a64efb8
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-02-15 05:19:54 -0500

    taking a suggestion from kLabz

--- std/haxe/format/JsonParser.hx
+++ std/haxe/format/JsonParser.hx
@@ -37,7 +37,7 @@ class JsonParser {
 		JSON objects are parsed into anonymous structures and JSON arrays
 		are parsed into `Array<Dynamic>`.
 
-		If given `str` is not a valid JSON, an exception will be thrown.
+		If given `str` is not valid JSON, an exception will be thrown.
 
 		If `str` is null, the result is unspecified.
 	**/
