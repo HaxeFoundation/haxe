610ea05f3852c01cf9a7a3e6d4f7f97bf25577a7
Mathieu Sénidre <matse@skwatt.com>
2023-03-20 22:16:35 +0100

    fixed wrong return and parameter types in DateTimeFormatter

--- std/flash/globalization/DateTimeFormatter.hx
+++ std/flash/globalization/DateTimeFormatter.hx
@@ -2,21 +2,21 @@ package flash.globalization;
 
 @:require(flash10_1) extern final class DateTimeFormatter {
 	@:flash.property var actualLocaleIDName(get,never) : String;
-	@:flash.property var lastOperationStatus(get,never) : LastOperationStatus;
+	@:flash.property var lastOperationStatus(get,never) : String;
 	@:flash.property var requestedLocaleIDName(get,never) : String;
-	function new(requestedLocaleIDName : String, ?dateStyle : DateTimeStyle, ?timeStyle : DateTimeStyle) : Void;
+	function new(requestedLocaleIDName : String, ?dateStyle : String, ?timeStyle : String) : Void;
 	function format(dateTime : Date) : String;
 	function formatUTC(dateTime : Date) : String;
-	function getDateStyle() : DateTimeStyle;
-	function getDateTimePattern() : DateTimeStyle;
+	function getDateStyle() : String;
+	function getDateTimePattern() : String;
 	function getFirstWeekday() : Int;
-	function getMonthNames(?nameStyle : DateTimeNameStyle, ?context : DateTimeNameContext) : flash.Vector<String>;
-	function getTimeStyle() : DateTimeStyle;
-	function getWeekdayNames(?nameStyle : DateTimeNameStyle, ?context : DateTimeNameContext) : flash.Vector<String>;
+	function getMonthNames(?nameStyle : String, ?context : String) : flash.Vector<String>;
+	function getTimeStyle() : String;
+	function getWeekdayNames(?nameStyle : String, ?context : String) : flash.Vector<String>;
 	private function get_actualLocaleIDName() : String;
-	private function get_lastOperationStatus() : LastOperationStatus;
+	private function get_lastOperationStatus() : String;
 	private function get_requestedLocaleIDName() : String;
 	function setDateTimePattern(pattern : String) : Void;
-	function setDateTimeStyles(dateStyle : DateTimeStyle, timeStyle : DateTimeStyle) : Void;
+	function setDateTimeStyles(dateStyle : String, timeStyle : String) : Void;
 	static function getAvailableLocaleIDNames() : flash.Vector<String>;
 }
