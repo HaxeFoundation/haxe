1e41e5c642db3dd9cf0cb681f96f12f504ed220e
Jesse Rosalia <jesse@bommer.io>
2023-03-28 14:12:07 -0700

    [python] Fixed python.net.SslSocket when python-version >= 3.4 (#8401)
    The issue where SslSocket throws an exception is due to hostName being
    null in wrap_socket.  This fix delays wrap_socket until after connect is
    called, so we can pass a host in.

---
+++ tests/unit/src/unit/issues/Issue8401.hx
@@ -0,0 +1,55 @@
+package unit.issues;
+
+import sys.net.Host;
+
+class Issue8401 extends unit.Test {
+	function testNew() {
+		var sock = new python.net.SslSocket();
+		// With Issue8401, construction fails immediately; if we get this far, it's a pass
+		utest.Assert.pass();
+	}
+
+	@:access(python.net.SslSocket.__s)
+	@:access(python.net.SslSocket.wrapSocketWithSslContext)
+	function testTimeout() {
+		var sock = new python.net.SslSocket();
+		// gettimeout is not currently defined in the python socket extern, but it's
+		// present in the python socket type.
+		eq(null, (cast sock.__s).gettimeout());
+		sock.setTimeout(500);
+		eq(500, (cast sock.__s).gettimeout());
+		// This will change __s.  Make sure we set the timeout properly.
+		sock.wrapSocketWithSslContext("127.0.0.1");
+		eq(500, (cast sock.__s).gettimeout());
+	}
+
+	#if (python_verion >= 3.7)
+	@:access(python.net.SslSocket.__s)
+	@:access(python.net.SslSocket.wrapSocketWithSslContext)
+	function testBlocking() {
+		var sock = new python.net.SslSocket();
+		// getblocking is not currently defined in the python socket extern, but it's
+		// present in the python socket type.
+		t((cast sock.__s).getblocking());
+		sock.setBlocking(false);
+		f((cast sock.__s).getblocking());
+		// This will change __s.  Make sure we set the timeout properly.
+		sock.wrapSocketWithSslContext("127.0.0.1");
+		f((cast sock.__s).getblocking());
+	}
+	#end
+
+	@:access(python.net.SslSocket.__s)
+	@:access(python.net.SslSocket.wrapSocketWithSslContext)
+	function testFastSend() {
+		var sock = new python.net.SslSocket();
+		// getsockopt is not currently defined in the python socket extern, but it's
+		// present in the python socket type.
+		eq(0, (cast sock.__s).getsockopt(python.lib.Socket.SOL_TCP, python.lib.Socket.TCP_NODELAY));
+		sock.setFastSend(true);
+		eq(1, (cast sock.__s).getsockopt(python.lib.Socket.SOL_TCP, python.lib.Socket.TCP_NODELAY));
+		// This will change __s.  Make sure we set the timeout properly.
+		sock.wrapSocketWithSslContext("127.0.0.1");
+		eq(1, (cast sock.__s).getsockopt(python.lib.Socket.SOL_TCP, python.lib.Socket.TCP_NODELAY));
+	}
+}
