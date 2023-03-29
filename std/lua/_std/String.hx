d91e1a8e09f7ad25e876d8a1bfe2fc777afd3a81
RblSb <msrblsb@gmail.com>
2023-03-29 20:29:26 +0300

    Hide lua String inlines

--- std/lua/_std/String.hx
+++ std/lua/_std/String.hx
@@ -63,14 +63,18 @@ class String {
 		return BaseString.lower(this);
 
 	public inline function indexOf(str:String, ?startIndex:Int):Int {
+		return _indexOf(this, str, startIndex);
+	}
+
+	static function _indexOf(sthis:String, str:String, ?startIndex:Int):Int {
 		if (startIndex == null)
 			startIndex = 1;
 		else
 			startIndex += 1;
 		if (str == "") {
-			return indexOfEmpty(this, startIndex - 1);
+			return indexOfEmpty(sthis, startIndex - 1);
 		}
-		var r = BaseString.find(this, str, startIndex, true).begin;
+		var r = BaseString.find(sthis, str, startIndex, true).begin;
 		if (r != null && r > 0)
 			return r - 1;
 		else
@@ -87,11 +91,15 @@ class String {
 	}
 
 	public inline function lastIndexOf(str:String, ?startIndex:Int):Int {
+		return _lastIndexOf(this, str, startIndex);
+	}
+
+	static function _lastIndexOf(sthis:String, str:String, ?startIndex:Int):Int {
 		var ret = -1;
 		if (startIndex == null)
-			startIndex = length;
+			startIndex = sthis.length;
 		while (true) {
-			var p = indexOf(str, ret + 1);
+			var p = _indexOf(sthis, str, ret + 1);
 			if (p == -1 || p > startIndex || p == ret)
 				break;
 			ret = p;
@@ -100,24 +108,28 @@ class String {
 	}
 
 	public inline function split(delimiter:String):Array<String> {
-		var idx = 1;
+		return _split(this, delimiter);
+	}
+
+	static function _split(sthis:String, delimiter:String):Array<String> {
+		var idx:Null<Int> = 1;
 		var ret = [];
 		while (idx != null) {
-			var newidx = 0;
+			var newidx:Null<Int> = 0;
 			if (delimiter.length > 0) {
-				newidx = BaseString.find(this, delimiter, idx, true).begin;
-			} else if (idx >= this.length) {
+				newidx = BaseString.find(sthis, delimiter, idx, true).begin;
+			} else if (idx >= sthis.length) {
 				newidx = null;
 			} else {
 				newidx = idx + 1;
 			}
 
 			if (newidx != null) {
-				var match = BaseString.sub(this, idx, newidx - 1).match;
+				var match = BaseString.sub(sthis, idx, newidx - 1).match;
 				ret.push(match);
 				idx = newidx + delimiter.length;
 			} else {
-				ret.push(BaseString.sub(this, idx, this.length).match);
+				ret.push(BaseString.sub(sthis, idx, sthis.length).match);
 				idx = null;
 			}
 		}
@@ -129,17 +141,21 @@ class String {
 	}
 
 	public inline function substring(startIndex:Int, ?endIndex:Int):String {
+		return _substring(this, startIndex, endIndex);
+	}
+
+	static function _substring(sthis:String, startIndex:Int, ?endIndex:Int):String {
 		if (endIndex == null)
-			endIndex = this.length;
+			endIndex = sthis.length;
 		if (endIndex < 0)
 			endIndex = 0;
 		if (startIndex < 0)
 			startIndex = 0;
 		if (endIndex < startIndex) {
 			// swap the index positions
-			return BaseString.sub(this, endIndex + 1, startIndex).match;
+			return BaseString.sub(sthis, endIndex + 1, startIndex).match;
 		} else {
-			return BaseString.sub(this, startIndex + 1, endIndex).match;
+			return BaseString.sub(sthis, startIndex + 1, endIndex).match;
 		}
 	}
 
@@ -152,15 +168,19 @@ class String {
 	}
 
 	public inline function substr(pos:Int, ?len:Int):String {
-		if (len == null || len > pos + this.length)
-			len = this.length;
+		return _substr(this, pos, len);
+	}
+
+	static function _substr(sthis:String, pos:Int, ?len:Int):String {
+		if (len == null || len > pos + sthis.length)
+			len = sthis.length;
 		else if (len < 0)
-			len = length + len;
+			len = sthis.length + len;
 		if (pos < 0)
-			pos = length + pos;
+			pos = sthis.length + pos;
 		if (pos < 0)
 			pos = 0;
-		return BaseString.sub(this, pos + 1, pos + len).match;
+		return BaseString.sub(sthis, pos + 1, pos + len).match;
 	}
 
 	public inline static function fromCharCode(code:Int):String {
