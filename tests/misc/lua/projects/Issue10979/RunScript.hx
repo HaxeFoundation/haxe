364527fa66ebc065627e167b7b976cb124ce90fc
tobil4sk <tobil4sk@outlook.com>
2023-04-04 00:07:15 +0100

    Prevent test failures on lua 5.1
    Lua 5.1 has slightly different error messages and doesn't print custom
    error objects.

--- tests/misc/lua/projects/Issue10979/RunScript.hx
+++ tests/misc/lua/projects/Issue10979/RunScript.hx
@@ -1,18 +1,38 @@
 using StringTools;
 
+function isLua5_1() {
+	final proc = new sys.io.Process("lua", ["-v"]);
+	final out = proc.stderr.readLine();
+	proc.close();
+	return out.startsWith("Lua 5.1.");
+}
+
+function matchesExpectedMessage(actual:String) {
+	// lua 5.1 doesn't print custom error objects
+	if (actual == "(error object is not a string)") {
+		return true;
+	}
+	return new EReg(Sys.args()[1], "").match(actual);
+}
+
 function main() {
 	final proc = new sys.io.Process("lua", [Sys.args()[0]]);
 
-	// if there is a line number, replace it with _
-	final exceptionMessage = ~/:(\d+):/.replace(proc.stderr.readLine().substr(5), ":_:");
+	// ignore "lua: "
+	final exceptionMessage = proc.stderr.readLine().substr(5);
+
+	final hasExpectedMessage = matchesExpectedMessage(exceptionMessage);
 	// we don't want a bare error without a stack trace
-	final hasStackTrace = proc.stderr.readLine().contains('stack traceback');
+	final hasStackTrace = try {
+		proc.stderr.readLine().contains('stack traceback');
+	} catch (_:haxe.io.Eof) {
+		false;
+	};
 
-	final code = proc.exitCode();
-
-	Sys.println('Error code: ${code}');
-	Sys.println('Exception message: ${exceptionMessage}');
-	Sys.println('Has call stack: ${hasStackTrace}');
+	Sys.println('Error code: ${proc.exitCode()}');
+	Sys.println('Expected exception message: ${hasExpectedMessage}');
+	// 5.1 interpreter doesn't handle custom objects
+	Sys.println('Has call stack: ${hasStackTrace || isLua5_1()}');
 
 	proc.close();
 }
--- tests/misc/lua/projects/Issue10979/runner.lua
+++ tests/misc/lua/projects/Issue10979/runner.lua
@@ -1,11 +1,10 @@
 -- error in loaded script should be caught here, instead of exiting everything
 local success, err = pcall(require, arg[1])
 
-local first_line = string.match(tostring(err), '^[^\n]+')
--- replace line number with _
-local exception_message = string.gsub(first_line, ':%d+:', ':_:')
+local exception_message = string.match(tostring(err), '^[^\n]+')
+local has_expected_message = string.match(exception_message, arg[2]) ~= nil
 local has_stack_trace = string.match(tostring(err), 'stack traceback') ~= nil
 
 print('Success: '..tostring(success))
-print('Exception message: '..exception_message)
+print('Expected exception message: '..tostring(has_expected_message))
 print('Has call stack: '..tostring(has_stack_trace))
