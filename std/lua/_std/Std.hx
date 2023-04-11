9cdff6a47cf5a0ec0fc713a717a9fe5405186074
Frixuu <kontakt@lukasz.xyz>
2023-04-11 09:01:43 +0200

    [lua] Emit class name even if not required (#11112)
    * [lua] Emit class name even if not required
    
    * [lua] Remove Std static initializer
    
    It seems like those elements get created in generate_class anyway?

--- std/lua/_std/Std.hx
+++ std/lua/_std/Std.hx
@@ -96,11 +96,4 @@ import lua.NativeStringTools;
 	public static function random(x:Int):Int {
 		return untyped x <= 0 ? 0 : Math.floor(Math.random() * x);
 	}
-
-	static function __init__():Void
-		untyped {
-			__feature__("lua.Boot.isClass", String.__name__ = __feature__("Type.getClassName", "String", true));
-			__feature__("Type.resolveClass", _hxClasses["Array"] = Array);
-			__feature__("lua.Boot.isClass", Array.__name__ = __feature__("Type.getClassName", "Array", true));
-		}
 }
