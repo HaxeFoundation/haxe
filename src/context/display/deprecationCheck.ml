2e8caec23e5df357fd1dbdeb34ca074b1d629531
Simon Krajewski <simon@haxe.org>
2023-04-11 15:28:33 +0200

    [typer] bring back "is" deprecation warning under haxe-next
    also activate haxe-next on unit tests

--- src/context/display/deprecationCheck.ml
+++ src/context/display/deprecationCheck.ml
@@ -133,6 +133,5 @@ let run_on_field ?(force=false) com cf = if_enabled ~force com (fun() -> run_on_
 let run ?(force=false) com = if_enabled ~force com (fun() -> run com)
 
 let check_is com name meta p =
-	()
-	(* if name = "is" && not (Meta.has Meta.Deprecated meta) then
-		warn_deprecation com "Using \"is\" as an identifier is deprecated" p *)
+	if is_next com && name = "is" && not (Meta.has Meta.Deprecated meta) then
+		warn_deprecation com "Using \"is\" as an identifier is deprecated" p
