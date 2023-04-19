0902eba710e21a3e5aa36463601a452854edaf1b
Rudy Ges <k@klabz.org>
2023-04-19 10:45:52 +0200

    [tests] add another example

--- tests/misc/projects/Issue11164/Main.hx
+++ tests/misc/projects/Issue11164/Main.hx
@@ -1,8 +1,13 @@
 typedef A<K = String, V> = haxe.ds.BalancedTree<K,V>;
 
+class B<T1 = Int, T2 = String, T3> {}
+
 class Main {
 	static function main() {
 		var a:A<_,Int> = null;
 		$type(a);
+
+		var b:B<Bool,_,Int> = null;
+		$type(b);
 	}
 }
