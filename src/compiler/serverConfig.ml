6a554bfb34cb5f6bb3029835a0e630a6e8d1e119
Rudy Ges <k@klabz.org>
2023-05-15 19:43:41 +0200

    [display] Config to disable populating cache from display requests (#11224)
    * Add compilation server config to disable populating cache from display requests
    
    * [tests] Add test for 11177 (including a disabled one for now)
    
    * [tests] Add test for 11184 (including a disabled one for now)

--- src/compiler/serverConfig.ml
+++ src/compiler/serverConfig.ml
@@ -1,2 +1,3 @@
 let do_not_check_modules = ref false
+let populate_cache_from_display = ref true
 let legacy_completion = ref false
---
+++ tests/server/test/templates/issues/Issue11177/KeyCode.hx
@@ -0,0 +1,3 @@
+enum abstract KeyCode(Int) {
+	var Backspace = 8;
+}
