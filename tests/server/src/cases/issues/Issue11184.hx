cce0df5d2e19da9ac65798f202b5b0273ebc1250
Rudy Ges <k@klabz.org>
2023-05-16 15:17:47 +0200

    [tests] Update test for 11184

--- tests/server/src/cases/issues/Issue11184.hx
+++ tests/server/src/cases/issues/Issue11184.hx
@@ -2,10 +2,7 @@ package cases.issues;
 
 class Issue11184 extends TestCase {
 	function testDiagnostics(_) {
-		final content = getTemplate("issues/Issue11184/Main.hx");
-		final transform = Marker.extractMarkers(content);
-
-		vfs.putContent("Main.hx", transform.source);
+		vfs.putContent("Main.hx", getTemplate("issues/Issue11184/Main.hx"));
 		var args = ["-main", "Main", "-js", "bin/test.js"];
 
 		runHaxe(args.concat(["--display", "Main.hx@0@diagnostics"]));
@@ -18,32 +15,8 @@ class Issue11184 extends TestCase {
 		Assert.isTrue(hasErrorMessage("Cannot use Void as value"));
 	}
 
-	function testFindReferences(_) {
-		final content = getTemplate("issues/Issue11184/Main.hx");
-		final transform = Marker.extractMarkers(content);
-
-		vfs.putContent("Main.hx", transform.source);
-		var args = ["-main", "Main", "-js", "bin/test.js"];
-
-		runHaxeJson(args, DisplayMethods.FindReferences, {
-			file: new FsPath("Main.hx"),
-			offset: transform.markers.get(1),
-			contents: transform.source
-		});
-		final errors = haxe.Json.parse(lastResult.stderr).error?.data;
-		Assert.equals(errors[0].message, "Cannot use Void as value");
-
-		runHaxe(args);
-		Assert.isTrue(hasErrorMessage("Cannot use Void as value"));
-		runHaxe(args);
-		Assert.isTrue(hasErrorMessage("Cannot use Void as value"));
-	}
-
 	function testWithoutCacheFromDisplay(_) {
-		final content = getTemplate("issues/Issue11184/Main.hx");
-		final transform = Marker.extractMarkers(content);
-
-		vfs.putContent("Main.hx", transform.source);
+		vfs.putContent("Main.hx", getTemplate("issues/Issue11184/Main.hx"));
 		var args = ["-main", "Main", "-js", "bin/test.js"];
 		runHaxeJson([], ServerMethods.Configure, {populateCacheFromDisplay: false});
 		runHaxe(args.concat(["--display", "Main.hx@0@diagnostics"]));
