c7b9007f1d2ac2dfcab808bd4d2e15b4c9bca196
Simon Krajewski <simon@haxe.org>
2023-07-11 12:44:40 +0200

    start working on local type parameters

--- tests/unit/src/unit/issues/Issue2889.hx
+++ tests/unit/src/unit/issues/Issue2889.hx
@@ -1,16 +1,14 @@
 package unit.issues;
 
 class Issue2889 extends Test {
-	public function test()
-	{
-		// function mapMappable <A,B>(m:Mappable<A>, f:A->B):Mappable<B> {
-		// 	return m.map(f);
-		// }
-		// var r = mapMappable([1], function (y) return y+1);
-		// var r:Array<Int> = cast r;
-		// eq(r.length,1);
-		// eq(2,r[0]);
-		utest.Assert.pass();
+	public function test() {
+		function mapMappable<A, B>(m:Mappable<A>, f:A->B):Mappable<B> {
+			return m.map(f);
+		}
+		var r = mapMappable([1], function(y) return y + 1);
+		var r:Array<Int> = cast r;
+		eq(r.length, 1);
+		eq(2, r[0]);
 	}
 
 	function testDynamic() {
@@ -25,14 +23,14 @@ class Issue2889 extends Test {
 	}
 
 	function testFilterStructure() {
-		var a:{ function filter(f:Int->Bool):Array<Int>; } = [1, 2];
+		var a:{function filter(f:Int->Bool):Array<Int>;} = [1, 2];
 		var b = a.filter(function(x) return x % 2 == 0);
 		eq(1, b.length);
 		eq(2, b[0]);
 	}
 
 	function testMapStructure() {
-		var a:{ function map(f:Int->Int):Array<Int>; } = [1, 2];
+		var a:{function map(f:Int->Int):Array<Int>;} = [1, 2];
 		var b = a.map(function(x) return x * 2);
 		eq(2, b.length);
 		eq(2, b[0]);
@@ -41,6 +39,5 @@ class Issue2889 extends Test {
 }
 
 private typedef Mappable<Y> = {
-	public function map <X>(f:Y->X):Array<X>;
+	public function map<X>(f:Y->X):Array<X>;
 }
-
