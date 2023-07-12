11054dcd091b8e877c6c9c3078a0b3b858172d87
Rudy Ges <k@klabz.org>
2023-07-12 08:08:45 +0200

    [tests] restore some tests

--- tests/unit/src/unit/issues/Issue6560.hx
+++ tests/unit/src/unit/issues/Issue6560.hx
@@ -1,13 +1,14 @@
 package unit.issues;
 
 class Issue6560 extends unit.Test {
-	function test() {
-		function foo<F>(a:F):Array<F> {
-			if (false)
-				foo(1);
-			return if (a == null) [] else foo(null);
-		}
-		var bar:Array<Int> = foo(1);
-		eq(0, bar.length);
-	}
+
+    function test() {
+        function foo<F>(a:F):Array<F> {
+            if (false) foo(1);
+            return if (a == null) [] else foo(null);
+        }
+        var bar:Array<Int> = foo(1);
+        eq(0, bar.length);
+    }
+
 }
