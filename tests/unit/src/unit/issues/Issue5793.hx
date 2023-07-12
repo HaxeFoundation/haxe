325bc4ca4964c9996f0f83e4992d2ecd95fc8937
Rudy Ges <k@klabz.org>
2023-07-12 08:08:45 +0200

    [tests] restore some tests

--- tests/unit/src/unit/issues/Issue5793.hx
+++ tests/unit/src/unit/issues/Issue5793.hx
@@ -1,24 +1,24 @@
 package unit.issues;
 
 class Issue5793 extends Test {
-	public function test() {
-		function run1() {
-			function foo<T>(expected:Array<T>, a:T) {
-				eq(expected[0], a);
-			}
-			foo([2], 2);
-		}
-		run1();
+  public function test() {
+    function run1() {
+      function foo <T> (expected:Array<T>, a:T) {
+        eq(expected[0], a);
+      }
+      foo([2], 2);
+    }
+    run1();
 
-		function run2() {
-			function bar<A>(expected:Array<A>, a:A) {
-				function baz<B>(expected:Array<B>, b:B) {
-					eq(expected[0], b);
-				}
-				baz(expected, a);
-			}
-			bar([42], 42);
-		}
-		run2();
-	}
+    function run2() {
+      function bar<A>(expected:Array<A>, a:A) {
+        function baz<B>(expected:Array<B>, b:B) {
+          eq(expected[0], b);
+        }
+        baz(expected,a);
+      }
+      bar([42],42);
+    }
+    run2();
+  }
 }
--- tests/unit/src/unit/issues/Issue6106.hx
+++ tests/unit/src/unit/issues/Issue6106.hx
@@ -4,8 +4,7 @@ class Issue6106 extends unit.Test {
 	function test() {
 		var code = 0;
 		function assertEquals<T>(expected:T, actual:T) {
-			if (expected != actual)
-				code++;
+			if(expected != actual) code++;
 		}
 
 		var f = function() assertEquals(1, 1);
