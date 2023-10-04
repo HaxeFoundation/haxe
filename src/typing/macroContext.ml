f4d4a526b04c13db461697bac1eaacbd283b1154
Rudy Ges <k@klabz.org>
2023-10-04 07:46:29 +0200

    small cleanup

--- src/typing/macroContext.ml
+++ src/typing/macroContext.ml
@@ -1053,14 +1053,12 @@ let call_init_macro com mctx e =
 	let api = make_macro_com_api com p in
 	(match !macro_interp_cache with
 	| None ->
-		(* trace "create macro interp for init macro"; *)
 		let init,_ = create_macro_interp api mctx in
 		init();
 	| _ -> ());
 
 	let mctx, (margs,_,mclass,mfield), call = load_macro mctx com mctx api false path meth p in
 	ignore(call_macro mctx args margs call p);
-	(* (Some mctx, Some api) *)
 	mctx
 
 let finalize_macro_api tctx mctx =
