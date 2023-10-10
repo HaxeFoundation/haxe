7eb8b686428c8f17c4b2abe811a43b2b0fa80f4e
Rudy Ges <k@klabz.org>
2023-10-10 16:07:36 +0200

    [ci] enable CI, disable tests that do not apply any more

--- tests/runci/targets/Macro.hx
+++ tests/runci/targets/Macro.hx
@@ -5,9 +5,9 @@ import runci.Config.*;
 
 class Macro {
 	static public function run(args:Array<String>) {
-		runCommand("haxe", ["compile-hxb-interp.hxml"].concat(args));
-		runCommand("haxe", ["compile-read-hxb-interp.hxml"].concat(args));
-		return; // don't overlook this
+		// runCommand("haxe", ["compile-hxb-interp.hxml"].concat(args));
+		// runCommand("haxe", ["compile-read-hxb-interp.hxml"].concat(args));
+		// return; // don't overlook this
 
 		runCommand("haxe", ["compile-macro.hxml"].concat(args));
 
