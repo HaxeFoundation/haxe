74865d1506b9adb782695765e762c5bf595fef9f
Rudy Ges <k@klabz.org>
2023-10-18 17:33:47 +0200

    [tests] make sure rtti still works for macros

--- tests/misc/projects/Issue11323/Macro.hx
+++ tests/misc/projects/Issue11323/Macro.hx
@@ -1,4 +1,7 @@
-@RuntimeMeta
+@RuntimeMeta([42,0])
 class Macro {
-	public static function init() {}
+	public static function init() {
+		var m = haxe.rtti.Meta.getType(Macro);
+		if (m.RuntimeMeta[0][0] != 42) throw "runtime meta failure";
+	}
 }
