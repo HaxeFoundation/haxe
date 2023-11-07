00e3fa5121621c8475d427402b0f180d921cd7ae
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-11-07 03:06:30 -0500

    Document `Context.resolvePath` exception

--- std/haxe/macro/Context.hx
+++ std/haxe/macro/Context.hx
@@ -112,6 +112,8 @@ class Context {
 
 		If a class path was declared relative, this method returns the relative
 		file path. Otherwise it returns the absolute file path.
+
+		If no type can be found, an exception of type `String` is thrown.
 	**/
 	public static function resolvePath(file:String):String {
 		return load("resolve_path", 1)(file);
