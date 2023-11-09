1e5b2a242ad62e1b3c8bb0304c862e2d2b679a87
Simon Krajewski <simon@haxe.org>
2023-11-09 07:41:06 +0100

    [typer] don't eagerly display in check_error
    We might want to raise instead if we're in_call_args in order to support overload resolution.
    closes #11274

--- src/typing/operators.ml
+++ src/typing/operators.ml
@@ -13,7 +13,7 @@ let check_error ctx err = match err.err_message with
 	| Module_not_found ([],name) when Diagnostics.error_in_diagnostics_run ctx.com err.err_pos ->
 		DisplayToplevel.handle_unresolved_identifier ctx name err.err_pos true
 	| _ ->
-		Common.display_error_ext ctx.com err
+		raise_or_display_error ctx err
 
 module BinopResult = struct
 
