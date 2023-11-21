9d9d5015855a90309e01c4c095a09397ea31159c
Simon Krajewski <simon@haxe.org>
2023-11-21 10:33:20 +0100

    [parser] remove strange Error catching
    closes #8687

---
+++ tests/server/src/cases/issues/Issue8687.hx
@@ -0,0 +1,12 @@
+package cases.issues;
+
+class Issue8687 extends TestCase {
+	function test(_) {
+		vfs.putContent("Main.hx", getTemplate("issues/Issue8687/Main.hx"));
+		var args = ["-main", "Main", "--interp"];
+		runHaxe(args.concat(["--display", "Main.hx@0@diagnostics"]));
+
+		var diag = parseDiagnostics();
+		Assert.equals("Invalid version string \"foo\". Should follow SemVer.", diag[0].args);
+	}
+}
