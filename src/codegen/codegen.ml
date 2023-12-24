8ab63f13ac09c2b4288437d3c6cdbcf563b1bac9
Simon Krajewski <simon@haxe.org>
2023-12-24 14:27:24 +0100

    [dump] fix module lookup
    closes #11447

--- src/codegen/codegen.ml
+++ src/codegen/codegen.ml
@@ -393,7 +393,7 @@ module Dump = struct
 		List.iter (fun m ->
 			print "%s:\n" (Path.UniqueKey.lazy_path m.m_extra.m_file);
 			PMap.iter (fun _ (sign,mpath) ->
-				let m2 = (com.cs#get_context sign)#find_module mpath in
+				let m2 = com.module_lut#find mpath in
 				let file = Path.UniqueKey.lazy_path m2.m_extra.m_file in
 				print "\t%s\n" file;
 				let l = try Hashtbl.find dep file with Not_found -> [] in
