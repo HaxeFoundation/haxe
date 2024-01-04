3e166683f69dece1ff57ab0d0d0b9e7e0b8f13e7
Simon Krajewski <simon@haxe.org>
2024-01-04 11:49:18 +0100

    [typer] set cf_expr_unoptimized only if we think we need it
    closes #11460

---
+++ tests/server/src/cases/issues/Issue11460.hx
@@ -0,0 +1,48 @@
+package cases.issues;
+
+using StringTools;
+
+class Issue11460 extends TestCase {
+	function testClass(_) {
+		var mainContent = getTemplate("issues/Issue11460/Main.hx");
+		vfs.putContent("Main.hx", mainContent);
+		vfs.putContent("C.hx", getTemplate("issues/Issue11460/C.hx"));
+		var args = ["Main", "--interp"];
+
+		// initial cache
+		runHaxe(args);
+		assertSilence();
+
+		// touching Main doesn't do anything
+		vfs.touchFile("Main.hx");
+		runHaxe(args);
+		assertSilence();
+
+		// touching both doesn't do anything
+		vfs.touchFile("Main.hx");
+		vfs.touchFile("C.hx");
+		runHaxe(args);
+		assertSilence();
+
+		// removing the inline is fine
+		vfs.putContent("Main.hx", mainContent.replace("inline", ""));
+		runHaxe(args);
+		assertSilence();
+
+		// adding it back is fine too because C is still cached
+		vfs.putContent("Main.hx", mainContent);
+		runHaxe(args);
+		assertSilence();
+
+		// removing the inline and changing C is still fine
+		vfs.putContent("Main.hx", mainContent.replace("inline", ""));
+		vfs.touchFile("C.hx");
+		runHaxe(args);
+		assertSilence();
+
+		// but adding it now gives us the warning
+		vfs.putContent("Main.hx", mainContent);
+		runHaxe(args);
+		utest.Assert.match(~/WInlineOptimizedField/, lastResult.stderr);
+	}
+}
