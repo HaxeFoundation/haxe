4e2f75232ed16aab26e2f8f622564d69cfb6f05d
Simon Krajewski <simon@haxe.org>
2024-01-04 13:11:15 +0100

    invalidate more because putContent doesn't do it...
    This is not a great testing experience I'm having but oh well...

--- tests/server/src/cases/issues/Issue11460.hx
+++ tests/server/src/cases/issues/Issue11460.hx
@@ -4,8 +4,9 @@ using StringTools;
 
 class Issue11460 extends TestCase {
 	function testClass(_) {
-		var mainContent = getTemplate("issues/Issue11460/Main.hx");
-		vfs.putContent("Main.hx", mainContent);
+		var mainContentWithInline = getTemplate("issues/Issue11460/Main.hx");
+		var mainContentWithoutInline = mainContentWithInline.replace("inline", "");
+		vfs.putContent("Main.hx", mainContentWithInline);
 		vfs.putContent("C.hx", getTemplate("issues/Issue11460/C.hx"));
 		var args = ["Main", "--interp"];
 
@@ -25,23 +26,27 @@ class Issue11460 extends TestCase {
 		assertSilence();
 
 		// removing the inline is fine
-		vfs.putContent("Main.hx", mainContent.replace("inline", ""));
+		vfs.putContent("Main.hx", mainContentWithoutInline);
+		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Main.hx")});
 		runHaxe(args);
 		assertSilence();
 
 		// adding it back is fine too because C is still cached
-		vfs.putContent("Main.hx", mainContent);
+		vfs.putContent("Main.hx", mainContentWithInline);
+		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Main.hx")});
 		runHaxe(args);
 		assertSilence();
 
 		// removing the inline and changing C is still fine
-		vfs.putContent("Main.hx", mainContent.replace("inline", ""));
+		vfs.putContent("Main.hx", mainContentWithoutInline);
+		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Main.hx")});
 		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("C.hx")});
 		runHaxe(args);
 		assertSilence();
 
 		// but adding it now gives us the warning
-		vfs.putContent("Main.hx", mainContent);
+		vfs.putContent("Main.hx", mainContentWithInline);
+		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Main.hx")});
 		runHaxe(args);
 		utest.Assert.match(~/WInlineOptimizedField/, lastResult.stderr);
 	}
