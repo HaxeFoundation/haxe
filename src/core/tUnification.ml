6ca5195fb4872b08742251663b746fcbd00a7922
Rudy Ges <k@klabz.org>
2024-01-12 21:36:28 +0100

    whoops

--- src/core/tUnification.ml
+++ src/core/tUnification.ml
@@ -592,7 +592,7 @@ let rec type_eq uctx a b =
 						| EqStrict | EqCoreType | EqDoNotFollowNull | EqStricter -> true
 						| _ -> false
 					in
-					if f1.cf_kind <> f2.cf_kind && (kind_should_match && not (unify_kind ~strict:uctx.strict_field_kind f1.cf_kind f2.cf_kind)) then error [invalid_kind n f1.cf_kind f2.cf_kind];
+					if f1.cf_kind <> f2.cf_kind && (kind_should_match || not (unify_kind ~strict:uctx.strict_field_kind f1.cf_kind f2.cf_kind)) then error [invalid_kind n f1.cf_kind f2.cf_kind];
 					let a = f1.cf_type and b = f2.cf_type in
 					(try type_eq uctx a b with Unify_error l -> error (invalid_field n :: l));
 					if (has_class_field_flag f1 CfPublic) != (has_class_field_flag f2 CfPublic) then error [invalid_visibility n];
