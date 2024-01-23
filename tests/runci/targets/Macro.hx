fe7534145345f5953183c29ff262d0f4d3f2d048
Simon Krajewski <simon@haxe.org>
2024-01-23 07:05:22 +0100

    clean up unit hxmls

--- tests/runci/targets/Macro.hx
+++ tests/runci/targets/Macro.hx
@@ -5,10 +5,8 @@ import runci.Config.*;
 
 class Macro {
 	static public function run(args:Array<String>) {
-		runCommand("haxe", ["compile-hxb-interp.hxml"].concat(args));
-		runCommand("haxe", ["compile-hxb-interp-read.hxml"].concat(args));
-
-		runCommand("haxe", ["compile-macro.hxml"].concat(args));
+		runCommand("haxe", ["compile-macro.hxml", "--hxb", "bin/hxb/eval.zip"].concat(args));
+		runCommand("haxe", ["compile-macro.hxml", "--hxb-lib", "bin/hxb/eval.zip"].concat(args));
 
 		changeDirectory(displayDir);
 		haxelibInstallGit("Simn", "haxeserver");
