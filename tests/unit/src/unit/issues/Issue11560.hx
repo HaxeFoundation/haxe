ddff346449eba9417c9253a86c1762c5c090d9dc
Simon Krajewski <simon@haxe.org>
2024-02-07 17:58:17 +0100

    fix JsonPrinter empty parent class
    closes #11560

---
+++ tests/unit/src/unit/issues/Issue11560.hx
@@ -0,0 +1,23 @@
+package unit.issues;
+
+@:keep
+private class ParentClass {
+	// no field variable, the issue does not reproduce when there is one
+	function anyFunc() {}
+
+	public function new() {}
+}
+
+@:keep
+private class ChildClass extends ParentClass {
+	var anyVar:String;
+}
+
+class Issue11560 extends Test {
+	function test() {
+		var c = new ChildClass();
+
+		var json = haxe.Json.stringify(c, "\t");
+		eq('{\n\t"anyVar": null\n}', json);
+	}
+}
