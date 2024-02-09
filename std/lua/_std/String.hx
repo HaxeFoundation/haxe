722fc4302e85143b299d36837469fc02c7e5ec03
Simon Krajewski <simon@haxe.org>
2024-02-09 15:36:06 +0100

    maybe fix lua?
    somehow I doubt it

--- std/lua/_std/String.hx
+++ std/lua/_std/String.hx
@@ -62,13 +62,13 @@ class String {
 		return BaseString.lower(this);
 
 	public function indexOf(str:String, ?startIndex:Int):Int {
-		if (startIndex == null)
-			startIndex = 1;
-		else
-			startIndex += 1;
 		if (str == "") {
 			return indexOfEmpty(this, startIndex - 1);
 		}
+		if (startIndex == null || startIndex < 0)
+			startIndex = 1;
+		else
+			startIndex += 1;
 		var r = BaseString.find(this, str, startIndex, true).begin;
 		if (r != null && r > 0)
 			return r - 1;
@@ -79,9 +79,7 @@ class String {
 	static function indexOfEmpty(s:String, startIndex:Int):Int {
 		var length = BaseString.len(s);
 		if (startIndex < 0) {
-			startIndex = length + startIndex;
-			if (startIndex < 0)
-				startIndex = 0;
+			startIndex = 0;
 		}
 		return startIndex > length ? length : startIndex;
 	}
