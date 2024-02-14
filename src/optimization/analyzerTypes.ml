3a49bf23e96e622b7239264a1e0e0e54cf4a0dc7
Simon Krajewski <simon@haxe.org>
2024-02-14 07:45:56 +0100

    remove analyzer changes

--- src/optimization/analyzerTypes.ml
+++ src/optimization/analyzerTypes.ml
@@ -73,7 +73,6 @@ module BasicBlock = struct
 		| SEWhile of t * t * pos                                 (* `while` with "body" and "next" *)
 		| SESubBlock of t * t                                    (* "sub" with "next" *)
 		| SEMerge of t                                           (* Merge to same block *)
-		| SESuspend of (suspend_call * t)                        (* Suspension point *)
 		| SENone                                                 (* No syntax exit *)
 
 	and syntax_switch = {
@@ -254,14 +253,7 @@ end
 module Graph = struct
 	open BasicBlock
 
-	type tfunc_info = {
-		tf_bb : BasicBlock.t;
-		tf_t : Type.t;
-		tf_pos : pos;
-		tf_tf : tfunc;
-		tf_coroutine : (tvar * tvar) option;
-	}
-
+	type tfunc_info = BasicBlock.t * Type.t * pos * tfunc
 	type texpr_lookup = BasicBlock.t * texpr_lookup_target
 	type var_write = BasicBlock.t list
 	type 'a itbl = (int,'a) Hashtbl.t
@@ -347,8 +339,8 @@ module Graph = struct
 
 	(* nodes *)
 
-	let add_function g tf_tf tf_t tf_pos tf_bb tf_coroutine =
-		Hashtbl.add g.g_functions tf_bb.bb_id ({tf_bb;tf_t;tf_pos;tf_tf;tf_coroutine})
+	let add_function g tf t p bb =
+		Hashtbl.add g.g_functions bb.bb_id (bb,t,p,tf)
 
 	let alloc_id =
 		let r = ref 1 in
@@ -598,13 +590,11 @@ module Graph = struct
 					loop scopes bb_next
 				| SEMerge bb ->
 					loop scopes bb
-				| SESuspend (_, bb) ->
-					loop scopes bb
 				| SENone ->
 					()
 			end
 		in
-		Hashtbl.iter (fun _ tfi -> loop [0] tfi.tf_bb) g.g_functions
+		Hashtbl.iter (fun _ (bb,_,_,_) -> loop [0] bb) g.g_functions
 end
 
 type analyzer_context = {
