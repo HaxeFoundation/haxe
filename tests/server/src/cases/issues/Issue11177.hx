90b789156dcaa753e4e32ddc4da865cba0eff8ce
Rudy Ges <k@klabz.org>
2024-06-18 09:50:35 +0200

    [tests] Update diagnostics tests

--- tests/server/src/cases/issues/Issue11177.hx
+++ tests/server/src/cases/issues/Issue11177.hx
@@ -17,4 +17,17 @@ class Issue11177 extends TestCase {
 	// 		Assert.equals(0, res.length);
 	// 	});
 	// }
+
+	// function testLegacyDiagnostics(_) {
+	// 	vfs.putContent("Main.hx", getTemplate("issues/Issue11177/Main.hx"));
+	// 	vfs.putContent("Buttons.hx", getTemplate("issues/Issue11177/Buttons.hx"));
+	// 	vfs.putContent("KeyCode.hx", getTemplate("issues/Issue11177/KeyCode.hx"));
+	// 	var args = ["-main", "Main", "--interp"];
+	// 	runHaxe(args.concat(["--display", "Buttons.hx@0@diagnostics"]));
+	// 	vfs.putContent("Main.hx", getTemplate("issues/Issue11177/Main2.hx"));
+	// 	runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Main.hx")});
+	// 	runHaxe(args);
+	// 	runHaxe(args.concat(["--display", "Buttons.hx@0@diagnostics"]));
+	// 	Assert.isTrue(lastResult.stderr.length == 2);
+	// }
 }
