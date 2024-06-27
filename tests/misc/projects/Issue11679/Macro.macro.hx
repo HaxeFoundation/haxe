16312f8ee7161105f58e47bcc1ff9da5021a8eaa
Rudy Ges <k@klabz.org>
2024-06-27 16:44:32 +0200

    Add test

---
+++ tests/misc/projects/Issue11679/Macro.macro.hx
@@ -0,0 +1,14 @@
+import haxe.macro.Context;
+
+	function build() {
+		switch (Context.getLocalClass().get().name) {
+			case "Foo":
+				switch Context.getType("Bar") {
+					case TInst(cls,_): cls.get();
+					case _:
+				}
+			case "Bar":
+				Context.typeExpr(Context.parseInlineString('& if (!r.can) {}', Context.currentPos()));
+		}
+		return null;
+	}
