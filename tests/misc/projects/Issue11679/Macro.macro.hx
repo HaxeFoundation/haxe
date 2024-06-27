850d9ba4aa4e9cbce3d51e515576ba1570905bac
Rudy Ges <k@klabz.org>
2024-06-27 16:51:49 +0200

    Improve test again..

--- tests/misc/projects/Issue11679/Macro.macro.hx
+++ tests/misc/projects/Issue11679/Macro.macro.hx
@@ -1,14 +1,14 @@
 import haxe.macro.Context;
 
-	function build() {
-		switch (Context.getLocalClass().get().name) {
-			case "Foo":
-				switch Context.getType("Bar") {
-					case TInst(cls,_): cls.get();
-					case _:
-				}
-			case "Bar":
-				Context.parseInlineString('& if (!r.can) {}', Context.currentPos());
-		}
-		return null;
+function build() {
+	switch (Context.getLocalClass().get().name) {
+		case "Foo":
+			switch Context.getType("Bar") {
+				case TInst(cls,_): cls.get();
+				case _:
+			}
+		case "Bar":
+			throw "Ohno";
 	}
+	return null;
+}
