bc5006d3c8c9674e12e4794b2d4daf99bc9893dc
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-09-03 19:05:53 -0400

    Update Int128Helper.hx

--- std/haxe/Int128Helper.hx
+++ std/haxe/Int128Helper.hx
@@ -122,7 +122,7 @@ class Int128Helper {
 	/**
 		The minimum `Int128` value.
 	 */
-	public static var minValue:Int128 = -maxValue;
+	public static var minValue:Int128 = ~maxValue;
 
 	/**
 		The maximum `Int64` value with the type `Int128`.
@@ -134,7 +134,7 @@ class Int128Helper {
 		The minimum `Int64` value with the type `Int128`.
 		This is handy for type comparison.
 	 */
-	public static var minValue64:Int128 = Int128.ofInt64(Int64Helper.minValue);
+	public static var minValue64:Int128 = ~maxValue64;
 
 	/**
 		The maximum `Int32` value with the type `Int128`.
@@ -146,7 +146,7 @@ class Int128Helper {
 		The minimum `Int32` value with the type `Int128`.
 		This is handy for type comparison.
 	 */
-	public static var minValue32:Int128 = Int128.ofInt64(Int64Helper.minValue32);
+	public static var minValue32:Int128 = ~maxValue32;
 
 	/**
 		The maximum unsigned `Int32` value with the type `Int128`.
