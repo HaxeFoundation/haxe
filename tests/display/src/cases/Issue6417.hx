99e8bb4338c713e11ba96f4dceeb33b27da4aace
Rudy Ges <k@klabz.org>
2024-09-10 16:05:30 +0200

    [display] Do not silently replace missing types with Dynamic (#11760)
    * [typer] don't ignore Context.resolveType errors in display
    
    * [tests] fix tests

--- tests/display/src/cases/Issue6417.hx
+++ tests/display/src/cases/Issue6417.hx
@@ -5,13 +5,13 @@ class Issue6417 extends DisplayTestCase {
 		class Main {
 			static function main() {}
 
-			macro function foo({-1-}body{-2-}:Expr) {
+			macro function foo({-1-}body{-2-}:haxe.macro.Expr) {
 				macro function() $bo{-3-}dy;
 			}
 		}
 	**/
 	function test() {
 		eq(range(1, 2), position(pos(3)));
-		eq("Dynamic", type(pos(3)));
+		eq("haxe.macro.Expr", type(pos(3)));
 	}
 }
