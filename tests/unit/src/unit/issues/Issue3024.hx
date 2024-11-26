17a4419dcdf373cc09142dbe2f87bed9feb8c00a
Simon Krajewski <simon@haxe.org>
2024-11-26 10:40:18 +0100

    enable related test

--- tests/unit/src/unit/issues/Issue3024.hx
+++ tests/unit/src/unit/issues/Issue3024.hx
@@ -1,13 +1,5 @@
 package unit.issues;
 
-#if hl
-
-// no support for recursive types atm
-class Issue3024 extends Test {
-}
-
-#else
-
 import haxe.ds.StringMap;
 import haxe.ds.Vector;
 
@@ -16,7 +8,6 @@ private typedef MyMap = StringMap<MyFunction>;
 private typedef MyFunction = MyVector->MyMap->Dynamic;
 
 class Issue3024 extends Test {
-
 	private var myVector:MyVector;
 
 	private var myMap:MyMap;
@@ -31,5 +22,3 @@ class Issue3024 extends Test {
 		this.myMap = myMap;
 	}
 }
-
-#end
