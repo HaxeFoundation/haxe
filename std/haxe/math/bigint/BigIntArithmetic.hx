274a2fd5d00e2f84aa46e152c20f5279d9e3cca3
flashultra <m_mail01@abv.bg>
2024-12-14 00:05:13 +0200

    Is the unsigned shift the problem ?

--- std/haxe/math/bigint/BigIntArithmetic.hx
+++ std/haxe/math/bigint/BigIntArithmetic.hx
@@ -37,12 +37,19 @@ class BigIntArithmetic {
 		returns 0 (`a == b`).
 	**/
 	public static function compareInt(a:BigInt_, b:Int):Int {
+		#if (lua)
+			trace("a.m_count: "+a.m_count);
+		#end
 		if (a.m_count > 1) {
 			return (a.sign() << 1) + 1;
 		}
 		var x:Int = a.m_data.get(0);
 		var lt:Int = (x - b) ^ ((x ^ b) & ((x - b) ^ x)); // "Hacker's Delight" p. 23
 		var gt:Int = (b - x) ^ ((x ^ b) & ((b - x) ^ b));
+		#if (lua)
+			trace("lt: "+(lt >> 31));
+			trace("gt: "+(gt >>> 31));
+		#end
 		return (lt >> 31) | (gt >>> 31);
 	}
 
