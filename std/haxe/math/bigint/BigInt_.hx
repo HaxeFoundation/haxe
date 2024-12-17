ebfc73605aa4b82c7402f4f09c455472e1e1acd4
flashultra <m_mail01@abv.bg>
2024-12-17 20:08:57 +0200

    Check why Python failed

--- std/haxe/math/bigint/BigInt_.hx
+++ std/haxe/math/bigint/BigInt_.hx
@@ -219,7 +219,13 @@ class BigInt_ {
 			rounds = 2;
 		}
 		rounds = (tolerance < rounds) ? tolerance : rounds;
+		#if python
+		var result:Bool = b.millerRabin(rounds);
+		if ( !result) trace("Failed: "+b.toString());
+		return result;
+		#else
 		return b.millerRabin(rounds);
+		#end
 	}
 
 	/**
