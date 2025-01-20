b9e0b68eba09e063d2a5001e2fedbec0a3b07ef2
Rudy Ges <k@klabz.org>
2025-01-20 17:23:35 +0100

    [hxb] only load up to EOF if at least one field is accessed (or full typing)

--- src/compiler/server.ml
+++ src/compiler/server.ml
@@ -453,7 +453,7 @@ class hxb_reader_api_server
 			(* We try to avoid reading expressions as much as possible, so we only do this for
 				 our current display file if we're in display mode. *)
 			if full_restore then ignore(f_next chunks EOM)
-			else delay PConnectField (fun () -> ignore(f_next chunks EOF));
+			else reader#set_delayed_field_loading (fun () -> ignore(f_next chunks EOF));
 			m
 		| BadModule reason ->
 			die (Printf.sprintf "Unexpected BadModule %s (%s)" (s_type_path path) (Printer.s_module_skip_reason reason)) __LOC__
@@ -605,7 +605,7 @@ and type_module sctx com delay mpath p =
 					(* We try to avoid reading expressions as much as possible, so we only do this for
 					   our current display file if we're in display mode. *)
 					if full_restore then ignore(f_next chunks EOM)
-					else delay PConnectField (fun () -> ignore(f_next chunks EOF));
+					else reader#set_delayed_field_loading (fun () -> ignore(f_next chunks EOF));
 					add_modules true m;
 				| Some reason ->
 					skip mpath reason
