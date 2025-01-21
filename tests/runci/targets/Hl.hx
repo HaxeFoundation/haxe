4a61935fb77fb5954627392519e07c803db9b464
Rudy Ges <k@klabz.org>
2025-01-21 09:02:11 +0100

    [CI] --preinstall has been renamed to --auto-update
    See https://github.com/Homebrew/brew/commit/fb4c9353bbd5b022137f6e0a48c71b85503d80ca

--- tests/runci/targets/Hl.hx
+++ tests/runci/targets/Hl.hx
@@ -39,7 +39,7 @@ class Hl {
 			case "Linux":
 				Linux.requireAptPackages(["libpng-dev", "libjpeg-turbo8-dev", "libturbojpeg", "zlib1g-dev", "libvorbis-dev", "libsqlite3-dev"]);
 			case "Mac":
-				runNetworkCommand("brew", ["update", '--preinstall']);
+				runNetworkCommand("brew", ["update", '--auto-update']);
 				runNetworkCommand("brew", ["bundle", '--file=${hlSrc}/Brewfile']);
 			case "Windows":
 				//pass
