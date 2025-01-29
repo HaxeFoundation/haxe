8b01df6318d587ed59acdf3c8f883d7ae70593c2
Rudy Ges <k@klabz.org>
2025-01-29 15:13:31 +0100

    [tests] showcase that the test was not working as expected
    Replace the 'using' with previous 'using Main' to see static extension was missing

--- tests/display/src/cases/Issue9554.hx
+++ tests/display/src/cases/Issue9554.hx
@@ -23,4 +23,23 @@ class Issue9554 extends DisplayTestCase {
 		eq(1, fields.length);
 		eq(true, hasField(fields, "foo", "Int", "var"));
 	}
+
+	/**
+		using cases.Issue9554.Main;
+
+		class Main {
+			static public function main() {
+				var a:{foo:Int} = {foo: 12};
+				a.{-1-}
+			}
+
+			static function staticExtension(a:{foo:Int}) {}
+		}
+
+	**/
+	function testStaticExtension() {
+		var fields = fields(pos(1));
+		eq(true, hasField(fields, "foo", "Int", "var"));
+		eq(true, hasField(fields, "staticExtension", "() -> Unknown<0>", "method"));
+	}
 }
