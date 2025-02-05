90ba8384ac40f3c76410a6290df9307270e0fade
Rudy Ges <k@klabz.org>
2025-02-05 11:28:30 +0100

    [tests] add test for 11417

---
+++ tests/misc/projects/Issue11417/Main.hx
@@ -0,0 +1,2 @@
+function foo(_:Int, _:Int) {} // OK
+function bar(i:Int, i:Int) {} // Not OK
---
+++ tests/misc/projects/Issue11417/Main1.hx
@@ -0,0 +1,5 @@
+function baz() {
+	var foo = function(i:Int, i:Int) {};
+	function bar(i:Int, i:Int) {}
+	var lambda = (i, i) -> {};
+}
