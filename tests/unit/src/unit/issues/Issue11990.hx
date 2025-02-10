34abdcc503967b7c01afe881d6f76595194111da
Yuxiao Mao <yuxiao.mao@outlook.com>
2025-02-10 12:53:24 +0100

    [tests] add test for F64 too

--- tests/unit/src/unit/issues/Issue11990.hx
+++ tests/unit/src/unit/issues/Issue11990.hx
@@ -4,7 +4,9 @@ class Issue11990 extends unit.Test {
 	public function test() {
 		#if (cpp || jvm || hl)
 		var x:Single = std.Math.NaN;
-		eq(false, x == x);
+		eq(true, x != x);
+		var x:Float = std.Math.NaN;
+		eq(true, x != x);
 		#else
 		noAssert();
 		#end
