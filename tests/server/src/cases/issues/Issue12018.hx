9f63f40297ad4aa865b79f51c39916e91ebb591b
Rudy Ges <k@klabz.org>
2025-02-17 10:57:19 +0100

    [tests] 12017 was open in the meantime..

--- tests/server/src/cases/issues/Issue12018.hx
+++ tests/server/src/cases/issues/Issue12018.hx
@@ -1,12 +1,12 @@
 package cases.issues;
 
-class Issue12017 extends TestCase {
+class Issue12018 extends TestCase {
 	function test(_) {
-		var content = getTemplate("issues/Issue12017/Main.hx");
+		var content = getTemplate("issues/Issue12018/Main.hx");
 		var transform = Markers.parse(content);
 
 		vfs.putContent("Main.hx", transform.source);
-		vfs.putContent("import.hx", getTemplate("issues/Issue12017/import.hx"));
+		vfs.putContent("import.hx", getTemplate("issues/Issue12018/import.hx"));
 		var args = ["-main", "Main"];
 		runHaxe(args);
 		assertSuccess();
