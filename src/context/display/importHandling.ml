c245fc23e786ae0a8de5778b820e60287fc6f416
Rudy Ges <k@klabz.org>
2025-02-18 08:46:37 +0100

    Invert check order

--- src/context/display/importHandling.ml
+++ src/context/display/importHandling.ml
@@ -114,7 +114,7 @@ let init_import ctx path mode p =
 			if not (name.[0] >= 'A' && name.[0] <= 'Z') then
 				raise_typing_error "Type aliases must start with an uppercase letter" pname;
 			(* Imports from import.hx should not match display position from current file *)
-			if ctx.m.is_display_file && (Path.UniqueKey.create pname.pfile) = (Path.UniqueKey.lazy_key ctx.m.curmod.m_extra.m_file) && DisplayPosition.display_position#enclosed_in pname then
+			if ctx.m.is_display_file && DisplayPosition.display_position#enclosed_in pname && (Path.UniqueKey.create pname.pfile) = (Path.UniqueKey.lazy_key ctx.m.curmod.m_extra.m_file) then
 				DisplayEmitter.display_alias ctx name (type_of_module_type mt) pname
 		in
 		let add_static_init t name s =
