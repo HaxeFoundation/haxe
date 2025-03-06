5743a95f2e50f0dae9ba70059f88d4650676eb11
Yuxiao Mao <yuxiao.mao@outlook.com>
2025-03-06 08:20:37 +0100

    Only do is_single check if OpDiv

--- src/typing/operators.ml
+++ src/typing/operators.ml
@@ -305,8 +305,13 @@ let make_binop ctx op e1 e2 is_assign_op p =
 		| KFloat, KFloat ->
 			result := tfloat
 		| KNumParam t1, KNumParam t2 when Type.type_iseq t1 t2 ->
-			let is_single = (match follow e2.etype with TAbstract({a_path=[],"Single"},_) -> true | _ -> false) in
-			if op <> OpDiv || is_single then result := t1
+			(match op with
+			| OpDiv ->
+				let is_single = (match follow e1.etype with TAbstract({a_path=[],"Single"},_) -> true | _ -> false) in
+				if is_single then result := t1
+			| _ ->
+				result := t1
+			)
 		| KNumParam _, KNumParam _ ->
 			result := tfloat
 		| KNumParam t, KInt | KInt, KNumParam t ->
