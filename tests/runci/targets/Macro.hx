5aa1d1de68611237f33cb8bc4e15a77ff34e0a16
Rudy Ges <k@klabz.org>
2025-03-28 11:02:14 +0100

    [tests] hxjava no longer needed for macro tests

--- tests/runci/targets/Macro.hx
+++ tests/runci/targets/Macro.hx
@@ -5,9 +5,6 @@ import runci.Config.*;
 
 class Macro {
 	static public function run(args:Array<String>) {
-		// Needed for jvm display tests..
-		haxelibInstallGit("HaxeFoundation", "hxjava", true);
-
 		runCommand("haxe", ["compile-macro.hxml", "--hxb", "bin/hxb/eval.zip"].concat(args));
 		runCommand("haxe", ["compile-macro.hxml", "--hxb-lib", "bin/hxb/eval.zip"].concat(args));
 
