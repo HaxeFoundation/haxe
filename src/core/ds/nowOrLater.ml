b57d68fb8a27baf76b6e082d5e04c523d5fd1e3a
Simon Krajewski <simon@haxe.org>
2025-04-02 07:29:28 +0200

    just use a mutexed Queue for now

--- src/core/ds/nowOrLater.ml
+++ src/core/ds/nowOrLater.ml
@@ -1,22 +1,25 @@
 type t = {
 	now : Mutex.t;
-	later: (unit -> unit) Mpsc_queue.t;
+	later: (unit -> unit) Queue.t;
+	later_mutex : Mutex.t;
 }
 
 let create () = {
 	now = Mutex.create ();
-	later = Mpsc_queue.create ();
+	later = Queue.create ();
+	later_mutex = Mutex.create ();
 }
 
 let try_now nol f =
 	if Mutex.try_lock nol.now then begin
 		f();
 		Mutex.unlock nol.now
-	end else
-		Mpsc_queue.push nol.later f
+	end else begin
+		Mutex.protect nol.later_mutex (fun () -> Queue.push f nol.later)
+	end
 
 let handle_later nol =
-	let rec loop () = match Mpsc_queue.pop nol.later with
+	let rec loop () = match Queue.take_opt nol.later with
 		| Some f ->
 			f ();
 			loop ()
