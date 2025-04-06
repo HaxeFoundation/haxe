6e309fabcd830035d53a47e5d04b9a0f6061dea0
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-04-06 13:16:09 +0100

    Use full debug paths in cppia in all cases
    Without these paths being absolute, cppia break points cannot work at
    all, so it makes more sense for this to just be the default. These paths
    are only ever added for debugging anyway.

--- src/generators/cpp/gen/cppCppia.ml
+++ src/generators/cpp/gen/cppCppia.ml
@@ -521,7 +521,6 @@ class script_writer ctx filename asciiOut =
     val fileTable = Hashtbl.create 0
     val identBuffer = Buffer.create 0
     val cppiaAst = not (Gctx.defined ctx.ctx_common Define.NoCppiaAst)
-    val absoluteSourcePaths = Gctx.defined ctx.ctx_common Define.CppiaAbsoluteSourcePaths
 
     method stringId name =
       try Hashtbl.find identTable name
@@ -786,12 +785,8 @@ class script_writer ctx filename asciiOut =
 
     method wpos p =
       if debug then
-        let filepath = if absoluteSourcePaths then
-          Path.get_full_path p.pfile
-        else p.pfile
-        in
         this#write
-          (this#fileText filepath ^ "\t"
+          (this#fileText (Path.get_full_path p.pfile) ^ "\t"
           ^ string_of_int (Lexer.get_error_line p)
           ^ indent)
 
