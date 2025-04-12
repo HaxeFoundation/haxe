0bd4a32208e11cbb67d9595b48e6c951101ce513
Simon Krajewski <simon@haxe.org>
2025-04-12 16:22:15 +0200

    adapt to development

--- src/context/dotGraph.ml
+++ src/context/dotGraph.ml
@@ -5,7 +5,7 @@ let platform_name_macro com =
 	else Globals.platform_name com.platform
 
 let get_dump_path com path name =
-	(Dump.dump_path com.defines) :: [platform_name_macro com] @ (fst path) @ [Printf.sprintf "%s.%s" (snd path) name]
+	com.dump_config.dump_path :: [platform_name_macro com] @ (fst path) @ [Printf.sprintf "%s.%s" (snd path) name]
 
 let start_graph ?(graph_config=[]) base_path suffix =
 	let ch = Path.create_file false suffix [] base_path in
