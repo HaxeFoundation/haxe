7522787f661c3f5335e803f1a249a9661b3a4c8b
Simon Krajewski <simon@haxe.org>
2025-04-16 22:54:48 +0200

    getOrThrow shouldn't be a coro

--- std/haxe/coro/continuations/RacingContinuation.hx
+++ std/haxe/coro/continuations/RacingContinuation.hx
@@ -64,7 +64,7 @@ private class Thread {
 		});
 	}
 
-	@:coroutine public function getOrThrow():RacingContinuation<T> {
+	public function getOrThrow():RacingContinuation<T> {
 		lock.acquire();
 
 		if (assigned) {
