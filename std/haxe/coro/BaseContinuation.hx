b07d714ac1444c5e7d7b793c29d70513bdc0bdd3
Simon Krajewski <simon@haxe.org>
2025-05-13 17:19:54 +0200

    avoid null issue
    see #75

--- std/haxe/coro/BaseContinuation.hx
+++ std/haxe/coro/BaseContinuation.hx
@@ -94,7 +94,8 @@ abstract class BaseContinuation<T> extends SuspensionResult<T> implements IConti
     public function buildCallStack() {
         var frame = callerFrame();
         if (frame != null) {
-            (cast result : Array<StackItem>).push(frame.getStackItem());
+			var result:Array<StackItem> = cast result ?? cast [];
+            result.push(frame.getStackItem());
         }
     }
 
