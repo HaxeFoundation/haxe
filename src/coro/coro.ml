e372bcf9dc677392f934835caf2c8e62c3c7a5a6
Simon Krajewski <simon@haxe.org>
2025-05-23 14:58:39 +0200

    disable optimizations for now
    too many distractions, will revisit this later

--- src/coro/coro.ml
+++ src/coro/coro.ml
@@ -532,8 +532,9 @@ let fun_to_coro ctx coro_type =
 	e
 
 let create_coro_context typer meta =
-	let optimize = not (Define.raw_defined typer.Typecore.com.defines "coroutine.noopt") in
-	let builder = new CoroElsewhere.texpr_builder typer.t in
+	(* let optimize = not (Define.raw_defined typer.Typecore.com.defines "coroutine.noopt") in *)
+	let optimize = false in
+	let builder = new CoroElsewhere.texpr_builder typer.Typecore.t in
 	let ctx = {
 		builder;
 		typer;
