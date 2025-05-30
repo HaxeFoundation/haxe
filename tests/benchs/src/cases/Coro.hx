b61bd62900fb7bb4518f07311a37b16996c69c46
Simon Krajewski <simon@haxe.org>
2025-05-30 17:56:27 +0200

    add some micro benchmarks

---
+++ tests/benchs/src/cases/Coro.hx
@@ -0,0 +1,93 @@
+package cases;
+
+import hxbenchmark.Suite;
+import hxcoro.Coro.*;
+import hxcoro.CoroRun;
+
+class Coro extends TestCase {
+	function measureYield() {
+		var suite = new Suite("yield");
+		suite.add("100", CoroRun.runScoped(node -> {
+			for (i in 0...100) {
+				yield();
+			}
+		}));
+		suite.add("1000", CoroRun.runScoped(node -> {
+			for (i in 0...1000) {
+				yield();
+			}
+		}));
+		suite.add("10000", CoroRun.runScoped(node -> {
+			for (i in 0...10000) {
+				yield();
+			}
+		}));
+		return suite.run();
+	}
+
+	function measureAsyncYield() {
+		var suite = new Suite("asyncYield");
+		suite.add("100", CoroRun.runScoped(node -> {
+			for (i in 0...100) {
+				node.async(_ -> yield());
+			}
+		}));
+		suite.add("1000", CoroRun.runScoped(node -> {
+			for (i in 0...1000) {
+				node.async(_ -> yield());
+			}
+		}));
+		suite.add("10000", CoroRun.runScoped(node -> {
+			for (i in 0...10000) {
+				node.async(_ -> yield());
+			}
+		}));
+		return suite.run();
+	}
+
+	function measureLazyYield() {
+		var suite = new Suite("lazyYield");
+		suite.add("100", CoroRun.runScoped(node -> {
+			for (i in 0...100) {
+				node.lazy(_ -> yield());
+			}
+		}));
+		suite.add("1000", CoroRun.runScoped(node -> {
+			for (i in 0...1000) {
+				node.lazy(_ -> yield());
+			}
+		}));
+		suite.add("10000", CoroRun.runScoped(node -> {
+			for (i in 0...10000) {
+				node.lazy(_ -> yield());
+			}
+		}));
+		return suite.run();
+	}
+
+	function measureHoising() {
+		var suite = new Suite("hoising");
+		suite.add("100", CoroRun.runScoped(node -> {
+			var a = 0;
+			for (i in 0...100) {
+				a++;
+				yield();
+			}
+		}));
+		suite.add("1000", CoroRun.runScoped(node -> {
+			var a = 0;
+			for (i in 0...1000) {
+				a++;
+				yield();
+			}
+		}));
+		suite.add("10000", CoroRun.runScoped(node -> {
+			var a = 0;
+			for (i in 0...10000) {
+				a++;
+				yield();
+			}
+		}));
+		return suite.run();
+	}
+}
