53b3622a5c007d67c678f1c76a2b13a3d2da6908
Rudy Ges <k@klabz.org>
2025-06-02 10:17:40 +0200

    Compute signature from sanitized defines

--- src/core/define.ml
+++ src/core/define.ml
@@ -159,8 +159,9 @@ let get_signature def =
 			(* If we add something here that might be used in conditional compilation it should be added to
 			   Grammar.parse_macro_ident as well (issue #5682).
 			*)
-			if DefineList.is_signature_neutral sanitized then acc else (k ^ "=" ^ v) :: acc
+			if DefineList.is_signature_neutral sanitized then acc else (sanitized ^ "=" ^ v) :: acc
 		) def.values [] in
+		let defines = Ast.remove_duplicates (fun a b -> a != b) defines in
 		let str = String.concat "@" (List.sort compare defines) in
 		let s = Digest.string str in
 		def.defines_signature <- Some s;
