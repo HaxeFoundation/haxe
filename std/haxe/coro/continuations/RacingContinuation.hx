4e8a2736086be8a3b3e93122107c3693c17e8c2a
Simon Krajewski <simon@haxe.org>
2025-06-02 19:17:36 +0200

    Don't have RacingContinuation hold onto its mutex for so long (#127)
    * don't have RacingContinuation hold onto its mutex for so long
    
    also avoid some unnecessary scheduling
    
    * go for yet another approach
    
    * inline by hand because HL hates me
    
    * avoid capturing `this`

--- std/haxe/coro/continuations/RacingContinuation.hx
+++ std/haxe/coro/continuations/RacingContinuation.hx
@@ -3,20 +3,17 @@ package haxe.coro.continuations;
 import haxe.coro.context.Context;
 import haxe.coro.schedulers.Scheduler;
 
-@:coreApi class RacingContinuation<T> extends SuspensionResult<T> implements IContinuation<T> {
+class RacingContinuation<T> extends SuspensionResult<T> implements IContinuation<T> {
 	final inputCont:IContinuation<T>;
 
-	final lock:Mutex;
-
-	var assigned:Bool;
+	var mutex:Null<Mutex>;
 
 	public var context(get, null):Context;
 
 	public function new(inputCont:IContinuation<T>) {
 		this.inputCont = inputCont;
 		context = inputCont.context;
-		assigned = false;
-		lock = new Mutex();
+		mutex = new Mutex();
 	}
 
 	inline function get_context() {
@@ -24,36 +21,58 @@ import haxe.coro.schedulers.Scheduler;
 	}
 
 	public function resume(result:T, error:Exception):Void {
-		context.get(Scheduler.key).schedule(0, () -> {
-			lock.acquire();
-
-			if (assigned) {
-				lock.release();
+		// store in a local to avoid `this` capturing.
+		final inputCont = inputCont;
+		inline function resumeContinue(result:T, error:Exception) {
+			context.get(Scheduler.key).schedule(0, () -> {
 				inputCont.resume(result, error);
-			} else {
-				assigned = true;
-				this.result = result;
-				this.error = error;
+			});
+		}
 
-				lock.release();
-			}
-		});
+		// Store mutex as stack value.
+		final mutex = mutex;
+		if (mutex == null) {
+			// If that's already null we're definitely done.
+			return resumeContinue(result, error);
+		}
+		// Otherwise we take the mutex now. We know that the stack value isn't null, so that's safe.
+		mutex.acquire();
+		if (this.mutex == null) {
+			// The shared reference has become null in the meantime, so we're done.
+			mutex.release();
+			return resumeContinue(result, error);
+		}
+		// At this point we own the mutex, so we're first. We can set the shared reference to null and release it.
+		this.mutex = null;
+		mutex.release();
+		this.result = result;
+		this.error = error;
 	}
 
+
 	public function resolve():Void {
-		lock.acquire();
-		if (assigned) {
+		// same logic as resume
+		final mutex = mutex;
+		if (mutex == null) {
 			if (error != null) {
 				state = Thrown;
-				lock.release();
 			} else {
 				state = Returned;
-				lock.release();
 			}
-		} else {
-			assigned = true;
-			state = Pending;
-			lock.release();
+			return;
 		}
+		mutex.acquire();
+		if (this.mutex == null) {
+			mutex.release();
+			if (error != null) {
+				state = Thrown;
+			} else {
+				state = Returned;
+			}
+			return;
+		}
+		this.mutex = null;
+		mutex.release();
+		state = Pending;
 	}
 }
