cf9fbefa45fe66ddb0d6a952cbe59f9fe34bbdfc
RblSb <msrblsb@gmail.com>
2025-06-11 02:41:49 +0300

    Error on typedef private accessors

--- tests/misc/projects/Issue3053/Main.hx
+++ tests/misc/projects/Issue3053/Main.hx
@@ -4,6 +4,92 @@ typedef BadType = {
 	var foo2(never, never):Int;
 }
 
+typedef PrivateVariationsType = {
+	var privateBoth(private get, private set):Int;
+	var privateGetSet(private get, set):Int;
+	var getPrivateSet(get, private set):Int;
+	var defaultPrivateSet(default, private set):Int;
+	var getNever(get, never):Int;
+}
+
+class PrivateVariations {
+	public function new() {}
+	var privateBoth(private get, private set):Int;
+	public var privateGetSet(private get, set):Int;
+	public var getPrivateSet(get, private set):Int;
+	public var defaultPrivateSet(default, private set):Int;
+
+	static var sprivateBoth(private get, private set):Int;
+	static public var sprivateGetSet(private get, set):Int;
+	static public var sgetPrivateSet(get, private set):Int;
+	static public var sdefaultPrivateSet(default, private set):Int;
+
+	function set_privateBoth(v):Int return 0;
+	function get_privateBoth():Int return 0;
+	function set_privateGetSet(v):Int return 0;
+	function get_privateGetSet():Int return 0;
+	function set_getPrivateSet(v):Int return 0;
+	function get_getPrivateSet():Int return 0;
+	function set_defaultPrivateSet(v):Int return 0;
+
+	static function set_sprivateBoth(v):Int return 0;
+	static function get_sprivateBoth():Int return 0;
+	static function set_sprivateGetSet(v):Int return 0;
+	static function get_sprivateGetSet():Int return 0;
+	static function set_sgetPrivateSet(v):Int return 0;
+	static function get_sgetPrivateSet():Int return 0;
+	static function set_sdefaultPrivateSet(v):Int return 0;
+}
+
+class CheckVariations {
+	static function main() {
+		final type:PrivateVariationsType = null;
+		type.getNever = 1; // err
+		type.getPrivateSet;
+		type.getPrivateSet = 1; // err
+		type.privateGetSet; // err
+		type.privateGetSet = 1;
+		type.privateBoth; // err
+		type.privateBoth = 1; // err
+		@:privateAccess type.privateBoth;
+		@:privateAccess type.privateBoth = 1;
+		@:bypassAccessor type.privateBoth; // err
+		@:bypassAccessor type.privateBoth = 1; // err
+
+		type.defaultPrivateSet;
+		type.defaultPrivateSet = 1; // err
+		@:privateAccess type.defaultPrivateSet = 1;
+		@:bypassAccessor type.defaultPrivateSet = 1;
+
+		final vars = new PrivateVariations();
+		vars.getPrivateSet;
+		vars.getPrivateSet = 1; // err
+		vars.privateGetSet; // err
+		vars.privateGetSet = 1;
+		vars.privateBoth; // err
+		vars.privateBoth = 1; // err
+		@:privateAccess vars.privateBoth;
+		@:privateAccess vars.privateBoth = 1;
+		vars.defaultPrivateSet;
+		vars.defaultPrivateSet = 1; // err
+		@:privateAccess vars.defaultPrivateSet = 1;
+		@:bypassAccessor vars.defaultPrivateSet = 1;
+
+		PrivateVariations.sgetPrivateSet;
+		PrivateVariations.sgetPrivateSet = 1; // err
+		PrivateVariations.sprivateGetSet; // err
+		PrivateVariations.sprivateGetSet = 1;
+		PrivateVariations.sprivateBoth; // err
+		PrivateVariations.sprivateBoth = 1; // err
+		@:privateAccess PrivateVariations.sprivateBoth;
+		@:privateAccess PrivateVariations.sprivateBoth = 1;
+		PrivateVariations.sdefaultPrivateSet;
+		PrivateVariations.sdefaultPrivateSet = 1; // err
+		@:privateAccess PrivateVariations.sdefaultPrivateSet = 1;
+		@:bypassAccessor PrivateVariations.sdefaultPrivateSet = 1;
+	}
+}
+
 typedef FooPrivateGetType = {
 	var foo(private get, set):Int;
 }
@@ -35,14 +121,8 @@ class Main {
 	}
 
 	public var notAllowed(private get, private set):Int; // err
-
-	function set_notAllowed(value:Int):Int {
-		throw new haxe.exceptions.NotImplementedException();
-	}
-
-	function get_notAllowed():Int {
-		throw new haxe.exceptions.NotImplementedException();
-	}
+	function set_notAllowed(v):Int return 0;
+	function get_notAllowed():Int return 0;
 
 	public function new() {
 		foo = 1;
@@ -110,14 +190,8 @@ class Rect implements Shape {
 
 	public function new() {}
 	public var width(get, private set):Int; // err
-
-	function set_width(value:Int):Int {
-		return 0;
-	}
-
-	function get_width():Int {
-		return 0;
-	}
+	function set_width(v):Int return 0;
+	function get_width():Int return 0;
 }
 
 @:build(PropertyMacro.addIntProperty("age"))
@@ -129,9 +203,7 @@ class Bar {
 	public var defaultNull(default, null):Int;
 
 	public var defaultPrivateSet(default, private set):Int;
-	function set_defaultPrivateSet(value:Int):Int {
-		return value;
-	}
+	function set_defaultPrivateSet(v):Int return v;
 
 	var width(private get, private set):Int;
 
@@ -146,17 +218,15 @@ class Bar {
 
 class Parent {
 	var width(private get, private set):Int;
-	function set_width(value:Int):Int {
-		return 0;
-	}
-	function get_width():Int {
-		return 0;
-	}
+	function set_width(v):Int return 0;
+	function get_width():Int return 0;
 }
 
 class Child extends Parent {
 	public function new() {
 		width = 0;
+		super.width;
+		super.width = 0;
 	}
 }
 
