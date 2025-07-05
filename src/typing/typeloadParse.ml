4638cb11fa0e9736b17f5c52b1e9938573bc264c
Rudy Ges <k@klabz.org>
2025-07-05 17:50:11 +0200

    Fix remap vs module resolution for sub types

--- src/typing/typeloadParse.ml
+++ src/typing/typeloadParse.ml
@@ -325,7 +325,7 @@ let parse_module com m p =
 										TPType (make_ptp_th_null (mk_type_path ([],fst tp.tp_name)))
 									) d.d_params
 								in
-								mk_type_path ~params (!remap,fst d.d_name)
+								mk_type_path ~params ~sub:(fst d.d_name) (!remap,snd m)
 						in
 						make_ptp_th_null tp
 					end
