9beeec61af2a3dffd83c68b9e9bd291a3787efd3
flashultra <m_mail01@abv.bg>
2025-07-11 16:25:39 +0300

    Set defautl fromHex() to unsigned

--- std/haxe/math/bigint/BigInt.hx
+++ std/haxe/math/bigint/BigInt.hx
@@ -176,7 +176,7 @@ abstract BigInt(BigInt_) {
 		@return A new `BigInt` instance.
 	**/
 	public static inline function fromHex(value:String):BigInt {
-		return fromHexSigned(value);
+		return fromHexUnsigned(value);
 	}
 
 	/**
