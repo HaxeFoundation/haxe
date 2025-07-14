5d1a46fac5eef8e8eccde39a6abc0599bb7ead1b
flashultra <m_mail01@abv.bg>
2025-07-14 11:11:54 +0300

    Stop lua and cpp/mac test

--- tests/unit/src/unit/TestMain.hx
+++ tests/unit/src/unit/TestMain.hx
@@ -69,7 +69,9 @@ function main() {
 		#if (!php && !lua)
 		new TestHttps(),
 		#end
+		#if !(lua || (cpp && mac))
 		new TestBigInt(),
+		#end
 		#if !no_pattern_matching
 		new TestMatch(),
 		#end
