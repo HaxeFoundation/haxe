337ff3fe9d88b53a6c0ab473aa6eb5b647feb70c
Joey <9813078+PXshadow@users.noreply.github.com>
2025-09-03 11:11:57 +0200

    gencpp: update -> rebuild your build tools

--- src/generators/gencpp.ml
+++ src/generators/gencpp.ml
@@ -160,7 +160,7 @@ let write_build_data common_ctx filename classes main_deps boot_deps build_extra
    output_string buildfile "<include name=\"${HXCPP}/build-tool/BuildCommon.xml\"/>\n";
    output_string buildfile build_extra;
    if (Gctx.defined common_ctx Define.HxcppSmartStings) then
-      output_string buildfile ("<error value=\"Hxcpp is out of date - please update, and/or update your build tools\" unlessApi=\"" ^ api_string ^ "\" />\n");
+      output_string buildfile ("<error value=\"Hxcpp is out of date - please update, and/or rebuild your build tools\" unlessApi=\"" ^ api_string ^ "\" />\n");
    output_string buildfile "</xml>\n";
    close_out buildfile
 
