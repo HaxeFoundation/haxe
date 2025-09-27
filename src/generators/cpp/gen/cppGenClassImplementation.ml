2ec20266193ec5b68f91d707771731557c03c64e
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-09-27 14:44:12 +0100

    [cpp] Fix toString breaking scriptable mapping
    The toString method is filtered out in the vtable mapping on line 973
    and is already present in the `__scriptableFunctions` of hx::Object.
    We need to filter it out here too to ensure a correct vtable can be
    constructed from merging all `__scriptableFunctions` arrays.

--- src/generators/cpp/gen/cppGenClassImplementation.ml
+++ src/generators/cpp/gen/cppGenClassImplementation.ml
@@ -977,18 +977,20 @@ let generate_managed_class base_ctx tcpp_class =
     if List.length tcpp_class.tcl_functions > 0 || List.length tcpp_class.tcl_static_functions > 0 then (
 
       let dump_script is_static f acc =
-        let signature = generate_script_function is_static f.tcf_field ("__s_" ^ f.tcf_field.cf_name) f.tcf_name in
-        let superCall = if is_static then "0" else "__s_" ^ f.tcf_field.cf_name ^ "<true>" in
-        let named =
-          Printf.sprintf
-            "\t::hx::ScriptNamedFunction(\"%s\", __s_%s, \"%s\", %s HXCPP_CPPIA_SUPER_ARG(%s))"
-            f.tcf_field.cf_name
-            f.tcf_field.cf_name
-            signature
-            (if is_static then "true" else "false")
-            superCall in
+        if f.tcf_name <> "toString" then
+          let signature = generate_script_function is_static f.tcf_field ("__s_" ^ f.tcf_field.cf_name) f.tcf_name in
+          let superCall = if is_static then "0" else "__s_" ^ f.tcf_field.cf_name ^ "<true>" in
+          let named =
+            Printf.sprintf
+              "\t::hx::ScriptNamedFunction(\"%s\", __s_%s, \"%s\", %s HXCPP_CPPIA_SUPER_ARG(%s))"
+              f.tcf_field.cf_name
+              f.tcf_field.cf_name
+              signature
+              (if is_static then "true" else "false")
+              superCall in
 
-        named :: acc
+          named :: acc
+        else acc
       in
 
       let sigs =
