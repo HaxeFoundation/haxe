41c8923b495581bb5e6d1d22891dc72345d0054c
Simon Krajewski <simon@haxe.org>
2025-11-24 09:16:37 +0100

    disable failing eval tests

--- tests/unit/src/unit/issues/Issue12264.hx
+++ tests/unit/src/unit/issues/Issue12264.hx
@@ -1,7 +1,9 @@
 package unit.issues;
 
 class Issue12264 extends Test {
+	#if !eval
 	function test() {
 		eq(true, Type.typeof(0i64).match(TInt64));
 	}
+	#end
 }
--- tests/unit/src/unitstd/Type.unit.hx
+++ tests/unit/src/unitstd/Type.unit.hx
@@ -123,12 +123,14 @@ Type.getEnumConstructs(EnumFlagTest) == ["EA", "EB", "EC"];
 // typeof
 Type.typeof(1.5) == TFloat;
 Type.typeof(-45) == TInt;
-Type.typeof(1.0) == TInt; 
 Type.typeof(1e10) == TFloat;
+#if !eval
+Type.typeof(1.0) == TInt;
 var i0 = haxe.Int64.ofInt(0);
 Type.typeof(i0) == TInt64;
 var ibig = haxe.Int64.make(1,0);
 Type.typeof(ibig) == TInt64;
+#end
 
 // enumEq
 Type.enumEq(NoArgs, NoArgs) == true;
