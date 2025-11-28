90fe2f6434eb5ef9f91ad8a9d6fff15c735c97fa
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2025-11-28 19:44:25 +0100

    [php] fix compatibility with PHP 8.5

--- tests/runci/targets/Php.hx
+++ tests/runci/targets/Php.hx
@@ -58,8 +58,8 @@ class Php {
 			case "Linux":
 				Linux.requireAptPackages(["php-cli", "php-mbstring", "php-sqlite3"]);
 			case "Mac":
-				runNetworkCommand("brew", ["install", "php@8.4"]);
-				runCommand("brew", ["link", "--overwrite", "--force", "php@8.4"]);
+				runNetworkCommand("brew", ["install", "php"]);
+				runCommand("brew", ["link", "--overwrite", "--force", "php"]);
 			case "Windows":
 				runNetworkCommand("cinst", ["php", "-version", "7.1.8", "-y"]);
 			case _:
