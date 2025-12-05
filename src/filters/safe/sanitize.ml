dee61aa02c37efb7da0d9f81cf01211601f2f7eb
Rudy Ges <k@klabz.org>
2025-12-05 16:02:31 +0100

    Add some context about a comment

--- src/filters/safe/sanitize.ml
+++ src/filters/safe/sanitize.ml
@@ -77,7 +77,7 @@ let sanitize_expr config e =
 				(* TODO: this should use get_underlying_type, but we do not have access to Codegen here.  *)
 				| TAbstract(a,tl) when not (Meta.has Meta.CoreType a.a_meta) -> loop (apply_params a.a_params tl a.a_this)
 				| _ ->
-					if config != Common.default_config then (* This is atrocious *)
+					if config != Common.default_config then (* This is atrocious; see #12105 *)
 						Error.raise_typing_error ("On static platforms, null can't be used as basic type " ^ s_type (print_context()) e.etype) e.epos
 			in
 			loop e.etype
