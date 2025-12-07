77d1122e019f627706a62b297f4d221122e7f050
RblSb <msrblsb@gmail.com>
2025-12-07 17:38:40 +0300

    test hxcpp branch

--- tests/runci/targets/Cpp.hx
+++ tests/runci/targets/Cpp.hx
@@ -27,7 +27,7 @@ class Cpp {
 			final path = getHaxelibPath("hxcpp");
 			infoMsg('hxcpp has already been installed in $path.');
 		} catch(e:Dynamic) {
-			haxelibInstallGit("HaxeFoundation", "hxcpp", true);
+			haxelibInstallGit("RblSb", "hxcpp", "substr", true);
 			final oldDir = Sys.getCwd();
 			changeDirectory(getHaxelibPath("hxcpp") + "tools/hxcpp/");
 			runCommand("haxe", ["-D", "source-header=''", "compile.hxml"]);
