dfebb1430baec81f128c640cd6d80616da2b210e
Rudy Ges <k@klabz.org>
2025-12-10 08:19:32 +0100

    remove weird test

--- tests/unit/src/unit/issues/Issue12415.hx
+++ tests/unit/src/unit/issues/Issue12415.hx
@@ -5,7 +5,6 @@ using unit.issues.misc.Issue12415Abstract;
 class Issue12415 extends Test {
 	function test() {
 		var value:Issue12415Abstract = null;
-		eq(#if static haxe.Int64.ofInt(0) #else null #end, value);
 		eq(true, value == null);
 		eq(false, value != null);
 		eq(true, value.isNull());
