2fe200a3ed3257110a3cfc0ec99814019921bba7
Simon Krajewski <simon@haxe.org>
2025-12-10 09:26:19 +0100

    also test straightforward propagation

--- tests/unit/src/unit/issues/Issue12435.hx
+++ tests/unit/src/unit/issues/Issue12435.hx
@@ -1,10 +1,23 @@
 package unit.issues;
 
 class Issue12435 extends Test {
-	function test() {
+	function testCast() {
 		var i:Int = cast null;
 		#if static
 		t(i == 0);
+		t(i == cast null);
+		#else
+		f(i == 0);
+		t(i == null);
+		#end
+	}
+
+	function testPropagation() {
+		var Null:Null<Int> = null;
+		var i = Null;
+		#if static
+		t(i == 0);
+		t(i == Null);
 		#else
 		f(i == 0);
 		t(i == null);
