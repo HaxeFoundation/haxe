9a19f43ed6181fc98eec4523badc81399781531f
Rudy Ges <k@klabz.org>
2025-12-11 15:19:37 +0100

    [tests] also test 12435 with Int64

--- tests/unit/src/unit/issues/Issue12435.hx
+++ tests/unit/src/unit/issues/Issue12435.hx
@@ -1,5 +1,7 @@
 package unit.issues;
 
+import haxe.Int64;
+
 class Issue12435 extends Test {
 	function testCast() {
 		var i:Int = cast null;
@@ -29,6 +31,38 @@ class Issue12435 extends Test {
 	function testPropagationNullable() {
 		var Null:Null<Int> = null;
 		var i:Null<Int> = Null;
+		f(i == 0);
+		t(i == null);
+	}
+
+	function testCast64() {
+		var i:Int64 = cast null;
+		#if static
+		#if !cppia // TODO
+		t(i == 0);
+		#end
+		t(i == cast null);
+		#else
+		f(i == 0);
+		t(i == null);
+		#end
+	}
+
+	function testPropagation64() {
+		var Null:Null<Int64> = null;
+		var i:Int64 = Null;
+		#if static
+		t(i == 0);
+		f(i == Null);
+		#else
+		f(i == 0);
+		t(i == null);
+		#end
+	}
+
+	function testPropagationNullable64() {
+		var Null:Null<Int64> = null;
+		var i:Null<Int64> = Null;
 		f(i == 0);
 		t(i == null);
 	}
