86fe65e4e4bd2751321231cc3de6fccfd574c507
Simon Krajewski <simon@haxe.org>
2025-12-12 11:34:28 +0100

    restrict to == and !=

--- tests/unit/src/unit/issues/Issue12444.hx
+++ tests/unit/src/unit/issues/Issue12444.hx
@@ -34,26 +34,6 @@ private abstract NotInt64(Int) {
 	@:op(A != B) public static function neqYesYes(a:Null<NotInt64>, b:Null<NotInt64>):String {
 		return "neqYesYes";
 	}
-
-	@:commutative @:op(A + B) public static function plusNoNo(a:NotInt64, b:Int):String {
-		return "plusNoNo";
-	}
-
-	@:commutative @:op(A + B) public static function plusYesNo(a:Null<NotInt64>, b:Int):String {
-		return "plusYesNo";
-	}
-
-	@:commutative @:op(A + B) public static function plusNoYes(a:NotInt64, b:Null<Int>):String {
-		return "plusNoYes";
-	}
-
-	@:commutative @:op(A + B) public static function plusYesYes(a:Null<NotInt64>, b:Null<Int>):String {
-		return "plusYesYes";
-	}
-
-	public inline function get() {
-		return this;
-	}
 }
 
 class Issue12444 extends Test {
@@ -72,14 +52,5 @@ class Issue12444 extends Test {
 		eq("neqYesNo", nullable != notNullable);
 		eq("neqNoYes", notNullable != nullable);
 		eq("neqNoNo", notNullable != notNullable);
-		// + with @:commutative
-		eq("plusYesYes", nullable + nullableInt);
-		eq("plusYesYes", nullableInt + nullable);
-		eq("plusYesNo", nullable + notNullableInt);
-		eq("plusYesNo", notNullableInt + nullable);
-		eq("plusNoYes", notNullable + nullableInt);
-		eq("plusNoYes", nullableInt + notNullable);
-		eq("plusNoNo", notNullable + notNullableInt);
-		eq("plusNoNo", notNullableInt + notNullable);
 	}
 }
