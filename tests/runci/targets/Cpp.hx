31ecd0bf1d0c02da5f908131a07fe6bc66f94886
Aidan Lee <aidan.lee63@gmail.com>
2025-12-13 15:43:29 +0000

    test using hxcpp fork

--- tests/runci/targets/Cpp.hx
+++ tests/runci/targets/Cpp.hx
@@ -27,7 +27,7 @@ class Cpp {
 			final path = getHaxelibPath("hxcpp");
 			infoMsg('hxcpp has already been installed in $path.');
 		} catch(e:Dynamic) {
-			haxelibInstallGit("HaxeFoundation", "hxcpp", true);
+			haxelibInstallGit("Aidan63", "hxcpp", "valuetype-marshal", true);
 			final oldDir = Sys.getCwd();
 			changeDirectory(getHaxelibPath("hxcpp") + "tools/hxcpp/");
 			runCommand("haxe", ["-D", "source-header=''", "compile.hxml"]);
