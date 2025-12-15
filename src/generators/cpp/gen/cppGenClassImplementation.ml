2e779b55f3295b666c9302e5677bd50561e5b7ba
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-12-15 11:55:58 +0000

    [cpp] Remove HXCPP_CPPIA_SUPER_ARG macro usage
    It is not needed as it is always defined the same way by hxcpp. We can
    instead just pass the argument directly, or omit it if it is empty.
    
    In some cases, HXCPP_CPPIA_SUPER_ARG(0) was being passed to the
    inIsStatic argument.

--- src/generators/cpp/gen/cppGenClassImplementation.ml
+++ src/generators/cpp/gen/cppGenClassImplementation.ml
@@ -1016,7 +1016,7 @@ let generate_managed_class base_ctx tcpp_class =
         let superCall = if is_static then "0" else "__s_" ^ f.tcf_field.cf_name ^ "<true>" in
         let named =
           Printf.sprintf
-            "\t::hx::ScriptNamedFunction(\"%s\", __s_%s, \"%s\", %s HXCPP_CPPIA_SUPER_ARG(%s), %s)"
+            "\t::hx::ScriptNamedFunction(\"%s\", __s_%s, \"%s\", %s, %s, %s)"
             f.tcf_field.cf_name
             f.tcf_field.cf_name
             signature
@@ -1029,15 +1029,12 @@ let generate_managed_class base_ctx tcpp_class =
       in
 
       let sigs =
-        [ "\t::hx::ScriptNamedFunction(0,0,0 HXCPP_CPPIA_SUPER_ARG(0) )" ]
+        [ "\t::hx::ScriptNamedFunction(0,0,0)" ]
         |> List.fold_right (dump_script false) tcpp_class.tcl_functions
         |> List.fold_right (dump_script true) tcpp_class.tcl_static_functions
         |> String.concat ",\n"
       in
 
-      output_cpp "#ifndef HXCPP_CPPIA_SUPER_ARG\n";
-      output_cpp "#define HXCPP_CPPIA_SUPER_ARG(x) ,0\n";
-      output_cpp "#endif\n";
       Printf.sprintf "static ::hx::ScriptNamedFunction __scriptableFunctions[] = {\n%s\n};\n\n" sigs |> output_cpp)
     else
       output_cpp "static ::hx::ScriptNamedFunction *__scriptableFunctions = 0;\n");
