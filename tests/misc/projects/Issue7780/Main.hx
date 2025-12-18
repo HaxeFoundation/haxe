b0163fc9428363601a81faad945b1f98bf1c97f5
RblSb <msrblsb@gmail.com>
2025-12-18 18:29:20 +0300

    use tabs

--- tests/misc/projects/Issue7780/Main.hx
+++ tests/misc/projects/Issue7780/Main.hx
@@ -16,20 +16,20 @@ class Main {
 
 
 class Main2 extends Foo {
-  static function main() {
-    Main2.trace = (d:Dynamic, infos:haxe.PosInfos) -> {};
-  }
-  function a(v:String) {}
-  function b(v:String = 'hey') {}
-  function c(v:String) {}
-  function d(?v:String) {}
+	static function main() {
+		Main2.trace = (d:Dynamic, infos:haxe.PosInfos) -> {};
+	}
+	function a(v:String) {}
+	function b(v:String = 'hey') {}
+	function c(v:String) {}
+	function d(?v:String) {}
 
 	public static dynamic function trace(v:Dynamic, ?infos:PosInfos):Void {}
 }
 
 abstract class Foo {
 	abstract function a(val:String = 'hey'):Void;
-  abstract function b(val:String):Void;
-  abstract function c(?val:String):Void;
-  abstract function d(val:Int):Void;
+	abstract function b(val:String):Void;
+	abstract function c(?val:String):Void;
+	abstract function d(val:Int):Void;
 }
