cf770bb913b22ca870c6305911ef612896f2fd03
Simon Krajewski <simon@haxe.org>
2025-12-30 09:58:43 +0100

    document things

--- std/haxe/coro/context/Context.hx
+++ std/haxe/coro/context/Context.hx
@@ -2,42 +2,76 @@ package haxe.coro.context;
 
 typedef ElementTree = Array<Any>;
 
+/**
+	An immutable context, which can be used like a map.
+**/
 abstract Context(ElementTree) {
+	/**
+		Creates a new immutable context containing the values in `tree`.
+	**/
 	public inline function new(tree:ElementTree) {
 		this = tree;
 	}
 
+	/**
+		Clones `this` context as an `AdjustableContext`.
+	**/
 	public inline function clone() {
 		return new AdjustableContext(this.copy());
 	}
 
-	public inline function get<T>(key:Key<T>):T {
+	/**
+		Returns the value associated with `key`, or `null` if no such value exists.
+	**/
+	public inline function get<T>(key:Key<T>):Null<T> {
 		return cast this[key.id];
 	}
 
+	/**
+		Returns a string representation of `this` context.
+	**/
 	public inline function toString() {
 		return this.toString();
 	}
 
+	/**
+		Creates a new `AdjustableContext` containing the `elements` associations.
+	**/
 	static public inline function create(...elements:IElement<Any>) {
 		return new AdjustableContext(new ElementTree()).with(...elements);
 	}
 }
 
+/**
+	Similar to `Context`, but mutable. This type should rarely be used explicitly and
+	mostly appears as an intermediate type via the `Context` API.
+**/
 abstract AdjustableContext(ElementTree) {
+	/**
+		Creates a new context containing the values in `tree`.
+	**/
 	public inline function new(tree:ElementTree) {
 		this = tree;
 	}
 
+	/**
+		Adds or replaces `element` associated with `key`.
+	**/
 	public inline function add<T>(key:Key<T>, element:T) {
 		this[key.id] = element;
 		return abstract;
 	}
 
-	public inline function get<T>(key:Key<T>):T {
+	/**
+		@see `Context.get`
+	**/
+	public inline function get<T>(key:Key<T>):Null<T> {
 		return cast this[key.id];
 	}
 
+	/**
+		Adds or replaces the `elements` associations in `this` context.
+	**/
 	public function with(...elements:IElement<Any>) {
 		for (element in elements) {
 			this[element.getKey().id] = element;
--- std/haxe/coro/context/Key.hx
+++ std/haxe/coro/context/Key.hx
@@ -24,13 +24,20 @@ private typedef WithKey<K> = {
 	final key:Key<K>;
 }
 
+/**
+	Represents the key of a `Context` element. See `haxe.coro.schedulers.Scheduler` for a usage example.
+**/
 @:forward
 @:forward.statics
 extern abstract Key<T>(KeyImpl<T>) {
+	/**
+		Creates a new key with a given `name`. The name is purely descriptive.
+	**/
 	public inline function new(name:String) {
 		this = KeyImpl.createNew(name);
 	}
 
+	@:noCompletion
 	@:from static public inline function fromClass<K, C:(Class<Any> & WithKey<K>)>(c:C):Key<K> {
 		return c.key;
 	}
