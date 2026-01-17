2561c3b402847c6b217cf6dd90c78be9e272f881
RblSb <msrblsb@gmail.com>
2026-01-17 07:44:52 +0300

    Fix `final var` in Printer

--- tests/unit/src/unit/issues/Issue8859.hx
+++ tests/unit/src/unit/issues/Issue8859.hx
@@ -10,7 +10,16 @@ class Issue8859 extends Test {
 		}
 		var printer = new haxe.macro.Printer();
 		var s = printer.printTypeDefinition(td);
-		s = ~/[\t\n\r]/g.replace(s, "");
+		final newLineRegex = ~/[\t\n\r]/g;
+		s = newLineRegex.replace(s, "");
 		eq("class X {final x : Int = 0;var y : Int = 0;private final z : Int = 0;private var w : Int = 0;}", s);
+
+		var td = macro class X {
+			static var x(get, never);
+		};
+		td.fields[0].access.push(AFinal);
+		var s = printer.printTypeDefinition(td);
+		s = newLineRegex.replace(s, "");
+		eq("class X {static final x(get, never);}", s);
 	}
 }
