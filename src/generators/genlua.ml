afbb1e2603467f0053b22845d9e3a2663cdeb555
Justin Donaldson <jjd@jjd.io>
2026-01-17 22:58:25 -0800

    [lua] Minor code cleanup in genlua.ml
    - Simplify `this` function (was pattern matching to same value)
    - Remove stale TODO comment on continue handling (already implemented)
    - Clarify index_of comment as OCaml 5.1 polyfill

--- src/generators/genlua.ml
+++ src/generators/genlua.ml
@@ -239,7 +239,7 @@ let open_block ctx =
         ctx.tabs <- oldt;
         ctx.declared_locals <- old_declared_locals)
 
-let this ctx = match ctx.in_value with None -> "self" | Some _ -> "self"
+let this _ctx = "self"
 
 let is_dot_access e cf =
     match follow(e.etype), cf with
@@ -256,7 +256,7 @@ let is_dot_access e cf =
         false
 
 (* Return index of first element in the list for which `f` returns true.
-   Note: List.find_index was added in OCaml 5.1, keeping custom impl for compatibility. *)
+   Polyfill for List.find_index (added in OCaml 5.1). *)
 let index_of f l =
     let rec find lst idx =
         match lst with
@@ -781,7 +781,7 @@ and gen_expr ?(local=true) ctx e = begin
         if ctx.loop_ctx.in_loop_try then
             print ctx "_G.error(\"_hx_pcall_break\", 0)"
         else
-            spr ctx "break" (*todo*)
+            spr ctx "break"
     | TBlock el ->
         let bend = open_block ctx in
         List.iter (gen_block_element ctx) el;
