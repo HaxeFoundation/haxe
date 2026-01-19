830265f8dcf407642b09d51d5204d9d16626ea8f
Aidan Lee <aidan.lee63@gmail.com>
2026-01-19 20:58:26 +0000

    Give dispatcher a scheduler field

--- std/haxe/coro/dispatchers/Dispatcher.hx
+++ std/haxe/coro/dispatchers/Dispatcher.hx
@@ -1,12 +1,17 @@
 package haxe.coro.dispatchers;
 
+import haxe.coro.schedulers.Scheduler;
 import haxe.coro.context.Key;
 import haxe.coro.context.IElement;
 
 abstract class Dispatcher implements IElement<Dispatcher> {
 	public static final key = new Key<Dispatcher>("Dispatcher");
 
+	public var scheduler (get, never) : Scheduler;
+
 	public abstract function dispatch(obj:IScheduleObject):Void;
+
+	public abstract function get_scheduler() : Scheduler;
 
 	public function getKey() {
 		return key;
