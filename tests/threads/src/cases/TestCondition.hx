bf901af336f3d4b3d6f00de25e19bab4ef275096
Simon Krajewski <simon@haxe.org>
2026-01-25 15:33:33 +0100

    add more Condition tests
    and fix some deprecations while I'm here

--- tests/threads/src/cases/TestCondition.hx
+++ tests/threads/src/cases/TestCondition.hx
@@ -1,5 +1,6 @@
 package cases;
 
+import utest.Assert;
 import sys.thread.Condition;
 import sys.thread.Thread;
 
@@ -14,6 +15,45 @@ class TestCondition extends utest.Test {
 		});
 		cond.wait();
 		cond.release();
+		Assert.pass();
+	}
+
+	function testTryAcquire() {
+		final cond = new Condition();
+		Assert.isTrue(cond.tryAcquire());
+		// I would prefer to specify it to be not recursive...
+		// Assert.isFalse(cond.tryAcquire());
+		cond.release();
+	}
+
+	function testGCDuringWait() {
+		final cond = new Condition();
+
+		var threadRunning = false;
+		Thread.create(() -> {
+			threadRunning = true;
+			cond.acquire();
+			cond.wait();
+			cond.release();
+		});
+
+		while (!threadRunning) {
+			Sys.sleep(0.001);
+		}
+		#if cpp
+		cpp.vm.Gc.run(true);
+		#elseif hl
+		hl.Gc.major();
+		#elseif interp
+		eval.vm.Gc.full_major();
+		#elseif neko
+		neko.vm.Gc.run(true);
+		#end
+
+		cond.acquire();
+		cond.signal();
+		cond.release();
+
 		utest.Assert.pass();
 	}
 }
