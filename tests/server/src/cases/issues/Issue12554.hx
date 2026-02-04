30098ecc460cf3e2676809dcab49fb23f8c5ad2d
Rudy Ges <k@klabz.org>
2026-02-04 15:56:24 +0100

    [tests] add test for #12554

---
+++ tests/server/src/cases/issues/Issue12554.hx
@@ -0,0 +1,34 @@
+package cases.issues;
+
+class Issue12554 extends TestCase {
+	function test(_) {
+		var mainTpl = storeAndParseTemplate("issues/Issue12554/Main.hx", "Main.hx");
+		var fooTpl = storeAndParseTemplate("issues/Issue12554/Foo.hx", "Foo.hx");
+
+		var args = ["-main", "Main", "--no-output"];
+		runHaxe(args);
+		assertSuccess();
+
+		var sig = "";
+		runHaxeJsonCb(args, ServerMethods.Contexts, null, res -> sig = res[0].signature);
+		Assert.notEquals(sig, "");
+
+		runHaxeJsonCb(args, ServerMethods.Module, {
+			path: "Main",
+			signature: sig
+		}, res -> Assert.equals("Good", res.cacheState));
+
+		runHaxeJson([], ServerMethods.Invalidate, {file: new FsPath("Foo.hx")});
+
+		runHaxeJsonCb(args, DisplayMethods.Hover, {
+			file: new FsPath("Main.hx"),
+			offset: mainTpl.markers[1]
+		}, res -> {});
+		assertSuccess();
+
+		runHaxeJsonCb(args, ServerMethods.Module, {
+			path: "Main",
+			signature: sig
+		}, res -> Assert.equals("Good", res.cacheState));
+	}
+}
---
+++ tests/server/test/templates/issues/Issue12554/Main.hx
@@ -0,0 +1,7 @@
+class Main {
+	static var foo:Foo;
+
+	public static function main() {
+		{-1-}false{-1-};
+	}
+}
