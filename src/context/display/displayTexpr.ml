18248e90b60103c59a15db605c4c03b63ceac1bc
Rudy Ges <k@klabz.org>
2026-02-04 15:57:18 +0100

    [hxb] do not remove display file from cache

--- src/context/display/displayTexpr.ml
+++ src/context/display/displayTexpr.ml
@@ -187,10 +187,12 @@ let check_display_file ctx cs =
 			in
 			check_display_module ctx cfile.c_decls m
 		with Not_found ->
-			let fkey = DisplayPosition.display_position#get_file_key in
-			(* force parsing again : if the completion point have been changed *)
-			cs#remove_files fkey;
-			cs#taint_modules fkey CheckDisplayFile;
+			if Define.defined ctx.com.defines DisableHxbCache then begin
+				let fkey = DisplayPosition.display_position#get_file_key in
+				(* force parsing again : if the completion point have been changed *)
+				cs#remove_files fkey;
+				cs#taint_modules fkey CheckDisplayFile
+			end
 		end
 	| None ->
 		()
