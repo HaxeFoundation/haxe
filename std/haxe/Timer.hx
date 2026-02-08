17c85a63aff098772ed58426045ecb50616e34a8
Jeremy Faivre <contact@jeremyfa.com>
2026-02-08 23:50:45 +0100

    [cs] Add a proper C# implementation of `Timer.milliseconds()`

--- std/haxe/Timer.hx
+++ std/haxe/Timer.hx
@@ -209,6 +209,8 @@ class Timer {
 		return java.lang.System.nanoTime() / 1000000i64;
 		#elseif eval
 		return @:privateAccess Sys.timestamp_ms();
+		#elseif cs
+		return untyped __cs__("System.Diagnostics.Stopwatch.GetTimestamp() * 1000L / System.Diagnostics.Stopwatch.Frequency");
 		#else
 		return Int64.mul(Int64.fromFloat(stamp()), 1000);
 		#end
