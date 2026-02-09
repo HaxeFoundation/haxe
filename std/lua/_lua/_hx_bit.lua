1d7f95e21d94ca57b6b4601e1fa4cc0398227b86
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-09 15:11:33 +0000

    [lua] Delay _hx_bit error until usage

--- std/lua/_lua/_hx_bit.lua
+++ std/lua/_lua/_hx_bit.lua
@@ -15,5 +15,7 @@ elseif _G.bit or pcall(require, 'bit') then
   _hx_bit_raw = _G.bit or require('bit')
   _hx_bit = _hx_bit_raw
 else
-  error("Failed to load bit or bit32")
+  _hx_bit = setmetatable({}, {__index = function()
+    error("Failed to load bit or bit32")
+  end})
 end
