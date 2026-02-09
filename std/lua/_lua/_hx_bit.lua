92a724032e7a2fa007ce2e580979872ca7e21139
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-09 14:51:33 +0000

    [lua] Apply bit op clamping only for bit32 library
    Running with the bit library in luajit passes the unit tests without
    these wrappers

--- std/lua/_lua/_hx_bit.lua
+++ std/lua/_lua/_hx_bit.lua
@@ -2,9 +2,14 @@ local hasBit32, bit32 = pcall(require, 'bit32')
 if hasBit32 then --if we are on Lua 5.1, bit32 will be the default.
   _hx_bit_raw = bit32
   _hx_bit = setmetatable({}, { __index = _hx_bit_raw })
-  -- lua 5.2 weirdness
+  -- bit32 weirdness
   _hx_bit.bnot = function(...) return _hx_bit_clamp(_hx_bit_raw.bnot(...)) end
   _hx_bit.bxor = function(...) return _hx_bit_clamp(_hx_bit_raw.bxor(...)) end
+  -- see https://github.com/HaxeFoundation/haxe/issues/8849
+  _hx_bit.bor = function(...) return _hx_bit_clamp(_hx_bit_raw.bor(...)) end
+  _hx_bit.band = function(...) return _hx_bit_clamp(_hx_bit_raw.band(...)) end
+  _hx_bit.arshift = function(...) return _hx_bit_clamp(_hx_bit_raw.arshift(...)) end
+  _hx_bit.lshift = function(...) return _hx_bit_clamp(_hx_bit_raw.lshift(...)) end
 else
   --If we do not have bit32, fallback to 'bit'
   local hasBit, bit = pcall(require, 'bit')
@@ -14,9 +19,3 @@ else
   _hx_bit_raw = bit
   _hx_bit = setmetatable({}, { __index = _hx_bit_raw })
 end
-
--- see https://github.com/HaxeFoundation/haxe/issues/8849
-_hx_bit.bor = function(...) return _hx_bit_clamp(_hx_bit_raw.bor(...)) end
-_hx_bit.band = function(...) return _hx_bit_clamp(_hx_bit_raw.band(...)) end
-_hx_bit.arshift = function(...) return _hx_bit_clamp(_hx_bit_raw.arshift(...)) end
-_hx_bit.lshift = function(...) return _hx_bit_clamp(_hx_bit_raw.lshift(...)) end
