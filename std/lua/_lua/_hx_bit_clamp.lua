cc30feb4112e7218f16fca9157d94c219b707b1e
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-11 18:23:31 +0000

    [lua] Fix indentation in _hx_bit_clamp

--- std/lua/_lua/_hx_bit_clamp.lua
+++ std/lua/_lua/_hx_bit_clamp.lua
@@ -20,14 +20,14 @@ if _hx_bit_clamp_native then
     _hx_bit_clamp = _hx_bit_clamp_native
 elseif _hx_bit_raw then
     _hx_bit_clamp = function(v)
-    if v <= 2147483647 and v >= -2147483648 then
-        if v > 0 then return _G.math.floor(v)
-        else return _G.math.ceil(v)
+        if v <= 2147483647 and v >= -2147483648 then
+            if v > 0 then return _G.math.floor(v)
+            else return _G.math.ceil(v)
+            end
         end
-    end
-    if v > 2251798999999999 then v = v*2 end;
-    if (v ~= v or math.abs(v) == _G.math.huge) then return nil end
-    return _hx_bit_raw.band(v, 2147483647 ) - math.abs(_hx_bit_raw.band(v, 2147483648))
+        if v > 2251798999999999 then v = v*2 end;
+        if (v ~= v or math.abs(v) == _G.math.huge) then return nil end
+        return _hx_bit_raw.band(v, 2147483647 ) - math.abs(_hx_bit_raw.band(v, 2147483648))
     end
 else
     -- Fallback for Lua without bit, bit32, or native bit ops: wrap using modulo
