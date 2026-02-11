17e7001281cd995f25a0e71ae2337703f9d12d94
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-11 13:08:07 +0000

    [tests] Update lua test #11013 for _hx_bit fix
    Since #12587, preloaded bit32 and bit may be used without require, so to
    properly test the fallback we should make them inaccessible.

--- tests/misc/lua/projects/Issue11013/Main.hx
+++ tests/misc/lua/projects/Issue11013/Main.hx
@@ -1,16 +1,15 @@
 class Main {
 	static function main() {
-		// Test Int32 wrapping without bit32 library available.
-		// require is set to nil before running, so bit32 cannot be loaded.
+		// Test Int32 wrapping without bit32/bit library available.
+		// require, bit32, and bit are set to nil before running, so they cannot be used
 		var max:haxe.Int32 = 2147483647;
 		var one:haxe.Int32 = 1;
 		var result:Int = max + one;
+		var expected = -2147483648;
 		if (result == -2147483648) {
-			Sys.stderr().writeString("Success");
-			Sys.stderr().flush();
+			Sys.println("Success");
 		} else {
-			Sys.stderr().writeString('Expected -2147483648 but got $result');
-			Sys.stderr().flush();
+			Sys.println('Expected $expected but got $result');
 		}
 	}
 }
