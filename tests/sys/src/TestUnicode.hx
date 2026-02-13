c862a338c3e3eed97fb857bb228af6f939b27a24
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-13 20:05:42 +0000

    [tests] Add lua to list of targets with \r\n issue

--- tests/sys/src/TestUnicode.hx
+++ tests/sys/src/TestUnicode.hx
@@ -304,17 +304,17 @@ class TestUnicode extends utest.Test {
 				// trace
 				assertUEnds(runUtility(["trace", '$i', mode]).stdout, str + endLine);
 				#if !jvm
-#if (hl || cpp) if (Sys.systemName() != "Windows") { #end // HL and C++ temporarily disabled (#8379)
+#if (hl || cpp || lua) if (Sys.systemName() != "Windows") { #end // HL and C++ temporarily disabled (#8379)
 				// putEnv + getEnv
 				assertUEquals(runUtility(["putEnv", "HAXE_TEST", '$i', mode, "getEnv", "HAXE_TEST"]).stdout, str + endLine);
 				// putEnv + environment
 				assertUEquals(runUtility(["putEnv", "HAXE_TEST", '$i', mode, "environment", "HAXE_TEST"]).stdout, str + endLine);
-#if (hl || cpp) } #end // HL and C++ temporarily disabled (#8379)
+#if (hl || cpp || lua) } #end // HL and C++ temporarily disabled (#8379)
 				#end
 			});
 
 		// args
-		if (#if (jvm || eval || hl || cpp) Sys.systemName() != "Windows" #else true #end) {
+		if (#if (jvm || eval || hl || cpp || lua) Sys.systemName() != "Windows" #else true #end) {
 			// https://stackoverflow.com/questions/7660651/passing-command-line-unicode-argument-to-java-code
 			UnicodeSequences.normalBoth(str -> {
 					assertUEquals(runUtility(["args", str]).stdout, str + endLine);
