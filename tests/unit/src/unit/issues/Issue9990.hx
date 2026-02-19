a680b3245564d70c5a43f0e9af6198630a8bdc43
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-19 22:48:51 +0000

    [tests] Add tests for cppia retyping issues
    These were solved in:
    https://github.com/HaxeFoundation/haxe/pull/11981/changes/4714cc32718fabf8a923efe2fa4578784c1c1319
    
    The following cases from that patch do not affect cppia:
    - FuncFromStaticFunction - this is not supported no cppia
    - is_array_splice_call - this case has the explicit `not forCppia`
    condition
    - _hx_create_array_length - this is not used directly on cppia
    - cpp_is_templated_call - templated calls are not supported on cppia
    
    There are also some cases where I have added tests, but they were
    working correctly before the patch:
    - testMapSet - technically the patch makes a difference with regards to
    the values stored in the map, but using map.get always converts to the
    correct type anyway.
    - testDynamicFieldCall - cppia always converts dynamic method arguments
    to the correct type. See:
    https://github.com/HaxeFoundation/hxcpp/blob/8600e4d49852a3e915c32f97c249ff7111936103/src/hx/cppia/CppiaFunction.cpp#L821
    - testObjectDeclaration - similar to map set, it affects the stored data
    but the conversion always occurs on dynamic field access.

---
+++ tests/unit/src/unit/issues/Issue9990.hx
@@ -0,0 +1,122 @@
+package unit.issues;
+
+import utest.Assert;
+
+private function wrap<T>(a:Array<T>):Array<T> {
+	return a;
+}
+
+private class Parent {
+	public var value:Null<Array<Int>>;
+
+	public function new(?a:Array<Int>) {
+		value = a?.copy();
+	}
+
+	public function method(a:Array<Int>):Array<Int> {
+		return a.copy();
+	}
+}
+
+private class Child extends Parent {
+	public function new(?a:Array<Int>) {
+		super(a != null ? wrap(a) : null);
+	}
+
+	override public function method(a:Array<Int>):Array<Int> {
+		return super.method(wrap(a));
+	}
+
+	// the type parameter here triggers a static cast on return
+	public function forStaticCast<T>(a:Array<Int>):T {
+		value = a.copy();
+		return cast this;
+	}
+}
+
+private enum MyEnum {
+	Value(arr:Array<Int>);
+}
+
+// Main test class
+class Issue9990 extends Test {
+	function checkArray(array:Array<Int>) {
+		eq(10, array[0]);
+		eq(20, array[1]);
+		eq(30, array[2]);
+		eq(3, array.length);
+	}
+
+	function testInstanceMethodCast() {
+		final child = new Child();
+		final result:Child = child.forStaticCast(wrap([10, 20, 30]));
+		checkArray(result.value);
+	}
+
+	function testSuperMethod() {
+		final child = new Child();
+		final result = child.method([10, 20, 30]);
+		checkArray(result);
+	}
+
+	function extractEnumArg(e:MyEnum) {
+		return switch (e) {
+			case Value(a): a.copy();
+		};
+	}
+
+	function testEnumConstruct() {
+		final e = MyEnum.Value(wrap([10, 20, 30]));
+		final result = extractEnumArg(e);
+		checkArray(result);
+	}
+
+	function testSuperConstructor() {
+		final child = new Child([10, 20, 30]);
+		checkArray(child.value);
+	}
+
+	function extractArray(array:Array<Array<Int>>) {
+		return array[0].copy();
+	}
+
+	function testArrayDeclaration() {
+		final arr:Array<Array<Int>> = [wrap([10, 20, 30])];
+		final result = extractArray(arr);
+		checkArray(result);
+	}
+
+	// these tests passed before, but are here for good measure:
+
+	function testMapSet() {
+		final map:Map<Int, Array<Int>> = [];
+
+		map[0] = wrap([10, 20, 30]);
+
+		// map.get uses a type parameter so it will convert to Array<Int> anyway
+		final result = map[0].copy();
+		checkArray(result);
+	}
+
+	function testDynamicFieldCall() {
+		final obj:{
+			function method(arr:Array<Int>):Array<Int>;
+		} = new Parent();
+		// cppia automatically handles Array<Dynamic> -> Array<Int> conversion for dynamic methods calls anyway
+		final result = obj.method(wrap([10, 20, 30]));
+		checkArray(result);
+	}
+
+	function extractObject(obj:{ value: Array<Int> }) {
+		return obj.value.copy();
+	}
+
+	function testObjectDeclaration() {
+		var obj = {
+			value: wrap([10, 20, 30])
+		};
+		// The generator always converts to Array<Int> on dynamic field access
+		final result = extractObject(obj);
+		checkArray(result);
+	}
+}
