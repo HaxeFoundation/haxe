7d1a974f670726dbad90c48fb631e544cfdafdb7
Simon Krajewski <simon@haxe.org>
2026-02-19 11:01:36 +0100

    add Context.getOrRaise

--- std/haxe/coro/context/Context.hx
+++ std/haxe/coro/context/Context.hx
@@ -1,5 +1,7 @@
 package haxe.coro.context;
 
+import haxe.exceptions.CoroutineException;
+
 typedef ElementTree = Array<Null<Any>>;
 
 /**
@@ -31,6 +33,10 @@ abstract Context(ElementTree) {
 	**/
 	public inline function get<T>(key:Key<T>):Null<T> {
 		return cast this[key.id];
+	}
+
+	public inline function getOrRaise<T>(key:Key<T>):T {
+		return get(key) ?? throw new CoroutineException("Key " + key.name + " not found in context");
 	}
 
 	/**
