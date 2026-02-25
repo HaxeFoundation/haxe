a66220042124f227d8fc4fa857e7fd5366f1b5bf
Justin Donaldson <jjd@jjd.io>
2026-02-25 11:42:19 -0800

    [lua] Fix createEmptyInstance losing fields on round-trip (#9484)
    Use _hx_new(cl.prototype) instead of a bare table so instances created
    by Type.createEmptyInstance (used by Unserializer) get __fields__ tracking
    and the __newindex metamethod, matching normal instance creation.

---
+++ tests/unit/src/unit/issues/Issue9484.hx
@@ -0,0 +1,24 @@
+package unit.issues;
+
+private class Hoge {
+	public var x:Int;
+
+	public function new(x:Int) {
+		this.x = x;
+	}
+}
+
+class Issue9484 extends Test {
+	function test() {
+		#if lua
+		var a = new Hoge(5);
+		var serializedA = haxe.Serializer.run(a);
+		var unserialized:Hoge = haxe.Unserializer.run(serializedA);
+		eq(unserialized.x, 5);
+		var serializedB = haxe.Serializer.run(unserialized);
+		eq(serializedA, serializedB);
+		#else
+		noAssert();
+		#end
+	}
+}
