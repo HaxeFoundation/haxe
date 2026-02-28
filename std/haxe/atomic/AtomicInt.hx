f7f06d418edd5654e899fd335a24cc0db7e89258
Simon Krajewski <simon@haxe.org>
2026-02-28 21:31:54 +0100

    sidestep Eval prototype problem

--- std/haxe/atomic/AtomicInt.hx
+++ std/haxe/atomic/AtomicInt.hx
@@ -18,6 +18,9 @@ private typedef AtomicIntData = AtomicIntValue;
 	Atomic integer.
 	(js) The Atomics and SharedArrayBuffer objects need to be available. Errors will be thrown if this is not the case.
 **/
+#if eval
+@:native("haxe.atomic.Atomic")
+#end
 abstract AtomicInt(AtomicIntData) {
 	public function new(value:Int):Void {
 		this = new AtomicIntData(value);
