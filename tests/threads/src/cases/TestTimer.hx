b9d4c63edab51a651a09a93a7be4c4467d97593e
Simon Krajewski <simon@haxe.org>
2026-03-07 15:40:27 +0100

    enable hanging test again to see how it behaves on other OS

--- tests/threads/src/cases/TestTimer.hx
+++ tests/threads/src/cases/TestTimer.hx
@@ -37,58 +37,58 @@ class TestTimer extends utest.Test {
 		});
 	}
 
-	// function testCallbackInSameThread(async:Async) {
-	// 	var mainThread = Thread.current();
+	function testCallbackInSameThread(async:Async) {
+		var mainThread = Thread.current();
 
-	// 	function work(n:Int) {
-	// 		var thread = Thread.current();
-	// 		var t = new Timer(100);
-	// 		var i = 0;
-	// 		var sameThread = true;
-	// 		t.run = () -> {
-	// 			sameThread = sameThread && thread == Thread.current();
-	// 			if(i++ > 5) {
-	// 				t.stop();
-	// 				mainThread.sendMessage({n:n, type:'interval', sameThread:sameThread});
-	// 			}
-	// 		}
-	// 		Timer.delay(() -> {
-	// 			mainThread.sendMessage({n:n, type:'delay', sameThread:thread == Thread.current()});
-	// 		}, 50);
-	// 	}
+		function work(n:Int) {
+			var thread = Thread.current();
+			var t = new Timer(100);
+			var i = 0;
+			var sameThread = true;
+			t.run = () -> {
+				sameThread = sameThread && thread == Thread.current();
+				if(i++ > 5) {
+					t.stop();
+					mainThread.sendMessage({n:n, type:'interval', sameThread:sameThread});
+				}
+			}
+			Timer.delay(() -> {
+				mainThread.sendMessage({n:n, type:'delay', sameThread:thread == Thread.current()});
+			}, 50);
+		}
 
-	// 	for(n in 0...10) {
-	// 		Thread.create(work.bind(n));
-	// 	}
+		for(n in 0...10) {
+			Thread.create(work.bind(n));
+		}
 
-	// 	//expect two messages with different types per thread
-	// 	var counters = [for(i in 0...10) ['delay', 'interval']];
-	// 	for(i in 0...20) {
-	// 		var msg:{n:Int, type:String, sameThread:Bool} = Thread.readMessage(true);
-	// 		isTrue(msg.sameThread);
-	// 		isTrue(counters[msg.n].remove(msg.type));
-	// 	}
-	// 	for(types in counters) {
-	// 		equals(0, types.length);
-	// 	}
+		//expect two messages with different types per thread
+		var counters = [for(i in 0...10) ['delay', 'interval']];
+		for(i in 0...20) {
+			var msg:{n:Int, type:String, sameThread:Bool} = Thread.readMessage(true);
+			isTrue(msg.sameThread);
+			isTrue(counters[msg.n].remove(msg.type));
+		}
+		for(types in counters) {
+			equals(0, types.length);
+		}
 
-	// 	//test in main thread
-	// 	async.branch(async -> {
-	// 		var t = new Timer(100);
-	// 		var i = 0;
-	// 		t.run = () -> {
-	// 			isTrue(mainThread == Thread.current());
-	// 			if(i++ > 5) {
-	// 				t.stop();
-	// 				async.done();
-	// 			}
-	// 		}
-	// 	});
-	// 	async.branch(async -> {
-	// 		Timer.delay(() -> {
-	// 			isTrue(mainThread == Thread.current());
-	// 			async.done();
-	// 		}, 50);
-	// 	});
-	// }
+		//test in main thread
+		async.branch(async -> {
+			var t = new Timer(100);
+			var i = 0;
+			t.run = () -> {
+				isTrue(mainThread == Thread.current());
+				if(i++ > 5) {
+					t.stop();
+					async.done();
+				}
+			}
+		});
+		async.branch(async -> {
+			Timer.delay(() -> {
+				isTrue(mainThread == Thread.current());
+				async.done();
+			}, 50);
+		});
+	}
 }
