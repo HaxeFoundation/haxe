ff4389f391e7f0e380d9a78bf078421f57bdc77d
Rudy Ges <k@klabz.org>
2026-06-01 08:40:25 +0200

    [server] only use 600 magic number once

--- src/compiler/server/serverConfig.ml
+++ src/compiler/server/serverConfig.ml
@@ -5,9 +5,10 @@ let max_completion_items = ref 0
 (* Maximum age in seconds for unused context caches before they are removed.
    10 minutes is long enough to survive bursts of display requests with
    varying defines, while still cleaning up contexts that are truly abandoned. *)
-let stale_context_max_age_seconds = ref 600
+let default_stale_context_max_age = 600
+let stale_context_max_age_seconds = ref default_stale_context_max_age
 
 let reset () =
 	do_not_check_modules := false;
 	max_completion_items := 0;
-	stale_context_max_age_seconds := 600
+	stale_context_max_age_seconds := default_stale_context_max_age
