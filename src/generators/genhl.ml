4de31b99103152e1dedaa3b74ea87801d77e951f
Rudy Ges <k@klabz.org>
2026-09-17 11:38:27 +0200

    [hl] keep argument optionality when generating abstract method stubs (#13038)
    * [hl] keep argument optionality when generating abstract method stubs
    
    * [hl] use t, add call implem tests
    
    * [tests] skip cppia
    
    ---------
    
    Co-authored-by: Yuxiao Mao <yuxiao.mao@outlook.com>

--- src/generators/genhl.ml
+++ src/generators/genhl.ml
@@ -3571,9 +3571,9 @@ let generate_member ctx c f =
 					| Coro(tl,tr) -> Common.expand_coro_type ctx.com.basic tl tr
 					| _ -> die "" __LOC__
 				in
-				let args = List.map (fun (n,_,t) ->
+				let args = List.map (fun (n,o,t) ->
 					let v = Type.alloc_var VGenerated n t null_pos in
-					(v,None)
+					(v,if o then Some (mk (TConst TNull) t null_pos) else None)
 				) tl in
 				{
 					tf_args = args;
---
+++ tests/unit/src/unit/issues/Issue13038.hx
@@ -0,0 +1,64 @@
+package unit.issues;
+
+#if !cppia
+@:keep private abstract class Base {
+	public function new() {}
+	public abstract function f( x : Int, flag : Bool = false ) : Int;
+	public abstract function g( x : Int, ?name : String ) : Int;
+}
+
+private abstract class BaseFoo {
+	public function new() {}
+	public abstract function fbool1( x : Int, flag : Bool = false ) : Int;
+	public abstract function fbool2( x : Int, flag : Bool = false ) : Int;
+	public abstract function fbool3( x : Int, flag : Bool = true ) : Int;
+	public abstract function fint1( x : Int, i : Int = 0 ) : Int;
+	public abstract function fint2( x : Int, i : Int = 0 ) : Int;
+	public abstract function fint3( x : Int, i : Int = 10 ) : Int;
+}
+
+private class Foo extends BaseFoo {
+	public function fbool1( x : Int, flag : Bool = false ) : Int {
+		return x + (flag ? 10 : 1);
+	}
+	public function fbool2( x : Int, flag : Bool = true ) : Int {
+		return x + (flag ? 10 : 1);
+	}
+	public function fbool3( x : Int, flag : Bool = false ) : Int {
+		return x + (flag ? 10 : 1);
+	}
+	public function fint1( x : Int, i : Int = 0 ) : Int {
+		return x + i;
+	}
+	public function fint2( x : Int, i : Int = 10 ) : Int {
+		return x + i;
+	}
+	public function fint3( x : Int, i : Int = 0 ) : Int {
+		return x + i;
+	}
+}
+#end
+
+class Issue13038 extends Test {
+	#if !cppia
+	@:keep static function callAbstract( b : Base ) {
+		return b.f(1) + b.f(1, true) + b.g(1) + b.g(1, "ab");
+	}
+	static function callAbstract1( b : BaseFoo ) {
+		return b.fbool1(100) + b.fbool1(100, true) + b.fint1(100) + b.fint1(100, 15);
+	}
+	static function callAbstract2( b : BaseFoo ) {
+		return b.fbool2(100) + b.fbool2(100, true) + b.fint2(100) + b.fint2(100, 15);
+	}
+	static function callAbstract3( b : BaseFoo ) {
+		return b.fbool3(100) + b.fbool3(100, true) + b.fint3(100) + b.fint3(100, 15);
+	}
+	function test() {
+		eq(true, callAbstract != null);
+		var foo = new Foo();
+		eq(426, callAbstract1(foo));
+		eq(445, callAbstract2(foo));
+		eq(426, callAbstract3(foo));
+	}
+	#end
+}
